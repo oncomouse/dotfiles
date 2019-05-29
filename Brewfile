1cfd837bdc14162f25f04c0cb4fc439187ff6bb9
oncomouse <oncomouse@gmail.com>
2019-05-29 11:33:13 -0500

    Initial Commit

---
+++ Brewfile
@@ -0,0 +1,53 @@
+cask_args appdir: '/Applications'
+
+tap "heroku/brew"
+tap "homebrew/bundle"
+tap "homebrew/cask"
+tap "homebrew/core"
+tap "homebrew/cask-fonts"
+
+# Shell stuff
+brew "fish"
+brew "diff-so-fancy"
+brew "thefuck"
+brew "z"
+brew "the_silver_searcher"
+brew "fzf"
+
+# Editor
+brew "neovim"
+
+# System
+brew "coreutils"
+brew "git"
+brew "grep"
+brew "tmux"
+brew "openssh"
+
+# Programming
+brew "node"
+brew "node-build"
+brew "nodenv"
+brew "python"
+brew "rbenv"
+brew "yarn"
+brew "heroku/brew/heroku"
+
+# Writing
+brew "diction"
+brew "pandoc"
+brew "pandoc-citeproc"
+
+# Other
+brew "tree"
+brew "wget"
+
+cask "font-firacode-nerd-font"
+cask "dropbox"
+cask "flux"
+cask "google-chrome"
+cask "iterm2"
+cask "slack"
+cask "virtualbox"
+cask "visual-studio-code"
+cask "zotero"
---
+++ bootstrap.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+## Get ready for fish:
+sudo echo "/usr/local/bin/fish" >> /etc/shells
+
+## Get ready for xcode/brew:
+xcode-select --install
+
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew bundle install
+
+## Diff-so-fancy Git stuff
+git config --global color.diff-highlight.oldNormal "red bold"
+git config --global color.diff-highlight.oldHighlight "red bold 52"
+git config --global color.diff-highlight.newNormal "green bold"
+git config --global color.diff-highlight.newHighlight "green bold 22"
+
+## Install Python Modules
+pip3 install mackup neovim
+
+## Setup Ruby
+mkdir -p "$(rbenv root)"/plugins
+git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+git clone https://github.com/momo-lab/rbenv-install-latest.git "$(rbenv root)"/plugins/rbenv-install-latest
+rbenv install 2.5.1 # Dreamhost Ruby
+rbenv install-latest
+rbenv global "$(rbenv versions | sed -e '$!d' -e 's/^[ \t]*//')"
+
+## Setup Oh My Tmux! (.tmux.conf.local will come from mackup)
+git clone https://github.com/gpakosz/.tmux ~/.tmux
+ln -s -f ~/.tmux/.tmux.conf ~/.tmux.conf
+
+## Setup $TERM
+tic -x ./tmux-256color.terminfo
+
+echo "When Dropbox is configured and $(tput bold)$(tput setaf 5)done syncing$(tput sgr0), run $(tput setaf 6)mackup restore$(tput sgr0) to load configuration files."
