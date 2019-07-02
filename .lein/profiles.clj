86e1ab484ea2023fe0faf0c724ddd2a381cc3d31
oncomouse <oncomouse@gmail.com>
2019-07-02 16:45:31 -0500

    Clojure

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -4,10 +4,16 @@
                  [lein-cljfmt "0.6.4"]
                  [javax.xml.bind/jaxb-api "2.4.0-b180830.0359"]
                  ]
-     :plugins [
-         [lein-cljfmt "0.6.4"]
-         [lein-ancient "0.6.15"]
-         [cider/cider-nrepl "0.21.1"]
-      ]
+  :dev {
+        :dependencies [
+                 [cider/piggieback "0.4.1"]
+                       ]
+        }
+  :plugins [
+      [lein-cljfmt "0.6.4"]
+      [lein-ancient "0.6.15"]
+      [cider/cider-nrepl "0.21.1"]
+   ]
+  :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}
   }
  }
