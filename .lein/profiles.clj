ad5940463b2625722c28df87adc32af2012944d5
oncomouse <oncomouse@gmail.com>
2019-07-02 15:05:37 -0500

    Lein stuff

---
+++ .lein/profiles.clj
@@ -0,0 +1,13 @@
+{:user
+ {
+  :dependencies [
+                 [lein-cljfmt "0.6.4"]
+                 [javax.xml.bind/jaxb-api "2.4.0-b180830.0359"]
+                 ]
+     :plugins [
+         [lein-cljfmt "0.6.4"]
+         [lein-ancient "0.6.15"]
+         [cider/cider-nrepl "0.21.1"]
+      ]
+  }
+ }
