5eec626dd34201d4b3d7f8ee5229776692ab28a1
oncomouse <oncomouse@gmail.com>
2019-07-15 15:54:59 -0500

    Equality checks

--- bootstrap/scripts/node-modules.sh
+++ bootstrap/scripts/node-modules.sh
@@ -4,7 +4,7 @@ if [ -z $SERVER ]; then
   if ! npm list --depth 1 --global neovim > /dev/null 2>&1; then
     os=`bash ~/dotfiles/bootstrap/scripts/os.sh`
     npm install -g neovim jsonlint
-    if os!="macos"; then
+    if [ "$os" != "macos" ]; then
       npm install -g diff-so-fancy
     fi
   fi
