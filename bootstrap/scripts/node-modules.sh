f80cf5e1ce2474e9ec8cda53855bb207fcb77eda
oncomouse <oncomouse@gmail.com>
2019-07-15 16:00:12 -0500

    More NPM stuff

--- bootstrap/scripts/node-modules.sh
+++ bootstrap/scripts/node-modules.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 ## Install Node.js Modules
-os=`bash ~/dotfiles/bootstrap/scripts/os.sh`
-if [ -z $SERVER ]; then
-  if ! npm list --depth 1 --global neovim > /dev/null 2>&1; then
-    npm install -g neovim jsonlint
-    if [ "$os" != "macos" ]; then
+if which npm > /dev/null 2>&1; then
+  os=`bash ~/dotfiles/bootstrap/scripts/os.sh`
+  if [ -z $SERVER ]; then
+    if ! npm list --depth 1 --global neovim > /dev/null 2>&1; then
+      npm install -g neovim jsonlint
+      if [ "$os" != "macos" ]; then
+        npm install -g diff-so-fancy
+      fi
+    fi
+  fi
+
+  if [ "$os" != "macos" ]; then
+    if ! npm list --depth 1 --global diff-so-fancy > /dev/null 2>&1; then
       npm install -g diff-so-fancy
     fi
   fi
 fi
-
-if [ "$os" != "macos" ]; then
-  if ! npm list --depth 1 --global diff-so-fancy > /dev/null 2>&1; then
-    npm install -g diff-so-fancy
-  fi
-fi
-
