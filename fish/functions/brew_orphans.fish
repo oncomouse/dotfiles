8b39eaec779df0aca0f3ee255f3d7030851e3e85
oncomouse <oncomouse@gmail.com>
2019-07-15 10:31:11 -0500

    Check for a single keg or all kegs

--- fish/functions/brew_orphans.fish
+++ fish/functions/brew_orphans.fish
@@ -1,3 +1,4 @@
-function brew_orphans
-  for cask in (brew list -1); echo -ne "\x1B[1;34m $cask \x1B[0m"; brew uses $cask --installed | awk '{printf(" %s ", $0)}'; echo ""; end
+function brew_orphans --description "List brew dependencies"
+  set -q argv[1]; and set -l target $argv[1]; or set -l target "all"
+  for keg in (if test $target = "all"; brew list -1; else; brew deps $target; end); echo -ne "\x1B[1;34m $keg \x1B[0m"; brew uses $keg --installed | awk '{printf(" %s ", $0)}'; echo ""; end
 end
