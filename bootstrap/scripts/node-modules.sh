68348f33bb79f053914c83550e3954664b2fb7a2
oncomouse <oncomouse@gmail.com>
2019-07-17 10:48:12 -0500

    trash-cli and refactor diff-so-fancy

--- bootstrap/scripts/node-modules.sh
+++ bootstrap/scripts/node-modules.sh
@@ -4,10 +4,7 @@ if which npm > /dev/null 2>&1; then
   os=`bash ~/dotfiles/bootstrap/scripts/os.sh`
   if [ -z $SERVER ]; then
     if ! npm list --depth 1 --global neovim > /dev/null 2>&1; then
-      npm install -g neovim jsonlint
-      if [ "$os" != "macos" ]; then
-        npm install -g diff-so-fancy
-      fi
+      npm install -g neovim jsonlint trash-cli
     fi
   fi
 
