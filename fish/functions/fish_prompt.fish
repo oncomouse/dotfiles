4f81196768191033247a80293f97be24989a04b4
oncomouse <oncomouse@gmail.com>
2019-07-17 18:13:53 -0500

    Better git check

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -36,7 +36,7 @@ function fish_prompt --description 'Write out the prompt'
         set -g __fish_git_prompt_char_conflictedstate "✖"
     end
     if not set -q __fish_git_prompt_char_cleanstate
-        set -g __fish_git_prompt_char_cleanstate "✔"
+        set -g __fish_git_prompt_char_cleanstate " "
     end
 
     if not set -q __fish_git_prompt_color_dirtystate
