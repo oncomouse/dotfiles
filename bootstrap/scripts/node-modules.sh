f87f6b48f538261b07f2e37e1006f1f5b0e6d567
oncomouse <oncomouse@gmail.com>
2019-07-17 14:18:49 -0500

    Better modularization for node modules

--- bootstrap/scripts/node-modules.sh
+++ bootstrap/scripts/node-modules.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 ## Install Node.js Modules
 if which npm > /dev/null 2>&1; then
-  os=`bash ~/dotfiles/bootstrap/scripts/os.sh`
+  if ! which mf > /dev/null 2>&1; then
+    npm install -g mf-cli
+  fi
+  if ! which trash > /dev/null 2>&1; then
+    npm install -g trash-cli
+  fi
+  if ! which diff-so-fancy > /dev/null 2>&1; then
+    npm install -g diff-so-fancy
+  fi
   if [ -z $SERVER ]; then
     if ! npm list --depth 1 --global neovim > /dev/null 2>&1; then
-      npm install -g neovim jsonlint trash-cli
-    fi
-  fi
-  if [ "$os" != "macos" ]; then
-    if ! npm list --depth 1 --global diff-so-fancy > /dev/null 2>&1; then
-      npm install -g diff-so-fancy
+      npm install -g neovim jsonlint
     fi
   fi
 fi
