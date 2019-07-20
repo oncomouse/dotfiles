ff4810080f2c91c226b4f7013c950e82b5b5c852
oncomouse <oncomouse@gmail.com>
2019-07-20 09:30:19 -0500

    Make sure to install fisher

--- bootstrap/scripts/fisher.sh
+++ bootstrap/scripts/fisher.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-fish -c "if not functions -q fisher;set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config; curl https://git.io/fisher --create-dirs -sLo \$XDG_CONFIG_HOME/fish/functions/fisher.fish;end; fish -c fisher"
+script=$(printf "%s;" \
+  "if not functions -q fisher" \
+  "  set -q XDG_CONFIG_HOME" \
+  "  or set XDG_CONFIG_HOME ~/.config" \
+  "  curl https://git.io/fisher --create-dirs -sLo \$XDG_CONFIG_HOME/fish/functions/fisher.fish" \
+  "end" \
+  "fish -c fisher")
+fish -c "$script"
