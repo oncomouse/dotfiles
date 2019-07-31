ebb960e86c27e224bb90076df7b80bd95ee701a3
oncomouse <oncomouse@gmail.com>
2019-07-31 10:19:29 -0500

    Better support for getting kitty colors

--- fish/functions/kitty-colors.fish
+++ fish/functions/kitty-colors.fish
@@ -1,3 +1,3 @@
 function kitty-colors
-  set colors (ag --nobreak color[0-9] ~/.config/kitty/kitty.conf --vimgrep | cut -d: -f4 | string replace -r "color[0-9]+\s+" ""| string join "," | string replace -a "#" "'#" | string replace -a "," "',"); echo -n "["$colors"']"
+  set colors (ag --nobreak "^color[0-9]" ~/.config/kitty/kitty.conf --vimgrep | cut -d: -f4 | string replace -r "color[0-9]+\s+" ""| string join "," | string replace -a "#" "'#" | string replace -a "," "',"); echo -n "["$colors"']"
 end
