7937f4c32d5154e7e1708f833e64c3458c46e7fa
oncomouse <oncomouse@gmail.com>
2019-08-03 15:01:08 -0500

    Renamed and reorganized ruby gems

--- bootstrap/ubuntu.sh
+++ bootstrap/ubuntu.sh
@@ -15,7 +15,7 @@ $bash ~/dotfiles/bootstrap/scripts/terms.sh
 $bash ~/dotfiles/bootstrap/scripts/diff-so-fancy.sh
 $bash ~/dotfiles/bootstrap/scripts/node-modules.sh
 $bash ~/dotfiles/bootstrap/scripts/python-modules.sh
-$bash ~/dotfiles/bootstrap/scripts/ruby-modules.sh
+$bash ~/dotfiles/bootstrap/scripts/ruby-gems.sh
 $bash ~/dotfiles/bootstrap/scripts/fisher.sh
 if [ -z $SERVER ]; then
   $bash ~/dotfiles/bootstrap/scripts/reason.sh
