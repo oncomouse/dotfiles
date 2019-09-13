532551a0e96bd13a0e28387e9132713e0416bcaf
oncomouse <oncomouse@gmail.com>
2019-09-13 09:47:37 -0500

    Now works with better formatting and doesn't change if FZF is cancelled

--- fish/functions/cdf.fish
+++ fish/functions/cdf.fish
@@ -1,5 +1,8 @@
 function cdf
   set -l target .
   if test (count $argv) -ne 0; set target $argv[1]; end
-  cd (fd $target --type d --color always 2> /dev/null | fzf +m)
+  set -l dir (fd $target --type d --color always 2> /dev/null | fzf +m --height=40%)
+  if test -n "$dir"
+    cd $dir
+  end
 end
