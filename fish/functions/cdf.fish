7ad81515a95bcb2bdfa575f1d00f9eeafcfc4300
oncomouse <oncomouse@gmail.com>
2019-09-15 12:12:28 -0500

    Works without option and reverse layout

--- fish/functions/cdf.fish
+++ fish/functions/cdf.fish
@@ -1,11 +1,10 @@
 function cdf
   set -l target .
-  set -l query ""
+  set -l query 
   if test (count $argv) -ne 0
     set query -q (string join " " $argv)
   end
-  echo $query
-  set -l dir (fd $target --type d --color always 2> /dev/null | fzf $query +m --height=40%)
+  set -l dir (fd $target --type d --color always 2> /dev/null | fzf $query +m --layout=reverse --height=40%)
   if test -n "$dir"
     cd $dir
   end
