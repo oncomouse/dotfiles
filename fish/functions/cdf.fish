f91e3133324a79e33f84c609a9b665cf7e4088c0
oncomouse <oncomouse@gmail.com>
2019-09-15 09:04:49 -0500

    Arguments to cdf are the start of the query to FZF

--- fish/functions/cdf.fish
+++ fish/functions/cdf.fish
@@ -1,7 +1,11 @@
 function cdf
   set -l target .
-  if test (count $argv) -ne 0; set target $argv[1]; end
-  set -l dir (fd $target --type d --color always 2> /dev/null | fzf +m --height=40%)
+  set -l query ""
+  if test (count $argv) -ne 0
+    set query -q (string join " " $argv)
+  end
+  echo $query
+  set -l dir (fd $target --type d --color always 2> /dev/null | fzf $query +m --height=40%)
   if test -n "$dir"
     cd $dir
   end
