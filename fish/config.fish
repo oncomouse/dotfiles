98b14c76e57678e29d703a9aea93f7146b3bd931
oncomouse <oncomouse@gmail.com>
2020-08-17 12:05:02 -0500

    Freshened up

--- fish/config.fish
+++ fish/config.fish
@@ -20,85 +20,82 @@ function ag; /usr/bin/env ag --path-to-ignore ~/.ignore --hidden $argv; end
 # SSH to Dreamhost:
 function pilsch.com; ssh eschaton@birkenfeld.dreamhost.com; end
 
-# Load RBEnv
-# if command -sq rbenv
-#   set -gx RUBY_CONFIGURE_OPTS --with-openssl-dir=(brew --prefix openssl@1.1)
-#   status --is-interactive; and source (rbenv init --no-rehash - | sed 's/setenv/set -gx/' | psub)
-# end
-
 # Vim related aliases:
-if command -sq nvim
-  set -gx EDITOR (which nvim)
-else
-  set -gx EDITOR (which vim)
+if not set -q EDITOR 
+  echo "Setting EDITOR"
+  if command -sq nvim
+    set -U EDITOR (which nvim)
+  else
+    set -U EDITOR (which vim)
+  end
 end
 
 # Configure FZF:
-set -gx FZF_DEFAULT_COMMAND "rg --files --hidden --follow"
-set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
-set -gx FZF_ALT_C_COMMAND "fd --type d --color=always . $HOME"
-set -gx FZF_DEFAULT_OPTS "
-  --ansi
-  --bind='ctrl-o:execute(open {})+abort'
-  --bind='ctrl-e:execute(code {})+abort'
-  "
+if not set -q FZF_DEFAULT_COMMAND
+  echo "Setting FZF"
+  set -U FZF_DEFAULT_COMMAND "rg --files --hidden --follow"
+  set -U FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+  set -U FZF_ALT_C_COMMAND "fd --type d --color=always . $HOME"
+  set -U FZF_DEFAULT_OPTS "--ansi --bind='ctrl-o:execute(open {})+abort'"\
+  " --bind='ctrl-e:execute(nvim {})+abort'"
 
-# Base 16 but our theme:
-set -l color00 '#232323'
-set -l color01 '#3a3a3a'
-set -l color02 '#515152'
-set -l color03 '#686869'
-set -l color04 '#807f80'
-set -l color05 '#8b8b8c'
-set -l color06 '#979698'
-set -l color07 '#aeadaf'
-set -l color08 '#d58888'
-set -l color09 '#d2813d'
-set -l color0A '#b1942b'
-set -l color0B '#96a42d'
-set -l color0C '#7aa880'
-set -l color0D '#8e9cc0'
-set -l color0E '#b58d88'
-set -l color0F '#d0913d'
+  # Base 16 but our theme:
+  set -l color00 '#232323'
+  set -l color01 '#3a3a3a'
+  set -l color02 '#515152'
+  set -l color03 '#686869'
+  set -l color04 '#807f80'
+  set -l color05 '#8b8b8c'
+  set -l color06 '#979698'
+  set -l color07 '#aeadaf'
+  set -l color08 '#d58888'
+  set -l color09 '#d2813d'
+  set -l color0A '#b1942b'
+  set -l color0B '#96a42d'
+  set -l color0C '#7aa880'
+  set -l color0D '#8e9cc0'
+  set -l color0E '#b58d88'
+  set -l color0F '#d0913d'
 
-set -l FZF_NON_COLOR_OPTS
+  set -l FZF_NON_COLOR_OPTS
 
-for arg in (echo $FZF_DEFAULT_OPTS | tr " " "\n")
-    if not string match -q -- "--color*" $arg
-        set -a FZF_NON_COLOR_OPTS $arg
-    end
+  for arg in (echo $FZF_DEFAULT_OPTS | tr " " "\n")
+      if not string match -q -- "--color*" $arg
+          set -a FZF_NON_COLOR_OPTS $arg
+      end
+  end
+
+  set -U FZF_DEFAULT_OPTS "$FZF_NON_COLOR_OPTS"\
+  " --color=bg+:$color01,bg:$color00,spinner:$color0C,hl:$color0D"\
+  " --color=fg:$color04,header:$color0D,info:$color0A,pointer:$color0C"\
+  " --color=marker:$color0C,fg+:$color06,prompt:$color0A,hl+:$color0D"
+  set -U FZF_CTRL_T_OPTS "--preview-window 'right:60%' --preview 'bat --color=always --style=header,grid --line-range :300 {}'"
 end
-
-set -U FZF_DEFAULT_OPTS "$FZF_NON_COLOR_OPTS"\
-" --color=bg+:$color01,bg:$color00,spinner:$color0C,hl:$color0D"\
-" --color=fg:$color04,header:$color0D,info:$color0A,pointer:$color0C"\
-" --color=marker:$color0C,fg+:$color06,prompt:$color0A,hl+:$color0D"
-set -gx FZF_CTRL_T_OPTS "
-  --preview-window 'right:60%' --preview 'bat --color=always --style=header,grid --line-range :300 {}'
-  "
 
 # Local paths:
 function add_to_user_paths -a dir
-  if not contains $dir $fish_user_paths
-    and test -d $dir
-    set -U fish_user_paths $fish_user_paths $dir
+  # Delete $dir if it is present but does not exist:
+  if set -l index (contains -i $dir $fish_user_paths)
+    if not test -d $dir
+      set --erase -U fish_user_paths[$index]
+    end
+  # Add $dir if it is not present but does exist:
+  else
+    if test -d $dir
+      set -U fish_user_paths $fish_user_paths $dir
+    end
   end
 end
-add_to_user_paths ~/.fzf/bin
+# Custom paths:
 add_to_user_paths ~/bin
 add_to_user_paths ~/.local/bin
 add_to_user_paths /usr/local/sbin
-
-# # Set emoji width:
-# set -g fish_emoji_width 2
-
-# Sets up Rust's Cargo thing:
-if test -e $HOME/.cargo/env
-  source $HOME/.cargo/env
-end
+# Set up Cargo:
+add_to_user_paths ~/.cargo/bin
+# Set up FZF:
+add_to_user_paths ~/.fzf/bin
 
 # Configure ASDF:
-
 if not contains $HOME/.asdf/shims $fish_user_paths
   echo "Loading ASDF"
   set -x ASDF_DIR (brew --prefix asdf)
@@ -119,33 +116,15 @@ end
 # Load the asdf wrapper function
 source $ASDF_DIR/lib/asdf.fish
 
-# Setup virtualenv support Fish:
-# if not set -q VIRTUALFISH_DEFAULT_PYTHON
-#   set -U VIRTUALFISH_DEFAULT_PYTHON (which python)
-#   echo "Setting up virtualfish"
-#   if python -c 'import pkgutil; import sys; sys.exit(0) if pkgutil.find_loader("virtualfish") else sys.exit(1)'
-#     eval (python -m virtualfish | sed -e "s/set \-g/set -U/g")
-#   end
-# end
-
 # Setup Fuck:
 if command -sq thefuck
   thefuck --alias | source
 end
-
-# Setup NPM:
-# if command -sq nodenv
-#   status --is-interactive; and source (nodenv init -|psub)
-# end
-
+#
 # Setup Kitty:
 if command -sq kitty
   kitty + complete setup fish | source
 end
-
-# if [ -x ~/.local/bin/nvim-osx64/bin/nvim ]
-#   function nvim;~/.local/bin/nvim-osx64/bin/nvim $argv;end
-# end
 
 # Fasd Aliases:
 if command -sq nvim
@@ -163,3 +142,18 @@ function standard;/usr/local/bin/semistandard $argv | /usr/local/bin/snazzy;end
 
 # Colors:
 set fish_color_cwd cyan
+
+# Source: https://github.com/SidOfc/dotfiles/blob/master/config.fish
+function kp --description "Kill processes"
+  set -l __kp__pid ''
+  set __kp__pid (ps -ef | sed 1d | eval "fzf $FZF_DEFAULT_OPTS -m --header='[kill:process]'" | awk '{print $2}')
+
+  if test "x$__kp__pid" != "x"
+    if test "x$argv[1]" != "x"
+      echo $__kp__pid | xargs kill $argv[1]
+    else
+      echo $__kp__pid | xargs kill
+    end
+    exec kp
+  end
+end
