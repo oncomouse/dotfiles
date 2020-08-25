24b5e3fb65ce7b7535aab5348bc73f6849f94c8c
oncomouse <oncomouse@gmail.com>
2020-08-25 17:25:32 -0500

    Working window management!

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,7 +1,36 @@
+--luacheck: globals hs
 hyper = { "ctrl", "alt", "cmd", "shift" }
 -- Capslock + space -> console
 hs.hotkey.bindSpec({ hyper, "space" }, hs.toggleConsole)
 
+-- window management:
+hs.window.animationDuration = 0
+hs.hotkey.bindSpec({ hyper, "left" }, function()
+	local win = hs.window.focusedWindow()
+	if not win then return end
+	win:moveToUnit(hs.layout.left50)
+end)
+hs.hotkey.bindSpec({ hyper, "up" }, function()
+	local win = hs.window.focusedWindow()
+	if not win then return end
+	win:moveToUnit(hs.layout.maximized)
+end)
+hs.hotkey.bindSpec({ hyper, "right" }, function()
+	local win = hs.window.focusedWindow()
+	if not win then return end
+	win:moveToUnit(hs.layout.right50)
+end)
+-- Create a nice centered reading window:
+hs.hotkey.bindSpec({ hyper, "down" }, function()
+	local win_percentage = .95
+	local offset = (1 - win_percentage) / 2
+	local win = hs.window.focusedWindow()
+	if not win then return end
+	win:moveToUnit(
+		hs.geometry.rect(offset, offset, win_percentage, win_percentage),
+		0
+	)
+end)
 -- Auto-reload configuration:
 function reloadConfig(files)
 	doReload = false
@@ -14,6 +43,9 @@ function reloadConfig(files)
 		hs.reload()
 	end
 end
+hs.hotkey.bind(hyper, "0", function()
+	hs.reload()
+end)
 configWatcher =
 	hs.pathwatcher.new(
 		os.getenv("HOME") .. "/.hammerspoon/",
