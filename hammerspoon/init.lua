a91accc836385da063a94136287c8317028b9c61
oncomouse <oncomouse@gmail.com>
2020-08-25 15:23:34 -0500

    Fixed mackup

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,1 +1,22 @@
 hyper = { "ctrl", "alt", "cmd", "shift" }
+-- Capslock + space -> console
+hs.hotkey.bindSpec({ hyper, "space" }, hs.toggleConsole)
+
+-- Auto-reload configuration:
+function reloadConfig(files)
+	doReload = false
+	for _, file in pairs(files) do
+		if file:sub(-4) == ".lua" then
+			doReload = true
+		end
+	end
+	if doReload then
+		hs.reload()
+	end
+end
+configWatcher =
+	hs.pathwatcher.new(
+		os.getenv("HOME") .. "/.hammerspoon/",
+		reloadConfig
+	):start()
+hs.alert.show("Config loaded")
