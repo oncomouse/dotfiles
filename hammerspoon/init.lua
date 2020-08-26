b048dfbd202f00c1c60bb8a85ca051eae979688b
oncomouse <oncomouse@gmail.com>
2020-08-26 10:59:37 -0500

    Organization of hammerspoon config

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,7 +1,11 @@
 --luacheck: globals hs
 -- Load libraries:
-window_movements = require("window_movements")
-string_literal = require("string_literal")
+ext = {
+	utils = {},
+	watchers = {},
+}
+ext.utils.window_movements = require("window_movements")
+ext.utils.string_literal = require("string_literal")
 --- Shortcut keys:
 hyper = { "ctrl", "alt", "cmd", "shift" }
 mash = { "ctrl", "alt", "cmd" }
@@ -13,21 +17,21 @@ hs.hotkey.bindSpec({ mash, "space" }, hs.toggleConsole)
 -- Window management:
 hs.window.animationDuration = 0
 -- Halves:
-hs.hotkey.bindSpec({ hyper, "left" }, window_movements.left)
-hs.hotkey.bindSpec({ hyper, "right" }, window_movements.right)
+hs.hotkey.bindSpec({ hyper, "left" }, ext.utils.window_movements.left)
+hs.hotkey.bindSpec({ hyper, "right" }, ext.utils.window_movements.right)
 -- Maximize:
-hs.hotkey.bindSpec({ hyper, "up" }, window_movements.up)
+hs.hotkey.bindSpec({ hyper, "up" }, ext.utils.window_movements.up)
 -- Custom "reading" window:
-hs.hotkey.bindSpec({ hyper, "down" }, window_movements.down)
+hs.hotkey.bindSpec({ hyper, "down" }, ext.utils.window_movements.down)
 -- Quarters:
-hs.hotkey.bindSpec({ mash, "left" }, window_movements.up_left)
-hs.hotkey.bindSpec({ mash, "right" }, window_movements.down_right)
-hs.hotkey.bindSpec({ mash, "up" }, window_movements.up_right)
-hs.hotkey.bindSpec({ mash, "down" }, window_movements.down_left)
+hs.hotkey.bindSpec({ mash, "left" }, ext.utils.window_movements.up_left)
+hs.hotkey.bindSpec({ mash, "right" }, ext.utils.window_movements.down_right)
+hs.hotkey.bindSpec({ mash, "up" }, ext.utils.window_movements.up_right)
+hs.hotkey.bindSpec({ mash, "down" }, ext.utils.window_movements.down_left)
 -- Spotify controls:
 function show_spotify_song()
 	hs.alert.show(
-		string_literal([[$artist - $song
+		ext.utils.string_literal([[$artist - $song
 
 *$album*]], {
 			artist = hs.spotify.getCurrentArtist(),
@@ -54,25 +58,34 @@ function show_date()
 	})
 end
 hs.hotkey.bindSpec({ hyper, "d" }, show_date)
-
--- Auto-reload configuration:
-function reloadConfig(files)
-	doReload = false
-	for _, file in pairs(files) do
-		if file:sub(-4) == ".lua" then
-			doReload = true
-		end
-	end
-	if doReload then
-		hs.reload()
-	end
-end
-hs.hotkey.bind(hyper, "0", function()
-	hs.reload()
+-- Open Email:
+hs.hotkey.bindSpec({ hyper, "m" }, function()
+	hs.urlevent.openURL("https://mail.google.com")
 end)
-configWatcher =
+-- App Switcher:
+local applicationHyperkeys = {
+	k = "Kitty",
+	f = "Firefox",
+}
+for key, app in pairs(applicationHyperkeys) do
+	hs.hotkey.bind(hyper, key, function()
+		hs.application.launchOrFocus(app)
+	end)
+end
+-- autoreload hammerspoon
+function ext.utils.reloadConfig()
+	-- stop watchers to avoid leaks
+	hs.fnutils.each(ext.watchers, function(watcher)
+		watcher:stop()
+	end)
+
+	hs.reload()
+end
+-- Auto-reload configuration:
+hs.hotkey.bind(hyper, "0", ext.utils.reloadConfig)
+ext.watchers.patchwatcher =
 	hs.pathwatcher.new(
 		os.getenv("HOME") .. "/.hammerspoon/",
-		reloadConfig
+		ext.utils.reloadConfig
 	):start()
 hs.alert.show("Config loaded")
