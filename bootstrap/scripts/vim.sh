0882c2af82774ca736149ad701289508e0772f57
oncomouse <oncomouse@gmail.com>
2020-09-02 19:55:28 -0500

    Update vim install script

--- bootstrap/scripts/vim.sh
+++ bootstrap/scripts/vim.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 ## Setup Vim and NeoVim
-if test ! "$HOME/.vim/autoload/plug.vim"; then
+if [ ! -d "$HOME/.vim/autoload/plug.vim" ]; then
   mkdir -p ~/.vim/autoload
   curl -fLo ~/.vim/autoload/plug.vim \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   vim +PlugInstall +qall
+  if command -v nvim &> /dev/null; then
+    nvim +PlugInstall +qall
+  fi
 else
   vim +PlugInstall +PlugClean +qall
+  if command -v nvim &> /dev/null; then
+    nvim +PlugInstall +PlugClean +qall
+  fi
 fi
