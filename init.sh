b1418776c4fd67d4ab46df9504dac61534228cce
oncomouse <oncomouse@gmail.com>
2020-10-07 10:18:17 -0500

    Making progress

---
+++ init.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+os=$(bash ~/dotfiles/bootstrap/scripts/os.sh)
+
+# Build bombadil:
+/usr/bin/env sh ~/dotfiles/bootstrap/bombadil.sh
+if [ -n "$SERVER" ]; then
+  bombadil install -c ~/dotfiles/bombadil-server.toml
+else
+  bombadil install -c ~/dotfiles/bombadil.toml
+fi
+
+if [ "$os" = "macos" ]; then
+  bombadil link -p macos
+else
+  bombadil link -p linux
+fi
