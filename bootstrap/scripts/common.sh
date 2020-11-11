2df0a6ba897b8e18aef914ee5bdb682873a68a5d
Andrew Pilsch <oncomouse@gmail.com>
2020-11-11 09:12:23 -0600

    Update build scripts

--- bootstrap/scripts/common.sh
+++ bootstrap/scripts/common.sh
@@ -13,8 +13,9 @@ $bash ~/dotfiles/bootstrap/scripts/ruby-gems.sh
 $bash ~/dotfiles/bootstrap/scripts/fisher.sh
 if [ -z "$SERVER" ]; then
   $bash ~/dotfiles/bootstrap/scripts/vale.sh
-  $bash ~/dotfiles/bootstrap/scripts/rbenv.sh
+  $bash ~/dotfiles/bootstrap/scripts/asdf.sh
   $bash ~/dotfiles/bootstrap/scripts/neovim.sh
+  $bash ~/dotfiles/bootstrap/scripts/haskell.sh
   $bash ~/dotfiles/bootstrap/scripts/csl.sh
   $bash ~/dotfiles/bootstrap/scripts/tex.sh
 fi
