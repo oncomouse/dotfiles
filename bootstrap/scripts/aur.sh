0d619a537f016883c727e4004a499d6266091e9d
oncomouse <oncomouse@gmail.com>
2020-11-15 12:52:10 -0600

    More AUR packages

--- bootstrap/scripts/aur.sh
+++ bootstrap/scripts/aur.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
-packages=( "nerd-fonts-fira-code" "otf-hasklig" "ncspot" "pandoc-bin" "vale" "zotero" )
+packages=( "nerd-fonts-fira-code" "otf-hasklig" "ncspot" "pandoc-bin" "vale" "visual-studio-code-bin" "zotero" )
 
 mkdir -p ~/aur
 for package in "${packages[@]}"; do
-  cd ~/aur
+  cd ~/aur || continue
   if [ -d "./$package" ]; then
-    # cd $package
+    true
+    # cd "$package" || continue
     # git pull
   else
     git clone "https://aur.archlinux.org/$package"
-    cd $package
+    cd "$package" || continue
     makepkg -si
   fi
 done
-cd ~/dotfiles
+cd ~/dotfiles || exit
