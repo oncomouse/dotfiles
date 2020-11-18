7130c6a2d29c92244bdeb944b13d0a0c8f37411a
oncomouse <oncomouse@gmail.com>
2020-11-18 11:01:29 -0600

    Start the process of organizing this better

---
+++ scripts/find-dotfiles.fish
@@ -0,0 +1,16 @@
+# fish scripts/find-dotfiles.fish > locations/(bash bootstrap/scripts/os.sh).json
+set files (fd . ~ --type l -E Projects -E Downloads -E Library -E .asdf -E .Trash -E Applications -H -l | rg dotfiles | sed -e "s/^[lrwx-]* [0-9] apilsch [0-9]* *[0-9]* *[A-Za-z]* *[0-9]* *[0-9:]* *//g")
+echo "["
+for file in $files;
+  echo $file | read -d " -> " link target
+  if test -e $target
+    echo -n (echo "{"
+    echo \"link\": \"$HOME/$link\",
+    echo \"target\": \"$target\"
+    echo "}") | string collect -N
+  else
+    rm $link
+    continue
+  end
+end | string join ","
+echo "]"
