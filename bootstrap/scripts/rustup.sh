facdc422c387d06ef438bc0adf992c5a63e0c778
oncomouse <oncomouse@gmail.com>
2020-11-29 10:56:40 -0600

    Rustup actually needs non-su to install

--- bootstrap/scripts/rustup.sh
+++ bootstrap/scripts/rustup.sh
@@ -1,7 +1,7 @@
 os=$(bash ~/dotfiles/bootstrap/scripts/os.sh)
 if [ "$os" = "arch" ]; then
-  sudo rustup toolchain install stable
-  sudo rustup default stable
+  rustup install stable
+  rustup default stable
 else
   if rustup -v COMMAND &> /dev/null; then
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
