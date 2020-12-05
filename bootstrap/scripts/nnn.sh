4a3b3e0a69c971f4cb91e6e84fa8ed08a137f410
oncomouse <oncomouse@gmail.com>
2020-12-05 12:07:24 -0600

    Working FZOpen plugin in NNN

--- bootstrap/scripts/nnn.sh
+++ bootstrap/scripts/nnn.sh
@@ -4,3 +4,6 @@ if [[ ! -d "$HOME/.config/nnn/plugins" ]];then
   curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs | sh
 fi
 fish -c "set -Ux NNN_PLUG 'p:fzcd;v:imgview'"
+# Make the fzopen plugin work:
+sed -i -e "s/biL/bL/g" ~/.config/nnn/plugins/fzopen
+sed -i -e "s/\"\${VISUAL:-\$EDITOR}\"/nvim/g" ~/.config/nnn/plugins/fzopen
