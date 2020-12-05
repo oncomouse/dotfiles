80ee2120737c2fbbf69ecf7415028c82714541c7
oncomouse <oncomouse@gmail.com>
2020-12-05 12:07:43 -0600

    NNN_PLUG

--- bootstrap/scripts/nnn.sh
+++ bootstrap/scripts/nnn.sh
@@ -3,7 +3,7 @@
 if [[ ! -d "$HOME/.config/nnn/plugins" ]];then
   curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs | sh
 fi
-fish -c "set -Ux NNN_PLUG 'p:fzcd;v:imgview'"
+fish -c "set -Ux NNN_PLUG 'p:fzcd;v:imgview;o:fzopen;z:fzz'"
 # Make the fzopen plugin work:
 sed -i -e "s/biL/bL/g" ~/.config/nnn/plugins/fzopen
 sed -i -e "s/\"\${VISUAL:-\$EDITOR}\"/nvim/g" ~/.config/nnn/plugins/fzopen
