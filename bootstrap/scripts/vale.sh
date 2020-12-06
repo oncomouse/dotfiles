54d37f73d0d0c080d76afa92c26d6f7cab71e1ad
oncomouse <oncomouse@gmail.com>
2020-12-06 15:52:34 -0600

    Gee, that was fun

--- bootstrap/scripts/vale.sh
+++ bootstrap/scripts/vale.sh
@@ -2,7 +2,10 @@
 mkdir -p ~/.config/vale/styles
 git clone https://github.com/errata-ai/proselint proselint-git
 git clone https://github.com/errata-ai/write-good write-good-git
+git clone https://github.com/errata-ai/vale vale-git
 mv proselint-git/proselint ~/.config/vale/styles/
 mv write-good-git/write-good ~/.config/vale/styles/
+mv vale-git/styles/vale ~/.config/vale/styles/
 rm -rf proselint-git
 rm -rf write-good-git
+rm -rf vale-git
