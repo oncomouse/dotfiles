4fddd08cc604353aeabf95f95dd7c5690db35d98
oncomouse <oncomouse@gmail.com>
2020-12-15 11:25:20 -0600

    Qtile is just never going to work right

--- stow/qtile/dot-config/qtile/hooks.py
+++
@@ -1,6 +0,0 @@
-# import subprocess
-# from keys import HOME
-# from libqtile import hook
-# @hook.subscribe.startup_once
-# def autostart():
-#     subprocess.call([HOME + ".config/qtile/autostart.sh"])
