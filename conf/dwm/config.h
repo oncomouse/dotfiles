ff567862250476cc40ad278a88402d1373c53619
oncomouse <oncomouse@gmail.com>
2020-12-27 11:39:42 -0600

    Fancy layout

--- conf/dwm/config.h
+++ conf/dwm/config.h
@@ -47,9 +47,12 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "", tile}, 
+	{ "", NULL}, 
+	{ "", monocle},
+	/* { "[]=",      tile },   first entry is default */
+	/*{ "><>",      NULL },    no layout function means floating behavior */
+	/* { "[M]",      monocle }, */
 };
 
 /* key definitions */
