fad34aedf4933b2795dda13f6a42df7e21d5bbe6
oncomouse <oncomouse@gmail.com>
2020-12-28 09:16:00 -0600

    Slock

---
+++ conf/slock/config.h
@@ -0,0 +1,22 @@
+/* user and group to drop privileges to */
+static const char *user  = "nobody";
+static const char *group = "nogroup";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "#005577",   /* during input */
+	[FAILED] = "#CC3333",   /* wrong password */
+	[CAPS] = "#FF0000",
+};
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+		{ "color0",       STRING,  &colorname[INIT] },
+		{ "color4",       STRING,  &colorname[INPUT] },
+		{ "color1",       STRING,  &colorname[FAILED] },
+		{ "color3",       STRING,  &colorname[CAPS] },
+};
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
