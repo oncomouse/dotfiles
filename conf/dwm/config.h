abe5c1b7e95ffb447b1c406beecef0c326b46e54
oncomouse <oncomouse@gmail.com>
2020-12-29 16:58:31 -0600

    Audio hotkeys

--- conf/dwm/config.h
+++ conf/dwm/config.h
@@ -1,11 +1,18 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <X11/XF86keysym.h>
+/* Add somewhere in your constants definition section */
+
+
+/* Add to keys[] array. With 0 as modifier, you are able to use the keys directly. */
+/* static Key keys[] = { */
+/* }; */
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 5;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 5;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 5;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 5;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -70,11 +77,24 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "rofi","-show","combi","-show-icons" };
 static const char *termcmd[]  = { "kitty", NULL };
-static const char *polybarcmd[] = { "polybar-msg", "cmd", "restart", NULL };
+static const char *polybar_restartcmd[] = { "polybar-msg", "cmd", "restart", NULL };
+static const char *upvol[]   = { "ponymix", "increase", "5%",     NULL };
+static const char *downvol[]   = { "ponymix", "decrease", "5%",     NULL };
+static const char *mutevol[] = { "ponymix", "toggle",  NULL };
+static const char *playpausencspot[] = { "dbus-send","--print-reply","--dest=org.mpris.MediaPlayer2.ncspot","/org/mpris/MediaPlayer2","org.mpris.MediaPlayer2.Player.PlayPause", NULL};
+/* static const char *stopncspot[] = { "dbus-send","--print-reply","--dest=org.mpris.MediaPlayer2.ncspot","/org/mpris/MediaPlayer2","org.mpris.MediaPlayer2.Player.Stop", NULL} */
+static const char *previousncspot[] = { "dbus-send","--print-reply","--dest=org.mpris.MediaPlayer2.ncspot","/org/mpris/MediaPlayer2","org.mpris.MediaPlayer2.Player.Previous", NULL};
+static const char *nextncspot[] = { "dbus-send","--print-reply","--dest=org.mpris.MediaPlayer2.ncspot","/org/mpris/MediaPlayer2","org.mpris.MediaPlayer2.Player.Next", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY, XK_q, spawn, {.v = polybarcmd }},
+	{ MODKEY, XK_q, spawn, {.v = polybar_restartcmd }},
+	{ 0,                       XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
+	{ 0,                       XF86XK_AudioMute, spawn, {.v = mutevol } },
+	{ 0,                       XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },
+	{ 0, XF86XK_AudioPlay, spawn, {.v = playpausencspot}},
+	{ 0, XF86XK_AudioPrev, spawn, {.v = previousncspot}},
+	{ 0, XF86XK_AudioNext, spawn, {.v = nextncspot}},
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
