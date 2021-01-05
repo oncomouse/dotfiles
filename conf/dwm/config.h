44fb4aa281f07a2906c81c5cd2076313e63aa13d
oncomouse <oncomouse@gmail.com>
2021-01-05 14:02:06 -0600

    spotifywm to make Spotify play well with others

--- conf/dwm/config.h
+++ conf/dwm/config.h
@@ -38,11 +38,12 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",       NULL,       NULL,       0,          1,           -1 },
-	{ "Thunar",     NULL,       NULL,       0,          1,           -1 },
-	{ "Zotero",     NULL,       NULL,       1 << 7,     0,           -1 },
-	{ "kitty",    NULL,       "ncspot",       1 << 8,     1,           -1 },
-	{ "Spotify",    NULL,       NULL,       1 << 8,     1,           -1 },
+	{ "Gimp"    , NULL , NULL     , 0      , 1 , -1 } ,
+	{ "Thunar"  , NULL , NULL     , 0      , 1 , -1 } ,
+	{ "feh"     , NULL , NULL     , 0      , 1 , -1 } ,
+	{ "Zotero"  , NULL , NULL     , 1 << 7 , 0 , -1 } ,
+	{ "kitty"   , NULL , "ncspot" , 1 << 8 , 1 , -1 } ,
+	{ "Spotify" , NULL , NULL     , 1 << 8 , 0 , -1 } ,
 };
 
 /* layout(s) */
