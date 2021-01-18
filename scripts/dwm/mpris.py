4d7f559c469ba674ecd04c01cece566d6799e746
oncomouse <oncomouse@gmail.com>
2021-01-18 10:51:53 -0600

    Updated status script

--- scripts/dwm/mpris.py
+++ scripts/dwm/mpris.py
@@ -13,25 +13,23 @@ for name in session_bus.list_names():
 if len(players) == 0:
     print("")
 else:
-    output = ""
+    active_player = ""
+    output = "栗"
     for player in players:
         player_bus = session_bus.get_object(player, "/org/mpris/MediaPlayer2")
         player_properties = dbus.Interface(
             player_bus, "org.freedesktop.DBus.Properties"
         )
-        status = player_properties.Get(
-            "org.mpris.MediaPlayer2.Player", "PlaybackStatus"
-        )
-        if status not in ("Playing", "Paused"):
-            output = "栗"
-            continue
-        metadata = player_properties.Get("org.mpris.MediaPlayer2.Player", "Metadata")
-        if status == "Playing":
-            output = "ﱘ"
-        else:
-            output = ""
-        # To just print the title
-        output += truncate(
-            " {} - {}".format(metadata["xesam:artist"][0], metadata["xesam:title"])
-        )
-    print(output + " ⁞ ")
+        s = player_properties.Get("org.mpris.MediaPlayer2.Player", "PlaybackStatus")
+        if s == "Playing" or (s == "Paused" and active_player == ""):
+            player_properties = dbus.Interface(
+                player_bus, "org.freedesktop.DBus.Properties"
+            )
+            metadata = player_properties.Get(
+                "org.mpris.MediaPlayer2.Player", "Metadata"
+            )
+            output = "ﱘ" if s == "Playing" else ""
+            output += " {} - {}".format(
+                metadata["xesam:artist"][0], metadata["xesam:title"]
+            )
+    print(truncate(output) + " ⁞ ")
