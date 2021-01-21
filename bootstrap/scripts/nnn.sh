aff224bdc381ef354769fc36c4f71e5feb80db8e
oncomouse <oncomouse@gmail.com>
2021-01-21 09:21:43 -0600

    Better

--- bootstrap/scripts/nnn.sh
+++ bootstrap/scripts/nnn.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Install NNN Plugins:
 curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs | sh
-fish -c "set -Ux NNN_PLUG 'p:fzcd;v:imgview;o:fzopen;z:fzz'"
+fish -c "setuvar NNN_PLUG 'p:fzcd;v:imgview;o:fzopen;z:fzz'"
 # Make the fzopen plugin work:
 sed -i -e "s/biL/bL/g" ~/.config/nnn/plugins/fzopen
 sed -i -e "s/\"\${VISUAL:-\$EDITOR}\"/nvim/g" ~/.config/nnn/plugins/fzopen
--- stow/fish/dot-config/fish/functions/setuvar.fish
+++ stow/fish/dot-config/fish/functions/setuvar.fish
@@ -1,7 +1,7 @@
 function setuvar -a var_name var_value --description "Set a universal variable if it is not already set"
   if not set -q -U $var_name
     echo "Setting $var_name"
-    set -Ux $var_name $var_value
+    set -U $var_name $var_value
     return 1
   end
   return 0
