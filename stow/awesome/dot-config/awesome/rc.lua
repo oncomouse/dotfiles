62d6cc87fb91c37d4dbf0ca422f7826343c739c5
oncomouse <oncomouse@gmail.com>
2021-03-04 10:29:16 -0600

    Move additional libraries to conf

--- stow/awesome/dot-config/awesome/rc.lua
+++ stow/awesome/dot-config/awesome/rc.lua
@@ -1,10 +1,19 @@
--- luacheck: globals awesome client root tag
+-- luacheck: globals awesome client root tag io
 -- If LuaRocks is installed, make sure that packages installed through it are
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
+
+-- Update package.path
+local addtional_path_prefix = os.getenv("HOME") .. "/dotfiles/conf/awesome/"
+local addtional_path =
+	";" .. addtional_path_prefix .. "?/init.lua;" .. addtional_path_prefix .. "?.lua"
+package.path = package.path .. addtional_path
+
+-- Set log file:
 local logfile = io.open("/tmp/myawesome.log", "a")
 logfile:write("\n\n\nLoading Awesome\nPath: ", package.path, "\n\n")
 io.stderr = logfile
+
 -- Includes {{{
 -- Standard awesome library
 local gears = require("gears")
