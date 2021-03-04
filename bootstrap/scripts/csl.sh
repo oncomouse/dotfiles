5f11992bb7a03ff9000a5684a6b1529e5aef0612
oncomouse <oncomouse@gmail.com>
2021-03-04 12:54:19 -0600

    CSL installer works, which is a nice change

--- bootstrap/scripts/csl.sh
+++ bootstrap/scripts/csl.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 ## Install CSL support:
-if test ! "$HOME/.csl" ; then
+if [ ! -d "$HOME/.csl" ]; then
   git clone https://github.com/citation-style-language/styles ~/.csl
+else
+  lwd="$(pwd)"
+  cd ~/.csl || exit
+  git pull
+  cd "$lwd" || exit
 fi
