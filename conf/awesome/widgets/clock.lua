266ccfe2609ca0dc2acd86725ad5f7050d8c3223
oncomouse <oncomouse@gmail.com>
2021-03-12 18:43:54 -0600

    Generic widget setup

--- conf/awesome/widgets/clock.lua
+++ conf/awesome/widgets/clock.lua
@@ -1,27 +1,28 @@
 local wibox = require("wibox")
 local gt = require("gears.timer")
--- local awful = require("awful")
--- local naughty = require("naughty")
 
 local clock_extend_timeout = 5
-local clock_widget = wibox.widget.textclock(" %a %l:%M %p ")
-clock_widget.extended = false
-clock_widget.restore = function()
-	clock_widget.extended = false
-	clock_widget.format = " %a %l:%M %p "
-end
-clock_widget.extend = function()
-	clock_widget.extended = true
-	clock_widget.format = "  %A, %B %d %Y "
-	gt.start_new(clock_extend_timeout, clock_widget.restore)
-end
-clock_widget.toggle = function()
-	if clock_widget.extended then
-		clock_widget.restore()
-	else
-		clock_widget.extend()
-	end
-end
+local clock_widget = wibox.widget{
+	widget = wibox.widget.textclock,
+	format = " %a %l:%M %p ",
+	extended = false,
+	restore = function(self)
+		self.extended = false
+		self.format = " %a %l:%M %p "
+	end,
+	extend = function(self)
+		self.extended = true
+		self.format = "  %A, %B %d %Y "
+		gt.start_new(clock_extend_timeout, self.restore)
+	end,
+	toggle = function(self)
+		if self.extended then
+			self.restore()
+		else
+			self.extend()
+		end
+	end,
+}
 clock_widget:connect_signal("button::press", clock_widget.toggle)
 
 return clock_widget
