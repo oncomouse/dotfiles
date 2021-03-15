7029fd000b392ed620526fcd66b21a114ea84808
oncomouse <oncomouse@gmail.com>
2021-03-15 13:47:31 -0500

    Logs

--- stow/awesome/dot-config/awesome/rc.lua
+++ stow/awesome/dot-config/awesome/rc.lua
@@ -16,11 +16,6 @@ local addtional_path_prefix = os.getenv("HOME") .. "/dotfiles/conf/awesome/"
 local addtional_path =
 	";" .. addtional_path_prefix .. "?/init.lua;" .. addtional_path_prefix .. "?.lua"
 package.path = package.path .. addtional_path
-
--- Set log file:
-local logfile = io.open("/tmp/myawesome.log", "a")
-logfile:write("\n\n\nLoading Awesome\nPath: ", package.path, "\n\n")
-io.stderr = logfile
 -- }}}
 
 -- ╻┏┓╻┏━╸╻  ╻ ╻╺┳┓┏━╸┏━┓
