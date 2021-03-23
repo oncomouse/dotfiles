a1ae884200c7af29b3e9e275320528ee8c4a8457
oncomouse <oncomouse@gmail.com>
2021-03-23 11:33:29 -0500

    Switch tags on blank tag

--- stow/awesome/dot-config/awesome/rc.lua
+++ stow/awesome/dot-config/awesome/rc.lua
@@ -244,6 +244,12 @@ end)
 -- ╻┏ ┏━╸╻ ╻   ┏┓ ╻┏┓╻╺┳┓╻┏┓╻┏━╸┏━┓
 -- ┣┻┓┣╸ ┗┳┛   ┣┻┓┃┃┗┫ ┃┃┃┃┗┫┃╺┓┗━┓
 -- ╹ ╹┗━╸ ╹    ┗━┛╹╹ ╹╺┻┛╹╹ ╹┗━┛┗━┛
+-- Mouse bindings{{{
+awful.mouse.append_global_mousebindings({
+    awful.button({ }, 4, awful.tag.viewprev),
+    awful.button({ }, 5, awful.tag.viewnext),
+})
+-- }}}
 -- {{{ Key bindings
 awful.keyboard.append_global_keybindings({
 	awful.key({ modkey, "Shift" }, "Return",
