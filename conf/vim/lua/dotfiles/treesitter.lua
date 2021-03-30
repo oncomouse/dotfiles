4000f616e1b1b54c2504b529698070853dd16d4b
oncomouse <oncomouse@gmail.com>
2021-03-30 09:44:57 -0500

    I don't think treesitter indent is ready for primetime

--- conf/vim/lua/dotfiles/treesitter.lua
+++ conf/vim/lua/dotfiles/treesitter.lua
@@ -1,49 +1,49 @@
 local ts_configs = require('nvim-treesitter.configs')
 
 ts_configs.setup{
-  ensure_installed = "maintained",
-  highlight = {
-	enable = true,
-  },
-  indent = {
-	enable = true,
-  },
-  textobjects = {
+	ensure_installed = "maintained",
+	highlight = {
+		enable = true,
+	},
+	indent = {
+		enable = false,
+	},
+	textobjects = {
 	select = {
-	  enable = true,
-	  keymaps = {
-		-- You can use the capture groups defined in textobjects.scm
-		["af"] = "@function.outer",
-		["if"] = "@function.inner",
-		["ac"] = "@class.outer",
-		["ic"] = "@class.inner",
-	  },
+		enable = true,
+		keymaps = {
+			-- You can use the capture groups defined in textobjects.scm
+			["af"] = "@function.outer",
+			["if"] = "@function.inner",
+			["ac"] = "@class.outer",
+			["ic"] = "@class.inner",
+			},
+		},
 	},
-  },
-  move = {
-	enable = true,
-	goto_next_start = {
-	  ["]m"] = "@function.outer",
-	  ["]]"] = "@class.outer",
+	move = {
+		enable = true,
+		goto_next_start = {
+			["]m"] = "@function.outer",
+			["]]"] = "@class.outer",
+		},
+		goto_next_end = {
+			["]M"] = "@function.outer",
+			["]["] = "@class.outer",
+		},
+		goto_previous_start = {
+			["[m"] = "@function.outer",
+			["[["] = "@class.outer",
+		},
+		goto_previous_end = {
+			["[M"] = "@function.outer",
+			["[]"] = "@class.outer",
+		},
 	},
-	goto_next_end = {
-	  ["]M"] = "@function.outer",
-	  ["]["] = "@class.outer",
+	lsp_interop = {
+		enable = true,
+		peek_definition_code = {
+			["df"] = "@function.outer",
+			["dF"] = "@class.outer",
+		},
 	},
-	goto_previous_start = {
-	  ["[m"] = "@function.outer",
-	  ["[["] = "@class.outer",
-	},
-	goto_previous_end = {
-	  ["[M"] = "@function.outer",
-	  ["[]"] = "@class.outer",
-	},
-  },
-  lsp_interop = {
-	enable = true,
-	peek_definition_code = {
-	  ["df"] = "@function.outer",
-	  ["dF"] = "@class.outer",
-	},
-  },
 }
