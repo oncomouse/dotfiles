4a319cfd212d64ad7d94013e930291f3ccd8a39d
oncomouse <oncomouse@gmail.com>
2021-04-01 08:47:56 -0500

    Fixed some script syntax problems

--- bootstrap/scripts/rustup.sh
+++ bootstrap/scripts/rustup.sh
@@ -1,9 +1,10 @@
+#!/usr/bin/env bash
 os=$(bash ~/dotfiles/bootstrap/scripts/os.sh)
 if [ "$os" = "arch" ]; then
   rustup install stable
   rustup default stable
 else
-  if rustup -v COMMAND &> /dev/null; then
+  if rustup -v COMMAND > /dev/null 2>&1; then
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
   fi
   if [ ! -d ~/.cargo ]; then
