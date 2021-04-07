18891344d0517f813403301c540e9daf83e3464a
oncomouse <oncomouse@gmail.com>
2021-04-07 16:09:06 -0500

    Reverse FZF for KP

--- stow/fish/dot-config/fish/functions/kp.fish
+++ stow/fish/dot-config/fish/functions/kp.fish
@@ -1,7 +1,7 @@
 # Source: https://github.com/SidOfc/dotfiles/blob/master/config.fish
 function kp --description "Kill processes"
   set -l __kp__pid ''
-  set __kp__pid (ps -ef | sed 1d | eval "fzf $FZF_DEFAULT_OPTS -m --header='[kill:process]'" | awk '{print $2}')
+  set __kp__pid (ps -ef | sed 1d | eval "fzf $FZF_DEFAULT_OPTS -m --header='[kill:process]' --reverse" | awk '{print $2}')
 
   if test "x$__kp__pid" != "x"
     if test "x$argv[1]" != "x"
