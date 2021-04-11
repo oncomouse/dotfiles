b31ddfe498748adef97cb2bc959f6e94fd190497
oncomouse <oncomouse@gmail.com>
2021-04-11 11:42:07 -0500

    Use awesome to start services instead of dex & xdg-autostart

--- stow/awesome/dot-config/awesome/rc.lua
+++ stow/awesome/dot-config/awesome/rc.lua
@@ -79,6 +79,23 @@ local heartbeat = require("utils.heartbeat")
 require("utils.border_gradient")
 -- }}}
 
+-- ┏━┓╻ ╻╺┳╸┏━┓┏━┓╺┳╸┏━┓┏━┓┏━┓╺┳╸
+-- ┣━┫┃ ┃ ┃ ┃ ┃┗━┓ ┃ ┣━┫┣┳┛┣┳┛ ┃
+-- ╹ ╹┗━┛ ╹ ┗━┛┗━┛ ╹ ╹ ╹╹┗╸╹┗╸ ╹
+-- Startup {{{
+local function run_once(cmd_arr)
+	for _, cmd in ipairs(cmd_arr) do
+		awful.spawn.with_shell(string.format("pgrep -u $USER -fx '%s' > /dev/null || (%s)", cmd, cmd))
+	end
+end
+run_once({
+	"unclutter -root",
+	"xscreensaver -nosplash",
+	"redshift",
+	"picom --experimental-backends -b"
+})
+-- }}}
+
 -- ┏━╸┏━┓┏━┓┏━┓┏━┓   ╻ ╻┏━┓┏┓╻╺┳┓╻  ╻┏┓╻┏━╸
 -- ┣╸ ┣┳┛┣┳┛┃ ┃┣┳┛   ┣━┫┣━┫┃┗┫ ┃┃┃  ┃┃┗┫┃╺┓
 -- ┗━╸╹┗╸╹┗╸┗━┛╹┗╸   ╹ ╹╹ ╹╹ ╹╺┻┛┗━╸╹╹ ╹┗━┛
