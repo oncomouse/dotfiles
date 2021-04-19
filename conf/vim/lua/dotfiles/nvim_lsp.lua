548c4b58b510e4e456988ac600d57bdbcdad6157
oncomouse <oncomouse@gmail.com>
2021-04-19 13:37:52 -0500

    formatexpr works with lsp

--- conf/vim/lua/dotfiles/nvim_lsp.lua
+++ conf/vim/lua/dotfiles/nvim_lsp.lua
@@ -12,7 +12,8 @@ local no_diagnostics = {
 local on_attach = function(_, bufnr)
 	-- Once codelens is setup:
 	-- vim.api.nvim_command [[autocmd CursorHold,CursorHoldI,InsertLeave <buffer> lua vim.lsp.codelens.refresh()]]
-	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+	vim.api.nvim_buf_set_option(bufnr, "formatexpr", "v:lua.lsp_formatexpr")
 end
 
 local servers = {
@@ -66,3 +67,41 @@ lspconfig.jsonls.setup {
 	handlers = no_diagnostics,
 	cmd = {'json-languageserver', '--stdio'},
 }
+
+-- setlocal formatexpr=v:lua.lsp_formatexpr
+-- vim.api.nvim_buf_set_option("formatexpr", _G.lsp_formatexpr)
+_G.lsp_formatexpr = function(start_line, end_line, timeout_ms)
+	timeout_ms = timeout_ms or 500
+
+	if not start_line or not end_line then
+	  if vim.fn.mode() == 'i' or vim.fn.mode() == 'R' then
+		-- `formatexpr` is also called when exceeding `textwidth` in insert mode
+		-- fall back to internal formatting
+		return 1
+	  end
+	  start_line = vim.v.lnum
+	  end_line = start_line + vim.v.count - 1
+	end
+
+	if start_line > 0 and end_line > 0 then
+	  local params = {
+		textDocument = vim.lsp.util.make_text_document_params();
+		range = {
+		  start = { line = start_line - 1; character = 0; };
+		  ["end"] = { line = end_line - 1; character = 0; };
+		};
+	  };
+	  local client_results = vim.lsp.buf_request_sync(0, "textDocument/rangeFormatting", params, timeout_ms)
+
+	  -- Apply the text edits from one and only one of the clients.
+	  for _, response in pairs(client_results) do
+		if response.result then
+		  vim.lsp.util.apply_text_edits(response.result, 0)
+		  return 0
+		end
+	  end
+	end
+
+	-- do not run builtin formatter.
+	return 0
+end
