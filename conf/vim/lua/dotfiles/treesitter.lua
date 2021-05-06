1752d3921d37475e69705b96fa5fccaa4eba07d9
oncomouse <oncomouse@gmail.com>
2021-05-06 10:13:10 -0500

    Treesitter function gotos

--- conf/vim/lua/dotfiles/treesitter.lua
+++ conf/vim/lua/dotfiles/treesitter.lua
@@ -23,19 +23,19 @@ ts_configs.setup{
 	move = {
 		enable = true,
 		goto_next_start = {
-			["]m"] = "@function.outer",
+			["]f"] = "@function.outer",
 			["]]"] = "@class.outer",
 		},
 		goto_next_end = {
-			["]M"] = "@function.outer",
+			["]F"] = "@function.outer",
 			["]["] = "@class.outer",
 		},
 		goto_previous_start = {
-			["[m"] = "@function.outer",
+			["[f"] = "@function.outer",
 			["[["] = "@class.outer",
 		},
 		goto_previous_end = {
-			["[M"] = "@function.outer",
+			["[F"] = "@function.outer",
 			["[]"] = "@class.outer",
 		},
 	},
