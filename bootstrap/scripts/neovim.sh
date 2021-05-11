28f002fcd67d8637bb350df27b504c56669c87d8
oncomouse <oncomouse@gmail.com>
2021-05-11 08:51:43 -0500

    Install HEAD Neovim

--- bootstrap/scripts/neovim.sh
+++ bootstrap/scripts/neovim.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
-# Prep for vim-plug install:
-mkdir -p ~/.local/share/nvim/site/autoload
+# Source Install:
+if [ ! -d ~/Projects/neovim ]; then
+	git clone https://github.com/neovim/neovim ~/Projects/neovim
+	owd="$(pwd)"
+	cd ~/Projects/neovim || exit
+else
+	owd="$(pwd)"
+	cd ~/Projects/neovim || exit
+	git pull
+fi
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
+cd "$owd" || exit
