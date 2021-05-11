450261532b162a15c93771c292ec7446e8730ef1
oncomouse <oncomouse@gmail.com>
2021-05-11 08:47:12 -0500

    Luarocks support

--- bootstrap/scripts/luarocks.sh
+++ bootstrap/scripts/luarocks.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 ## Install Lua Rocks
 lualist=$(luarocks list --porcelain | cut -f 1)
+os=$(bash ~/dotfiles/bootstrap/scripts/os.sh)
 function luainstall() {
-  local repo=$1
-  # local version="${2:5.4}"
-  local version="5.4"
-  if ! [[ $lualist =~ $repo ]]; then 
-    sudo luarocks --lua-version "$version" install "$repo"
-  fi
+	local repo=$1
+	local version=$2-5.4
+	if ! [[ $lualist =~ $repo ]]; then 
+		sudo luarocks --lua-version "$version" install "$repo"
+	fi
 }
 if [ -z "$SERVER" ];then
-  luainstall 'luacheck'
-  # if [[ $os == "arch" ]]; then
-  #   luainstall 'lgi' '5.3'
-  #   luainstall 'penlight' '5.3'
-  #   luainstall 'ldoc' '5.3'
-  # fi
+	luainstall 'luacheck'
+	if [[ $os == "arch" ]]; then
+		# Awesome:
+		luainstall 'lgi' '5.3'
+		luainstall 'penlight' '5.3'
+		luainstall 'ldoc' '5.3'
+	fi
 fi
 
