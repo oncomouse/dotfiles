8630e458b174d6828d1f9ca4a81e271e3de56ea0
oncomouse <oncomouse@gmail.com>
2021-05-16 09:08:57 -0500

    Updates

--- conf/awesome/themes/laptop.lua
+++ conf/awesome/themes/laptop.lua
@@ -5,27 +5,23 @@ local themes_path = gfs.get_themes_dir()
 
 -- inherit default theme
 local theme = dofile(themes_path.."xresources/theme.lua")
-theme.font = "FiraCode Nerd Font Normal 12"
+theme.font = "FiraCode Nerd Font Normal 10"
 theme.wibar_right = function()
 	-- Clock Widget:
 	local clock_widget = require("widgets.clock")
 	-- Setup Weather Widget:
-	local volume_widget = nil
-	local weather_widget = nil
-	local battery_widget = nil
-	local brightness_widget = nil
-	if not gfs.file_readable(os.getenv("HOME").."/.config/awesome/json.lua") then
-		volume_widget = require('awesome-wm-widgets.volume-widget.volume')
-		battery_widget = require("awesome-wm-widgets.battery-widget.battery")
-		weather_widget = require("awesome-wm-widgets.weather-widget.weather")
-		brightness_widget = require("awesome-wm-widgets.brightness-widget.brightness")
-	end
+	local volume_widget = require("widgets.volume")
+	local battery_widget = require("awesome-wm-widgets.battery-widget.battery")
+	local weather_widget = require("awesome-wm-widgets.weather-widget.weather")
+	local brightness_widget = require("awesome-wm-widgets.brightness-widget.brightness")
 	return {
 		layout = wibox.layout.fixed.horizontal,
-		spacing = 20,
+		spacing = 0,
 		brightness_widget == nil and nil or brightness_widget({program="xbacklight"}),
-		volume_widget == nil and nil or volume_widget(),
-		battery_widget == nil and nil or battery_widget(),
+		volume_widget,
+		battery_widget == nil and nil or battery_widget({
+			show_current_level=true,
+		}),
 		weather_widget == nil and nil or weather_widget({
 			api_key='7092b2d31fe650e586336bc51e657814',
 			coordinates = {30.663606864996588, -96.34546254147835},
