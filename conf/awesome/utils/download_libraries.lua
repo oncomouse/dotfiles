652ac070892ee8163efbbaf7ac1f3218d27b2020
oncomouse <oncomouse@gmail.com>
2021-05-18 10:46:47 -0500

    Working with single files

--- conf/awesome/utils/download_libraries.lua
+++ conf/awesome/utils/download_libraries.lua
@@ -15,12 +15,23 @@ local function download_libraries(libraries)
 		return parts[#parts]
 	end
 	for _,gh_url in ipairs(libraries) do
-		local dir = os.getenv("HOME") .. "/.config/awesome/" .. string.gsub(tail(gh_url), "%.", "_")
-		if not gears.filesystem.is_dir(dir) then
-			run_count = run_count + 1
-			awful.spawn.with_line_callback("git clone https://github.com/" .. gh_url .. " " .. dir, {
-				exit=cb
-			})
+		local is_url = string.find(gh_url, 'http%l*:')
+		if is_url == nil then
+			local dir = os.getenv("HOME") .. "/.config/awesome/" .. string.gsub(tail(gh_url), "%.", "_")
+			if not gears.filesystem.is_dir(dir) then
+				run_count = run_count + 1
+				awful.spawn.with_line_callback("git clone https://github.com/" .. gh_url .. " " .. dir, {
+					exit=cb
+				})
+			end
+		else
+			local file = os.getenv("HOME") .. "/.config/awesome/" .. tail(gh_url)
+			if not gears.filesystem.file_readable(file) then
+				run_count = run_count + 1
+				awful.spawn.with_line_callback("curl -so " .. file .. " " ..gh_url, {
+					exit=cb
+				})
+			end
 		end
 	end
 end
