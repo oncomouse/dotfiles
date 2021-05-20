93175a7a11351caf0a4f81d088c439abf9be95e3
oncomouse <oncomouse@gmail.com>
2021-05-20 10:06:36 -0500

    Signals for volume, too

--- conf/awesome/evil/init.lua
+++ conf/awesome/evil/init.lua
@@ -1,5 +1,7 @@
 local mpris = require("evil.mpris")
+local volume = require("evil.volume")
 
 return function()
+	volume()
 	mpris()
 end
--- conf/awesome/widgets/volume.lua
+++ conf/awesome/widgets/volume.lua
@@ -1,64 +1,44 @@
-local awful = require("awful")
+-- luacheck: globals awesome
 local wibox = require("wibox")
-local gears = require("gears")
 
 local volume_widget = {}
 
-local function create()
-	local function set_volume(widget, stdout)
-		local volume = (not stdout:find("x")) and string.gsub(stdout, "^%s*(.-)%s*$", "%1") .. "%" or stdout
-		widget:set_text(" 墳 " .. volume)
+local current_volume = ""
+awesome.connect_signal("evil::volume::update", function(volume)
+	current_volume = (not volume:find("x")) and string.gsub(volume, "^%s*(.-)%s*$", "%1") .. "%" or volume
+	if volume_widget.widget ~= nil then
+		volume_widget.widget:set_value(current_volume)
 	end
-	local function run_script(widget, argument)
-		argument = argument or ""
-		awful.spawn.easy_async_with_shell(
-			"~/dotfiles/scripts/volume.sh " .. argument,
-			function(stdout)
-				set_volume(widget, stdout)
-			end
-		)
-	end
-	local function change_volume(widget, change)
-		local argument = ""
-		if change == "mute" then
-			argument = "mute"
-		elseif change == "up" then
-			argument = "up 5"
-		elseif change == "down" then
-			argument = "down 5"
-		end
-		run_script(widget, argument)
-	end
+end)
 
+local function create()
 	volume_widget.widget = wibox.widget{
 		widget = wibox.widget.textbox,
-		text = "",
+		text = current_volume,
+		set_value = function(self, volume)
+			self:set_text(" 墳 " .. volume)
+		end
 	}
-	gears.timer{
-		timeout = 5,
-		call_now = true,
-		autostart = true,
-		callback = function()
-			run_script(volume_widget.widget)
-		end,
-	}
+	local function change_volume(command)
+		awesome.emit_signal("evil::volume::change", command)
+	end
 	function volume_widget:mute()
-		change_volume(volume_widget.widget.widget, "mute")
+		change_volume("mute")
 	end
 	function volume_widget:up()
-		change_volume(volume_widget.widget, "up")
+		change_volume("up")
 	end
 	function volume_widget:down()
-		change_volume(volume_widget.widget, "down")
+		change_volume("down")
 	end
 
 	volume_widget.widget:connect_signal("button::press", function(_, _, _, button)
 		if button == 1 then
-			change_volume(volume_widget.widget, "mute")
+			change_volume("mute")
 		elseif button == 4 then
-			change_volume(volume_widget.widget, "up")
+			change_volume("up")
 		elseif button == 5 then
-			change_volume(volume_widget.widget, "down")
+			change_volume("down")
 		end
 	end)
 
