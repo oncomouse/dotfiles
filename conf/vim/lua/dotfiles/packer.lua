c87225e038149c61a77e483b3d0929dc6a189372
oncomouse <oncomouse@gmail.com>
2021-05-29 13:45:08 -0500

    Colorizer

--- conf/vim/lua/dotfiles/packer.lua
+++ conf/vim/lua/dotfiles/packer.lua
@@ -68,8 +68,8 @@ return require("packer").startup({
 			end
 		} -- Autopairs + Endwise
 		use { "norcalli/nvim-colorizer.lua", cond = function() return vim.fn.exists("+termguicolors") == 1 end, config = function()
-			vim.cmd("let &t_8f=\""..vim.api.nvim_replace_termcodes('<Esc>', true, true, true) .. "[38;2;%lu;%lu;%lum\"")
-			vim.cmd("let &t_8b=\"" .. vim.api.nvim_replace_termcodes('<Esc>', true, true, true) .. "[48;2;%lu;%lu;%lum\"")
+			-- vim.cmd("let &t_8f=\""..vim.api.nvim_replace_termcodes('<Esc>', true, true, true) .. "[38;2;%lu;%lu;%lum\"")
+			-- vim.cmd("let &t_8b=\"" .. vim.api.nvim_replace_termcodes('<Esc>', true, true, true) .. "[48;2;%lu;%lu;%lum\"")
 			vim.o.termguicolors = true
 			require('colorizer').setup{
 				'*',
