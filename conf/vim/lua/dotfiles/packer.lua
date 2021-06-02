05a47c6e84758687cef0eb26b013cf708098925c
oncomouse <oncomouse@gmail.com>
2021-06-02 16:11:39 -0500

    Formatting and FZF loading

--- conf/vim/lua/dotfiles/packer.lua
+++ conf/vim/lua/dotfiles/packer.lua
@@ -26,7 +26,10 @@ return require("packer").startup({
 		-- Getting Started:
 		use "tpope/vim-sensible" -- Agreeable vim settings:
 		use "xero/securemodelines" -- Secure modelines
-		use { "oncomouse/vim-grep", cmd = { "Grep", "LGrep" } } -- :Grep and :LGrep
+		use {
+			"oncomouse/vim-grep",
+			cmd = { "Grep", "LGrep" }
+		} -- :Grep and :LGrep
 		use "tpope/vim-commentary" -- gc<motion> to (un)comment
 		-- General Editing:
 		use "sickill/vim-pasta" -- Indentation-forward pasting
@@ -49,60 +52,65 @@ return require("packer").startup({
 		vim.g.fastfold_minlines = 0
 		use "Konfekt/FastFold" -- Better fold support
 		use "wellle/targets.vim" -- add next block n]) targets, plus words in commas (a,), asterisks (a*), etc
-		-- use { "cohama/lexima.vim",
-		-- 	config = function()
-		-- 		local function make_rule(at, e, filetype, syntax)
-		-- 			return {
-		-- 				char = "<CR>",
-		-- 				input = "<CR>",
-		-- 				input_after = "<CR>" .. e,
-		-- 				at = at,
-		-- 				except = "\\C\\v^(\\s*)\\S.*%#\\n%(%(\\s*|\\1\\s.+)\\n)*\\1" .. e,
-		-- 				filetype = filetype,
-		-- 				syntax = syntax,
-		-- 			}
-		-- 		end
-		-- 		function _G.extend_endwise_for_lua()
-		-- 			-- Lua endwise rules:
-		-- 			vim.fn["lexima#add_rule"](make_rule("^\\s*if\\>.*then\\%(.*[^.:@$]\\<end\\>\\)\\@!.*\\%#", "end", "lua", {}))
-		-- 			vim.fn["lexima#add_rule"](make_rule("^\\s*\\%(for\\|while\\)\\>.*do\\%(.*[^.:@$]\\<end\\>\\)\\@!.*\\%#", "end", "lua", {}))
-		-- 			vim.fn["lexima#add_rule"](make_rule("^\\s*\\%(local\\)\\=.*function\\>\\%(.*[^.:@$]\\<end\\>\\)\\@!.*\\%#", "end", "lua", {}))
-		-- 		end
-		-- 		vim.cmd[[autocmd! dotfiles-settings FileType lua lua extend_endwise_for_lua()]]
-		-- 		-- extend_endwise()
-		-- 		-- vim.api.nvim_set_keymap("i", "<C-l>", "<C-r>=lexima#insmode#leave_till_eol("")<CR>", { noremap = true })
-		-- 	end
-		-- } -- Autopairs + Endwise
+		use {
+			"cohama/lexima.vim",
+			config = function()
+				local function make_rule(at, e, filetype, syntax)
+					return {
+						char = "<CR>",
+						input = "<CR>",
+						input_after = "<CR>" .. e,
+						at = at,
+						except = "\\C\\v^(\\s*)\\S.*%#\\n%(%(\\s*|\\1\\s.+)\\n)*\\1" .. e,
+						filetype = filetype,
+						syntax = syntax,
+					}
+				end
+				function _G.extend_endwise_for_lua()
+					-- Lua endwise rules:
+					vim.fn["lexima#add_rule"](make_rule("^\\s*if\\>.*then\\%(.*[^.:@$]\\<end\\>\\)\\@!.*\\%#", "end", "lua", {}))
+					vim.fn["lexima#add_rule"](make_rule("^\\s*\\%(for\\|while\\)\\>.*do\\%(.*[^.:@$]\\<end\\>\\)\\@!.*\\%#", "end", "lua", {}))
+					vim.fn["lexima#add_rule"](make_rule("^\\s*\\%(local\\)\\=.*function\\>\\%(.*[^.:@$]\\<end\\>\\)\\@!.*\\%#", "end", "lua", {}))
+				end
+				vim.cmd[[autocmd! dotfiles-settings FileType lua lua extend_endwise_for_lua()]]
+				vim.api.nvim_set_keymap("i", "<C-l>", "<C-r>=lexima#insmode#leave_till_eol(\"\")<CR>", { noremap = true })
+			end
+		} -- Autopairs + Endwise
 
-		use { "windwp/nvim-autopairs", config = function()
-			require('nvim-autopairs').setup()
-		end }
-
-		use { "norcalli/nvim-colorizer.lua", cond = function() return vim.fn.exists("+termguicolors") == 1 end, config = function()
-			require('colorizer').setup{
-				'*',
-				markdown = {
-					names = false
-				},
-				text={
-					names = false
-				},
-				["gina-commit"] = {
-					names = false
-				},
-			}
-		end } -- HTML codes and HTML color words to colors
+		use {
+			"norcalli/nvim-colorizer.lua",
+			ft = { "html", "css", "scss", "markdown" },
+			config = function()
+				if vim.fn.exists("+termguicolors") == 1 then
+					require('colorizer').setup{
+						'*',
+						markdown = {
+							names = false
+						},
+						text={
+							names = false
+						},
+						["gina-commit"] = {
+							names = false
+						},
+					}
+				end
+			end
+		} -- HTML codes and HTML color words to colors
 		-- Git Support:
-		use { "lambdalisue/gina.vim", config = function()
-			vim.fn["gina#custom#command#option"]("status", "--opener", vim.o.previewheight .. "split")
-			vim.fn["gina#custom#command#option"]("commit", "--opener", vim.o.previewheight .. "split")
-			vim.fn["gina#custom#command#option"]("diff", "--opener", vim.o.previewheight .. "split")
-			vim.fn["gina#custom#command#option"]("status", "--group", "short")
-			vim.fn["gina#custom#command#option"]("commit", "--group", "short")
-			vim.fn["gina#custom#command#option"]("diff", "--group", "short")
-			-- Implement vim-fugitive commands in Gina:
-			vim.fn["gina#custom#mapping#nmap"]("status", "cc", ":<C-u>Gina commit<CR>", {noremap = 1, silent = 1})
-		end} -- :Gina status to schedule; :Gina commit to commit
+		use {
+			"lambdalisue/gina.vim",
+			config = function()
+				vim.fn["gina#custom#command#option"]("status", "--opener", vim.o.previewheight .. "split")
+				vim.fn["gina#custom#command#option"]("commit", "--opener", vim.o.previewheight .. "split")
+				vim.fn["gina#custom#command#option"]("diff", "--opener", vim.o.previewheight .. "split")
+				vim.fn["gina#custom#command#option"]("status", "--group", "short")
+				vim.fn["gina#custom#command#option"]("commit", "--group", "short")
+				vim.fn["gina#custom#command#option"]("diff", "--group", "short")
+				-- Implement vim-fugitive commands in Gina:
+				vim.fn["gina#custom#mapping#nmap"]("status", "cc", ":<C-u>Gina commit<CR>", {noremap = 1, silent = 1})
+			end
+		} -- :Gina status to schedule; :Gina commit to commit
 		-- FZF Support:
 		if vim.fn.isdirectory("/usr/local/opt/fzf") == 1 then
 			use "/usr/local/opt/fzf"
@@ -113,46 +121,74 @@ return require("packer").startup({
 		elseif vim.fn.isdirectory("~/.fzf") == 1 then
 			use "~/.fzf"
 		else
-			use { "junegunn/fzf", run = "./install --all" }
-		end
-		use { "junegunn/fzf.vim", cond = function() return vim.fn.executable("fzf") == 1 end, config = function()
-			vim.cmd[[command! -bang -nargs=? -complete=dir Files call fzf#vim#files(<q-args>, fzf#vim#with_preview({'options': ['--reverse', '--info=inline']}), <bang>0)]]
-			vim.g.fzf_layout = { window = { width = 1, height = 0.4, yoffset = 1, border = 'top' } }
-			vim.g.fzf_action = {
-				["ctrl-s"] = 'split',
-				["ctrl-v"] = 'vsplit',
-				["ctrl-t"] = 'tabnew',
-				["ctrl-e"] = 'edit',
+			use {
+				"junegunn/fzf",
+				run = "./install --all",
 			}
-			vim.g.fzf_nvim_statusline = 0 -- disable statusline overwriting
-			-- Standard dotfiles bindings:
-			vim.api.nvim_set_keymap("n", "<Plug>(dotfiles-files)", "<cmd>Files<CR>", {})
-			vim.api.nvim_set_keymap("n", "<Plug>(dotfiles-home-files)", "<cmd>Files ~<CR>", {})
-			vim.api.nvim_set_keymap("n", "<Plug>(dotfiles-buffers)", "<cmd>Buffers<CR>", {})
-			vim.api.nvim_set_keymap("n", "<Plug>(dotfiles-windows)", "<cmd>Windows<CR>", {})
-			vim.api.nvim_set_keymap("n", "<Plug>(dotfiles-lines)", "<cmd>BLines<CR>", {})
-			vim.api.nvim_set_keymap("n", "<Plug>(dotfiles-commands)", "<cmd>Commands<CR>", {})
-		end}  -- Add shorcuts for FZF
+		end
+		use {
+			"junegunn/fzf.vim",
+			on = { "Files", "Buffers", "Windows", "BLines", "Commands" },
+			config = function()
+				vim.cmd[[command! -bang -nargs=? -complete=dir Files call fzf#vim#files(<q-args>, fzf#vim#with_preview({'options': ['--reverse', '--info=inline']}), <bang>0)]]
+				vim.g.fzf_layout = { window = { width = 1, height = 0.4, yoffset = 1, border = 'top' } }
+				vim.g.fzf_action = {
+					["ctrl-s"] = 'split',
+					["ctrl-v"] = 'vsplit',
+					["ctrl-t"] = 'tabnew',
+					["ctrl-e"] = 'edit',
+				}
+				vim.g.fzf_nvim_statusline = 0 -- disable statusline overwriting
+				-- Standard dotfiles bindings:
+				vim.api.nvim_set_keymap("n", "<Plug>(dotfiles-files)", "<cmd>Files<CR>", {})
+				vim.api.nvim_set_keymap("n", "<Plug>(dotfiles-home-files)", "<cmd>Files ~<CR>", {})
+				vim.api.nvim_set_keymap("n", "<Plug>(dotfiles-buffers)", "<cmd>Buffers<CR>", {})
+				vim.api.nvim_set_keymap("n", "<Plug>(dotfiles-windows)", "<cmd>Windows<CR>", {})
+				vim.api.nvim_set_keymap("n", "<Plug>(dotfiles-lines)", "<cmd>BLines<CR>", {})
+				vim.api.nvim_set_keymap("n", "<Plug>(dotfiles-commands)", "<cmd>Commands<CR>", {})
+			end
+		}  -- Add shorcuts for FZF
 		-- Syntax:
-		use { "nvim-treesitter/nvim-treesitter",
+		use {
+			"nvim-treesitter/nvim-treesitter",
 			run = ":TSUpdate",
 			config = function()
-				require('dotfiles.treesitter')
+				require('nvim-treesitter.configs').setup{
+					ensure_installed = "maintained",
+					highlight = {
+						enable = true,
+					},
+					indent = {
+						enable = false,
+					},
+					autotag = {
+						enable = true,
+					}
+				}
 			end
 		}
-		use { "windwp/nvim-ts-autotag", ft={"html", "javascript", "javascriptreact"} }  -- Automatically close HTML tags
-		use { "nvim-treesitter/nvim-treesitter-textobjects" }
+		use {
+			"windwp/nvim-ts-autotag",
+			ft={"html", "javascript", "javascriptreact"},
+			after="nvim-treesitter"
+		}  -- Automatically close HTML tags
 		vim.g.vim_markdown_frontmatter = 1 -- Format YAML
 		vim.g.vim_markdown_strikethrough = 0 -- Don"t format strikethrough
 		vim.g.vim_markdown_conceal = 0 -- Don"t conceal
 		vim.g.vim_markdown_conceal_code_blocks = 0 -- Don"t conceal code blocks
 		vim.g.vim_markdown_math = 1 -- Do process MathJaX and LaTeX math
 		use "plasticboy/vim-markdown" -- Markdown Syntax
-		use { "godlygeek/tabular", cmd={ "Tabular", "TableFormat" } } -- :Tabular /| to auto-align tables (also :TableFormat in markdown)
+		use {
+			"godlygeek/tabular",
+			cmd={ "Tabular", "TableFormat" }
+		} -- :Tabular /| to auto-align tables (also :TableFormat in markdown)
 		-- LSP:
-		use { "neovim/nvim-lspconfig", config = function()
-			require('dotfiles.nvim_lsp')
-		end } -- LSP Configuration
+		use {
+			"neovim/nvim-lspconfig",
+			config = function()
+				require('dotfiles.nvim_lsp')
+			end
+		} -- LSP Configuration
 	end,
 	config = {
 		package_root = packer_dir,
