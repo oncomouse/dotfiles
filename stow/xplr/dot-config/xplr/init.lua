bc9d19791f7f85d79c6ca97b9dee087ed8840ba9
oncomouse <oncomouse@gmail.com>
2021-06-27 09:32:31 -0500

    XPLR

---
+++ stow/xplr/dot-config/xplr/init.lua
@@ -0,0 +1,5 @@
+-- luacheck: globals xplr
+version = "0.14.3"
+xplr.config.node_types.directory.meta.icon = ""
+xplr.config.node_types.file.meta.icon = ""
+xplr.config.node_types.symlink.meta.icon = ""
