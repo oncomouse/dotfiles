497a7c14eae17708d0f678892516b52a849a2c5a
oncomouse <oncomouse@gmail.com>
2021-07-20 10:10:25 -0500

    Neovim install/update script

--- bootstrap/scripts/neovim.sh
+++ bootstrap/scripts/neovim.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
-# Source Install:
-if [ ! -d ~/Projects/neovim ]; then
-	git clone https://github.com/neovim/neovim ~/Projects/neovim
-	owd="$(pwd)"
-	cd ~/Projects/neovim || exit
+# Get plugins:
+if (( $(git submodule status -- ~/dotfiles | wc -l) == 0 )); then
+	git submodule init
+	git submodule update
 else
-	owd="$(pwd)"
-	cd ~/Projects/neovim || exit
-	git pull
+	echo "Updaing plugins…"
+	git submodule update --remote
 fi
-make CMAKE_BUILD_TYPE=RelWithDebInfo
-sudo make install
-cd "$owd" || exit
+
+# Source Install:
+# if [ ! -d ~/Projects/neovim ]; then
+# 	git clone https://github.com/neovim/neovim ~/Projects/neovim
+# 	owd="$(pwd)"
+# 	cd ~/Projects/neovim || exit
+# else
+# 	owd="$(pwd)"
+# 	cd ~/Projects/neovim || exit
+# 	git pull
+# fi
+# make CMAKE_BUILD_TYPE=RelWithDebInfo
+# sudo make install
+# cd "$owd" || exit
