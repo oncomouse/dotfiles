9c002e639d5d2815d9d47d17543dfbd129d79035
oncomouse <oncomouse@gmail.com>
2021-08-01 13:32:34 -0500

    Snippet support for LSPs that need it.

--- conf/vim/lua/dotfiles/nvim_lsp.lua
+++ conf/vim/lua/dotfiles/nvim_lsp.lua
@@ -1,8 +1,5 @@
 -- luacheck: globals vim dotfiles
-dotfiles = _G.dotfiles or {}
 local lspconfig = require("lspconfig")
-local configs = require("lspconfig/configs")
-local util = require("lspconfig/util")
 local map = require("dotfiles.utils.map")
 
 local function show_documentation()
@@ -12,36 +9,11 @@ local function show_documentation()
 		vim.lsp.buf.hover()
 	end
 end
-map.nnoremap("<silent>", "K", function() show_documentation() end)
 
--- Extend configs for citation-langserver:
--- configs.citation_langserver = {
--- 	default_config = {
--- 	filetypes = {"markdown", "pandoc"};
--- 	root_dir = function(fname)
--- 		return util.find_git_ancestor(fname) or util.path.dirname(fname)
--- 	end;
--- 	log_level = vim.lsp.protocol.MessageType.Warning;
--- 	cmd = {"/usr/bin/env", "citation-langserver"};
--- 	-- cmd = {"env", "PYTHONPATH=~/Projects/citation-langserver", "python3", "-m", "citation_langserver"}
--- 	}
--- }
-
--- Disable diagnostics
+-- Disable dipagnostics
 local handler_no_diagnostics = {
 	["textDocument/publishDiagnostics"] = function() end,
 	["textDocument/formatting"] = function() end,
-}
-
-local handler_diagnostics = {
-	["textDocument/publishDiagnostics"] = vim.lsp.with(
-		vim.lsp.diagnostic.on_publish_diagnostics, {
-			signs = false,
-			underline = true,
-			update_in_insert = true,
-			virtual_text = false,
-		}
-	)
 }
 
 local vscode_capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -62,50 +34,18 @@ local on_attach = function(client, _)
 	map.nnoremap("<silent><buffer>", "gl", function() vim.lsp.codelens.run() end)
 	map.nnoremap("<silent><buffer>", "ga", function() vim.lsp.buf.code_action() end)
 	map.vnoremap("<silent><buffer>", "ga", function() vim.lsp.buf.range_code_action() end)
+	map.nnoremap("<silent><buffer>", "K", function() show_documentation() end)
 	map.nnoremap("<silent><buffer>", "<F5>", ":<CR>")
-	-- vim.api.nvim_command("command! Format lua vim.lsp.buf.formatting()")
-	-- vim.opt.wrapscan = true
-	-- map.nnoremap("<silent><buffer>", "]d", function() vim.lsp.diagnostic.goto_next() end)
-	-- map.nnoremap("<silent><buffer>", "[d", function() vim.lsp.diagnostic.goto_prev() end)
+	-- Set snippet integration:
+	vim.cmd[[
+		packadd vim-vsnip
+		packadd vim-vsnip-integ
+	]]
+	map.imap("<expr>", "<C-l>", "vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'")
+	map.smap("<expr>", "<C-l>", "vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'")
 end
 
--- local on_attach_diagnostics = function(...)
--- 	vim.opt.updatetime = 300
--- 	vim.cmd[[autocmd! User LspDiagnosticsChanged lua vim.lsp.diagnostic.set_loclist({open_loclist = false})]]
--- 	vim.cmd[[autocmd! CursorMoved,CursorHold,InsertLeave * lua vim.lsp.diagnostic.show_line_diagnostics({ focusable = false, show_header = false, })]]
--- 	on_attach(...)
--- end
-
 local servers = {
-	-- efm = {
-	-- 	root_dir =	lspconfig.util.root_pattern(
-	-- 		"Rakefile",
-	-- 		"yarn.lock",
-	-- 		"lerna.json",
-	-- 		".git",
-	-- 		"poetry.toml"
-	-- 	),
-	-- 	filetypes = {
-	-- 		"css",
-	-- 		"html",
-	-- 		"javascript",
-	-- 		"json",
-	-- 		"lua",
-	-- 		"python",
-	-- 		"scss",
-	-- 		"sh",
-	-- 		"vim",
-	-- 		"yaml",
-	-- 	},
-	-- 	init_options = {
-	-- 		documentFormatting = true,
-	-- 		hover = true,
-	-- 		documentSymbol = true,
-	-- 		codeAction = true,
-	-- 		completion = true
-	-- 	},
-	-- 	diagnostics = true,
-	-- },
 	sumneko_lua ={
 		cmd = {"sumneko-lua-language-server"},
 		settings ={
@@ -123,16 +63,6 @@ local servers = {
 			}
 		},
 	},
-	-- citation_langserver ={
-	-- 	autostart = false,
-	-- 	settings = {
-	-- 		citation = {
-	-- 			bibliographies = {
-	-- 				"~/Seadrive/My Libraries/My Library/Documents/Academic Stuff/library.bib"
-	-- 			}
-	-- 		}
-	-- 	}
-	-- },
 	cssls = {
 		capabilities = vscode_capabilities,
 		cmd = {"css-languageserver", "--stdio"},
@@ -145,9 +75,7 @@ local servers = {
 		capabilities = vscode_capabilities,
 		cmd = {"json-languageserver", "--stdio"},
 	},
-	solargraph = {
-		diagnostics = false
-	},
+	solargraph = {},
 	vimls = {},
 	bashls = {},
 	pyright = {},
@@ -159,12 +87,6 @@ for lsp, settings in pairs(servers) do
 		handlers = handler_no_diagnostics,
 	}
 	if #vim.tbl_keys(settings) > 0 then
-		if settings.diagnostics ~= nil then
-			tbl = {
-				on_attach = on_attach_diagnostics,
-				handlers = handler_diagnostics,
-			}
-		end
 		tbl = vim.tbl_extend("keep", tbl, settings)
 	end
 	lspconfig[lsp].setup(tbl)
