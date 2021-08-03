c4522670667d493db206a9261356fff29ce087cb
oncomouse <oncomouse@gmail.com>
2021-08-03 09:37:00 -0500

    Simplified clock

--- conf/awesome/widgets/clock.lua
+++ conf/awesome/widgets/clock.lua
@@ -6,14 +6,11 @@ local recolor_icons = require("widgets.utils.recolor-icons")
 local clock_widget = {}
 
 local function create()
-	local clock_extend_timeout = 5
 	local formats = {
-		regular = "%a %l:%M %p",
-		extended = "%A, %B %d %Y",
+		regular = "%a %m/%d %-I:%M %p ",
 	}
 	local icons = recolor_icons({
 		"clock",
-		"calendar",
 	}, function(x) return "apps/scalable/"..x..".svg" end)
 	clock_widget = wibox.widget{
 		{
@@ -35,32 +32,7 @@ local function create()
 			layout = wibox.layout.fixed.horizontal,
 		},
 		widget = wibox.container.margin,
-		extended = false,
 	}
-	clock_widget.extended = false
-	clock_widget.restore = function()
-		clock_widget.extended = false
-		clock_widget:get_children_by_id("clock")[1].format = formats.regular
-		clock_widget:get_children_by_id("image")[1]:set_image(icons["clock"])
-	end
-	clock_widget.extend = function()
-		clock_widget.extended = true
-		clock_widget:get_children_by_id("clock")[1].format = formats.extended
-		clock_widget:get_children_by_id("image")[1]:set_image(icons["calendar"])
-		gears.timer.start_new(clock_extend_timeout, clock_widget.restore)
-	end
-	clock_widget.toggle = function()
-		if clock_widget.extended then
-			clock_widget.restore()
-		else
-			clock_widget.extend()
-		end
-	end
-	clock_widget:connect_signal("button::press", function(_, _, _, button)
-		if button == 1 then
-			clock_widget.toggle()
-		end
-	end)
 
 	return clock_widget
 end
