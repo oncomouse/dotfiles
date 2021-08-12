82e0d57a0ad23be2efdee5b1c2971349f9f25821
oncomouse <oncomouse@gmail.com>
2021-08-12 09:26:24 -0500

    Clean up

--- conf/awesome/utils/block.lua
+++ conf/awesome/utils/block.lua
@@ -4,13 +4,13 @@ local gears = require("gears")
 local wibox = require("wibox")
 local blocks = 1
 
-function block_watch(command, timeout, callback, base_widget)
+local function watch(command, timeout, callback, base_widget)
 	timeout = timeout or 5
 	base_widget = base_widget or wibox.widget.textbox()
 	callback = callback or function(widget, stdout, stderr, exitreason, exitcode) -- luacheck: no unused args
 		widget:set_text(stdout)
 	end
-	local t = timer { timeout = timeout }
+	local t = gears.timer { timeout = timeout }
 	t:connect_signal("timeout", function()
 		t:stop()
 		awful.spawn.easy_async_with_shell(command, function(stdout, stderr, exitreason, exitcode)
@@ -23,36 +23,36 @@ function block_watch(command, timeout, callback, base_widget)
 	return base_widget, t
 end
 
+-- Helper Functions:
+local function update(widget, output)
+	widget:set_markup(output)
+end
+local function run(command, widget)
+	awful.spawn.easy_async_with_shell(command, function(stdout)
+		update(widget, stdout)
+	end)
+end
+
 local function block(command, timeout)
-	-- Update:
-	local function update(widget, output)
-		widget:set_markup(output)
-	end
 	-- Signal:
 	local signal_name = blocks
 	blocks = blocks + 1
 	local signal = "dotfiles::block::" .. signal_name
 	-- Widget and Click Handler:
-	local widget,_ = block_watch(command, timeout, function(widget, stdout)
-		update(widget, stdout)
-	end)
+	local widget = watch(command, timeout, update)
 	-- Attach click handlers for each button:
 	for i,key in ipairs(gears.table.keys(awful.button.names)) do
 		widget:add_button(awful.button({
 			modifiers = {},
 			button = awful.button.names[key],
-			on_press = function() 
-				awful.spawn.easy_async_with_shell("env BUTTON='"..i.."' " ..command, function(stdout)
-					update(widget, stdout)
-				end)
+			on_press = function()
+				run("env BUTTON='"..i.."' " ..command, widget)
 			end
 		}))
 	end
 	-- Connect the control signal for internal updates:
 	awesome.connect_signal(signal, function()
-		awful.spawn.easy_async_with_shell(command, function(stdout)
-			update(widget, stdout)
-		end)
+		run(command, widget)
 	end)
 	return widget, signal
 end
