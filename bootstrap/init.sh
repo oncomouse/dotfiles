934ab8e728ebd15924f50b8f6f335dea31a2e193
oncomouse <oncomouse@gmail.com>
2021-08-26 12:15:52 -0500

    Target

--- bootstrap/init.sh
+++ bootstrap/init.sh
@@ -3,6 +3,10 @@
 os=$(bash ~/dotfiles/bootstrap/scripts/os.sh)
 bash=$(which bash)
 env=$(which env)
+echo -n "Name this target: "
+read -r target
+mkdir -p ~/.local/share/dotfiles
+echo "$target" > ~/.local/share/dotfiles/target
 case $os in
   macos)
     $env SERVER="$SERVER" "$bash" "$HOME/dotfiles/bootstrap/macos.sh"
