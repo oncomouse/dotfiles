cf486ce14539c214ad06db3f2bc3eb9e296c3d3f
oncomouse <oncomouse@gmail.com>
2021-09-11 19:30:56 -0500

    moveresize patch for DWM

--- conf/dwm/config.h
+++ conf/dwm/config.h
@@ -147,6 +147,22 @@ static Key keys[] = {
 	TAGKEYS(                XK_7,                                       6)
 	TAGKEYS(                XK_8,                                       7)
 	TAGKEYS(                XK_9,                                       8)
+	{ MODKEY,                       XK_Down,   moveresize,     {.v = "0x 25y 0w 0h" } },
+	{ MODKEY,                       XK_Up,     moveresize,     {.v = "0x -25y 0w 0h" } },
+	{ MODKEY,                       XK_Right,  moveresize,     {.v = "25x 0y 0w 0h" } },
+	{ MODKEY,                       XK_Left,   moveresize,     {.v = "-25x 0y 0w 0h" } },
+	{ MODKEY|ShiftMask,             XK_Down,   moveresize,     {.v = "0x 0y 0w 25h" } },
+	{ MODKEY|ShiftMask,             XK_Up,     moveresize,     {.v = "0x 0y 0w -25h" } },
+	{ MODKEY|ShiftMask,             XK_Right,  moveresize,     {.v = "0x 0y 25w 0h" } },
+	{ MODKEY|ShiftMask,             XK_Left,   moveresize,     {.v = "0x 0y -25w 0h" } },
+	{ MODKEY|ControlMask,           XK_Up,     moveresizeedge, {.v = "t"} },
+	{ MODKEY|ControlMask,           XK_Down,   moveresizeedge, {.v = "b"} },
+	{ MODKEY|ControlMask,           XK_Left,   moveresizeedge, {.v = "l"} },
+	{ MODKEY|ControlMask,           XK_Right,  moveresizeedge, {.v = "r"} },
+	{ MODKEY|ControlMask|ShiftMask, XK_Up,     moveresizeedge, {.v = "T"} },
+	{ MODKEY|ControlMask|ShiftMask, XK_Down,   moveresizeedge, {.v = "B"} },
+	{ MODKEY|ControlMask|ShiftMask, XK_Left,   moveresizeedge, {.v = "L"} },
+	{ MODKEY|ControlMask|ShiftMask, XK_Right,  moveresizeedge, {.v = "R"} },
 	{ MODKEY|ShiftMask,     XK_q,                       quit,           {0} },
 	{ 0,                    XF86XK_KbdBrightnessDown,   spawn,          SHCMD("sudo /usr/local/bin/keyboard-backlight down") },
 	{ 0,                    XF86XK_KbdBrightnessUp,     spawn,          SHCMD("sudo /usr/local/bin/keyboard-backlight up") },
