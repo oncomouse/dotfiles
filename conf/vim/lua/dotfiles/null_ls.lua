fe53440cd79dba0a6c5fe0e4e872325069d5521f
oncomouse <oncomouse@gmail.com>
2021-09-26 08:57:44 -0500

    htmlhint support

--- conf/vim/lua/dotfiles/null_ls.lua
+++ conf/vim/lua/dotfiles/null_ls.lua
@@ -1,3 +1,8 @@
+local h = require("null-ls.helpers")
+local methods = require("null-ls.methods")
+
+local DIAGNOSTICS = methods.internal.DIAGNOSTICS
+
 require("null-ls").config({
 	sources = {
 		require("null-ls").builtins.formatting.prettier.with({
@@ -8,5 +13,46 @@ require("null-ls").config({
 		require("null-ls").builtins.formatting.fish_indent,
 		require("null-ls").builtins.formatting.shfmt,
 		require("null-ls").builtins.formatting.rufo,
+		require("null-ls").builtins.diagnostics.shellcheck,
+		require("null-ls").builtins.diagnostics.luacheck,
+		require("null-ls").builtins.diagnostics.eslint,
+		require("null-ls").builtins.diagnostics.flake8,
+		require("null-ls").builtins.diagnostics.vint,
+		h.make_builtin({
+			method = DIAGNOSTICS,
+			filetypes = { "html" },
+			generator_opts = {
+				command = "htmlhint",
+				to_stdin = true,
+				args = {
+					"-f", "json", "stdin"
+				},
+				format = "json",
+				check_exit_code = function(code)
+					return code <= 1
+				end,
+				on_output = function(params)
+					local severities = {
+						["error"] = 1,
+						["warning"] = 2,
+						["information"] = 3,
+						["hint"] = 4,
+					}
+					local diagnostics = {}
+					for _,diagnostic in ipairs(params.output[1].messages) do
+						table.insert(diagnostics, {
+							row = diagnostic.line,
+							col = diagnostic.col,
+							code = diagnostic.rule.id,
+							message = diagnostic.message,
+							severity = severities[diagnostic.type],
+						})
+					end
+					return diagnostics
+				end,
+			},
+			factory = h.generator_factory,
+		})
+		-- rubocop, yamllint
 	},
 })
