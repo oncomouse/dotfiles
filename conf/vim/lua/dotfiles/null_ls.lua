05f99b537910861ff83e25447be689ea6b2bc931
oncomouse <oncomouse@gmail.com>
2021-09-26 09:00:58 -0500

    yamllint support for null_ls

--- conf/vim/lua/dotfiles/null_ls.lua
+++ conf/vim/lua/dotfiles/null_ls.lua
@@ -20,6 +20,32 @@ require("null-ls").config({
 		require("null-ls").builtins.diagnostics.vint,
 		h.make_builtin({
 			method = DIAGNOSTICS,
+			filetypes = { "yaml" },
+			generator_opts = {
+				command = "yamllint",
+				to_stdin = true,
+				args = {
+					"-f", "parsable", "-"
+				},
+				format = "line",
+				check_exit_code = function(code)
+					return code <= 1
+				end,
+				on_output = h.diagnostics.from_pattern(
+					[=[(%w+):(%d+):(%d+): %[(%w+)] (.+) %((.+)%)]=],
+					{ "file", "row", "col", "severity", "message", "source" },
+					{
+						severities = {
+							Error = h.diagnostics.severities["error"],
+							Warning = h.diagnostics.severities["warning"],
+						},
+					}
+				),
+			},
+			factory = h.generator_factory,
+		}),
+		h.make_builtin({
+			method = DIAGNOSTICS,
 			filetypes = { "html" },
 			generator_opts = {
 				command = "htmlhint",
