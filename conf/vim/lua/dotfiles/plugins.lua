b2ad8c92a856fcb3bd3c779ef2ccf4e8452578e3
oncomouse <oncomouse@gmail.com>
2021-09-26 10:41:26 -0500

    Updates to formatting of plugins.lua

--- conf/vim/lua/dotfiles/plugins.lua
+++ conf/vim/lua/dotfiles/plugins.lua
@@ -4,16 +4,19 @@ return require('packer').startup(function(use)
 	-- use { 'dstein64/vim-startuptime', cmd = 'StartupTime' }
 	use 'tpope/vim-sensible'
 	use 'xero/securemodelines'
-	use { 'stevearc/qf_helper.nvim' , config = function()
-		require("qf_helper").setup({
-			quickfix = {
-				default_bindings = false,
-			},
-			loclist = {
-				default_bindings = false,
-			}
-		})
-	end}
+	use {
+		'stevearc/qf_helper.nvim',
+		config = function()
+			require("qf_helper").setup({
+				quickfix = {
+					default_bindings = false,
+				},
+				loclist = {
+					default_bindings = false,
+				}
+			})
+		end
+	}
 	use 'sickill/vim-pasta' -- fix block paste for Neovim
 	use 'tpope/vim-commentary' -- gc<motion> to (un)comment
 	use 'tpope/vim-repeat'
@@ -32,18 +35,22 @@ return require('packer').startup(function(use)
 	use 'airblade/vim-rooter' -- Set CWD for projects
 	use 'wellle/targets.vim' -- add next block n]) targets, plus words in commas (a,), asterisks (a*), etc
 	use 'cohama/lexima.vim' -- Autopairs + Endwise
-	use { 'norcalli/nvim-colorizer.lua', event = 'BufReadPre', config = function()
-		if vim.opt.termguicolors:get() then
-			require("colorizer").setup({
-				['*'] = {},
-				markdown = { names = false },
-				text = { names = false },
-				["gina-commit"] = { names = false },
-				css = { css = true },
-				scss = { css = true },
-			})
+	use {
+		'norcalli/nvim-colorizer.lua',
+		event = 'BufReadPre',
+		config = function()
+			if vim.opt.termguicolors:get() then
+				require("colorizer").setup({
+					['*'] = {},
+					markdown = { names = false },
+					text = { names = false },
+					["gina-commit"] = { names = false },
+					css = { css = true },
+					scss = { css = true },
+				})
+			end
 		end
-	end }
+	}
 	use 'junegunn/fzf.vim' -- Add shorcuts for FZF
 	vim.cmd [[command! -bang -nargs=? -complete=dir Files call fzf#vim#files(<q-args>, fzf#vim#with_preview({'options': ['--reverse', '--info=inline']}), <bang>0)]]
 	vim.g.fzf_layout = { window = { width = 1, height = 0.4, yoffset = 1, border = 'top' } }
@@ -54,57 +61,67 @@ return require('packer').startup(function(use)
 		['ctrl-e'] = 'edit',
 	}
 	vim.g.fzf_nvim_statusline = 0 -- disable statusline overwriting
-	use { 'lambdalisue/gina.vim', cmd = 'Gina', config = function()
-		vim.fn['gina#custom#command#option']('status', '--opener', vim.opt.previewheight:get() .. 'split')
-		vim.fn['gina#custom#command#option']('commit', '--opener', vim.opt.previewheight:get() .. 'split')
-		vim.fn['gina#custom#command#option']('diff', '--opener', vim.opt.previewheight:get() .. 'split')
-		vim.fn['gina#custom#command#option']('status', '--group', 'short')
-		vim.fn['gina#custom#command#option']('commit', '--group', 'short')
-		vim.fn['gina#custom#command#option']('diff', '--group', 'short')
-		-- Implement vim-fugitive commands in Gina:
-		vim.fn['gina#custom#mapping#nmap']('status', 'cc', ':<C-u>Gina commit<CR>', {noremap = 1, silent = 1})
-	end }
+	use {
+		'lambdalisue/gina.vim',
+		cmd = 'Gina',
+		config = function()
+			vim.fn['gina#custom#command#option']('status', '--opener', vim.opt.previewheight:get() .. 'split')
+			vim.fn['gina#custom#command#option']('commit', '--opener', vim.opt.previewheight:get() .. 'split')
+			vim.fn['gina#custom#command#option']('diff', '--opener', vim.opt.previewheight:get() .. 'split')
+			vim.fn['gina#custom#command#option']('status', '--group', 'short')
+			vim.fn['gina#custom#command#option']('commit', '--group', 'short')
+			vim.fn['gina#custom#command#option']('diff', '--group', 'short')
+			-- Implement vim-fugitive commands in Gina:
+			vim.fn['gina#custom#mapping#nmap']('status', 'cc', ':<C-u>Gina commit<CR>', {noremap = 1, silent = 1})
+		end,
+	}
 
-	-- Treesitter: {{{
+	-- Treesitter:
 	use { 'nvim-treesitter/nvim-treesitter-textobjects', opt = true }
 	use { 'windwp/nvim-ts-autotag', ft = { 'html', 'javascript', 'javascriptreact' } }
-	use { 'nvim-treesitter/nvim-treesitter', run = function()
-		vim.cmd [[ TSUpdate ]]
-	end, config = function()
-		vim.opt_local.foldexpr = 'nvim_treesitter#foldexpr()'
-		vim.opt_local.foldmethod = 'expr'
-		vim.cmd [[ packadd nvim-treesitter-textobjects ]]
-		require('nvim-treesitter.configs').setup({
-			ensure_installed = 'maintained',
-			highlight = { enable = true },
-			indent = { enable = true },
-			autotag = { enable = true },
-			textobjects = {
-				select = {
-					enable = true,
-					lookahead = true,
-					keymaps = {
-						['af'] = '@function.outer',
-						['if'] = '@function.inner',
-						['ac'] = '@class.outer',
-						['ic'] = '@class.inner',
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = function() vim.cmd [[ TSUpdate ]] end,
+		config = function()
+			vim.opt_local.foldexpr = 'nvim_treesitter#foldexpr()'
+			vim.opt_local.foldmethod = 'expr'
+			vim.cmd [[ packadd nvim-treesitter-textobjects ]]
+			require('nvim-treesitter.configs').setup({
+				ensure_installed = 'maintained',
+				highlight = { enable = true },
+				indent = { enable = true },
+				autotag = { enable = true },
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							['af'] = '@function.outer',
+							['if'] = '@function.inner',
+							['ac'] = '@class.outer',
+							['ic'] = '@class.inner',
+						}
 					}
 				}
-			}
-		})
-	end}
-	-- }}}
+			})
+		end,
+	}
 
-	-- Non-Treesitter Syntax: {{{
-	use { 'plasticboy/vim-markdown', ft = 'markdown' } -- Markdown Syntax
+	-- Non-Treesitter Syntax:
+	use {
+		'plasticboy/vim-markdown',
+		ft = 'markdown',
+	} -- Markdown Syntax
 	vim.g.vim_markdown_frontmatter = 1 -- Format YAML
 	vim.g.vim_markdown_strikethrough = 0 -- Don't format strikethrough
 	vim.g.vim_markdown_conceal = 0 -- Don't conceal
 	vim.g.vim_markdown_conceal_code_blocks = 0 -- Don't conceal code blocks
 	vim.g.vim_markdown_math = 1 -- Do process MathJaX and LaTeX math
-	use { 'godlygeek/tabular',  cmd = { 'Tabularize', 'Tabular' }}
-	-- }}}
-	-- LSP: {{{
+	use { -- Required for TableFormat in vim-markdown but also useful elsewhere
+		'godlygeek/tabular',
+		cmd = { 'Tabularize' }
+	}
+	-- LSP:
 	local lsp_types = {
 		'fish',
 		'css',
@@ -132,7 +149,4 @@ return require('packer').startup(function(use)
 			require('dotfiles.nvim_lsp')
 		end
 	}
--- }}}
 end)
--- }}}
--- # vim:foldmethod=marker
