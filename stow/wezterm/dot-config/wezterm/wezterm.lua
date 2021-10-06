77051cd801badbdc56a583edda8ff01d4ad15922
oncomouse <oncomouse@gmail.com>
2021-10-06 10:48:45 -0500

    I don't think this is going to work for my needs.

--- stow/wezterm/dot-config/wezterm/wezterm.lua
+++ stow/wezterm/dot-config/wezterm/wezterm.lua
@@ -5,6 +5,18 @@ return {
 		"Fira Code",
 		"FiraCode Nerd Font",
 	}),
+	font_antialias = "Subpixel",
+	font_rules = {
+		{
+			italic = true,
+			font = wezterm.font("Fira Code", { italic = false }),
+		},
+		{
+			italic = true,
+			intensity = "Bold",
+			font = wezterm.font("Fira Code", { bold = true, italic = false }),
+		},
+	},
 	font_size = os.getenv("DOTFILES_TARGET") == "laptop" and 11.0 or 18.0,
 	hide_tab_bar_if_only_one_tab = true,
 	color_scheme_dirs = { wezterm.home_dir .. "/.cache/wal/" },
