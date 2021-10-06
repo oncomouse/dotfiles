5574f82a23de699d7191889d5d481ba7af9f1782
oncomouse <oncomouse@gmail.com>
2021-10-06 10:41:07 -0500

    target-based sizing for wizterm

--- stow/wezterm/dot-config/wezterm/wezterm.lua
+++ stow/wezterm/dot-config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ return {
 		"Fira Code",
 		"FiraCode Nerd Font",
 	}),
-	font_size = 18.0,
+	font_size = os.getenv("DOTFILES_TARGET") == "laptop" and 11.0 or 18.0,
 	hide_tab_bar_if_only_one_tab = true,
 	color_scheme_dirs = { wezterm.home_dir .. "/.cache/wal/" },
 	color_scheme = "wal",
