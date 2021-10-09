9a224847c37980ab69fd78a2b14469fe8c77e575
oncomouse <oncomouse@gmail.com>
2021-10-09 12:51:36 -0500

    Moveresize now floats client when move/resize requested

--- conf/awesome/keybindings/utils/moveresize.lua
+++ conf/awesome/keybindings/utils/moveresize.lua
@@ -1,8 +1,10 @@
+local awful = require("awful")
 local function move_resize(move)
 	return function(c)
-		if c.floating then
-			c:relative_move(move.x or 0, move.y or 0, move.w or 0, move.h or 0)
+		if not c.floating then
+			awful.client.floating.toggle(c)
 		end
+		c:relative_move(move.x or 0, move.y or 0, move.w or 0, move.h or 0)
 	end
 end
 
