94d25b3533f422cee6b7f598a3dd93269a72aa1d
oncomouse <oncomouse@gmail.com>
2021-10-09 16:02:44 -0500

    Nope

--- stow/river/dot-config/river/swaybg.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-killall -q swaybg
-while pgrep -x swaybg >/dev/null; do sleep 1; done
-exec swaybg -m fill -i $HOME/.config/river/background.png
--- stow/river/dot-config/yambar/yambar.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-killall -q yambar
-while pgrep -x yambar >/dev/null; do sleep 1; done
-#exec yambar --config $HOME/.config/yambar/bottom.yml & disown
-exec yambar
