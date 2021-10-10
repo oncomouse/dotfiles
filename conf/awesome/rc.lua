84c5d06eda4ef9ec93fbe08b35eaef3e61f44da4
oncomouse <oncomouse@gmail.com>
2021-10-10 17:20:26 -0500

    Bar mouse bindings work now

--- conf/awesome/rc.lua
+++ conf/awesome/rc.lua
@@ -5,7 +5,7 @@ local addtional_path_prefix = os.getenv("HOME") .. "/dotfiles/conf/awesome/"
 local addtional_path = ";" .. addtional_path_prefix .. "?/init.lua;" .. addtional_path_prefix .. "?.lua"
 package.path = package.path .. addtional_path
 require("appearance")
-require("bar")
 require("keybindings")
 require("mousebindings")
+require("bar")
 require("rules")
