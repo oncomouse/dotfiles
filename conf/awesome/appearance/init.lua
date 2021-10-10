d8adc5bc9b6f61df080b1236fde29642eaecbc06
oncomouse <oncomouse@gmail.com>
2021-10-10 17:20:46 -0500

    Formatting

--- conf/awesome/appearance/init.lua
+++ conf/awesome/appearance/init.lua
@@ -18,10 +18,13 @@ beautiful.fg_icon = xrdb.color7
 beautiful.icon_size = 16
 beautiful.notification_icon_size = 16
 beautiful.useless_gap = 0 -- No gaps
-beautiful.border_color_active = beautiful.bg_focus -- Normal border color
-beautiful.border_focus = beautiful.bg_focus -- Focused border color
+-- Border Colors:
+beautiful.border_color_normal = xrdb.color0
+beautiful.border_color_active = beautiful.bg_focus
+beautiful.border_color_floating_normal = xrdb.color0
+beautiful.border_color_floating_active = beautiful.border_color_active
 beautiful.border_width = 1
--- Fonts
+-- Fonts:
 beautiful.hotkeys_font = "FiraCode Nerd Font Normal 12"
 beautiful.hotkeys_description_font = "FiraCode Nerd Font Normal 12"
 -- Wibar stuff:
@@ -34,14 +37,6 @@ beautiful.taglist_shape_border_color_focus = xrdb.color5
 -- local tag_width = dpi(tonumber(last(gears.string.split(beautiful.font, " "))) + 5)
 beautiful.taglist_squares_sel = nil
 beautiful.taglist_squares_unsel = nil
--- Titlebar formatting:
-beautiful.titlebar_font = "FiraCode Nerd Font Bold 12"
-beautiful.titlebar_bg_normal = xrdb.color8
-beautiful.titlebar_fg_normal = xrdb.color7
-beautiful.titlebar_close_button_focus = gears.filesystem.get_themes_dir() .. "default/titlebar/close_normal.png"
-beautiful.titlebar_close_button_focus = gears.filesystem.get_themes_dir() .. "default/titlebar/close_focus.png"
-beautiful.titlebar_bg_focus = xrdb.color0
-beautiful.titlebar_fg_focus = xrdb.color15
 -- Tasklist formatting:
 beautiful.tasklist_bg_focus = xrdb.color0
 beautiful.tasklist_fg_focus = xrdb.color7
