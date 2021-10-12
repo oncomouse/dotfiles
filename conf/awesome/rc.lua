e863ca5c1a1a4405c32445c5ffd3fa3487304ba6
oncomouse <oncomouse@gmail.com>
2021-10-12 18:58:58 -0500

    Only load heartbeat on desktop

--- conf/awesome/rc.lua
+++ conf/awesome/rc.lua
@@ -8,4 +8,6 @@ require("keybindings")
 require("mousebindings")
 require("bar")
 require("rules")
-require("utils.heartbeat") -- Prevent screensaver when designated apps are running
+if not require("utils.is_laptop") then
+	require("utils.heartbeat") -- Prevent screensaver when designated apps are running
+end
