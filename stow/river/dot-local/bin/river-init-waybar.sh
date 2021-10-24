8fb539e76eea0f7d55887e6035dad672b1234dbc
oncomouse <oncomouse@gmail.com>
2021-10-24 14:10:12 -0500

    Laptop waybar

--- stow/river/dot-local/bin/river-init-waybar.sh
+++ stow/river/dot-local/bin/river-init-waybar.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
+DOTFILES_TARGET="$( [ -e ~/.local/share/dotfiles/target ] && /bin/cat ~/.local/share/dotfiles/target)"
+if [ "$DOTFILES_TARGET" == "laptop" ]; then
+	config="-c $HOME/.config/waybar/config-laptop"
+else
+	config=""
+fi
 
 killall -q waybar
 while pgrep -x waybar >/dev/null; do sleep 1; done
-exec waybar
+exec waybar "$config"
