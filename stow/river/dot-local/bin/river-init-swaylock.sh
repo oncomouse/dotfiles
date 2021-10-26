2b48edcff2dd241ec323dcf6b6a961200b77e422
oncomouse <oncomouse@gmail.com>
2021-10-25 19:14:43 -0500

    Maybe working swayidle

--- stow/river/dot-local/bin/river-init-swaylock.sh
+++ stow/river/dot-local/bin/river-init-swaylock.sh
@@ -4,5 +4,7 @@ source ~/.cache/wal/colors.sh
 pkill -f swayidle
 exec swayidle -w \
 	timeout 300 "swaylock -c ${color0//#/}" \
+	timeout 450 "wlopm --off *" \
+	resume "wlopm --on *" \
 	before-sleep "swaylock -c ${color0//#/}" \
 # resume "wlopm --on $(wlopm | sed -e "s/ .*//")"
