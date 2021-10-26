b230cbcd55fbe1afcf49a9fae6ac355ac3d22146
oncomouse <oncomouse@gmail.com>
2021-10-26 13:14:46 -0500

    One other check from DWM config

--- stow/river/dot-config/river/bindings.sh
+++ stow/river/dot-config/river/bindings.sh
@@ -133,6 +133,7 @@ riverctl set-repeat 50 300
 # Set app-ids of views which should float
 riverctl float-filter-add app-id "float"
 riverctl float-filter-add app-id "popup"
+riverctl float-filter-add app-id "pop-up"
 riverctl float-filter-add app-id "mpv"
 # Set app-ids of views which should use client side decorations
 # riverctl csd-filter-add "gedit"
