d94a556ada78512df8f1229b734775fe5497f6b2
oncomouse <oncomouse@gmail.com>
2021-11-02 08:39:31 -0500

    Finally got Lexima config out of the main file

--- conf/vim/lua/dotfiles/plugins.lua
+++ conf/vim/lua/dotfiles/plugins.lua
@@ -22,7 +22,15 @@ return require("packer").startup({
 		}
 		use("airblade/vim-rooter") -- Set CWD for projects
 		use("wellle/targets.vim") -- add next block n]) targets, plus words in commas (a,), asterisks (a*), etc
-		use("cohama/lexima.vim") -- Autopairs + Endwise
+		use({
+			"cohama/lexima.vim",
+			config = function()
+				vim.cmd([[autocmd! dotfiles-settings FileType lua call dotfiles#lexima#extend_endwise()]])
+				local map = require("dotfiles.utils.map")
+				map.inoremap("<silent>", "<Plug>(dotfiles-lexima)", '<C-r>=lexima#insmode#leave_till_eol("")<CR>')
+				map.imap("<silent>", "<C-l>", "<Plug>(dotfiles-lexima)")
+			end,
+		}) -- Autopairs + Endwise
 		use({
 			"norcalli/nvim-colorizer.lua",
 			event = "BufReadPre",
