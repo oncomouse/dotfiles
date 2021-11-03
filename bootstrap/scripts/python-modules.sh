011fc3f9ebaf80c1b91ff6b9e38b8b0d5eb9ccbb
oncomouse <oncomouse@gmail.com>
2021-11-03 14:50:58 -0500

    Use pacman to manage Python modules

--- bootstrap/scripts/python-modules.sh
+++ bootstrap/scripts/python-modules.sh
@@ -3,44 +3,53 @@
 pip3list=$(pip3 list | cut -d " " -f 1 | sed 1,2d)
 os=$(bash ~/dotfiles/bootstrap/scripts/os.sh)
 function pip3install() {
-  local repo=$1
-  if ! [[ $pip3list =~ $repo ]]; then 
-    pip3 install "$1"
-  fi
+	local repo=$1
+	if ! [[ $pip3list =~ $repo ]]; then 
+		pip3 install "$1"
+	fi
 }
 if [ "$os" == 'macos' ]; then
-  pip3install 'mackup'
+	pip3install 'mackup'
 fi
-if [ -z "$SERVER" ];then
-  pip3install 'neovim'
-  # Vim Linters:
-  pip3install 'vim-vint'
-  # Python Linters:
-  pip3install 'jedi'
-  pip3install 'flake8'
-  pip3install 'bandit'
-  pip3install 'autopep8'
-  pip3install 'reorder-python-imports'
-  pip3install 'mypy'
-  pip3install 'black'
-  # Yaml Linters:
-  pip3install 'yamllint'
-  # Markdown Linters:
-  pip3install 'proselint'
-  # BibTeX
-  # pip3install 'bibtexparser'
-  pip3install 'bibparse'
-  # LSP
-  pip3install 'citation-langserver'
-  # Image support for Ranger:
-  pip3install 'pillow'
-  # Pywal:
-  pip3install 'pywal'
-  pip3install 'pywalfox'
-  pip3install 'neovim-remote'
+if [ "$os" == "arch" ]; then
+	if [ -z "$SERVER" ];then
+		sudo pacman -S vint
+		sudo pacman -S flake8
+		sudo pacman -S python-black
+		sudo pacman -S yamllint
+		sudo pacman -S python-pywal
+	fi
+	sudo pacman -S python-poetry
+	sudo pacman -S python-pynvim
+else
+	if [ -z "$SERVER" ];then
+		# Vim Linters:
+		pip3install 'vim-vint'
+		# Python Linters:
+		pip3install 'flake8'
+		pip3install 'black'
+		# Yaml Linters:
+		pip3install 'yamllint'
+		# Pywal:
+		pip3install 'pywal'
+	fi
+	# Install poetry
+	curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3
+	pip3install 'pynvim'
 fi
-pip3install 'pynvim'
+if [ -z "$SERVER" ]; then
+	pip3install 'neovim'
+	pip3install 'reorder-python-imports'
+	pip3install 'bibparse'
+	# LSP
+	pip3install 'citation-langserver'
+	pip3install 'pywalfox'
+fi
 pip3install 'virtualfish'
-pip3install 'ranger-fm'
-# Install poetry
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3
+# pip3install 'ranger-fm'
+# pip3install 'jedi'
+# pip3install 'mypy'
+# pip3install 'bandit'
+# pip3install 'autopep8'
+# Markdown Linters:
+# pip3install 'proselint'
