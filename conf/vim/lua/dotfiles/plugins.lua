5f3293fda93a1826107b96dcda3c7ea4f677beef
oncomouse <oncomouse@gmail.com>
2021-11-10 12:15:43 -0600

    Full modularization for plugins

--- conf/vim/lua/dotfiles/plugins.lua
+++ conf/vim/lua/dotfiles/plugins.lua
@@ -10,17 +10,22 @@ return require("packer").startup({
 		use("tpope/vim-commentary") -- gc<motion> to (un)comment
 		use("tpope/vim-repeat")
 		use("oncomouse/vim-surround") -- ys to add, cs to change, ds to delete. f, F for function, t, T for tag
-		vim.opt.path = ",,"
-		vim.g.rooter_patterns = {
-			"Rakefile",
-			"package.json",
-			".git/",
-			"Gemfile",
-			"pyproject.toml",
-			"setup.py",
-			"Makefile",
-		}
-		use("airblade/vim-rooter") -- Set CWD for projects
+		use({
+			"airblade/vim-rooter",
+			event = "VimEnter",
+			setup = function()
+				vim.opt.path = ",,"
+				vim.g.rooter_patterns = {
+					"Rakefile",
+					"package.json",
+					".git/",
+					"Gemfile",
+					"pyproject.toml",
+					"setup.py",
+					"Makefile",
+				}
+			end,
+		}) -- Set CWD for projects
 		use("wellle/targets.vim") -- add next block n]) targets, plus words in commas (a,), asterisks (a*), etc
 		use({
 			"cohama/lexima.vim",
@@ -33,7 +38,6 @@ return require("packer").startup({
 		}) -- Autopairs + Endwise
 		use({
 			"norcalli/nvim-colorizer.lua",
-			event = "BufReadPre",
 			config = function()
 				if vim.opt.termguicolors:get() then
 					require("colorizer").setup({
@@ -46,18 +50,23 @@ return require("packer").startup({
 				end
 			end,
 		})
-		use("junegunn/fzf.vim") -- Add shorcuts for FZF
-		vim.cmd(
-			[[command! -bang -nargs=? -complete=dir Files call fzf#vim#files(<q-args>, fzf#vim#with_preview({'options': ['--reverse', '--info=inline']}), <bang>0)]]
-		)
-		vim.g.fzf_layout = { window = { width = 1, height = 0.4, yoffset = 1, border = "top" } }
-		vim.g.fzf_action = {
-			["ctrl-s"] = "split",
-			["ctrl-v"] = "vsplit",
-			["ctrl-t"] = "tabnew",
-			["ctrl-e"] = "edit",
-		}
-		vim.g.fzf_nvim_statusline = 0 -- disable statusline overwriting
+		use({
+			"junegunn/fzf.vim",
+			event = "VimEnter",
+			setup = function()
+				vim.cmd(
+					[[command! -bang -nargs=? -complete=dir Files call fzf#vim#files(<q-args>, fzf#vim#with_preview({'options': ['--reverse', '--info=inline']}), <bang>0)]]
+				)
+				vim.g.fzf_layout = { window = { width = 1, height = 0.4, yoffset = 1, border = "top" } }
+				vim.g.fzf_action = {
+					["ctrl-s"] = "split",
+					["ctrl-v"] = "vsplit",
+					["ctrl-t"] = "tabnew",
+					["ctrl-e"] = "edit",
+				}
+				vim.g.fzf_nvim_statusline = 0 -- disable statusline overwriting
+			end,
+		}) -- Add shorcuts for FZF
 		use({
 			"lambdalisue/gina.vim",
 			cmd = "Gina",
@@ -130,16 +139,20 @@ return require("packer").startup({
 		use({
 			"plasticboy/vim-markdown",
 			ft = "markdown",
+			setup = function()
+				vim.g.vim_markdown_frontmatter = 1 -- Format YAML
+				vim.g.vim_markdown_strikethrough = 0 -- Don't format strikethrough
+				vim.g.vim_markdown_conceal = 0 -- Don't conceal
+				vim.g.vim_markdown_conceal_code_blocks = 0 -- Don't conceal code blocks
+				vim.g.vim_markdown_math = 1 -- Do process MathJaX and LaTeX math
+			end,
+			requires = {
+				{ -- Required for TableFormat in vim-markdown but also useful elsewhere
+					"godlygeek/tabular",
+					cmd = { "Tabularize" },
+				},
+			},
 		}) -- Markdown Syntax
-		vim.g.vim_markdown_frontmatter = 1 -- Format YAML
-		vim.g.vim_markdown_strikethrough = 0 -- Don't format strikethrough
-		vim.g.vim_markdown_conceal = 0 -- Don't conceal
-		vim.g.vim_markdown_conceal_code_blocks = 0 -- Don't conceal code blocks
-		vim.g.vim_markdown_math = 1 -- Do process MathJaX and LaTeX math
-		use({ -- Required for TableFormat in vim-markdown but also useful elsewhere
-			"godlygeek/tabular",
-			cmd = { "Tabularize" },
-		})
 
 		-- Snippets:
 		use({
