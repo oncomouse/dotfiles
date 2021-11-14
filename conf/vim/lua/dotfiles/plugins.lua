63b86c3a7d72673301aa4b9d0667ded384322ec1
oncomouse <oncomouse@gmail.com>
2021-11-14 10:11:58 -0600

    Indent guides + treesitter playground rough-in

--- conf/vim/lua/dotfiles/plugins.lua
+++ conf/vim/lua/dotfiles/plugins.lua
@@ -168,9 +168,28 @@ return require("packer").startup({
 						context_commentstring = {
 							enable = true,
 						},
+						-- playground = {
+						-- 	enable = true,
+						-- 	disable = {},
+						-- 	updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+						-- 	persist_queries = false, -- Whether the query persists across vim sessions
+						-- 	keybindings = {
+						-- 		toggle_query_editor = "o",
+						-- 		toggle_hl_groups = "i",
+						-- 		toggle_injected_languages = "t",
+						-- 		toggle_anonymous_nodes = "a",
+						-- 		toggle_language_display = "I",
+						-- 		focus_language = "f",
+						-- 		unfocus_language = "F",
+						-- 		update = "R",
+						-- 		goto_node = "<cr>",
+						-- 		show_help = "?",
+						-- 	},
+						-- },
 					})
 				end,
 				requires = {
+					-- "nvim-treesitter/playground",
 					{ "nvim-treesitter/nvim-treesitter-textobjects" },
 					{ "windwp/nvim-ts-autotag", ft = { "html", "javascript", "javascriptreact" } },
 					{
@@ -206,6 +225,36 @@ return require("packer").startup({
 					},
 				},
 			}, -- Markdown Syntax
+			{
+				"lukas-reineke/indent-blankline.nvim",
+				config = function()
+					-- Indent character color:
+					vim.cmd(
+						"highlight IndentBlanklineChar guifg="
+							.. require("dotfiles.wal-colors").color8
+							.. " gui=nocombine"
+					)
+					-- Context indent character color:
+					vim.cmd(
+						"highlight IndentBlanklineContextChar guifg="
+							.. require("dotfiles.wal-colors").color6
+							.. " gui=nocombine"
+					)
+					-- Start of context underline color:
+					-- vim.cmd(
+					-- 	"highlight IndentBlanklineContextStart guisp="
+					-- 		.. require("dotfiles.wal-colors").color6
+					-- 		.. " gui=underline"
+					-- )
+					require("indent_blankline").setup({
+						buftype_exclude = { "terminal" },
+						filetype_exclude = { "diff", "gina-status" },
+						show_current_context = true,
+						context_patterns = { "class", "function", "method", "^if", "table", },
+						-- show_current_context_start = false,
+					})
+				end,
+			},
 		})
 	end,
 })
