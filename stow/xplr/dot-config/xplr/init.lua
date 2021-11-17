1753b1ddb73bdbc7ffb76a2ca50de1bd147927b4
oncomouse <oncomouse@gmail.com>
2021-11-17 10:19:06 -0600

    Unused app configuration

--- stow/xplr/dot-config/xplr/init.lua
+++
@@ -1,5 +0,0 @@
--- luacheck: globals xplr
-version = "0.14.3"
-xplr.config.node_types.directory.meta.icon = ""
-xplr.config.node_types.file.meta.icon = ""
-xplr.config.node_types.symlink.meta.icon = ""
