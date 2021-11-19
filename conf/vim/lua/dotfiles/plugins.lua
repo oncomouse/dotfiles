3f38353881a52aa36beabad7f1c21b25d368bfc6
oncomouse <oncomouse@gmail.com>
2021-11-19 10:03:30 -0600

    Turn off playground again

--- conf/vim/lua/dotfiles/plugins.lua
+++ conf/vim/lua/dotfiles/plugins.lua
@@ -177,28 +177,28 @@ return require("packer").startup({
 						context_commentstring = {
 							enable = true,
 						},
-						playground = {
-							enable = true,
-							disable = {},
-							updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-							persist_queries = false, -- Whether the query persists across vim sessions
-							keybindings = {
-								toggle_query_editor = "o",
-								toggle_hl_groups = "i",
-								toggle_injected_languages = "t",
-								toggle_anonymous_nodes = "a",
-								toggle_language_display = "I",
-								focus_language = "f",
-								unfocus_language = "F",
-								update = "R",
-								goto_node = "<cr>",
-								show_help = "?",
-							},
-						},
+						-- playground = {
+						-- 	enable = true,
+						-- 	disable = {},
+						-- 	updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+						-- 	persist_queries = false, -- Whether the query persists across vim sessions
+						-- 	keybindings = {
+						-- 		toggle_query_editor = "o",
+						-- 		toggle_hl_groups = "i",
+						-- 		toggle_injected_languages = "t",
+						-- 		toggle_anonymous_nodes = "a",
+						-- 		toggle_language_display = "I",
+						-- 		focus_language = "f",
+						-- 		unfocus_language = "F",
+						-- 		update = "R",
+						-- 		goto_node = "<cr>",
+						-- 		show_help = "?",
+						-- 	},
+						-- },
 					})
 				end,
 				requires = {
-					"nvim-treesitter/playground",
+					-- "nvim-treesitter/playground",
 					{ "nvim-treesitter/nvim-treesitter-textobjects" },
 					{ "windwp/nvim-ts-autotag", ft = { "html", "javascript", "javascriptreact" } },
 					{
