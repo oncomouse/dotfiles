d9ff6eb8a5dfb29feefae89d18c6fe1d361d34ba
oncomouse <oncomouse@gmail.com>
2021-11-29 11:19:14 -0600

    Should work without having first run the browser

--- bootstrap/scripts/firefox.sh
+++ bootstrap/scripts/firefox.sh
@@ -15,18 +15,20 @@ update_chrome() {
 		return 1
 	fi
 	dir="$(find "$moz_dir" -maxdepth 1 -iname "*default-release*")"
-	if [ -d "$dir" ]; then
-		mkdir -p "$dir/chrome"
-		ln -sf ~/dotfiles/conf/firefox/userChrome.css "$dir/chrome"
-		curl -so "$dir/chrome/userContent.css" https://raw.githubusercontent.com/migueravila/SimplerentFox/master/Linux/userContent.css
-		cat << EOF > "$dir/user.js"
+	if [ ! -d "$dir" ]; then
+		$target --headless --first-startup& # Run the browser
+		sleep 5
+		pkill "$target"
+		dir="$(find "$moz_dir" -maxdepth 1 -iname "*default-release*")"
+	fi
+	mkdir -p "$dir/chrome"
+	ln -sf ~/dotfiles/conf/firefox/userChrome.css "$dir/chrome"
+	curl -so "$dir/chrome/userContent.css" https://raw.githubusercontent.com/migueravila/SimplerentFox/master/Linux/userContent.css
+	cat << EOF > "$dir/user.js"
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.compactmode.show", true);
 user_pref("svg.context-properties.content.enabled", true);
 EOF
-	else
-		echo "No profile found. Perhaps you've not yet run Firefox?"
-	fi
 }
 
 update_chrome "firefox"
