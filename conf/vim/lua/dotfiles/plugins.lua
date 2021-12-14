692c2f933920233a82c971f0aa2127bd25eafebb
oncomouse <oncomouse@gmail.com>
2021-12-14 11:57:00 -0600

    Custom markdown highlight queries for TS

--- conf/vim/lua/dotfiles/plugins.lua
+++ conf/vim/lua/dotfiles/plugins.lua
@@ -1,6 +1,6 @@
 --luacheck: globals vim dotfiles
 dotfiles = _G.dotfiles or {}
-vim.g.dotfiles_ts_playground = false -- Load playground
+vim.g.dotfiles_ts_playground = true -- Load playground
 return require("packer").startup({
 	function(use)
 		use({
@@ -242,13 +242,50 @@ return require("packer").startup({
 							},
 						},
 					})
+					
+					require("vim.treesitter.query").set_query("markdown", "highlights", [[
+[
+	(atx_heading)
+	(setext_heading)
+] @text.title
+
+(code_fence_content) @none
+
+[
+	(indented_code_block)
+	(fenced_code_block)
+	(code_span)
+] @text.literal
+
+
+(emphasis) @text.emphasis
+
+(strong_emphasis) @text.strong
+
+(link_text) @string.special
+
+(link_destination) @text.uri
+
+(link_label) @text.reference
+
+[
+	(list_marker_plus)
+	(list_marker_minus)
+	(list_marker_star)
+	(list_marker_dot)
+	(list_marker_parenthesis)
+] @punctuation.special
+
+[
+	(backslash_escape)
+	(hard_line_break)
+] @string.escape
+	]])
 				end,
 				requires = {
 					{
 						"nvim-treesitter/playground",
-						cond = function()
-							return vim.g.dotfiles_ts_playground or false
-						end,
+						command = "TSPlaygroundToggle",
 					},
 					{ "nvim-treesitter/nvim-treesitter-textobjects" },
 					{ "windwp/nvim-ts-autotag", ft = { "html", "javascript", "javascriptreact" } },
