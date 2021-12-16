baa11f365ee29adb99aaf3ae8b5f8a06c2dbde35
oncomouse <oncomouse@gmail.com>
2021-12-16 15:05:11 -0600

    Adding strikethrough support

--- conf/vim/lua/dotfiles/plugins.lua
+++ conf/vim/lua/dotfiles/plugins.lua
@@ -276,7 +276,7 @@ return require("packer").startup({
 				ft = "markdown",
 				setup = function()
 					vim.g.vim_markdown_frontmatter = 1 -- Format YAML
-					vim.g.vim_markdown_strikethrough = 0 -- Don't format strikethrough
+					vim.g.vim_markdown_strikethrough = 1 -- Don't format strikethrough
 					vim.g.vim_markdown_conceal = 0 -- Don't conceal
 					vim.g.vim_markdown_conceal_code_blocks = 0 -- Don't conceal code blocks
 					vim.g.vim_markdown_math = 1 -- Do process MathJaX and LaTeX math
