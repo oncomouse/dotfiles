695aa3f1e172bc50d83859a512a11f13ec22fdc2
oncomouse <oncomouse@gmail.com>
2021-12-18 07:50:05 -0600

    That's why notifications never time out

--- conf/awesome/rules/init.lua
+++ conf/awesome/rules/init.lua
@@ -1,8 +1,20 @@
 local ruled = require("ruled")
 local beautiful = require("beautiful")
+local awful = require("awful")
 require("rules.config")
 ruled.client.connect_signal("request::rules", function()
 	for _, rule in ipairs(beautiful.rules) do
 		ruled.client.append_rule(rule)
 	end
+end)
+
+ruled.notification.connect_signal('request::rules', function()
+    -- All notifications will match this rule.
+    ruled.notification.append_rule {
+        rule       = { },
+        properties = {
+            screen           = awful.screen.preferred,
+            implicit_timeout = 5,
+        }
+    }
 end)
