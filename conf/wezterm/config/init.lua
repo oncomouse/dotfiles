a32b924cf6248921cd395f9f5245bff7081c38fb
oncomouse <oncomouse@gmail.com>
2021-12-29 13:48:25 -0600

    Use a lua file to generate tab bar colors

--- conf/wezterm/config/init.lua
+++ conf/wezterm/config/init.lua
@@ -4,5 +4,6 @@ local table_merge = require("utils.table_merge").table_merge
 local defaults = require("config.base")
 local fonts = require("config.fonts")
 local keys = require("config.keys")
+local colors = require("wezterm_colors")
 
-return table_merge(defaults, fonts, keys)
+return table_merge(defaults, fonts, keys, colors)
--- stow/wezterm/dot-config/wezterm/wezterm.lua
+++ stow/wezterm/dot-config/wezterm/wezterm.lua
@@ -1,6 +1,11 @@
 -- Extend path:
-local addtional_path_prefix = os.getenv("HOME") .. "/dotfiles/conf/wezterm/"
-local addtional_path = ";" .. addtional_path_prefix .. "?/init.lua;" .. addtional_path_prefix .. "?.lua"
-package.path = package.path .. addtional_path
+local function extend_path(add_dir)
+	local addtional_path_prefix = add_dir
+	local addtional_path = ";" .. addtional_path_prefix .. "?/init.lua;" .. addtional_path_prefix .. "?.lua"
+	package.path = package.path .. addtional_path
+end
+
+extend_path(os.getenv("HOME") .. "/dotfiles/conf/wezterm/")
+extend_path(require("config.env").XDG_CACHE_HOME .. "/wal/")
 
 return require("config")
