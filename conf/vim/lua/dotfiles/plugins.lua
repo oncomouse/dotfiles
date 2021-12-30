5914160ac391a6e32b3eaca7713d903cc0c3e9e8
oncomouse <oncomouse@gmail.com>
2021-12-29 19:15:16 -0600

    NO idea

--- conf/vim/lua/dotfiles/plugins.lua
+++ conf/vim/lua/dotfiles/plugins.lua
@@ -16,6 +16,22 @@ return require("packer").startup({
 				end,
 			},
 			{
+				"airblade/vim-rooter",
+				event = "VimEnter",
+				setup = function()
+					vim.opt.path = ",,"
+					vim.g.rooter_patterns = {
+						"Rakefile",
+						"package.json",
+						".git/",
+						"Gemfile",
+						"pyproject.toml",
+						"setup.py",
+						"Makefile",
+					}
+				end,
+			}, -- Set CWD for projects
+			{
 				"ahmedkhalf/project.nvim",
 				config = function()
 					require("project_nvim").setup({
@@ -176,6 +192,8 @@ return require("packer").startup({
 							autocomplete = false,
 						},
 					})
+					local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+					cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 				end,
 			}, -- Completion
 			{
