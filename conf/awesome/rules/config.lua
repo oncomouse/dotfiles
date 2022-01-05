5cd3d8aeeea5eadc7adf761c1765eb6e8f2386f1
oncomouse <oncomouse@gmail.com>
2022-01-05 15:58:46 -0600

    Install thunar

--- conf/awesome/rules/config.lua
+++ conf/awesome/rules/config.lua
@@ -20,6 +20,7 @@ beautiful.rules = {
 			class = {
 				"mpv",
 				"zoom",
+				"Thunar",
 			},
 			-- Note that the name property shown in xprop might be set slightly after creation of the client
 			-- and the name shown there might not match defined rules here.
