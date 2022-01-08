3ba908e3c754811a01581fba1d2ecff88b32d732
oncomouse <oncomouse@gmail.com>
2022-01-08 11:17:19 -0600

    Add the ability to add from the database

--- stow/mpd/dot-local/bin/mpd_rofi.sh
+++ stow/mpd/dot-local/bin/mpd_rofi.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+# shellcheck disable=SC2119,SC2120
 
-rofi_cmd="fd --type d --exact-depth 2 . /mnt/Media/Music/ | cut -d / -f 5-6"
+rofi_bar() {
+	rofi -match fuzzy -auto-select -dmenu -i -location 1 -theme-str "window { width: 100%; }" "$@"
+}
 
 action-search() {
-	match="$(mpc playlist | $rofi_cmd)"
+	match="$(mpc playlist | rofi_bar)"
 	if [[ "$match" != "" ]]; then
 		match_pos="$(mpc playlist | grep -n "$match" | cut -d : -f 1)"
 		mpc play "$match_pos" > /dev/null
@@ -11,10 +14,10 @@ action-search() {
 }
 
 action-add() {
-	match="$(fd --type d --exact-depth 2 . /mnt/Media/Music/ | cut -d / -f 5-6 | $rofi_cmd)"
+	match="$(fd --type d --exact-depth 2 . /mnt/Media/Music/ | cut -d / -f 5-6 | rofi_bar)"
 	if [[ "$match" != "" ]]; then
-		for x in $(/mnt/Media/$(match)/*.{flac,mp3} 2> /dev/null); do
-			mpc add "$x"
+		fd -e mp3 -e flac . /mnt/Media/Music/"$match" --print0 | while read -rd $'\0' file; do
+			mpc add "$(echo "$file" | sed -e "s/\/mnt\/Media\/Music\///")"
 		done
 	fi
 }
