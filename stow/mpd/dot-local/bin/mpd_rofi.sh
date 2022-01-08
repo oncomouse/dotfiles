659e5f6116b09d4403eae6229331bc743636ce6a
oncomouse <oncomouse@gmail.com>
2022-01-08 11:35:18 -0600

    MPD-only but slower version

--- stow/mpd/dot-local/bin/mpd_rofi.sh
+++ stow/mpd/dot-local/bin/mpd_rofi.sh
@@ -9,11 +9,23 @@ action-search() {
 	match="$(mpc playlist | rofi_bar)"
 	if [[ "$match" != "" ]]; then
 		match_pos="$(mpc playlist | grep -n "$match" | cut -d : -f 1)"
-		mpc play "$match_pos" > /dev/null
+		mpc play "$match_pos" >/dev/null
 	fi
 }
 
 action-add() {
+	# albums="$(mpc ls | while IFS= read -r artist; do
+	# 	mpc ls "$artist" 2> /dev/null | while IFS= read -r album; do
+	# 		printf "%s\n" "$album"
+	# 	done
+	# done)"
+	# match="$(printf "%s" "$albums" | rofi_bar)"
+	# if [[ "$match" != "" ]]; then
+	# 	 mpc ls "$match" 2> /dev/null | while IFS= read -r song; do
+	# 	 mpc add "$song"
+	#  done
+	# fi
+
 	match="$(fd --type d --exact-depth 2 . /mnt/Media/Music/ | cut -d / -f 5-6 | rofi_bar)"
 	if [[ "$match" != "" ]]; then
 		fd -e mp3 -e flac . /mnt/Media/Music/"$match" --print0 | while read -rd $'\0' file; do
