7556b9426c2d5bd33ef3f8ce3b3f43145ff1f000
oncomouse <oncomouse@gmail.com>
2022-01-14 16:49:02 -0600

    diagnostic signs

--- conf/vim/init.lua
+++ conf/vim/init.lua
@@ -363,6 +363,13 @@ dotfiles.fasd_update = function()
 	end
 end
 -- }}}
+-- Signs: {{{
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
+-- }}}
 -- Writing {{{
 vim.g.bibfiles = "~/Seadrive/My Libraries/My Library/Documents/Academic Stuff/library.bib"
 -- }}}
