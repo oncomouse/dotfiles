81f159efabad67e02435fce945521c2e72944816
oncomouse <oncomouse@gmail.com>
2022-01-27 12:23:58 -0600

    Mf command

---
+++ conf/vim/plugin/mf.lua
@@ -0,0 +1,8 @@
+-- luacheck: globals vim
+vim.api.nvim_add_user_command("Mf", function(args)
+	vim.fn.system("mf " .. args.args)
+	vim.cmd("e " .. args.args)
+end, {
+	complete = "dir",
+	nargs = 1,
+})
