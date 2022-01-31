56b4598d62941d90bca072ea8c897772db8c0030
oncomouse <oncomouse@gmail.com>
2022-01-31 16:35:39 -0600

    Add -k to support downloading symlinks as dirs

--- stow/scripts/dot-local/bin/sync_music.sh
+++ stow/scripts/dot-local/bin/sync_music.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-rsync -av -e ssh --progress "andrew@192.168.1.48:/mnt/Media/Devices" "$HOME/Music"
+rsync -akv -e ssh --progress "andrew@192.168.1.48:/mnt/Media/Devices" "$HOME/Music"
