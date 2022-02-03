d0405684970bd12ae05ba68a7ac1c59cb2aa604b
oncomouse <oncomouse@gmail.com>
2022-02-03 09:25:23 -0600

    Always start with a clean build:wq

--- stow/scripts/dot-local/bin/compile_awesome.sh
+++ stow/scripts/dot-local/bin/compile_awesome.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 owd="$(pwd)"
 cd ~/Projects/awesome || exit
+sudo rm -rf build
 CMAKE_ARGS="-DLUA_EXECUTABLE=/usr/bin/lua5.3 -DGENERATE_DOC=OFF -DGENERATE_MANPAGES=OFF  -DLUA_LIBRARY=/usr/lib/liblua.so.5.3 -DLUA_INCLUDE_DIR=/usr/include/lua5.3" make
 sudo make install
 sudo cp /usr/local/share/xsessions/awesome.desktop /usr/share/xsessions
