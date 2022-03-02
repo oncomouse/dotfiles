51e923419c7aaa52fb5ed78dfee6b00b54043376
oncomouse <oncomouse@gmail.com>
2022-03-01 18:58:52 -0600

    Autostart isn't working

--- conf/awesome/rc.lua
+++ conf/awesome/rc.lua
@@ -4,7 +4,7 @@ local addtional_path_prefix = os.getenv("HOME") .. "/dotfiles/conf/awesome/"
 local addtional_path = ";" .. addtional_path_prefix .. "?/init.lua;" .. addtional_path_prefix .. "?.lua"
 package.path = package.path .. addtional_path
 -- Autostart
-require("awful").spawn(os.getenv("HOME") .. "/dotfiles/conf/awesome/autostart", false)
+-- require("awful").spawn(os.getenv("HOME") .. "/dotfiles/conf/awesome/autostart", false)
 require("appearance")
 require("keybindings")
 require("mousebindings")
