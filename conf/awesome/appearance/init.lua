06e9990fd269c8c9082db0c72af93902e347f54f
oncomouse <oncomouse@gmail.com>
2022-03-06 09:04:29 -0600

    Don't turn this back on

--- conf/awesome/appearance/init.lua
+++ conf/awesome/appearance/init.lua
@@ -60,7 +60,8 @@ screen.connect_signal("request::desktop_decoration", function(s)
 end)
 require("awful.autofocus")
 require("appearance.behaviors.sloppyfocus")
-require("appearance.behaviors.attachbelow") -- attachbelow patch
+-- You don't want to turn this on anymore:
+-- require("appearance.behaviors.attachbelow") -- attachbelow patch
 require("appearance.behaviors.smartborders") -- smartborder patch
 require("appearance.behaviors.shadow_for_floating") -- shadows on floating windows only
 require("appearance.titlebars")
