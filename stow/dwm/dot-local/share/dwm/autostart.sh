484baf409e7ef58f9992fa19f0a4ca02774a8411
oncomouse <oncomouse@gmail.com>
2022-03-13 11:58:27 -0500

    Autostart for DWM

--- stow/dwm/dot-local/share/dwm/autostart.sh
+++ stow/dwm/dot-local/share/dwm/autostart.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 source ~/dotfiles/conf/xorg/xinit-common
 dex -a
-source ~/.cache/wal/colors.sh
+
 xrdb -merge ~/.cache/wal/dwm.Xresources
 xrdb -merge ~/.cache/wal/dmenu.Xresources
+
+source ~/dotfiles/conf/xorg/xinit.d/no-xfce4
+source ~/dotfiles/conf/xorg/xinit.d/dunst
 
 convert ~/.cache/wal/background.svg ~/.cache/wal/background.png
 hsetroot -tile ~/.cache/wal/background.png
 
-dunst&
-
 pkill dwmblocks
 dwmblocks&
-
