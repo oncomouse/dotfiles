15e1f1065b8587b56d8fb2edb5922413a4a230b6
oncomouse <oncomouse@gmail.com>
2022-03-12 18:58:41 -0600

    This sort of works now

--- stow/shod/dot-local/share/shod/autostart.sh
+++ stow/shod/dot-local/share/shod/autostart.sh
@@ -2,6 +2,7 @@
 source ~/dotfiles/conf/xorg/xinit-common
 dex -a
 picom --experimental-backends -b --config ~/dotfiles/conf/picom/common.conf
+sxhkd 2> ~/.cache/sxhkd.log &
 
 convert ~/.cache/wal/background.svg ~/.cache/wal/background.png
 hsetroot -tile ~/.cache/wal/background.png
