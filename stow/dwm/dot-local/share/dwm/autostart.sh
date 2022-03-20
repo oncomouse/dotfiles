02e20500408a6a15007412b4fc1eb84a3336957d
oncomouse <oncomouse@gmail.com>
2022-03-20 09:08:43 -0500

    Lingering sxhkd cleanup

--- stow/dwm/dot-local/share/dwm/autostart.sh
+++ stow/dwm/dot-local/share/dwm/autostart.sh
@@ -5,7 +5,6 @@ picom --experimental-backends -b --config ~/dotfiles/conf/picom/dwm.conf
 
 source ~/dotfiles/conf/xorg/xinit.d/no-xfce4
 source ~/dotfiles/conf/xorg/xinit.d/dunst
-source ~/dotfiles/conf/xorg/xinit.d/sxhkd
 
 convert ~/.cache/wal/background.svg ~/.cache/wal/background.png
 hsetroot -tile ~/.cache/wal/background.png
