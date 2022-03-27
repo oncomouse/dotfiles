339c0002bd29685004bdcf9d356793c5de35007b
oncomouse <oncomouse@gmail.com>
2022-03-27 10:10:46 -0500

    Wayland (ugh) support

--- conf/vim/lua/dotfiles/utils/use_termguicolors.lua
+++ conf/vim/lua/dotfiles/utils/use_termguicolors.lua
@@ -1,5 +1,5 @@
 local function use_termguicolors()
-	return vim.fn.has("mac") == 1 or vim.fn.exists("$DISPLAY") == 1
+	return vim.fn.has("mac") == 1 or vim.fn.exists("$DISPLAY") == 1 or vim.fn.exists("$WAYLAND_DISPLAY") == 1
 end
 
 return use_termguicolors
