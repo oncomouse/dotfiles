9e8524f46e770fab411d549d8c5779fed09858d1
oncomouse <oncomouse@gmail.com>
2022-04-10 14:18:06 -0500

    Default for togglefloating

--- conf/dwm/config.h
+++ conf/dwm/config.h
@@ -215,7 +215,7 @@ static Key keys[] = {
 	{ MODKEY|ControlMask|ShiftMask, XK_t,                       setlayout,           {.v = &layouts[3]} },
 	{ MODKEY|ControlMask,           XK_f,                       setlayout,           {.v = &layouts[floatidx]} },
 	{ MODKEY|ControlMask,           XK_m,                       setlayout,           {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,                   togglefloating,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,                   togglefloating,      {0} },
 	{ MODKEY,                       XK_0,                       view,                {.ui = ~0} },
 	{ MODKEY|ShiftMask,             XK_0,                       tag,                 {.ui = ~0} },
 	{ MODKEY,                       XK_comma,                   focusmon,            {.i = -1} },
