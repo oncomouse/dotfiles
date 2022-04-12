f807171417d316ff7903521733b2b3ba81f5499b
oncomouse <oncomouse@gmail.com>
2022-04-11 19:01:48 -0500

    Cleaned up ST config

--- conf/st/targets/laptop/target.h
+++ conf/st/targets/laptop/target.h
@@ -1,8 +1,4 @@
 static char *font = "JetBrainsMono Nerd Font:size=13";
 static char *font2[] = {
-	"JoyPixels:size=12",
-	"FiraCode Nerd Font:size=13"
+	"JoyPixels:size=12"
 };
-
-static const char dmenufont[]      = "JetBrainsMono Nerd Font:size=13";
-static const char rofifont[]       = "JetBrainsMono Nerd Font Regular 13";
