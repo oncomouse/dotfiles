87205771fd47389455f96a850aaa195bbdbde8e3
oncomouse <oncomouse@gmail.com>
2022-04-17 12:29:06 -0500

    Simplified config for Tabbed

--- conf/tabbed/config.h
+++ conf/tabbed/config.h
@@ -47,34 +47,36 @@ ResourcePref resources[] = {
 		{ "color6",       STRING,  &urgfgcolor },
 };
 
-#define MODKEY ControlMask
+#define MODKEY ControlMask|ShiftMask
 static Key keys[] = {
 	/* modifier                        key        function     argument */
-	{ MODKEY|ShiftMask,                XK_Return, focusonce,   { 0 } },
-	{ MODKEY|ShiftMask,                XK_t,      spawn,       { 0 } },
+	{ MODKEY,          XK_Return, focusonce,   { 0 } },
+	{ MODKEY,          XK_t,      spawn,       { 0 } },
 
-	{ MODKEY,                          XK_Next,   rotate,      { .i = +1 } },
-	{ MODKEY,                          XK_Prior,  rotate,      { .i = -1 } },
-	{ MODKEY|ShiftMask,                XK_Next,   movetab,     { .i = -1 } },
-	{ MODKEY|ShiftMask,                XK_Prior,  movetab,     { .i = +1 } },
-	{ MODKEY|ShiftMask,                XK_Tab,    rotate,      { .i = 0 } },
+	{ ControlMask,     XK_Prior,  rotate,      { .i = -1 } },
+	{ ControlMask,     XK_Next,   rotate,      { .i = +1 } },
+	{ MODKEY,          XK_h,      rotate,      { .i = -1 } },
+	{ MODKEY,          XK_l,      rotate,      { .i = +1 } },
+	{ MODKEY,          XK_Next,   movetab,     { .i = -1 } },
+	{ MODKEY,          XK_Prior,  movetab,     { .i = +1 } },
+	{ MODKEY,          XK_Tab,    rotate,      { .i = 0 } },
 
-	{ MODKEY|ShiftMask,                XK_grave,  spawn,       SETPROP("_TABBED_SELECT_TAB") },
-	{ MODKEY|ShiftMask,                XK_1,      move,        { .i = 0 } },
-	{ MODKEY|ShiftMask,                XK_2,      move,        { .i = 1 } },
-	{ MODKEY|ShiftMask,                XK_3,      move,        { .i = 2 } },
-	{ MODKEY|ShiftMask,                XK_4,      move,        { .i = 3 } },
-	{ MODKEY|ShiftMask,                XK_5,      move,        { .i = 4 } },
-	{ MODKEY|ShiftMask,                XK_6,      move,        { .i = 5 } },
-	{ MODKEY|ShiftMask,                XK_7,      move,        { .i = 6 } },
-	{ MODKEY|ShiftMask,                XK_8,      move,        { .i = 7 } },
-	{ MODKEY|ShiftMask,                XK_9,      move,        { .i = 8 } },
-	{ MODKEY|ShiftMask,                XK_0,      move,        { .i = 9 } },
+	{ MODKEY,          XK_grave,  spawn,       SETPROP("_TABBED_SELECT_TAB") },
+	{ MODKEY,          XK_1,      move,        { .i = 0 } },
+	{ MODKEY,          XK_2,      move,        { .i = 1 } },
+	{ MODKEY,          XK_3,      move,        { .i = 2 } },
+	{ MODKEY,          XK_4,      move,        { .i = 3 } },
+	{ MODKEY,          XK_5,      move,        { .i = 4 } },
+	{ MODKEY,          XK_6,      move,        { .i = 5 } },
+	{ MODKEY,          XK_7,      move,        { .i = 6 } },
+	{ MODKEY,          XK_8,      move,        { .i = 7 } },
+	{ MODKEY,          XK_9,      move,        { .i = 8 } },
+	{ MODKEY,          XK_0,      move,        { .i = 9 } },
 
-	{ MODKEY|ShiftMask,                XK_w,      killclient,  { 0 } },
+	{ MODKEY,          XK_w,      killclient,  { 0 } },
 
-	{ MODKEY|ShiftMask,                XK_u,      focusurgent, { 0 } },
-	{ MODKEY|ShiftMask|ControlMask,    XK_u,      toggle,      { .v = (void*) &urgentswitch } },
+	{ MODKEY,          XK_u,      focusurgent, { 0 } },
+	{ MODKEY|Mod1Mask, XK_u,      toggle,      { .v = (void*) &urgentswitch } },
 
-	{ 0,                               XK_F11,    fullscreen,  { 0 } },
+	{ 0,                         XK_F11,    fullscreen,  { 0 } },
 };
