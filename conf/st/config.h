51f8b8439c8cbb2dcc9b923f8a567dff02c9d5b6
oncomouse <oncomouse@gmail.com>
2022-04-20 12:48:26 -0500

    Now newterm works

--- conf/st/config.h
+++ conf/st/config.h
@@ -249,7 +249,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ TERMMOD,              XK_t,           newterm,        {.i =  0} },
+	{ TERMMOD,              XK_T,           newterm,        {.i =  0} },
 };
 
 /*
