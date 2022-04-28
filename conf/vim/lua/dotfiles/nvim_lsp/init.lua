dd4c09a2d258a069f5a0f914fb27e50390ee5c09
oncomouse <oncomouse@gmail.com>
2022-04-28 18:33:48 -0500

    Updated to latest nvim-lsp-installer

--- conf/vim/lua/dotfiles/nvim_lsp/init.lua
+++ conf/vim/lua/dotfiles/nvim_lsp/init.lua
@@ -5,7 +5,6 @@ vim.diagnostic.config({
 	severity_sort = true,
 })
 local servers = require("dotfiles.nvim_lsp.servers")
-local lsp_installer_servers = require("nvim-lsp-installer.servers")
 
 -- LSP Logging:
 -- vim.lsp.set_log_level("trace")
@@ -19,6 +18,9 @@ local on_attach = require("dotfiles.nvim_lsp.on_attach")
 local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 require("dotfiles.null-ls")
+require("nvim-lsp-installer").setup({
+	ensure_installed = vim.tbl_keys(servers),
+})
 
 for lsp, settings in pairs(servers) do
 	local opts = {
@@ -31,15 +33,7 @@ for lsp, settings in pairs(servers) do
 	if not vim.tbl_contains(servers[lsp].provides or {}, "diagnostics") then
 		opts.handlers = handler_no_diagnostics
 	end
-	local ok, lsp_server = lsp_installer_servers.get_server(lsp)
-	if ok then
-		lsp_server:on_ready(function()
-			lsp_server:setup(opts)
-		end)
-		if not lsp_server:is_installed() then
-			lsp_server:install()
-		end
-	elseif vim.tbl_contains(require("lspconfig").available_servers(), lsp) then
+	if lsp ~= "null-ls" then
 		require("lspconfig")[lsp].setup(opts)
 	end
 end
