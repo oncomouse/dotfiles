83233f146a9b56895521c2d2b749b1346eb4edc0
oncomouse <oncomouse@gmail.com>
2022-05-10 18:17:44 -0500

    Disable powerbutton for DWM (for Aug)

--- stow/dwm/dot-local/share/dwm/autostart.sh
+++ stow/dwm/dot-local/share/dwm/autostart.sh
@@ -5,6 +5,7 @@ picom --experimental-backends -b --config ~/dotfiles/conf/picom/dwm.conf
 
 source ~/dotfiles/conf/xorg/xinit.d/no-xfce4
 source ~/dotfiles/conf/xorg/xinit.d/dunst
+source ~/dotfiles/conf/xorg/xinit.d/no-powerbutton
 
 convert ~/.cache/wal/background.svg ~/.cache/wal/background.png
 hsetroot -tile ~/.cache/wal/background.png
