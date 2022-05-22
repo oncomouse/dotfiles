88a2e25879c85b02f283f3e9156a7b56bbeb0621
oncomouse <oncomouse@gmail.com>
2022-05-22 16:25:59 -0500

    Another attempt at a fancy prompt with Rofi

---
+++ stow/ratpoison/dot-local/bin/rp-prompt.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
+export XDG_CACHE_HOME
+mkdir -p "$XDG_CACHE_HOME/ratpoison"
+
+file="$XDG_CACHE_HOME/ratpoison/command-history"
+
+if [ "$1" = "startup" ]; then
+	rm -f "$file"
+	exit
+fi
+
+if [ ! -f "$file" ]; then
+	touch "$file"
+fi
+
+command="$(tac "$file" | rofi -dmenu -match fuzzy -p ":")"
+echo "$command" >> "$file"
+ratpoison -c "$command"
