91ebbe7b0eb17b56118cfdcdba8cc87406b71a65
oncomouse <oncomouse@gmail.com>
2022-05-22 11:57:02 -0500

    Reading the ratpoison manual helps. Super key bindings

---
+++ conf/ratpoison/colors.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+source ~/.cache/wal/colors.sh
+ratpoison -c "set bgcolor $color0"
+ratpoison -c "set fgcolor $color15"
--- conf/ratpoison/workspaces.sh
+++ conf/ratpoison/workspaces.sh
@@ -2,6 +2,6 @@
 workspaces="$1"
 rpws init "$workspaces" -a
 for i in $(seq 1 "$workspaces"); do
-	ratpoison -c "definekey top C-M-$i exec rpws $i"
-	ratpoison -c "definekey top C-M-S-$i exec rpwsm$i"
+	ratpoison -c "definekey top s-$i exec rpws $i"
+	ratpoison -c "definekey top s-S-$i exec rpwsm$i"
 done
