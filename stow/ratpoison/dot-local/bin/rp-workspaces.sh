112fb8668229482d0504ece6e4c99afbe5b48f7e
oncomouse <oncomouse@gmail.com>
2022-05-22 14:22:49 -0500

    Workspace switching is weird

--- stow/ratpoison/dot-local/bin/rp-workspaces.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-workspaces="$1"
-rpws init "$workspaces" -a
-for i in $(seq 1 "$workspaces"); do
-	ratpoison -c "definekey top s-$i exec rpws $i"
-	ratpoison -c "definekey top s-S-$i exec rpwsm$i"
-done
