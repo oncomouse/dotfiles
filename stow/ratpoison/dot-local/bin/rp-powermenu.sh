8048e396845d74db96e57bc06e25cbf67503b9b5
oncomouse <oncomouse@gmail.com>
2022-05-24 14:55:24 -0500

    Better fonts for Rofi

--- stow/ratpoison/dot-local/bin/rp-powermenu.sh
+++ stow/ratpoison/dot-local/bin/rp-powermenu.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 DOTFILES_TARGET="$( [ -e ~/.local/share/dotfiles/target ] && /bin/cat ~/.local/share/dotfiles/target)"
-choice="$(printf "Lock\nSuspend\nLogoff\nRestart\nShutdown" | rofi -dmenu -match fuzzy -auto-select -i -p Powermenu -location 1 -theme-str "window { width: 100%; }" -no-fixed-num-lines)"
+font=${1:-"FiraCode Nerd Font 12"}
+choice="$(printf "Lock\nSuspend\nLogoff\nRestart\nShutdown" | rofi -dmenu -match fuzzy -auto-select -i -p Powermenu -location 1 -theme-str "window { width: 100%; }" -no-fixed-num-lines -font "$font")"
 case $choice in
 	Lock)
 		xscreensaver-command -lock
