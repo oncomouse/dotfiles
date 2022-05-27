cdb982092e3baa31b88f6145a3bfd1101523a0f1
oncomouse <oncomouse@gmail.com>
2022-05-27 08:25:49 -0500

    Switch out of zoom if switching window

--- stow/ratpoison/dot-local/bin/rp-zoom.sh
+++ stow/ratpoison/dot-local/bin/rp-zoom.sh
@@ -1,15 +1,30 @@
 #!/usr/bin/env bash
 
+unzoom() {
+	ratpoison -c "frestore $(cat "$file")"
+	rm "$file"
+}
+
+zoom() {
+	ratpoison -c fdump > "$file"
+	ratpoison -c only
+}
+
 XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
 export XDG_CACHE_HOME
 mkdir -p "$XDG_CACHE_HOME/ratpoison"
 
 file="$XDG_CACHE_HOME/ratpoison/zoom-$(rpws current)"
 
+if [ "$1" = "check" ]; then
+	if [ -e "$file" ]; then
+		unzoom
+	fi
+	exit
+fi
+
 if [ -e "$file" ]; then
-	ratpoison -c "frestore $(cat "$file")"
-	rm "$file"
+	unzoom
 else
-	ratpoison -c fdump > "$file"
-	ratpoison -c only
+	zoom
 fi
