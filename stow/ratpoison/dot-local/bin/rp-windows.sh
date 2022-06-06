e28bb300e9785afb0f048be40d28c8ed5aa65462
oncomouse <oncomouse@gmail.com>
2022-06-06 11:44:00 -0500

    Working smart window switcher

--- stow/ratpoison/dot-local/bin/rp-windows.sh
+++ stow/ratpoison/dot-local/bin/rp-windows.sh
@@ -5,6 +5,6 @@ if [ "$(ratpoison -c windows | grep -c ^.)" = 1 ]; then
 fi
 font=${1:-"FiraCode Nerd Font 12"}
 choice="$(ratpoison -c "windows (%n)%a:%t" | rofi -dmenu -match fuzzy -auto-select -i -p Window -location 1 -theme-str "window { width: 100%; }" -no-fixed-num-lines -font "$font" | sed -e "s/(\([0-9]\+\)).*\$/\1/")"
-if [ "$choice" != "" ]; then
+if [ -n "$choice" ]; then
 	ratpoison -c "select $choice" &
 fi
