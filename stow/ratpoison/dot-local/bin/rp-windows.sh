181e6d51889ee3d7f966b16c2083916bdc2366fb
oncomouse <oncomouse@gmail.com>
2022-06-05 20:03:56 -0500

    Bail out if no choice is made

--- stow/ratpoison/dot-local/bin/rp-windows.sh
+++ stow/ratpoison/dot-local/bin/rp-windows.sh
@@ -4,4 +4,7 @@ if [ "$(ratpoison -c windows | grep -c ^.)" = 1 ]; then
 	exit
 fi
 font=${1:-"FiraCode Nerd Font 12"}
-ratpoison -c "select $(ratpoison -c "windows (%n)%a:%t" | rofi -dmenu -match fuzzy -auto-select -i -p Window -location 1 -theme-str "window { width: 100%; }" -no-fixed-num-lines -font "$font" | sed -e "s/(\([0-9]\+\)).*\$/\1/")" &
+choice="$(ratpoison -c "windows (%n)%a:%t" | rofi -dmenu -match fuzzy -auto-select -i -p Window -location 1 -theme-str "window { width: 100%; }" -no-fixed-num-lines -font "$font" | sed -e "s/(\([0-9]\+\)).*\$/\1/")"
+if [ "$choice" != "" ]; then
+	ratpoison -c "select $choice" &
+fi
