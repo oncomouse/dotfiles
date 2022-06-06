19f4832882f0408045e57100880896ab16a1b16d
oncomouse <oncomouse@gmail.com>
2022-06-06 11:58:13 -0500

    Native-mpc add

--- stow/mpd/dot-local/bin/mpd_rofi.sh
+++ stow/mpd/dot-local/bin/mpd_rofi.sh
@@ -16,23 +16,11 @@ action-search() {
 }
 
 action-add() {
-	# albums="$(mpc ls | while IFS= read -r artist; do
-	# 	mpc ls "$artist" 2> /dev/null | while IFS= read -r album; do
-	# 		printf "%s\n" "$album"
-	# 	done
-	# done)"
-	# match="$(printf "%s" "$albums" | rofi_bar)"
-	# if [[ "$match" != "" ]]; then
-	# 	 mpc ls "$match" 2> /dev/null | while IFS= read -r song; do
-	# 	 mpc add "$song"
-	#  done
-	# fi
-
-	match="$(fd --type d --exact-depth 2 . /mnt/Media/Music/ | cut -d / -f 5-6 | rofi_bar)"
-	if [[ "$match" != "" ]]; then
-		fd -e mp3 -e flac . /mnt/Media/Music/"$match" --print0 | while read -rd $'\0' file; do
-			mpc add "$(echo "$file" | sed -e "s/\/mnt\/Media\/Music\///")"
-		done
+	choice="$(mpc ls | while IFS= read -r artist; do mpc ls "$artist" 2> /dev/null | while IFS= read -r album; do printf "%s\n" "$album"; done; done | rofi_bar)"
+	if [[ -n "$choice" ]]; then
+			 mpc ls "$choice" 2> /dev/null | while IFS= read -r song; do
+			 mpc add "$song"
+		 done
 	fi
 }
 
