1b787b99222f661b9990da75a17af417bd280786
oncomouse <oncomouse@gmail.com>
2022-06-05 19:34:07 -0500

    Fancy rp-windows

--- stow/ratpoison/dot-local/bin/rp-windows.sh
+++ stow/ratpoison/dot-local/bin/rp-windows.sh
@@ -3,4 +3,5 @@ if [ "$(ratpoison -c windows | grep -c ^.)" = 1 ]; then
 	ratpoison -c "echo No other window"
 	exit
 fi
-ratpoison -c "select $(ratpoison -c "windows (%n)%a:%t" | rofi -dmenu -auto-select | sed -e "s/(\([0-9]\+\)).*\$/\1/")" &
+font=${1:-"FiraCode Nerd Font 12"}
+ratpoison -c "select $(ratpoison -c "windows (%n)%a:%t" | rofi -dmenu -match fuzzy -auto-select -i -p Window -location 1 -theme-str "window { width: 100%; }" -no-fixed-num-lines -font "$font" | sed -e "s/(\([0-9]\+\)).*\$/\1/")" &
