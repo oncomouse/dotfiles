b470c3f97602de84158466fa330e5d237d7d0258
oncomouse <oncomouse@gmail.com>
2022-06-07 12:45:07 -0500

    Works with ultimate output location

--- stow/scripts/dot-local/bin/fzf-mp3.sh
+++ stow/scripts/dot-local/bin/fzf-mp3.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
 music_dir=/mnt/Media/Music
+out_dir=/mnt/Media/Devices
 if [ ! -d "$music_dir" ]; then
 	music_dir="$HOME/Music"
+	out_dir="$HOME/My Music/out"
 fi
 
+# shellcheck disable=SC1083
 dirs="$(fd -t f -e flac . "$music_dir" -x echo {//}  | sort | uniq)"
 
 IFS=$'\n'
 for dir in $(printf "%s" "$dirs" | fzf --reverse -m |  sed -e "s/\n/ /g"); do
-	output_dir="$HOME/My Music/out/$(echo "$dir" | awk -F/ '{printf("%s/%s", $(NF-1), $(NF))}')"
+	output_dir="$out_dir/$(echo "$dir" | awk -F/ '{printf("%s/%s", $(NF-1), $(NF))}')"
 	mkdir -p "$output_dir"
 	fd -t f -e flac . "$dir" -x ffmpeg -i "{}" -qscale:a 0 "$output_dir/{/.}.mp3"
 done
