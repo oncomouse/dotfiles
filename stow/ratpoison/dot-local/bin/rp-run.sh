1004d37fdd4df19fb0656fdfb40f5524a24133fb
oncomouse <oncomouse@gmail.com>
2022-06-10 14:01:59 -0500

    Keep track of the font internally for ratpoison specific launchers

--- stow/ratpoison/dot-local/bin/rp-run.sh
+++ stow/ratpoison/dot-local/bin/rp-run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-font=${1:-"FiraCode Nerd Font 12"}
+# font=${1:-"FiraCode Nerd Font 12"}
 # shellcheck disable=1083
 # rofi \
 # 	-match fuzzy \
@@ -11,4 +11,5 @@ font=${1:-"FiraCode Nerd Font 12"}
 # 	-font "$font"
 # font="$(echo "$font" | sed -e "s/ \([0-9]\+\)\$/:size=\1/")"
 # dmenu -i -n -l 6 -p drun -fn 'JetBrains Mono Nerd Font:size=11'
+font="$(ratpoison -c 'set font' | sed -e 's/:size=/ /')"
 j4-dmenu-desktop --dmenu="rofi -match fuzzy -auto-select -dmenu -i -location 1 -theme-str 'window { width: 100%; }' -font \"$font\"" --term="st" &
