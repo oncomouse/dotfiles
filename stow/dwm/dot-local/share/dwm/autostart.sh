a4fc002e2d6cf8db1d966fe809a3466dc9ee9bcf
oncomouse <oncomouse@gmail.com>
2022-06-10 19:42:27 -0500

    Normalized some autostart stuff into xinit.d

--- stow/dwm/dot-local/share/dwm/autostart.sh
+++ stow/dwm/dot-local/share/dwm/autostart.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+#shellcheck disable=1090
 source ~/dotfiles/conf/xorg/xinit-common
 dex -a
 picom --experimental-backends -b --config ~/dotfiles/conf/picom/dwm.conf
@@ -6,15 +7,8 @@ picom --experimental-backends -b --config ~/dotfiles/conf/picom/dwm.conf
 source ~/dotfiles/conf/xorg/xinit.d/no-xfce4
 source ~/dotfiles/conf/xorg/xinit.d/dunst
 source ~/dotfiles/conf/xorg/xinit.d/no-powerbutton
-
-convert ~/.cache/wal/background.svg ~/.cache/wal/background.png
-hsetroot -tile ~/.cache/wal/background.png
-
-pkill -f dotfiles-media
-dotfiles-media daemon&
-
-pkill dwmblocks
-dwmblocks&
+source ~/dotfiles/conf/xorg/xinit.d/dotfiles-media
+source ~/dotfiles/conf/xorg/xinit.d/tile-background
 
 DOTFILES_TARGET="$( [ -e ~/.local/share/dotfiles/target ] && /bin/cat ~/.local/share/dotfiles/target)"
 [ "$DOTFILES_TARGET" = "laptop" ] && cbatticon&
