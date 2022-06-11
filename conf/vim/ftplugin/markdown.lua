481d7c348a6ccc208497bc035a33d1e6b8cbbce7
oncomouse <oncomouse@gmail.com>
2022-06-11 17:31:46 -0500

    Switched to lua

---
+++ conf/vim/ftplugin/markdown.lua
@@ -0,0 +1,30 @@
+vim.opt_local.wrap = true
+vim.opt_local.linebreak = true
+vim.opt_local.list = false
+vim.opt_local.spell = true
+-- Turn conceal on and off in a buffer:
+vim.keymap.set("n", "<leader>cc", function()
+	vim.opt_local.conceallevel = vim.opt_local.conceallevel == 0 and 2 or 0
+end, {
+	buffer = true,
+	silent = true,
+})
+
+vim.cmd([[compiler markdown_combo]])
+
+vim.opt_local.iskeyword = vim.opt_local.iskeyword + "',-,@-@"
+vim.opt_local.omnifunc = "bibliography_omnifunc#omnifunc"
+
+-- Pandoc <format> to compile documents quickly and easily:
+vim.api.nvim_create_user_command("Pandoc", function(args)
+	vim.cmd(
+		"!pandoc -i "
+			.. vim.fn.fnameescape(vim.fn.expand("%"))
+			.. " -o "
+			.. vim.fn.fnameescape(vim.fn.expand("%:r"))
+			.. "."
+			.. args.args
+	)
+end, {
+	nargs = 1,
+})
