6afb242c5a3f920f140a89024fedd205c4431b3e
oncomouse <oncomouse@gmail.com>
2022-06-11 17:47:13 -0500

    Volume does change on its own some times

--- conf/lemonaid/config.h
+++ conf/lemonaid/config.h
@@ -18,8 +18,8 @@ static const Block blocks[] = {
 
 	{ "rpbar-desktop.sh",       120,                    1 },
 	{ "rpbar-curwin.sh",        30,                     2 },
-	{ "rpbar-volume.sh",        0,                      3 },
-	{ "rpbar-brightness.sh",    120,                      4 },
+	{ "rpbar-volume.sh",        120,                    3 },
+	{ "rpbar-brightness.sh",    120,                    4 },
 	{ "rpbar-battery.sh",       30,                     5 },
 	{ "rpbar-clock.sh",         60,                     6 },
 	/* 
