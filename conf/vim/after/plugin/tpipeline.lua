264570f6870c2c2a8a31bd9e368bb307869cb105
oncomouse <oncomouse@gmail.com>
2022-06-17 16:15:01 -0500

    Better support for tpipeline

--- conf/vim/after/plugin/tpipeline.lua
+++ conf/vim/after/plugin/tpipeline.lua
@@ -1,8 +1,11 @@
-local function recolor_statusline()
-	vim.api.nvim_set_hl(0, "StatusLine", { bg = vim.api.nvim_get_hl_by_name("AnsiColor0", true).foreground })
+local ok = pcall(require, "tpipeline.main")
+if ok then
+	local function recolor_statusline()
+		vim.api.nvim_set_hl(0, "StatusLine", {})
+	end
+	vim.api.nvim_create_autocmd(
+		"ColorScheme",
+		{ group = "dotfiles-settings", pattern = "*", callback = recolor_statusline }
+	)
+	recolor_statusline()
 end
-vim.api.nvim_create_autocmd(
-	"ColorScheme",
-	{ group = "dotfiles-settings", pattern = "*", callback = recolor_statusline }
-)
-recolor_statusline()
