2aec8625fa55c5c35c6106c587c5e64a7f94465e
oncomouse <oncomouse@gmail.com>
2022-06-17 17:49:12 -0500

    Delete files if deleted on server

--- stow/scripts/dot-local/bin/sync_music.sh
+++ stow/scripts/dot-local/bin/sync_music.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-rsync -akv -e ssh --progress "andrew@192.168.1.48:/mnt/Media/Devices/*" "$HOME/Music"
+rsync -akv -e ssh --progress "andrew@192.168.1.48:/mnt/Media/Devices/*" "$HOME/Music" --delete
