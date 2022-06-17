a9211a1655c0f31a084c3f8ffa9c25d80ec431df
oncomouse <oncomouse@gmail.com>
2022-06-17 16:15:32 -0500

    Formatting

--- conf/vim/after/plugin/tpipeline.lua
+++ conf/vim/after/plugin/tpipeline.lua
@@ -3,9 +3,10 @@ if ok then
 	local function recolor_statusline()
 		vim.api.nvim_set_hl(0, "StatusLine", {})
 	end
-	vim.api.nvim_create_autocmd(
-		"ColorScheme",
-		{ group = "dotfiles-settings", pattern = "*", callback = recolor_statusline }
-	)
+	vim.api.nvim_create_autocmd("ColorScheme", {
+		group = "dotfiles-settings",
+		pattern = "*",
+		callback = recolor_statusline,
+	})
 	recolor_statusline()
 end
