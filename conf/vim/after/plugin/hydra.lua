89252f330cd051df040f1bb6d71187f81ba182af
oncomouse <oncomouse@gmail.com>
2022-06-21 11:29:13 -0500

    Formatting + guards

--- conf/vim/after/plugin/hydra.lua
+++ conf/vim/after/plugin/hydra.lua
@@ -1,37 +1,41 @@
-local Hydra = require("hydra")
+local ok, Hydra = pcall(require,"hydra")
 
-Hydra({
-	name = "Side scroll",
-	mode = "n",
-	body = "z",
-	heads = {
-		{ "h", "5zh" },
-		{ "l", "5zl", { desc = "←/→" } },
-		{ "H", "zH" },
-		{ "L", "zL", { desc = "half screen ←/→" } },
-	},
-})
-Hydra({
-	mode = "n",
-	body = "<C-w>",
-	heads = {
-		-- Move focus
-		{ "h", "<C-w>h" },
-		{ "j", "<C-w>j" },
-		{ "k", "<C-w>k" },
-		{ "l", "<C-w>l", { desc = "focus ←/↓/↑/→" } },
-		-- Split
-		{ "s", "<C-w>s" },
-		{ "S", "<C-w>v", { desc = "split —/|" } },
-		{ "q", "<Cmd>try | close | catch | endtry<CR>", { desc = "close" } },
-		-- Size
-		{ "+", "<C-w>+" },
-		{ "-", "<C-w>-", { desc = "height +/-" } },
-		{ ">", "2<C-w>>" },
-		{ "<", "2<C-w><", { desc = "width +/-" } },
-		{ "=", "<C-w>=", { desc = "equalize" } },
-		--
-		{ "b", "<Cmd>Buffers<CR>", { exit = true, desc = "choose buffer" } },
-		{ "<Esc>", nil, { exit = true } },
-	},
-})
+if ok then
+
+	Hydra({
+		name = "Side scroll",
+		mode = "n",
+		body = "z",
+		heads = {
+			{ "h", "5zh" },
+			{ "l", "5zl", { desc = "←/→" } },
+			{ "H", "zH" },
+			{ "L", "zL", { desc = "half screen ←/→" } },
+		},
+	})
+
+	Hydra({
+		mode = "n",
+		body = "<C-w>",
+		heads = {
+			-- Move focus
+			{ "h", "<C-w>h" },
+			{ "j", "<C-w>j" },
+			{ "k", "<C-w>k" },
+			{ "l", "<C-w>l", { desc = "focus ←/↓/↑/→" } },
+			-- Split
+			{ "s", "<C-w>s" },
+			{ "S", "<C-w>v", { desc = "split —/|" } },
+			{ "q", "<Cmd>try | close | catch | endtry<CR>", { desc = "close" } },
+			-- Size
+			{ "+", "<C-w>+" },
+			{ "-", "<C-w>-", { desc = "height +/-" } },
+			{ ">", "2<C-w>>" },
+			{ "<", "2<C-w><", { desc = "width +/-" } },
+			{ "=", "<C-w>=", { desc = "equalize" } },
+			--
+			{ "b", "<Cmd>Buffers<CR>", { exit = true, desc = "choose buffer" } },
+			{ "<Esc>", nil, { exit = true } },
+		},
+	})
+end
