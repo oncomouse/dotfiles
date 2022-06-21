443aa2ef8f699e4556d2445656187d7c5d025b85
oncomouse <oncomouse@gmail.com>
2022-06-21 16:21:45 -0500

    Include default bindings so I remember them

--- conf/vim/after/plugin/fzf-lua.lua
+++ conf/vim/after/plugin/fzf-lua.lua
@@ -42,6 +42,40 @@ if ok then
 			["header"] = { "fg", "Comment" },
 			["gutter"] = { "bg", "Normal" },
 		},
+		keymap = {
+			-- These override the default tables completely
+			-- no need to set to `false` to disable a bind
+			-- delete or modify is sufficient
+			builtin = {
+				-- neovim `:tmap` mappings for the fzf win
+				["<F1>"] = "toggle-help",
+				["<F2>"] = "toggle-fullscreen",
+				-- Only valid with the 'builtin' previewer
+				["<F3>"] = "toggle-preview-wrap",
+				["<F4>"] = "toggle-preview",
+				-- Rotate preview clockwise/counter-clockwise
+				["<F5>"] = "toggle-preview-ccw",
+				["<F6>"] = "toggle-preview-cw",
+				["<S-down>"] = "preview-page-down",
+				["<S-up>"] = "preview-page-up",
+				["<S-left>"] = "preview-page-reset",
+			},
+			fzf = {
+				-- fzf '--bind=' options
+				["ctrl-z"] = "abort",
+				["ctrl-u"] = "unix-line-discard",
+				["ctrl-f"] = "half-page-down",
+				["ctrl-b"] = "half-page-up",
+				["ctrl-a"] = "beginning-of-line",
+				["ctrl-e"] = "end-of-line",
+				["alt-a"] = "toggle-all",
+				-- Only valid with fzf previewers (bat/cat/git/etc)
+				["f3"] = "toggle-preview-wrap",
+				["f4"] = "toggle-preview",
+				["shift-down"] = "preview-page-down",
+				["shift-up"] = "preview-page-up",
+			},
+		},
 	})
 
 	-- Register fzf-lua as vim.ui.select
