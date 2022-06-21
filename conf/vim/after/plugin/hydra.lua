f683f1659d211cfdaece5437bab7af9c85bce8d3
oncomouse <oncomouse@gmail.com>
2022-06-21 13:57:51 -0500

    Hydra for switching windows is an anti-pattern

--- conf/vim/after/plugin/hydra.lua
+++ conf/vim/after/plugin/hydra.lua
@@ -15,27 +15,15 @@ if ok then
 	})
 
 	Hydra({
+		name = "Resize windows",
 		mode = "n",
 		body = "<C-w>",
 		heads = {
-			-- Move focus
-			{ "h", "<C-w>h" },
-			{ "j", "<C-w>j" },
-			{ "k", "<C-w>k" },
-			{ "l", "<C-w>l", { desc = "focus ←/↓/↑/→" } },
-			-- Split
-			{ "s", "<C-w>s" },
-			{ "S", "<C-w>v", { desc = "split —/|" } },
-			{ "q", "<Cmd>try | close | catch | endtry<CR>", { desc = "close" } },
-			-- Size
 			{ "+", "<C-w>+" },
 			{ "-", "<C-w>-", { desc = "height +/-" } },
 			{ ">", "2<C-w>>" },
 			{ "<", "2<C-w><", { desc = "width +/-" } },
 			{ "=", "<C-w>=", { desc = "equalize" } },
-			--
-			{ "b", "<Cmd>Buffers<CR>", { exit = true, desc = "choose buffer" } },
-			{ "<Esc>", nil, { exit = true } },
 		},
 	})
 end
