fa285d5bf5cdab8870f3954b5ccc75ddaec93f48
oncomouse <oncomouse@gmail.com>
2022-06-25 15:02:04 -0500

    Better listchars

--- conf/vim/ftplugin/javascriptreact.lua
+++ conf/vim/ftplugin/javascriptreact.lua
@@ -4,6 +4,7 @@ vim.opt_local.tabstop = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.softtabstop = 2
 vim.opt_local.expandtab = true
+vim.opt_local.listchars = vim.opt_local.listchars - "tab:| "
 vim.opt_local.listchars = vim.opt_local.listchars + "multispace:│ "
 
 local function start_flow_for_javascript()
--- conf/vim/ftplugin/python.lua
+++ conf/vim/ftplugin/python.lua
@@ -1,2 +1,4 @@
 vim.cmd([[compiler flake8]])
+vim.opt_local.listchars = vim.opt_local.listchars - "tab:| "
+vim.opt_local.listchars = vim.opt_local.listchars + "multispace:│   "
 vim.opt_local.formatprg = "black --quiet -|reorder-python-imports -"
