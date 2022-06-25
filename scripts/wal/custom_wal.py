7b5cc9e13a4fca2d0906ec92ba373af2f6db11d1
oncomouse <oncomouse@gmail.com>
2022-06-25 14:58:27 -0500

    xdotool to simulate xrdb reloading in DWM

--- scripts/wal/custom_wal.py
+++ scripts/wal/custom_wal.py
@@ -79,6 +79,8 @@ if re.search("(-R|-i|--(theme|backend) [^-])", new_args) is not None:
     if pgrep("ratpoison"):
         system("/bin/bash -c rp-colors.sh")
         system("/bin/bash -c rp-bar.sh")
+    if pgrep("dwm"):
+        system("xdotool key super+F5")
 
     if re.match(r"kitty", environ["TERM"]) is not None:
         message("Reloading kitty")
