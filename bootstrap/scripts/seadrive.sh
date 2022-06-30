ab7315473e97d8d21d953ed334ee72e41ec7e9c9
oncomouse <oncomouse@gmail.com>
2022-06-30 18:43:20 -0500

    Seadrive instructions for Arch

--- bootstrap/scripts/seadrive.sh
+++ bootstrap/scripts/seadrive.sh
@@ -6,6 +6,9 @@ oldpwd=$(pwd)
 mkdir -p "$HOME/Projects"
 cd "$HOME/Projects" || exit
 
+gpg --recv-key 910397D88D29319A --keyserver hkp://keyserver.ubuntu.com:80
+paru --skipreview --noconfirm -S libevent-compat libsearpc
+
 git clone https://github.com/haiwen/seadrive-fuse
 cd seadrive-fuse || exit
 ./autogen.sh
@@ -20,6 +23,7 @@ mkdir -p ~/.local/share/seadrive/data
 mkdir -p ~/.local/share/seadrive/logs
 cp "$HOME/dotfiles/conf/seadrive/seadrive.conf.template" ~/.config/seadrive/seadrive.conf
 
-echo 'Run curl -d "username=USERNAME" -d "password=PASSWORD" -H "X-SEAFILE-OTP: 2FA_KEY" https://SEAFILE_SERVER/api2/auth-token/'
+echo 'Run curl -d "username=<USERNAME>" -d "password=<PASSWORD>" -H "X-SEAFILE-OTP: <CURRENT 2FA_KEY>" https://SEAFILE_SERVER/api2/auth-token/'
 echo "And paste token into ~/.config/seafile/seafile.conf"
-echo "Then run systemctl start --user seadrive"
+echo " (In seafile.conf, server needs protocol)"
+echo "Then run systemctl --user enable --now seadrive"
