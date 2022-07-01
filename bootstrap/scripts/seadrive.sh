9cd15f3393591ab2ac60eccc6118d3694400f18b
oncomouse <oncomouse@gmail.com>
2022-07-01 14:32:07 -0500

    Working seadrive-daemon install for Arch

--- bootstrap/scripts/seadrive.sh
+++ bootstrap/scripts/seadrive.sh
@@ -2,27 +2,25 @@
 
 # Seadrive install for Arch
 
-oldpwd=$(pwd)
-mkdir -p "$HOME/Projects"
-cd "$HOME/Projects" || exit
-
+# Install prerequisites
 gpg --recv-key 910397D88D29319A --keyserver hkp://keyserver.ubuntu.com:80
 paru --skipreview --noconfirm -S libevent-compat libsearpc
 
-git clone https://github.com/haiwen/seadrive-fuse
-cd seadrive-fuse || exit
-./autogen.sh
-./configure
-make
-sudo make install
-systemctl enable --user seadrive
+# Install the fuse client daemon
+oldpwd=$(pwd)
+mkdir -p ~/aur
+https://github.com/oncomouse/seadrive-daemon-git ~/aur/seadrive-daemon-git
+cd ~/aur/seadrive-daemon-git || exit
+makepkg -si
 cd "$oldpwd" || exit
 
+# Make directories
 mkdir -p ~/.config/seadrive
 mkdir -p ~/.local/share/seadrive/data
 mkdir -p ~/.local/share/seadrive/logs
-cp "$HOME/dotfiles/conf/seadrive/seadrive.conf.template" ~/.config/seadrive/seadrive.conf
+cp ~/dotfiles/conf/seadrive/seadrive.conf.template ~/.config/seadrive/seadrive.conf
 
+# Print instructions
 echo 'Run curl -d "username=<USERNAME>" -d "password=<PASSWORD>" -H "X-SEAFILE-OTP: <CURRENT 2FA_KEY>" https://SEAFILE_SERVER/api2/auth-token/'
 echo "And paste token into ~/.config/seafile/seafile.conf"
 echo " (In seafile.conf, server needs protocol)"
