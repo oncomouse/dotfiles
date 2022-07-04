4538dc1db3c2cb28cda544f5c655cf58b5e887d6
oncomouse <oncomouse@gmail.com>
2022-07-04 11:53:54 -0500

    Add mpd support to Lemonbar and auto turn it off

--- conf/lemonaid/targets/laptop/target.h
+++ conf/lemonaid/targets/laptop/target.h
@@ -1,6 +1,6 @@
 static const unsigned short int left = 2;     // no. of left aligned blocks
 static const unsigned short int centre = 0;   // no. of centre aligned blocks
-static const unsigned short int right = 4;    // no. of right aligned blocks
+static const unsigned short int right = 5;    // no. of right aligned blocks
 
 // Blocks must be in the order of their alignment
 static const Block blocks[] = {
@@ -8,6 +8,7 @@ static const Block blocks[] = {
 
 	{ "rpbar-desktop.sh",       120,                    1 },
 	{ "rpbar-curwin.sh",        30,                     2 },
+	{ "rpbar-mpris.sh",         0,                      10 },
 	{ "rpbar-volume.sh",        120,                    3 },
 	{ "rpbar-brightness.sh",    120,                    4 },
 	{ "rpbar-battery.sh",       30,                     5 },
@@ -16,6 +17,5 @@ static const Block blocks[] = {
 	 * dotfiles-media runs pkill -44 for status updates and lemonaid (which doesn't use
 	 * mpris) dies if it receives an undefined signal. So we define a dummy signal.
 	 * */
-	{ "echo ''",                0,                      10 },
 
 };
--- stow/ratpoison/dot-local/bin/rpbar-mpris.sh
+++ stow/ratpoison/dot-local/bin/rpbar-mpris.sh
@@ -4,14 +4,16 @@ status="$(dotfiles-media status)"
 if [ "${#status}" -gt 100 ]; then
 	status="$(echo "$status" | head -c 99)…"
 fi
-echo -n "["
-echo -n "%{A:dotfiles-media play:}"
-echo -n "%{A2:dotfiles-media prev:}"
-echo -n "%{A3:dotfiles-media next:}"
-echo -n "$status"
-echo -n "]"
-echo -n "%{A}"
-echo -n "%{A}"
-echo -n "%{A}"
+if [ "$status" != "栗" ]; then
+	echo -n "["
+	echo -n "%{A:dotfiles-media play:}"
+	echo -n "%{A2:dotfiles-media prev:}"
+	echo -n "%{A3:dotfiles-media next:}"
+	echo -n "$status"
+	echo -n "]"
+	echo -n "%{A}"
+	echo -n "%{A}"
+	echo -n "%{A}"
+fi
 echo ""
 # vim:ft=sh
