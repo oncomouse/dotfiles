65dd1274952dd591fd0cff0c2aff1f074bfe66de
oncomouse <oncomouse@gmail.com>
2022-07-06 11:12:54 -0500

    Zoom works

--- stow/sdorfehs/dot-local/bin/sf-zoom.sh
+++ stow/sdorfehs/dot-local/bin/sf-zoom.sh
@@ -14,7 +14,7 @@ XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
 export XDG_CACHE_HOME
 mkdir -p "$XDG_CACHE_HOME/sdorfehs"
 
-file="$XDG_CACHE_HOME/sdorfehs/zoom-$(rpws current)"
+file="$XDG_CACHE_HOME/sdorfehs/zoom-$(sdorfehs -c vscreens | grep -f "*" | cut -d "*" -f 1)"
 
 if [ "$1" = "check" ]; then
 	if [ -e "$file" ]; then
