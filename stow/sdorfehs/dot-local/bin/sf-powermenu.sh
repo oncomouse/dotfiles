7987cb47c626f11944e839fb6e46b592aa78af1e
oncomouse <oncomouse@gmail.com>
2022-07-06 11:03:49 -0500

    Fix powermenu stuff

--- stow/sdorfehs/dot-local/bin/sf-powermenu.sh
+++ stow/sdorfehs/dot-local/bin/sf-powermenu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 font="$(sdorfehs -c 'set font' | sed -e 's/:size=/ /')"
-choice="$(printf "Lock\nSuspend\nLogoff\nRestart\nShutdown" | rofi -dmenu -match fuzzy -auto-select -i -p Powermenu -location 1 -theme-str "window { width: 100%; }" -no-fixed-num-lines -font "$font")"
+choice="$(printf "Lock\nSuspend\nLogoff\nRestart\nShutdown" | rofi -dmenu -match fuzzy -auto-select -i -p Powermenu -location 1 -theme-str "window { width: 100%; y-offset: 20px; }" -no-fixed-num-lines -font "$font")"
 case $choice in
 	Lock)
 		xscreensaver-command -lock
--- stow/sdorfehs/dot-local/bin/sf-windows.sh
+++ stow/sdorfehs/dot-local/bin/sf-windows.sh
@@ -3,8 +3,8 @@ if [ "$(sdorfehs -c windows | grep -c ^.)" = 1 ]; then
 	sdorfehs -c "echo No other window"
 	exit
 fi
-font=${1:-"FiraCode Nerd Font 12"}
-choice="$(sdorfehs -c "windows (%n)%a:%t" | rofi -dmenu -match fuzzy -auto-select -i -p Window -no-fixed-num-lines -font "$font" | sed -e "s/(\([0-9]\+\)).*\$/\1/")"
+font="$(sdorfehs -c 'set font' | sed -e 's/:size=/ /')"
+choice="$(sdorfehs -c "windows (%n)%a:%t" | rofi -dmenu -match fuzzy -auto-select -i -p Window -location 1 -theme-str "window { width: 100%; y-offset: 20px; }" -no-fixed-num-lines -font "$font" | sed -e "s/(\([0-9]\+\)).*\$/\1/")"
 if [ -n "$choice" ]; then
 	sdorfehs -c "select $choice" &
 fi
