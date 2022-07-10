bf743612ff3986775e334dd7e541396cbdbebd5c
oncomouse <oncomouse@gmail.com>
2022-07-10 10:20:57 -0500

    Done with this

--- conf/luastatus/widgets/brightness.lua
+++
@@ -1,9 +0,0 @@
-local brightness = 100
-widget = luastatus.require_plugin("backlight-linux").widget({
-	cb = function(br)
-		if br ~= nil and br ~= brightness then
-			brightness = br * 100
-		end
-		return string.format("[%s %0.0f%%]", "", brightness)
-	end
-})
--- conf/luastatus/widgets/clock.lua
+++
@@ -1,9 +0,0 @@
-widget = {
-	plugin = 'timer',
-	opts = {
-		period = 15
-	},
-	cb = function()
-		return os.date("[%a %m/%d %I:%M %p]")
-	end,
-}
