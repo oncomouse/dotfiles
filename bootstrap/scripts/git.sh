c5201e3af1e4fec7a553b2beb0e3dc2a7f905bb5
oncomouse <oncomouse@gmail.com>
2022-07-15 11:08:15 -0500

    A few fixes

--- bootstrap/scripts/git.sh
+++ bootstrap/scripts/git.sh
@@ -4,12 +4,13 @@ git config --global user.email "oncomouse@gmail.com"
 # Encrypted netrc credential helper
 if ! command -v git-credential-netrc > /dev/null; then
 	owd="$(pwd)"
-	git clone https://github.com/git/git ~/.cache/git
-	cd ~/.cache/git/contrib/credential/netrc || exit
+	git clone https://github.com/git/git ~/.cache/git-project
+	cd ~/.cache/git-project/contrib/credential/netrc || exit
 	make
 	cp git-credential-netrc ~/.local/bin
 	cd "$owd" || exit
 	rm -rf ~/.cache/git
+	git config --global credential.helper netrc
 fi
 git config --global pull.rebase false
 git config --global init.defaultBranch master
