e647cc5a56a2ef557a7db69b02ad2a8d9bea8b31
oncomouse <oncomouse@gmail.com>
2022-07-15 11:04:58 -0500

    Clean-up

--- bootstrap/scripts/common.sh
+++ bootstrap/scripts/common.sh
@@ -4,7 +4,6 @@ os=$(bash ~/dotfiles/bootstrap/scripts/os.sh)
 
 ~/dotfiles/bootstrap/scripts/stow.sh
 ~/dotfiles/bootstrap/scripts/git.sh
-~/dotfiles/bootstrap/scripts/vim.sh
 ~/dotfiles/bootstrap/scripts/diff-so-fancy.sh
 ~/dotfiles/bootstrap/scripts/luarocks.sh
 ~/dotfiles/bootstrap/scripts/node-modules.sh
@@ -12,10 +11,10 @@ os=$(bash ~/dotfiles/bootstrap/scripts/os.sh)
 ~/dotfiles/bootstrap/scripts/ruby-gems.sh
 ~/dotfiles/bootstrap/scripts/fisher.sh
 ~/dotfiles/bootstrap/scripts/rustup.sh
-~/dotfiles/bootstrap/scripts/theme.sh
 ~/dotfiles/bootstrap/scripts/tmux.sh
 ~/dotfiles/bootstrap/scripts/neovim.sh
 if [ -z "$SERVER" ]; then
+	~/dotfiles/bootstrap/scripts/theme.sh
 	~/dotfiles/bootstrap/scripts/vale.sh
 	~/dotfiles/bootstrap/scripts/firefox.sh
 	~/dotfiles/bootstrap/scripts/asdf.sh
