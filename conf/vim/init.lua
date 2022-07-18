355fbbbffbc660e106c5a6c96eda7f58cec20bcc
oncomouse <oncomouse@gmail.com>
2022-07-18 16:21:24 -0500

    Move custom :Git into it's own file

--- conf/vim/init.lua
+++ conf/vim/init.lua
@@ -375,18 +375,6 @@ end, {
 	nargs = 1,
 })
 
--- Custom :Git command to utilize fzf-lua for status
-vim.api.nvim_create_user_command("Git", function(args)
-	if args.args:match("^status") then
-		vim.cmd("GitStatus")
-	else
-		vim.cmd("Gina " .. args.args)
-	end
-end, {
-	force = true,
-	nargs = "+",
-})
-
 -- }}}
 -- Signs {{{
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
---
+++ conf/vim/plugin/git.lua
@@ -0,0 +1,13 @@
+if vim.g.loaded_gina and vim.g.loaded_fzf_lua then
+	-- Custom :Git command to utilize fzf-lua for status
+	vim.api.nvim_create_user_command("Git", function(args)
+		if args.args:match("^status") then
+			vim.cmd("GitStatus")
+		else
+			vim.cmd("Gina " .. args.args)
+		end
+	end, {
+		force = true,
+		nargs = "+",
+	})
+end
