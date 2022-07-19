798898318008ba3e2b7ac5f580b08c37800a0ba7
oncomouse <oncomouse@gmail.com>
2022-07-19 10:44:47 -0500

    Correct guard now

--- conf/vim/after/plugin/git.lua
+++ conf/vim/after/plugin/git.lua
@@ -1,4 +1,4 @@
-if vim.g.loaded_gina == 1 and vim.g.loaded_fzf_lua == 1 then
+if vim.g.loaded_fzf_lua == 1 then
 	-- Custom :Git command to utilize fzf-lua for status
 	vim.api.nvim_create_user_command("Git", function(args)
 		if args.args:match("^status") then
