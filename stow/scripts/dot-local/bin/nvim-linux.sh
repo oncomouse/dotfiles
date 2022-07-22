b77c265218199c145d2478e9daee9bfc0ac00ea5
oncomouse <oncomouse@gmail.com>
2022-07-22 08:33:31 -0500

    Try to run this correctly

--- stow/scripts/dot-local/bin/nvim-linux.sh
+++ stow/scripts/dot-local/bin/nvim-linux.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
 curl -sLo ~/.local/nvim-linux64.tar.gz https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
-owd="$(pwd)"
-cd ~/.local || exit
-[[ -d nvim-linux64 ]] && rm -rf nvim-linux64
-tar zxvf nvim-linux64.tar.gz
-rm nvim-linux64.tar.gz
-sudo install -d nvim-linux64/ /usr/local
-
-cd "$owd" || exit
+[[ -d ~/.local/nvim-linux64 ]] && rm -rf ~/.local/nvim-linux64
+tar zxvf ~/.local/nvim-linux64.tar.gz --directory ~/.local
+rm ~/.local/nvim-linux64.tar.gz
+sudo install -d ~/.local/nvim-linux64/ /usr/local
