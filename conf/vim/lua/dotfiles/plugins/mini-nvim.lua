5e40739596111957f27339c03fdee6e755a478c6
oncomouse <oncomouse@gmail.com>
2022-08-04 12:21:01 -0500

    Sentence object for mini.ai

--- conf/vim/lua/dotfiles/plugins/mini-nvim.lua
+++ conf/vim/lua/dotfiles/plugins/mini-nvim.lua
@@ -1,4 +1,6 @@
 local function config_mini()
+	-- local augroup = vim.api.nvim_create_augroup("dotfiles-mini.nvim", { clear = true })
+	local spec_pair = require("mini.ai").gen_spec.pair
 	require("mini.ai").setup({
 		custom_textobjects = {
 			e = function()
@@ -9,12 +11,57 @@ local function config_mini()
 				}
 				return { from = from, to = to }
 			end,
+			s = function(type)
+				local punc_matcher = "[.?!]"
+				local pos = vim.fn.getpos(".")
+				local lnum = pos[2]
+				local col = pos[3]
+				local line = vim.api.nvim_get_current_line()
+				local end_of_sentence = string.find(string.sub(line, col, #line), punc_matcher)
+				if not end_of_sentence then
+					return nil
+				end
+				end_of_sentence = end_of_sentence + col - 1
+				local _, start_of_sentence = string.find(string.sub(line, 1, col), punc_matcher .. "%s+[A-Z]")
+				if start_of_sentence == nil and string.match(string.sub(line, 1, 1), "[A-Z]") then
+					start_of_sentence = 1
+				end
+				if not start_of_sentence then
+					return nil
+				end
+				end_of_sentence = type == "i" and end_of_sentence - 1 or end_of_sentence
+				return {
+					from = {
+						line = lnum,
+						col = start_of_sentence,
+					},
+					to = {
+						line = lnum,
+						col = end_of_sentence,
+					},
+				}
+			end,
+			["*"] = spec_pair("*", "*", { type = "greedy" }),
+			["_"] = spec_pair("_", "_", { type = "greedy" }),
 		},
 		mappings = {
 			around_last = "aN",
 			inside_last = "iN",
 		},
 	})
+	-- vim.api.nvim_create_autocmd("FileType", {
+	-- 	pattern = "markdown",
+	-- 	group = augroup,
+	-- 	callback = function()
+	-- 		-- local spec_pair = require("mini.ai").gen_spec.pair
+	-- 		-- vim.b.miniai_config = {
+	-- 		-- 	custom_textobjects = {
+	-- 		-- 		["*"] = spec_pair("*", "*", { type = "greedy" }),
+	-- 		-- 		["_"] = spec_pair("_", "_", { type = "greedy" }),
+	-- 		-- 	},
+	-- 		-- }
+	-- 	end,
+	-- })
 	require("mini.comment").setup({})
 	require("mini.indentscope").setup({
 		options = {
