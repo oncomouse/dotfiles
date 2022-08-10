e62520817408da4635c53683c60e212f4c4cf205
oncomouse <oncomouse@gmail.com>
2022-08-10 15:22:40 -0500

    Simplified extract_sentence() usage

--- conf/vim/lua/dotfiles/plugins/mini-nvim.lua
+++ conf/vim/lua/dotfiles/plugins/mini-nvim.lua
@@ -14,32 +14,36 @@ local function config_mini()
 
 	---@param inside boolean
 	---@return Pair top, Pair bottom
-	local function extract_sentence(inside)
+	local function extract_sentence(type)
+		local inside = type == "i"
 		-- Get the end of the sentence:
 		vim.cmd([[normal! )]])
-		local p_bottom = make_point()
+		local to = make_point()
 
 		-- Get the beginning of the sentence:
 		vim.cmd([[normal! (]])
-		local p_top = make_point()
+		local from = make_point()
 
 		local punc_matcher = "[.?!]"
 		-- Try to find any punctuation at the end of our sentence:
-		local target_line = p_bottom.line
-		while target_line >= p_top.line and target_line > 0 do
+		local target_line = to.line
+		while target_line >= from.line and target_line > 0 do
 			-- From where should we start searching?
-			local start_pos = target_line == p_top.line and p_top.col or 0
+			local start_pos = target_line == from.line and from.col or 0
 			-- Get a match, if one exists:
 			local match_start, match_stop = vim.regex(punc_matcher):match_line(0, target_line - 1, start_pos)
 			if match_start ~= nil then
-				p_bottom.col = start_pos + (inside and match_start or match_stop)
-				p_bottom.line = target_line
+				to.col = start_pos + (inside and match_start or match_stop)
+				to.line = target_line
 				break
 			end
 			target_line = target_line - 1
 		end
 
-		return p_top, p_bottom
+		return {
+			from = from,
+			to = to
+		}
 	end
 
 	require("mini.ai").setup({
@@ -55,14 +59,7 @@ local function config_mini()
 					to = to,
 				}
 			end,
-			s = function(type) -- Sentences (using sentence-wise move commands `(` and `)`)
-				local from, to = extract_sentence(type == "i")
-
-				return {
-					from = from,
-					to = to,
-				}
-			end,
+			s = extract_sentence, -- Sentences (using sentence-wise move commands `(` and `)`)
 			[","] = { -- Grammatically correct comma matching
 				{
 					"[%.?!][ ]*()()[^,%.?!]+(),[ ]*()", -- Start of sentence
