5e1d941b1b7ff75a1681346611305717ce0e2e10
oncomouse <oncomouse@gmail.com>
2022-08-13 14:25:51 -0500

    Simplified fish colors

--- stow/fish/dot-config/fish/config.fish
+++ stow/fish/dot-config/fish/config.fish
@@ -1,74 +1,70 @@
-if status is-interactive
-	# Setup Pywal colors:
-	if test -d $HOME/.cache/wal
-		source ~/.cache/wal/colors.fish
-	end
+if status is-login
 
-	# EDITOR
-	set -gx EDITOR (which nvim)
+    # Colors:
+    set -gx fish_color_normal normal
+    set -gx fish_color_command green
+    set -gx fish_color_param blue
+    set -gx fish_color_redirection $fish_color_param
+    set -gx fish_color_comment white
+    set -gx fish_color_escape brmagenta
+    set -gx fish_color_operator $fish_color_escape
+    set -gx fish_color_end blue
+    set -gx fish_color_quote cyan
+    set -gx fish_color_autosuggestion 555 brblack
+    set -gx fish_color_user brgreen
+    set -gx fish_color_host $fish_color_normal
+    set -gx fish_color_valid_path --underline
+    set -gx fish_color_cwd cyan
+    set -gx fish_color_cwd_root red
+    set -gx fish_color_match --background=brblue
+    set -gx fish_color_search_match bryellow --background=brblack
+    set -gx fish_color_selection white --bold --background=brblack
+    set -gx fish_color_cancel -r
+    set -gx fish_pager_color_prefix white --bold --underline
+    set -gx fish_pager_color_completion
+    set -gx fish_pager_color_description $fish_color_quote yellow
+    set -gx fish_pager_color_progress brwhite --background=cyan
+    set -gx fish_color_history_current --bold
 
-	# Tell sxhkd to not use fish:
-	set -gx SXHKD_SHELL sh
+    # Setup Pywal colors:
+    if test -d $HOME/.cache/wal
+        source ~/.cache/wal/colors.fish
+    end
 
-	# NNN Theme
-	set -gx NNN_FCOLORS "0603040200050E070D09abc4"
-	if test -e $HOME/.ow_credentials.json
-		set -gx OW_KEY (cat ~/.ow_credentials.json | jq -r .key)
-		set -gx OW_LAT (cat ~/.ow_credentials.json | jq .coordinates[0])
-		set -gx OW_LONG (cat ~/.ow_credentials.json | jq .coordinates[1])
-	end
+    # EDITOR
+    set -gx EDITOR (which nvim)
 
-	# DOTFILES_TARGET
-	if test -e $HOME/.local/share/dotfiles/target
-		set -gx DOTFILES_TARGET (/bin/cat $HOME/.local/share/dotfiles/target)
-	end
+    # Tell sxhkd to not use fish:
+    set -gx SXHKD_SHELL sh
 
-	# Setup FZF themes:
-	set -gx FZF_DEFAULT_OPTS "--ansi --bind='ctrl-o:execute(open {})+abort'"
-	set -gx FZF_DEFAULT_COMMAND "fd -t f --follow --hidden"
-	set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
-	set -gx FZF_ALT_C_COMMAND "fd --type d --follow"
-	if test -d "$HOME/.cache/wal"
-		set -gx FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS --color=bg+:$color8,bg:$background,spinner:$color14,hl:$color12"\
-		"--color=fg:$foreground,header:$color12,info:$background,pointer:$color14"\
-		"--color=marker:$color14,fg+:$foreground,prompt:$color3,hl+:$color12"
-	end
-	set -gx FZF_CTRL_T_OPTS "--preview-window 'right:60%' --preview 'bat --theme=wal --color=always --style=header,grid --line-range :300 {}'"
-	set -gx FZF_ALT_C_OPTS "--preview 'ls --color=always {} | head -200'"
+    # NNN Theme
+    set -gx NNN_FCOLORS 0603040200050E070D09abc4
 
-	# XDG Stuff:
-	set -gx XDG_DATA_DIRS ~/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share
+    # Cache Openweather credentials (if they exist)
+    if test -e $HOME/.ow_credentials.json
+        set -gx OW_KEY (cat ~/.ow_credentials.json | jq -r .key)
+        set -gx OW_LAT (cat ~/.ow_credentials.json | jq .coordinates[0])
+        set -gx OW_LONG (cat ~/.ow_credentials.json | jq .coordinates[1])
+    end
 
-	# Configure ASDF:
-	# if not contains $HOME/.asdf/shims $fish_user_paths
-	#		and test (uname) = "Darwin"
-	#		echo "Loading ASDF"
-	#		set -Ux ASDF_DIR (brew --prefix asdf)
-	#		set -l asdf_data_dir (
-	#			if test -n "$ASDF_DATA_DIR"; echo $ASDF_DATA_DIR;
-	#			else; echo $HOME/.asdf; end)
+    # DOTFILES_TARGET
+    if test -e $HOME/.local/share/dotfiles/target
+        set -gx DOTFILES_TARGET (/bin/cat $HOME/.local/share/dotfiles/target)
+    end
 
-	#		# Add asdf to PATH
-	#		set -l asdf_bin_dirs $ASDF_DIR/bin $ASDF_DIR/shims $asdf_data_dir/shims
-	#		for x in $asdf_bin_dirs
-	#			if test -d $x
-	#				and not contains $x $fish_user_paths
-	#				fish_add_path $x
-	#			end
-	#		end
-	# end
+    # Setup FZF themes:
+    set -gx FZF_DEFAULT_OPTS "--ansi --bind='ctrl-o:execute(open {})+abort'"
+    set -gx FZF_DEFAULT_COMMAND "fd -t f --follow --hidden"
+    set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+    set -gx FZF_ALT_C_COMMAND "fd --type d --follow"
+    if test -d "$HOME/.cache/wal"
+        set -gx FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS --color=bg+:$color8,bg:$background,spinner:$color14,hl:$color12" \
+            "--color=fg:$foreground,header:$color12,info:$background,pointer:$color14" \
+            "--color=marker:$color14,fg+:$foreground,prompt:$color3,hl+:$color12"
+    end
+    set -gx FZF_CTRL_T_OPTS "--preview-window 'right:60%' --preview 'bat --theme=wal --color=always --style=header,grid --line-range :300 {}'"
+    set -gx FZF_ALT_C_OPTS "--preview 'ls --color=always {} | head -200'"
 
-	# Load the asdf wrapper function
-	source $ASDF_DIR/lib/asdf.fish
-
-	# Configure Pisces (fish pairing):
-	set -gx pisces_only_insert_at_eol 1
-	#
-	# Setup Kitty:
-	if command -sq kitty
-		kitty + complete setup fish | source
-	end
-
-	# Colors:
-	set fish_color_cwd cyan
+    # XDG Stuff:
+    set -gx XDG_DATA_DIRS ~/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share
 end
--- stow/wal/dot-config/wal/templates/colors.fish
+++ stow/wal/dot-config/wal/templates/colors.fish
@@ -1,52 +1,18 @@
-set foreground "{foreground}"
-set background "{background}"
-set color0 "{color0}"
-set color1 "{color1}"
-set color2 "{color2}"
-set color3 "{color3}"
-set color4 "{color4}"
-set color5 "{color5}"
-set color6 "{color6}"
-set color7 "{color7}"
-set color8 "{color8}"
-set color9 "{color9}"
-set color10 "{color10}"
-set color11 "{color11}"
-set color12 "{color12}"
-set color13 "{color13}"
-set color14 "{color14}"
-set color15 "{color15}"
-# set fish_color_normal normal
-set fish_color_normal $foreground
-# set fish_color_command #005fd7
-set fish_color_command $color2
-# set fish_color_param #00afff
-set fish_color_param $color4
-# set fish_color_redirection #00afff
-set fish_color_redirection $fish_color_param
-# set fish_color_comment #990000
-set fish_color_comment $color7
-set fish_color_error #ff0000
-# set fish_color_escape 00a6b2
-set fish_color_escape $color13
-# set fish_color_operator 00a6b2
-set fish_color_operator $fish_color_escape
-set fish_color_end $color4
-set fish_color_quote $color6
-set fish_color_autosuggestion 555 brblack
-set fish_color_user brgreen
-# set fish_color_host normal
-set fish_color_host $fish_color_normal
-set fish_color_valid_path --underline
-set fish_color_cwd green
-set fish_color_cwd_root red
-set fish_color_match --background=brblue
-set fish_color_search_match bryellow --background=brblack
-set fish_color_selection white --bold --background=brblack
-set fish_color_cancel -r
-set fish_pager_color_prefix white --bold --underline
-set fish_pager_color_completion
-# set fish_pager_color_description B3A06D yellow
-set fish_pager_color_description $fish_color_quote yellow
-set fish_pager_color_progress brwhite --background=cyan
-set fish_color_history_current --bold
+set -gx foreground "{foreground}"
+set -gx background "{background}"
+set -gx color0 "{color0}"
+set -gx color1 "{color1}"
+set -gx color2 "{color2}"
+set -gx color3 "{color3}"
+set -gx color4 "{color4}"
+set -gx color5 "{color5}"
+set -gx color6 "{color6}"
+set -gx color7 "{color7}"
+set -gx color8 "{color8}"
+set -gx color9 "{color9}"
+set -gx color10 "{color10}"
+set -gx color11 "{color11}"
+set -gx color12 "{color12}"
+set -gx color13 "{color13}"
+set -gx color14 "{color14}"
+set -gx color15 "{color15}"
