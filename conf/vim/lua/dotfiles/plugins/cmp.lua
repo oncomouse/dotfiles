b75e9f88bc28db9c8c15ad4ed68179686820e3af
oncomouse <oncomouse@gmail.com>
2022-08-14 12:04:58 -0500

    Unused

--- conf/vim/lua/dotfiles/plugins/cmp.lua
+++ conf/vim/lua/dotfiles/plugins/cmp.lua
@@ -1,9 +1,5 @@
 local function config_cmp()
 	local cmp = require("cmp")
-	local has_words_before = function()
-		local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-		return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-	end
 
 	local feedkey = function(key, mode)
 		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode or "", true)
