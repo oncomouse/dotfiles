4fe2df6b21f37798a80d9d8a5d32bfcd8f86594d
oncomouse <oncomouse@gmail.com>
2022-08-31 10:25:55 -0500

    s for [[]] in Lua for mini.ai

--- conf/vim/after/ftplugin/lua.lua
+++ conf/vim/after/ftplugin/lua.lua
@@ -1,6 +1,14 @@
+local spec_pair = require("mini.ai").gen_spec.pair
+
+ -- s for [[]] in ai and surround
+vim.b.miniai_config = {
+	custom_textobjects = {
+		["s"] = spec_pair("[[", "]"),
+	},
+}
 vim.b.minisurround_config = {
 	custom_surroundings = {
-		s = { -- s for [[]]
+		s = {
 			input = { '%[%[().-()%]%]' },
 			output = { left = "[[", right = "]]" },
 		},
