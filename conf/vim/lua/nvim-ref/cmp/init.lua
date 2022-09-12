89cd302ed2a92a32642b70bdb7c814c5a0c83360
oncomouse <oncomouse@gmail.com>
2022-09-12 13:31:19 -0500

    cmp registration moved; don't assume top-level commands only

--- conf/vim/lua/nvim-ref/cmp/init.lua
+++ conf/vim/lua/nvim-ref/cmp/init.lua
@@ -1,18 +1,25 @@
-local source = {}
+local M = {}
 
-source.new = function()
-	return setmetatable({}, { __index = source })
+function M.register()
+	local ok, cmp = pcall(require, "cmp")
+	if ok then
+		cmp.register_source("nvim_ref", require("nvim-ref.cmp").new())
+	end
 end
 
-source.is_available = function()
+M.new = function()
+	return setmetatable({}, { __index = M })
+end
+
+M.is_available = function()
 	return vim.b.nvim_ref_loaded
 end
 
-source.get_keyword_pattern = function()
+M.get_keyword_pattern = function()
 	return [[\k\+]]
 end
 
-source.complete = function(_, params, callback)
+M.complete = function(_, params, callback)
 	local items = {}
 
 	local start = require("nvim-ref.filetypes").require().find_start()
@@ -20,7 +27,7 @@ source.complete = function(_, params, callback)
 		-- Clean target:
 		local target = params.context.cursor_before_line:sub(start):match("[^, \t{}@}\n]*$")
 		local entries = require("nvim-ref.bibliography").query(target)
-		for _,entry in pairs(entries) do
+		for _, entry in pairs(entries) do
 			table.insert(items, require("nvim-ref.utils.lsp").make_lsp_item(entry))
 		end
 	end
@@ -28,4 +35,4 @@ source.complete = function(_, params, callback)
 	callback({ items = items })
 end
 
-return source
+return M
