f991776f365678360b9c03a3e4f10fea7c733f50
oncomouse <oncomouse@gmail.com>
2022-09-18 11:37:24 -0500

    Move autolist.nvim

--- conf/vim/lua/dotfiles/plugins/init.lua
+++ conf/vim/lua/dotfiles/plugins/init.lua
@@ -338,6 +338,53 @@ local function plugins()
 				}, -- Zoomed, minimalist mode (<leader>z and gz<motion>)
 
 				{
+					"gaoDean/autolist.nvim",
+					ft = { "markdown", "text" },
+					config = function()
+						require("autolist").setup({
+							colon = {
+								preferred = "",
+								indent = false,
+								indent_raw = false,
+							},
+							invert = {
+								ul_marker = "*",
+							},
+							insert_mappings = {
+								invert = {
+									""
+								}
+							}
+						})
+						-- <C-d> to delete list marker if that's all that's left
+						vim.api.nvim_create_autocmd("FileType", {
+							pattern = "markdown,text",
+							group = "dotfiles-settings",
+							callback = function(args)
+								vim.keymap.set("i", "<C-d>", function()
+									local line = vim.api.nvim_get_current_line()
+									-- Check line for unordered list:
+									local match = string.match(line, "^[*-] ")
+									-- Check line for ordered list:
+									if not match then
+										match = string.match(line, "^%d+%. ")
+									end
+									if match then
+										local savepos = vim.fn.winsaveview().col
+										local jump = (savepos == #line) and "$a" or savepos - #match .. "li"
+										return "<Esc>0\"_2dl" .. jump
+									end
+									return "<C-d><Cmd>lua require('autolist').detab()<CR>"
+								end, {
+									expr = true,
+									buffer = args.buf,
+								})
+							end,
+						})
+					end,
+				}, -- Auto-increment and indent/dedent lists
+
+				{
 					"nvim-treesitter/nvim-treesitter",
 					config = require("dotfiles.plugins.nvim-treesitter"),
 					run = function()
@@ -393,51 +440,6 @@ local function plugins()
 						{ -- Required for TableFormat in vim-markdown but also useful elsewhere
 							"godlygeek/tabular",
 							cmd = { "Tabularize" },
-						},
-						{
-							"gaoDean/autolist.nvim",
-							config = function()
-								require("autolist").setup({
-									colon = {
-										preferred = "",
-										indent = false,
-										indent_raw = false,
-									},
-									invert = {
-										ul_marker = "*",
-									},
-									insert_mappings = {
-										invert = {
-											""
-										}
-									}
-								})
-								-- <C-d> to delete list marker if that's all that's left
-								vim.api.nvim_create_autocmd("FileType", {
-									pattern = "markdown,text",
-									group = "dotfiles-settings",
-									callback = function(args)
-										vim.keymap.set("i", "<C-d>", function()
-											local line = vim.api.nvim_get_current_line()
-											-- Check line for unordered list:
-											local match = string.match(line, "^[*-] ")
-											-- Check line for ordered list:
-											if not match then
-												match = string.match(line, "^%d+%. ")
-											end
-											if match then
-												local savepos = vim.fn.winsaveview().col
-												local jump = (savepos == #line) and "$a" or savepos - #match .. "li"
-												return "<Esc>0\"_2dl" .. jump
-											end
-											return "<C-d><Cmd>lua require('autolist').detab()<CR>"
-										end, {
-											expr = true,
-											buffer = args.buf,
-										})
-									end,
-								})
-							end,
 						},
 					},
 				}, -- Markdown Syntax
