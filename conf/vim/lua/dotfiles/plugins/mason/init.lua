39c8baeb08e42c60e0365124c51ceb62739611da
oncomouse <oncomouse@gmail.com>
2022-09-19 17:37:35 -0500

    Updated mason

--- conf/vim/lua/dotfiles/plugins/mason/init.lua
+++ conf/vim/lua/dotfiles/plugins/mason/init.lua
@@ -13,16 +13,6 @@ M.packages = {
 	-- "vint",
 }
 
-M.local_packages = {}
-
-local function generate_local_packages()
-	if #M.local_packages ~= 0 then
-		return
-	end
-	M.local_packages["rubocop"] = require("dotfiles.plugins.mason.registry.rubocop")
-	M.local_packages["standardrb"] = require("dotfiles.plugins.mason.registry.standardrb")
-end
-
 local function install_maybe(pkg)
 	if vim.fn.executable(pkg) ~= 1 then
 		local in_registry, spec = pcall(require("mason-registry").get_package, pkg)
@@ -45,7 +35,6 @@ end
 
 function M.install_tools()
 	configure_mason()
-	generate_local_packages()
 	vim.tbl_map(install_maybe, M.packages)
 end
 
