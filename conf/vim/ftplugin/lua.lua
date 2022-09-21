68f2fabd687b11cf8f8a98205de4764832f01c4f
oncomouse <oncomouse@gmail.com>
2022-09-21 16:26:27 -0500

    gf matches with 'include'

--- conf/vim/ftplugin/lua.lua
+++ conf/vim/ftplugin/lua.lua
@@ -1,3 +1,3 @@
 vim.opt_local.formatprg="stylua -"
-vim.opt_local.include=[[^.*=\s*require(["']\zs[^"']\+\ze["'])]]
+vim.opt_local.include=[==[^.*require\s*(\{0,1\}["']\zs[^"']\+\ze["']]==]
 vim.opt_local.includeexpr="v:lua.require'dotfiles.lua'.includeexpr(v:fname)"
--- conf/vim/lua/dotfiles/lua.lua
+++ conf/vim/lua/dotfiles/lua.lua
@@ -1,4 +1,5 @@
 local M = {}
+
 local runtime_path = vim.split(package.path, ";")
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
@@ -6,7 +7,15 @@ for _, path in pairs(vim.api.nvim_list_runtime_paths()) do
 	table.insert(runtime_path, path .. "/lua/?.lua")
 	table.insert(runtime_path, path .. "/lua/?/init.lua")
 end
+
 function M.includeexpr(fname)
+	if vim.fn.filereadable(vim.fn.fnamemodify(fname, ":p")) == 1 then
+		return fname
+	end
+
+	local line = vim.api.nvim_get_current_line()
+	local start, finish = vim.regex(vim.opt.include:get()):match_str(line)
+	fname = line:sub(start + 1, finish)
 	fname = fname:gsub("%.", "/")
 	for _, path in pairs(runtime_path) do
 		local expanded = vim.fn.substitute(path, "?", fname, "g")
@@ -16,4 +25,5 @@ function M.includeexpr(fname)
 	end
 	return fname
 end
+
 return M
