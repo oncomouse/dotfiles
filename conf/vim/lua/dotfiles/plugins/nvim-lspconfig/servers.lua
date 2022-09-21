519cb3ae9d8df278ca882489864c18a4c5bb7c24
oncomouse <oncomouse@gmail.com>
2022-09-21 13:09:53 -0500

    Better Lua configuration (maybe) but still have high CPU usage on macOS

--- conf/vim/lua/dotfiles/plugins/nvim-lspconfig/servers.lua
+++ conf/vim/lua/dotfiles/plugins/nvim-lspconfig/servers.lua
@@ -1,6 +1,33 @@
+-- put this file somewhere in your nvim config, like: ~/.config/nvim/lua/config/lua-lsp.lua
+-- usage: require'lspconfig'.sumneko_lua.setup(require("config.lua-lsp"))
+
+local library = {}
+
 local runtime_path = vim.split(package.path, ";")
+
+-- this is the ONLY correct way to setup your path
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
+
+local function add(lib)
+	for _, p in pairs(vim.fn.expand(lib, false, true)) do
+		p = vim.loop.fs_realpath(p)
+		if p ~=nil then
+			library[p] = true
+		end
+	end
+end
+
+-- add runtime
+add("$VIMRUNTIME")
+
+-- add your config
+add("~/dotfiles/conf/vim")
+
+-- add plugins
+-- if you're not using packer, then you might need to change the paths below
+add("~/.local/share/packer.nvim/pack/packer/opt/*")
+add("~/.local/share/packer.nvim/pack/packer/start/*")
 
 local server_map = {
 	{
@@ -62,25 +89,33 @@ local server_map = {
 				provides = {
 					"snippets",
 				},
+				on_new_config = function(config, root)
+					local libs = vim.tbl_deep_extend("force", {}, library)
+					libs[root] = nil
+					config.settings.Lua.workspace.library = libs
+					return config
+				end,
 				settings = {
 					Lua = {
 						runtime = {
+							-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
 							version = "LuaJIT",
+							-- Setup your lua path
 							path = runtime_path,
 						},
-						workspace = {
-							library = vim.api.nvim_get_runtime_file("", true),
-						},
-						completion = {
-							callSnippet = "Replace",
-							keywordSnippet = "Replace",
-						},
+						completion = { callSnippet = "Both" },
 						diagnostics = {
+							-- Get the language server to recognize the `vim` global
 							globals = { "vim" },
 						},
-						telemetry = {
-							enable = false,
+						workspace = {
+							-- Make the server aware of Neovim runtime files
+							library = library,
+							maxPreload = 2000,
+							preloadFileSize = 50000,
 						},
+						-- Do not send telemetry data containing a randomized but unique identifier
+						telemetry = { enable = false },
 					},
 				},
 				flags = {
