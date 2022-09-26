e692eb5ca8a316a47ed3209d1676cccd845c041f
oncomouse <oncomouse@gmail.com>
2022-09-26 10:30:04 -0500

    This is overkill, but I really think these should be plug bindings

--- conf/vim/lua/dotfiles/plugins/init.lua
+++ conf/vim/lua/dotfiles/plugins/init.lua
@@ -232,22 +232,37 @@ local function plugins()
 								ul_marker = "*",
 							},
 							insert_mappings = {
-								detab = {
-									"",
-								},
-								invert = {
-									"",
-								},
-								indent = {
-									"",
-								},
+								invert = { "" },
+								new = { "" },
+								detab = { "" },
+								tab = { "" },
+								recal = { "" },
+								indent = { "" },
 							},
 						})
+						vim.keymap.set({"i", "n"}, "<Plug>(autolist-invert)", "<cmd>lua require('autolist').invert()<CR>")
+						vim.keymap.set({"i", "n"}, "<Plug>(autolist-new)", "<cmd>lua require('autolist').new()<CR>")
+						vim.keymap.set({"i", "n"}, "<Plug>(autolist-tab)", "<cmd>lua require('autolist').tab()<CR>")
+						vim.keymap.set({"i", "n"}, "<Plug>(autolist-detab)", "<cmd>lua require('autolist').detab()<CR>")
+						vim.keymap.set({"i", "n"}, "<Plug>(autolist-recal)", "<cmd>lua require('autolist').recal()<CR>")
+						vim.keymap.set(
+							{"i", "n"},
+							"<Plug>(autolist-indent-increase)",
+							"<cmd>lua require('autolist').indent('>>')<CR>"
+						)
+						vim.keymap.set(
+							{"i", "n"},
+							"<Plug>(autolist-indent-decrease)",
+							"<cmd>lua require('autolist').indent('<<')<CR>"
+						)
 						-- <C-d> to delete list marker if that's all that's left
 						vim.api.nvim_create_autocmd("FileType", {
 							pattern = "markdown,text",
 							group = "dotfiles-settings",
 							callback = function(args)
+								vim.keymap.set("i", "<C-z>", "<Plug>(autolist-recal)")
+								vim.keymap.set("i", "<C-t>", "<C-t><Plug>(autolist-tab)")
+								vim.keymap.set("i", "<CR>", "<CR><Plug>(autolist-new)")
 								vim.keymap.set("i", "<C-d>", function()
 									local line = vim.api.nvim_get_current_line()
 									-- Check line for unordered list:
@@ -261,7 +276,7 @@ local function plugins()
 										local jump = (savepos == #line) and "$a" or savepos - #match .. "li"
 										return '<Esc>0"_2dl' .. jump
 									end
-									return "<C-d><Cmd>lua require('autolist').detab()<CR>"
+									return "<C-d><Plug>(autolist-detab)"
 								end, {
 									expr = true,
 									buffer = args.buf,
