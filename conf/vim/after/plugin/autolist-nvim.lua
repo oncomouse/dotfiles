9576a779e177b37593ea7bbead17bfb4f3abd0b8
oncomouse <oncomouse@gmail.com>
2022-09-27 10:07:47 -0500

    Redo all the bindings!

--- conf/vim/after/plugin/autolist-nvim.lua
+++ conf/vim/after/plugin/autolist-nvim.lua
@@ -7,6 +7,12 @@ require("autolist").setup({
 	invert = {
 		ul_marker = "*",
 	},
+	normal_mappings = {
+		new = { "" },
+		tab = { "" },
+		detab = { "" },
+		recal = { "" },
+	},
 	insert_mappings = {
 		invert = { "" },
 		new = { "" },
@@ -18,6 +24,7 @@ require("autolist").setup({
 })
 vim.keymap.set({ "i", "n" }, "<Plug>(autolist-invert)", "<cmd>lua require('autolist').invert()<CR>")
 vim.keymap.set({ "i", "n" }, "<Plug>(autolist-new)", "<cmd>lua require('autolist').new()<CR>")
+vim.keymap.set({ "i", "n" }, "<Plug>(autolist-new-above)", "<cmd>lua require('autolist').new(true)<CR>")
 vim.keymap.set({ "i", "n" }, "<Plug>(autolist-tab)", "<cmd>lua require('autolist').tab()<CR>")
 vim.keymap.set({ "i", "n" }, "<Plug>(autolist-detab)", "<cmd>lua require('autolist').detab()<CR>")
 vim.keymap.set({ "i", "n" }, "<Plug>(autolist-recal)", "<cmd>lua require('autolist').recal()<CR>")
@@ -28,9 +35,14 @@ vim.api.nvim_create_autocmd("FileType", {
 	group = "dotfiles-settings",
 	callback = function(args)
 		-- Set the bindings we want:
-		vim.keymap.set("i", "<C-z>", "<Plug>(autolist-recal)")
-		vim.keymap.set("i", "<C-t>", "<C-t><Plug>(autolist-tab)")
-		vim.keymap.set("i", "<CR>", "<CR><Plug>(autolist-new)")
+		vim.keymap.set("i", "<C-z>", "<Plug>(autolist-invert)", { buffer = args.buf })
+		vim.keymap.set("i", "<C-t>", "<C-t><Plug>(autolist-tab)", { buffer = args.buf })
+		vim.keymap.set("i", "<CR>", "<CR><Plug>(autolist-new)", { buffer = args.buf })
+		vim.keymap.set("n", ">>", ">><Plug>(autolist-tab)", { buffer = args.buf })
+		vim.keymap.set("n", "<<", "<<<Plug>(autolist-detab)", { buffer = args.buf })
+		vim.keymap.set("n", "o", "o<Plug>(autolist-new)", { buffer = args.buf })
+		vim.keymap.set("n", "O", "O<Plug>(autolist-new-above)", { buffer = args.buf })
+		vim.keymap.set("n", "<C-z>", "<Plug>(autolist-recal)", { buffer = args.buf })
 		-- <C-d> to delete list marker if that's all that's left
 		vim.keymap.set("i", "<C-d>", function()
 			local line = vim.api.nvim_get_current_line()
