fd19df65ac247d6987baf112c185bf5ae2aa875d
oncomouse <oncomouse@gmail.com>
2022-09-28 10:49:53 -0500

    npminstall function wasn't working

--- bootstrap/scripts/node-modules.sh
+++ bootstrap/scripts/node-modules.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 ## Install Node.js Modules
 # Setup a local to user global npm path:
+function npminstall() {
+  local repo=$1
+  if ! [[ $npmlist =~ $repo ]]; then 
+    npm install -g "$1"
+  fi
+}
 mkdir -p "${HOME}/.npm-packages/lib"
 npm config set prefix "${HOME}/.npm-packages"
 if which npm > /dev/null 2>&1; then
   if [ -z "$SERVER" ]; then 
     npmlist=$(npm list -g)
-    function npminstall() {
-      local repo=$1
-      if ! [[ $npmlist =~ $repo ]]; then 
-        npm install -g "$1"
-      fi
-    }
     npminstall 'mf-cli' 
     npminstall 'trash-cli' 
     npminstall 'empty-trash-cli'
