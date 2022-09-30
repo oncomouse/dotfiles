12f2cfd29752f0784c32ecdaac7801f9112433ff
oncomouse <oncomouse@gmail.com>
2022-09-30 10:04:50 -0500

    THAT'S WHY THIS HASN'T BEEN WORKING

--- conf/vim/after/ftplugin/lua.lua
+++ conf/vim/after/ftplugin/lua.lua
@@ -3,7 +3,7 @@ local spec_pair = require("mini.ai").gen_spec.pair
  -- s for [[]] in ai and surround
 vim.b.miniai_config = {
 	custom_textobjects = {
-		["s"] = spec_pair("[[", "]"),
+		["s"] = spec_pair("[[", "]]"),
 	},
 }
 vim.b.minisurround_config = {
