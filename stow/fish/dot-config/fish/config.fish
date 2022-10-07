8d56adbf29fa4ee3a4fa530e78fb92862be3bee5
oncomouse <oncomouse@gmail.com>
2022-10-07 09:44:28 -0500

    This is not working otherwise for some reason

--- stow/fish/dot-config/fish/config.fish
+++ stow/fish/dot-config/fish/config.fish
@@ -1,30 +1,30 @@
-if status is-login
+# Colors:
+set fish_color_normal normal
+set fish_color_command green
+set fish_color_param blue
+set fish_color_redirection $fish_color_param
+set fish_color_comment white
+set fish_color_escape brpurple
+set fish_color_operator $fish_color_escape
+set fish_color_end blue
+set fish_color_quote cyan
+set fish_color_autosuggestion 555 brblack
+set fish_color_user brgreen
+set fish_color_host $fish_color_normal
+set fish_color_valid_path --underline
+set fish_color_cwd cyan
+set fish_color_cwd_root red
+set fish_color_match --background=brblue
+set fish_color_search_match bryellow --background=brblack
+set fish_color_selection white --bold --background=brblack
+set fish_color_cancel -r
+set fish_pager_color_prefix white --bold --underline
+set fish_pager_color_completion
+set fish_pager_color_description $fish_color_quote yellow
+set fish_pager_color_progress brwhite --background=cyan
+set fish_color_history_current --bold
 
-    # Colors:
-    set fish_color_normal normal
-    set fish_color_command green
-    set fish_color_param blue
-    set fish_color_redirection $fish_color_param
-    set fish_color_comment white
-    set fish_color_escape brpurple
-    set fish_color_operator $fish_color_escape
-    set fish_color_end blue
-    set fish_color_quote cyan
-    set fish_color_autosuggestion 555 brblack
-    set fish_color_user brgreen
-    set fish_color_host $fish_color_normal
-    set fish_color_valid_path --underline
-    set fish_color_cwd cyan
-    set fish_color_cwd_root red
-    set fish_color_match --background=brblue
-    set fish_color_search_match bryellow --background=brblack
-    set fish_color_selection white --bold --background=brblack
-    set fish_color_cancel -r
-    set fish_pager_color_prefix white --bold --underline
-    set fish_pager_color_completion
-    set fish_pager_color_description $fish_color_quote yellow
-    set fish_pager_color_progress brwhite --background=cyan
-    set fish_color_history_current --bold
+if status is-login
 
     # Setup Pywal colors:
     if test -d $HOME/.cache/wal
