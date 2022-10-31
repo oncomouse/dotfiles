e7cdbef01a2fa166c8598336d42a8156b689a61a
oncomouse <oncomouse@gmail.com>
2022-10-31 13:39:16 -0500

    Make markdown editing more powerful (J now clears markup)

--- conf/vim/ftplugin/markdown.lua
+++ conf/vim/ftplugin/markdown.lua
@@ -30,3 +30,40 @@ vim.api.nvim_create_user_command("Pandoc", function(args)
 end, {
 	nargs = 1,
 })
+
+-- Markdown helper maps:
+
+-- Remove markdown markup when joining line
+local function match_all_lines(lines, pattern)
+	local test = nil
+	for _,line in pairs(lines) do
+		test = line:match(pattern)
+		if not test then
+			return false, nil
+		end
+	end
+	return true, test
+end
+local patterns = {
+	"^> ",
+	"^[0-9]+%. ",
+	"^[*-] ",
+}
+vim.keymap.set("n", "J", function()
+	local linenr = vim.fn.line(".") - 1
+	local start, next = unpack(vim.api.nvim_buf_get_lines(0, linenr, linenr + 2, false))
+	for _, pattern in pairs(patterns) do
+		local matches, match = match_all_lines({ start, next}, pattern)
+		if matches then
+			next = next:sub(#match + 1)
+			break
+		end
+	end
+	vim.api.nvim_buf_set_lines(0, linenr, linenr + 2, false, {
+		start .. " " .. next
+	})
+end, {
+	buffer = true,
+})
+
+-- Continue markdown markup on "o" and "O"
