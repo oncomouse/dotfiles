bba6b0f16d306ac017d9418cf2b2653ca571858f
oncomouse <oncomouse@gmail.com>
2022-11-06 12:49:45 -0600

    Still don't like nvim-autopairs

--- conf/vim/after/plugin/nvim-autopairs.lua
+++ conf/vim/after/plugin/nvim-autopairs.lua
@@ -2,39 +2,20 @@ local ok, npairs = pcall(require, "nvim-autopairs")
 
 if ok then
 	local Rule = require("nvim-autopairs.rule")
+	local vim_regex_cond = require("nvim-autopairs.vim-regex-cond")
 	-- local cond = require("nvim-autopairs.conds")
 	-- local ts_cond = require("nvim-autopairs.ts-conds")
 	local utils = require("nvim-autopairs.utils")
 
 	npairs.setup({})
 
-	local function match_vim_regex(line, start_pos, end_pos, regex)
-		local re = vim.regex(regex)
-		local target = line:sub(start_pos, end_pos)
-		return re:match_str(target)
-	end
-
-	local function line_matches_vim_regex(regex)
-		return function(opts)
-			return match_vim_regex(opts.line, 0, #opts.line, regex)
-		end
-	end
-
-	local function not_line_matches_vim_regex(regex)
-		return function(opts)
-			return not match_vim_regex(opts.line, 0, #opts.line, regex)
-		end
-	end
-
 	local function markdown_bold_italic_rule(char)
-		local c = vim.fn.fnnameescape(char)
-		local regex = c .. c .. "[^".. char .. "]*" .. [[\%#]] .. "[^".. char .. "]*" .. c .. c
-		return Rule(char, char, "markdown")
-			:with_pair(not_line_matches_vim_regex(regex))
-			:with_del(function(opts)
-				opts.col = utils.get_cursor()
-				return not_line_matches_vim_regex(regex)
-			end)
+		local c = vim.fn.fnameescape(char)
+		local regex = c .. c .. "[^" .. char .. "]*" .. [[\%#]] .. "[^" .. char .. "]*" .. c .. c
+		return Rule(char, char, "markdown"):with_pair(vim_regex_cond.not_line(regex)):with_del(function(opts)
+			opts.col = utils.get_cursor()
+			return vim_regex_cond.not_line(regex)
+		end)
 	end
 
 	local function at_pos(col)
@@ -44,9 +25,19 @@ if ok then
 	end
 
 	npairs.add_rules({
-		Rule("*", " ", "markdown")
-			:with_pair(at_pos(0)),
+		Rule("*", " ", "markdown"):with_pair(at_pos(0)),
 		markdown_bold_italic_rule("*"),
 		markdown_bold_italic_rule("_"),
+	})
+	npairs.add_rules(require("nvim-autopairs.rules.endwise-lua"))
+	npairs.add_rules(require("nvim-autopairs.rules.endwise-ruby"))
+	local endwise = require("nvim-autopairs.ts-rule").endwise
+
+	-- Shell Rules:
+	npairs.add_rules({
+		endwise("%sdo$",            "done", "sh", nil),
+		endwise("%sdo%s|.*|$",      "done", "sh", nil),
+		endwise("[%s=]%sif%s.+$",   "fi",   "sh", nil),
+		endwise("[%s=]%scase%s.+$", "esac", "sh", nil),
 	})
 end
--- conf/vim/lua/nvim-autopairs/vim-regex-cond.lua
+++ conf/vim/lua/nvim-autopairs/vim-regex-cond.lua
@@ -1,87 +1,95 @@
-local cond = require('nvim-autopairs.cond')
-local utils = require('nvim-autopairs.utils')
-local log = require('nvim-autopairs._log')
+local cond = require("nvim-autopairs.conds")
+local utils = require("nvim-autopairs.utils")
+local log = require("nvim-autopairs._log")
 
 local M = {}
 
+function M.line(regex)
+	if not regex then
+		return cond.none()
+	end
+	return function(opts)
+		local start, _ = vim.regex(regex):match_str(opts.line)
+		return start ~= nil
+	end
+end
+
+function M.not_line(regex)
+	if not regex then
+		return cond.none()
+	end
+	return function(opts)
+		local start, _ = vim.regex(regex):match_str(opts.line)
+		return start == nil
+	end
+end
+
 function M.before(regex, length)
-    length = length or 1
-    if not regex then
-        return cond.none()
-    end
-    ---@param opts CondOpts
-    return function(opts)
-        log.debug('before_regex')
-        if length < 0 then
-            length = opts.col
-        end
-        local str = utils.text_sub_char(opts.line, opts.col - 1, -length)
+	length = length or 1
+	if not regex then
+		return cond.none()
+	end
+	---@param opts CondOpts
+	return function(opts)
+		log.debug("before_regex")
+		if length < 0 then
+			length = opts.col
+		end
+		local str = utils.text_sub_char(opts.line, opts.col - 1, -length)
 		local start, _ = vim.regex(regex):match_str(str)
-        if start ~= nil then
-            return true
-        end
-        return false
-    end
+		return start ~= nil
+	end
 end
 
 function M.after(regex, length)
-    length = length or 1
-    if not regex then
-        return cond.none()
-    end
-    ---@param opts CondOpts
-    return function(opts)
-        log.debug('after_regex')
-        if length < 0 then
-            length = #opts.line
-        end
-        local str = utils.text_sub_char(opts.line, opts.col, length)
-		local start, _ = vim.regex(regex):match_str(str)
-		if start ~= nil then
-			return true
+	length = length or 1
+	if not regex then
+		return cond.none()
+	end
+	---@param opts CondOpts
+	return function(opts)
+		log.debug("after_regex")
+		if length < 0 then
+			length = #opts.line
 		end
-		return false
-    end
+		local str = utils.text_sub_char(opts.line, opts.col, length)
+		local start, _ = vim.regex(regex):match_str(str)
+		return start ~= nil
+	end
 end
 
 function M.not_before(regex, length)
-    length = length or 1
-    if not regex then
-        return cond.none()
-    end
-    ---@param opts CondOpts
-    return function(opts)
-        log.debug('not_before_regex')
-        if length < 0 then
-            length = opts.col
-        end
-        local str = utils.text_sub_char(opts.line, opts.col - 1, -length)
-		local start, _ = vim.regex(regex):match_str(str)
-		if start == nil then
-			return true
+	length = length or 1
+	if not regex then
+		return cond.none()
+	end
+	---@param opts CondOpts
+	return function(opts)
+		log.debug("not_before_regex")
+		if length < 0 then
+			length = opts.col
 		end
-		return false
-    end
+		local str = utils.text_sub_char(opts.line, opts.col - 1, -length)
+		local start, _ = vim.regex(regex):match_str(str)
+		return start == nil
+	end
 end
 
 function M.not_after(regex, length)
-    length = length or 1
-    if not regex then
-        return cond.none()
-    end
-    ---@param opts CondOpts
-    return function(opts)
-        log.debug('not_after_regex')
-        if length < 0 then
-            length = #opts.line
-        end
-        local str = utils.text_sub_char(opts.line, opts.col, length)
-		local start, _ = vim.regex(regex):match_str(str)
-		if start == nil then
-			return true
+	length = length or 1
+	if not regex then
+		return cond.none()
+	end
+	---@param opts CondOpts
+	return function(opts)
+		log.debug("not_after_regex")
+		if length < 0 then
+			length = #opts.line
 		end
-		return false
-    end
+		local str = utils.text_sub_char(opts.line, opts.col, length)
+		local start, _ = vim.regex(regex):match_str(str)
+		return start == nil
+	end
 end
 
 return M
