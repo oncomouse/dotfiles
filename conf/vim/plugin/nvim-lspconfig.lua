2425ad1354999f42070c9af2bbaf6cbd6c3c5d78
oncomouse <oncomouse@gmail.com>
2022-11-08 09:37:32 -0600

    Guard for lspconfig

--- conf/vim/plugin/nvim-lspconfig.lua
+++ conf/vim/plugin/nvim-lspconfig.lua
@@ -1,40 +1,42 @@
-local server_maps = require("dotfiles.plugins.nvim-lspconfig").servers
-local on_attach = require("dotfiles.plugins.nvim-lspconfig").on_attach
+if pcall(require, "lspconfig") then
+	local server_maps = require("dotfiles.plugins.nvim-lspconfig").servers
+	local on_attach = require("dotfiles.plugins.nvim-lspconfig").on_attach
 
-local capabilities = nil
-local handler_no_diagnostics = {
-	["textDocument/publishDiagnostics"] = function() end,
-}
+	local capabilities = nil
+	local handler_no_diagnostics = {
+		["textDocument/publishDiagnostics"] = function() end,
+	}
 
-for _, servers in pairs(server_maps) do
-	vim.api.nvim_create_autocmd("BufReadPre", {
-		pattern = servers.pattern,
-		group = "dotfiles-settings",
-		once = true,
-		callback = function()
-			if capabilities == nil then
-				local ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-				if ok then
-					capabilities = cmp_nvim_lsp.default_capabilities()
-				else
-					capabilities = vim.lsp.protocol.make_client_capabilities()
-				end
-			end
-			for lsp, settings in pairs(servers.servers) do
-				if lsp ~= "null-ls" then
-					local opts = {
-						on_attach = on_attach,
-						capabilities = capabilities,
-					}
-					if #vim.tbl_keys(settings) > 0 then
-						opts = vim.tbl_extend("keep", opts, settings)
+	for _, servers in pairs(server_maps) do
+		vim.api.nvim_create_autocmd("BufReadPre", {
+			pattern = servers.pattern,
+			group = "dotfiles-settings",
+			once = true,
+			callback = function()
+				if capabilities == nil then
+					local ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+					if ok then
+						capabilities = cmp_nvim_lsp.default_capabilities()
+					else
+						capabilities = vim.lsp.protocol.make_client_capabilities()
 					end
-					if not vim.tbl_contains(server_maps[lsp].provides or {}, "diagnostics") then
-						opts.handlers = handler_no_diagnostics
-					end
-					require("lspconfig")[lsp].setup(opts)
 				end
-			end
-		end,
-	})
+				for lsp, settings in pairs(servers.servers) do
+					if lsp ~= "null-ls" then
+						local opts = {
+							on_attach = on_attach,
+							capabilities = capabilities,
+						}
+						if #vim.tbl_keys(settings) > 0 then
+							opts = vim.tbl_extend("keep", opts, settings)
+						end
+						if not vim.tbl_contains(server_maps[lsp].provides or {}, "diagnostics") then
+							opts.handlers = handler_no_diagnostics
+						end
+						require("lspconfig")[lsp].setup(opts)
+					end
+				end
+			end,
+		})
+	end
 end
