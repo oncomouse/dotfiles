9b9b46b54e976406226f0f0a444978613e577098
oncomouse <oncomouse@gmail.com>
2022-11-09 10:06:02 -0600

    fix: use vim.notify() for notifications

--- conf/vim/lua/nvim-ref/utils/notifications.lua
+++ conf/vim/lua/nvim-ref/utils/notifications.lua
@@ -1,26 +1,14 @@
 local M = {}
--- This is from fzf-lua:
-local function _echo_multiline(msg)
-	for _, s in ipairs(vim.fn.split(msg, "\n")) do
-		vim.cmd("echom '" .. s:gsub("'", "''") .. "'")
-	end
-end
 
 function M.info(msg)
-	vim.cmd("echohl DiagnosticInfo")
-	_echo_multiline("[nvim-ref] " .. msg)
-	vim.cmd("echohl None")
+	vim.notify("[nvim-ref] " .. msg, vim.log.levels.INFO)
 end
 
 function M.warn(msg)
-	vim.cmd("echohl WarningMsg")
-	_echo_multiline("[nvim-ref] " .. msg)
-	vim.cmd("echohl None")
+	vim.notify("[nvim-ref] " .. msg, vim.log.levels.WARN)
 end
 
 function M.error(msg)
-	vim.cmd("echohl ErrorMsg")
-	_echo_multiline("[nvim-ref] " .. msg)
-	vim.cmd("echohl None")
+	vim.notify("[nvim-ref] " .. msg, vim.log.levels.ERROR)
 end
 return M
