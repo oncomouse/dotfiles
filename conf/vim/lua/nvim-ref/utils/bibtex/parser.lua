67b63d51c6a07efbc1de4ac787f62217ccf020d8
oncomouse <oncomouse@gmail.com>
2022-11-09 07:47:52 -0600

    fix: use a metatable to access contents in a more object-y way.

--- conf/vim/lua/nvim-ref/utils/bibtex/parser.lua
+++ conf/vim/lua/nvim-ref/utils/bibtex/parser.lua
@@ -1,21 +1,22 @@
 local parser = require("lpeg-bibtex")
 
 local M = {}
+local entry_metatable = {
+	__index = function(entry, idx)
+		if entry.contents and entry.contents[idx] then
+			return entry.contents[idx]
+		end
+		return nil
+	end,
+}
 function parse_bibtex(data)
 	local matches = {}
-	for _,entry in pairs(data) do
-		local entry_content = {
-			key = entry.key,
-			kind = entry.kind,
-			file = entry.file,
-		}
-		for _,item in pairs(entry.contents or {}) do
-			entry_content[item.key] = item.value:gsub("[,}{]", "")
-		end
-		table.insert(matches, entry_content)
+	for _, entry in pairs(data) do
+		table.insert(matches, setmetatable(entry, entry_metatable))
 	end
 	return matches
 end
+
 local function escape_bibfile(file)
 	return vim.fn.fnamemodify(file, ":p")
 end
@@ -25,17 +26,17 @@ local function parse_bibfiles(bibfiles)
 	end
 	return escape_bibfile(bibfiles)
 end
+function M.parse_bibtex_string(bibtex)
+	return parse_bibtex(parser:match(bibtex))
+end
 function M.read_bibfile(bibfile)
 	local fp = io.open(bibfile, "rb")
 	if fp ~= nil then
-		local contents = parser:match(fp:read("*a"))
+		local contents = M.parse_bibtex_string(fp:read("*a"))
 		fp:close()
 		return contents
 	end
 	return nil
-end
-function M.parse_bibtex_string(bibtex)
-	return parser:match(bibtex)
 end
 function M.query_bibtex(bibfiles, key)
 	if not string.match(key, "^@") then
@@ -43,10 +44,10 @@ function M.query_bibtex(bibfiles, key)
 	end
 	bibfiles = parse_bibfiles(type(bibfiles) == "string" and { bibfiles } or bibfiles)
 	local results = {}
-	for _,bibfile in pairs(bibfiles) do
+	for _, bibfile in pairs(bibfiles) do
 		local contents = M.read_bibfile(bibfile)
 		if contents ~= nil then
-			for _,entry in pairs(contents) do
+			for _, entry in pairs(contents) do
 				if entry.type == "entry" and ("@" .. entry.key):match("^" .. key) then
 					entry.file = bibfile
 					table.insert(results, entry)
@@ -56,7 +57,7 @@ function M.query_bibtex(bibfiles, key)
 			require("nvim-ref.utils.notifications").info("Unable to open bibliography file, " .. bibfile .. ".")
 		end
 	end
-	return parse_bibtex(results)
+	return results
 end
 
 return M
