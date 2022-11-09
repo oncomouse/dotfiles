a3e2b015b98eb24177052d1fcce971cb226e9751
oncomouse <oncomouse@gmail.com>
2022-11-09 09:36:36 -0600

    feat: use binary search to insert a new citation

--- conf/vim/lua/nvim-ref/utils/bibtex/writer.lua
+++ conf/vim/lua/nvim-ref/utils/bibtex/writer.lua
@@ -11,8 +11,8 @@ local function deprocess(citation)
 		type = "entry",
 		contents = {},
 	}
-	for key,value in pairs(citation) do
-		if not vim.tbl_contains({"key", "file", "kind"}, key) then
+	for key, value in pairs(citation) do
+		if not vim.tbl_contains({ "key", "file", "kind" }, key) then
 			table.insert(output.contents, {
 				key = key,
 				value = value,
@@ -32,7 +32,10 @@ end
 -- input is raw parsed BibTeX
 -- output is a string of BibTeX
 function M.convert(citation)
-	assert(type(citation) == "table" and citation.key ~= nil, string.format("Citation, %s, is not a valid citation object",vim.inspect(citation))) 
+	assert(
+		type(citation) == "table" and citation.key ~= nil,
+		string.format("Citation, %s, is not a valid citation object", vim.inspect(citation))
+	)
 	if citation.contents == nil then
 		citation = deprocess(citation)
 	end
@@ -40,7 +43,7 @@ function M.convert(citation)
 	if citation.type == "entry" then
 		output = string.format("@%s{%s,\n", citation.kind, citation.key)
 		local values = {}
-		for _,entry in ipairs(citation.contents) do
+		for _, entry in ipairs(citation.contents) do
 			table.insert(values, string.format("    %s = {%s}", entry.key, entry.value))
 		end
 		output = output .. vim.fn.join(values, ",\n") .. "\n}"
@@ -51,30 +54,48 @@ function M.convert(citation)
 	return output
 end
 
+local function binary_search_insert(citations, citation)
+	local start = 1
+	local stop = #citations
+	while true do
+		local pick = math.ceil((stop - start) / 2)
+		local check = citations[pick]
+		if check.key < citation.key then
+			stop = pick - 1
+			if stop == 0 then
+				return 1
+			end
+			if citations[stop] > citation.key then
+				return stop
+			end
+		elseif check.key > citation.key then
+			start = pick + 1
+			if start > #citations then
+				return #citations
+			end
+			if citations[start] < citation.key then
+				return start
+			end
+		end
+	end
+end
+
 -- @param citation ProcessedCitation
 -- @param add boolean
 -- return raw parsed BibTex
 local function mutate(citation, add)
-	local output = {}
 	local file = citation.file
 	local citations = parser.read_bibfile(file)
-	local inserted = false
-	for _,c in pairs(citations) do
-		if add then
-			if not inserted and c.key > citation.key then
-				table.insert(output, deprocess(citation))
-				inserted = true
-			end
-			table.insert(output, c)
-		else
+	if add then
+		table.insert(citations, binary_search_insert(citations, citation), citation)
+	else
+		for i, c in ipairs(citations) do
 			if c.key and c.key == citation.key then
-				table.insert(output, deprocess(citation))
-			else
-				table.insert(output, c)
+				citations[i] = deprocess(citation)
 			end
 		end
 	end
-	write(file, output)
+	write(file, citations)
 end
 
 function M.update(citation)
