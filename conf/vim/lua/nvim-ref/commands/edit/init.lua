2de3f374587d13a9c0031d239db25ce265f651c1
oncomouse <oncomouse@gmail.com>
2022-11-09 15:22:46 -0600

    feat: editor window now opens and can display citation

--- conf/vim/lua/nvim-ref/commands/edit/init.lua
+++ conf/vim/lua/nvim-ref/commands/edit/init.lua
@@ -2,12 +2,37 @@ local hooks = require("nvim-ref.hooks")
 local writer = require("nvim-ref.utils.bibtex").writer
 local M = {}
 
+local function open_editor_window(title)
+	-- Create the scratch buffer displayed in the floating window
+	local buf = vim.api.nvim_create_buf(false, true)
+
+	-- Get the current UI
+	local ui = vim.api.nvim_list_uis()[1]
+
+	-- Create the floating window
+	local opts = {
+		relative = "win",
+		width = ui.width - 4,
+		height = ui.height - 4,
+		col = 2,
+		row = 2,
+		anchor = "NW",
+		border = "single",
+		title = string.format("[nvim-ref] %s", title)
+	}
+	local win = vim.api.nvim_open_win(buf, true, opts)
+	vim.api.nvim_buf_set_option(buf, "filetype", "bibtex")
+	return win, buf
+end
+
 function M.edit(citation)
-	print(vim.inspect(writer.convert(citation)))
+	local win, buf = open_editor_window(string.format("Edit %s", citation.key))
+	vim.api.nvim_buf_set_lines(buf, 0, 0, true, vim.split(writer.convert(citation), "\n", {}))
 end
 
 function M.edit_file(files)
 	print(vim.inspect(files))
+	open_editor_window("Edit File")
 end
 
 function M.setup()
@@ -28,7 +53,8 @@ function M.setup()
 						M.edit(require("nvim-ref.citations").get_citation(args))
 					end
 				end,
-			},{
+			},
+			{
 				id = "file",
 				name = "Edit a BibTeX File",
 				callback = function(args)
@@ -38,7 +64,7 @@ function M.setup()
 						M.edit_file(args)
 					end
 				end,
-			}
+			},
 		},
 	})
 end
