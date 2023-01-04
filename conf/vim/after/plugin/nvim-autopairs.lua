430467bef8186b5749a7293e554d5b3519b920c8
oncomouse <oncomouse@gmail.com>
2023-01-04 16:24:45 -0600

    Blockquotes working; tasks partways

--- conf/vim/after/plugin/nvim-autopairs.lua
+++ conf/vim/after/plugin/nvim-autopairs.lua
@@ -1,9 +1,10 @@
 local ok, npairs = pcall(require, "nvim-autopairs")
 
 if ok then
+	local t = require("dotfiles.utils.termcode")
 	local Rule = require("nvim-autopairs.rule")
 	local vim_regex_cond = require("nvim-autopairs.vim-regex-cond")
-	-- local cond = require("nvim-autopairs.conds")
+	local cond = require("nvim-autopairs.conds")
 	-- local ts_cond = require("nvim-autopairs.ts-conds")
 	local utils = require("nvim-autopairs.utils")
 
@@ -35,9 +36,71 @@ if ok then
 
 	-- Shell Rules:
 	npairs.add_rules({
-		endwise("%sdo$",            "done", "sh", nil),
-		endwise("%sdo%s|.*|$",      "done", "sh", nil),
-		endwise("[%s=]%sif%s.+$",   "fi",   "sh", nil),
+		endwise("%sdo$", "done", "sh", nil),
+		endwise("%sdo%s|.*|$", "done", "sh", nil),
+		endwise("[%s=]%sif%s.+$", "fi", "sh", nil),
 		endwise("[%s=]%scase%s.+$", "esac", "sh", nil),
+	})
+
+	-- Add spaces inside brackets
+	local brackets = { { "(", ")" }, { "[", "]" }, { "{", "}" } }
+	npairs.add_rules({
+		Rule(" ", " "):with_pair(function(opts)
+			local pair = opts.line:sub(opts.col - 1, opts.col)
+			return vim.tbl_contains({
+				brackets[1][1] .. brackets[1][2],
+				brackets[2][1] .. brackets[2][2],
+				brackets[3][1] .. brackets[3][2],
+			}, pair)
+		end),
+	})
+	for _, bracket in pairs(brackets) do
+		npairs.add_rules({
+			Rule(bracket[1] .. " ", " " .. bracket[2])
+				:with_pair(function()
+					return false
+				end)
+				:with_move(function(opts)
+					return opts.prev_char:match(".%" .. bracket[2]) ~= nil
+				end)
+				:use_key(bracket[2]),
+		})
+	end
+
+	-- Blockquotes
+	npairs.add_rules({
+		Rule(">", " ", "markdown"):with_pair(function(opts)
+			return opts.col == 1
+		end):set_end_pair_length(-1),
+		Rule("> ", "", "markdown")
+			:use_regex(true)
+			:with_pair(function(opts)
+				return opts.col == 1
+			end)
+			:with_del(function(opts)
+				return opts.col == 2
+			end)
+			:replace_map_cr(function(opts)
+				if opts.line == "> " then
+					return [[<BS><BS><CR>]]
+				end
+				return t([[<C-g>u<CR><C-c>O> ]])
+			end),
+	})
+
+	-- Tasks
+	npairs.add_rules({
+		Rule("[", " ]", "markdown")
+			:with_pair(function(opts)
+				return opts.line:match("^%s*[%*-]%s*")
+			end)
+			:set_end_pair_length(-2),
+		-- Rule("[ ]", "", "markdown")
+		-- 	:use_regex(true)
+		-- 	:with_pair(cond.none)
+		-- 	:with_del(function(opts)
+		-- 		return opts.line:match("^%s*[%*-]")
+		-- 	end)
+
 	})
 end
