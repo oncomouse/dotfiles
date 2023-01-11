638f3f98aca28f262a390f4c7942be10f2bf84a7
oncomouse <oncomouse@gmail.com>
2023-01-11 14:19:04 -0600

    Fix an error

--- conf/vim/init-minimal.lua
+++ conf/vim/init-minimal.lua
@@ -557,10 +557,7 @@ if paq_available then
 		search_method = "cover_or_next",
 	})
 	-- Remap adding surrounding to Visual mode selection
-	vim.keymap.del("x", "ys")
 	vim.keymap.set("x", "S", [[:<C-u>lua MiniSurround.add('visual')<CR>]], { noremap = true })
-	-- Make special mapping for "add surrounding for line"
-	vim.keymap.set("n", "yss", "ys_", { noremap = false })
 
 	-- Per-file surroundings:
 	local custom_surroundings = {
