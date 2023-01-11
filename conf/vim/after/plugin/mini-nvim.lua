c62cd344829749422a855e9907acd33bbfc7ab87
oncomouse <oncomouse@gmail.com>
2023-01-11 10:26:06 -0600

    Update to standard mini.nvim config

--- conf/vim/after/plugin/mini-nvim.lua
+++ conf/vim/after/plugin/mini-nvim.lua
@@ -189,23 +189,24 @@ if pcall(require, "mini.ai") then
 			},
 		},
 		mappings = {
-			add = "ys",
-			delete = "ds",
-			find = "",
-			find_left = "",
-			highlight = "",
-			replace = "cs",
-			update_n_lines = "",
-			suffix_last = "N",
+			add = "sa",
+			delete = "sd",
+			find = "sf",
+			find_left = "sF",
+			highlight = "sh",
+			replace = "sc",
+			update_n_lines = "sn",
+			suffix_last = "l",
+			suffix_next = "n"
 		},
 		n_lines = 50,
 		search_method = "cover_or_next",
 	})
 	-- Remap adding surrounding to Visual mode selection
-	vim.keymap.del("x", "ys")
+	-- vim.keymap.del("x", "ys")
 	vim.keymap.set("x", "S", [[:<C-u>lua MiniSurround.add('visual')<CR>]], { noremap = true })
 	-- Make special mapping for "add surrounding for line"
-	vim.keymap.set("n", "yss", "ys_", { noremap = false })
+	-- vim.keymap.set("n", "yss", "ys_", { noremap = false })
 
 	--Per-file surroundings:
 	local custom_surroundings = {
