ac7a229cf89ff9a1eec8fdd984650edad82f205b
oncomouse <oncomouse@gmail.com>
2023-01-24 13:52:14 -0600

    Guard for yop.nvim

--- conf/vim/after/plugin/yop-nvim.lua
+++ conf/vim/after/plugin/yop-nvim.lua
@@ -1,22 +1,24 @@
-local utils = require("yop.utils")
-require("yop").op_map({ "n", "v" }, "gs", function(lines, opts)
-	-- We don't care about anything non alphanumeric here
-	local sort_without_leading_space = function(a, b)
-		-- true = a then b
-		-- false = b then a
-		local pattern = [[^%W*]]
-		return string.gsub(a, pattern, "") < string.gsub(b, pattern, "")
-	end
-	if #lines == 1 then
-		-- If only looking at 1 line, sort that line split by some char gotten from input
-		local delimeter = utils.get_input("Delimeter: ")
-		local split = vim.split(lines[1], delimeter, { trimempty = true })
-		-- Remember! `table.sort` mutates the table itself
-		table.sort(split, sort_without_leading_space)
-		return { utils.join(split, delimeter) }
-	else
-		-- If there are many lines, sort the lines themselves
-		table.sort(lines, sort_without_leading_space)
-		return lines
-	end
-end)
+local has_yop, utils = pcall(require, "yop.utils")
+if has_yop then
+	require("yop").op_map({ "n", "v" }, "gs", function(lines, opts)
+		-- We don't care about anything non alphanumeric here
+		local sort_without_leading_space = function(a, b)
+			-- true = a then b
+			-- false = b then a
+			local pattern = [[^%W*]]
+			return string.gsub(a, pattern, "") < string.gsub(b, pattern, "")
+		end
+		if #lines == 1 then
+			-- If only looking at 1 line, sort that line split by some char gotten from input
+			local delimeter = utils.get_input("Delimeter: ")
+			local split = vim.split(lines[1], delimeter, { trimempty = true })
+			-- Remember! `table.sort` mutates the table itself
+			table.sort(split, sort_without_leading_space)
+			return { utils.join(split, delimeter) }
+		else
+			-- If there are many lines, sort the lines themselves
+			table.sort(lines, sort_without_leading_space)
+			return lines
+		end
+	end)
+end
