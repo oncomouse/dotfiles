07c37c384bb96d22dcd2461b6110fe90309c2c32
oncomouse <oncomouse@gmail.com>
2023-02-08 08:44:42 -0600

    Switch to modular mini.nvim configuration

--- conf/vim/lua/dotfiles/plugins/mini-nvim.lua
+++ conf/vim/lua/dotfiles/plugins/mini-nvim.lua
@@ -4,236 +4,250 @@
 
 ---@alias ai_type "i" | "a"
 return {
-	"echasnovski/mini.nvim",
-	dependencies = {
-		{ "preservim/vim-textobj-sentence" }, -- Sentence object
-	},
-	event = "VeryLazy",
-	config = function()
-
-		-- vim-textobj-sentence configuration
-		-- vim.g["textobj#sentence#doubleDefault"] = "“”"
-		-- vim.g["textobj#sentence#singleDefault"] = "‘’"
-		vim.g["textobj#sentence#doubleDefault"] = [[""]]
-		vim.g["textobj#sentence#singleDefault"] = [['']]
-		vim.g["textobj#sentence#abbreviations"] = {
-			"[ABCDIMPSUabcdegimpsv]",
-			"l[ab]",
-			"[eRr]d",
-			"Ph",
-			"[Ccp]l",
-			"[Lli]n",
-			"[cn]o",
-			"[Oe]p",
-			"[DJMSh]r",
-			"[MVv]s",
-			"[CFMPScfpw]t",
-			"alt",
-			"[Ee]tc",
-			"div",
-			"es[pt]",
-			"[Ll]td",
-			"min",
-			"[MD]rs",
-			"[Aa]pt",
-			"[Aa]ve?",
-			"[Ss]tr?",
-			"[Aa]ssn",
-			"[Bb]lvd",
-			"[Dd]ept",
-			"incl",
-			"Inst",
-			"Prof",
-			"Univ",
-			"Messrs",
-		}
-
-		---@param map { [string]: fun() } A list of textobject functions
-		---@return fun(type:ai_type):Pair
-		local function from_textobject_user(map)
-			local function textobject_result_to_point(result)
-				return {
-					line = result[2],
-					col = result[3],
-				}
-			end
-
-			---@param type ai_type
-			---@return Pair
-			return function(type)
-				local results = map["select_function_" .. type]()
-				if results == 0 then
-					return nil
-				end
-				return {
-					from = textobject_result_to_point(results[2]),
-					to = textobject_result_to_point(results[3]),
-				}
-			end
-		end
-
-		---@return Pair #The Pair at the current cursor position
-		local function make_point()
-			local _, l, c, _ = unpack(vim.fn.getpos("."))
-			return {
-				line = l,
-				col = c,
+	{
+		"echasnovski/mini.ai",
+		dependencies = {
+			{ "preservim/vim-textobj-sentence" }, -- Sentence object
+		},
+		event = "VeryLazy",
+		opts = function()
+			-- vim-textobj-sentence configuration
+			-- vim.g["textobj#sentence#doubleDefault"] = "“”"
+			-- vim.g["textobj#sentence#singleDefault"] = "‘’"
+			vim.g["textobj#sentence#doubleDefault"] = [[""]]
+			vim.g["textobj#sentence#singleDefault"] = [['']]
+			vim.g["textobj#sentence#abbreviations"] = {
+				"[ABCDIMPSUabcdegimpsv]",
+				"l[ab]",
+				"[eRr]d",
+				"Ph",
+				"[Ccp]l",
+				"[Lli]n",
+				"[cn]o",
+				"[Oe]p",
+				"[DJMSh]r",
+				"[MVv]s",
+				"[CFMPScfpw]t",
+				"alt",
+				"[Ee]tc",
+				"div",
+				"es[pt]",
+				"[Ll]td",
+				"min",
+				"[MD]rs",
+				"[Aa]pt",
+				"[Aa]ve?",
+				"[Ss]tr?",
+				"[Aa]ssn",
+				"[Bb]lvd",
+				"[Dd]ept",
+				"incl",
+				"Inst",
+				"Prof",
+				"Univ",
+				"Messrs",
 			}
-		end
 
-		local function get_indentation(line)
-			return vim.api.nvim_buf_get_lines(0, line - 1, line, false)[1]:match("^%s+")
-		end
+			---@param map { [string]: fun() } A list of textobject functions
+			---@return fun(type:ai_type):Pair
+			local function from_textobject_user(map)
+				local function textobject_result_to_point(result)
+					return {
+						line = result[2],
+						col = result[3],
+					}
+				end
 
-		local function check_indentation(line, target)
-			if target == nil then
-				return true
+				---@param type ai_type
+				---@return Pair
+				return function(type)
+					local results = map["select_function_" .. type]()
+					if results == 0 then
+						return nil
+					end
+					return {
+						from = textobject_result_to_point(results[2]),
+						to = textobject_result_to_point(results[3]),
+					}
+				end
 			end
-			local l = vim.api.nvim_buf_get_lines(0, line - 1, line, false)[1]
-			if #l == 0 then
-				return true
+
+			---@return Pair #The Pair at the current cursor position
+			local function make_point()
+				local _, l, c, _ = unpack(vim.fn.getpos("."))
+				return {
+					line = l,
+					col = c,
+				}
 			end
-			return l:sub(1, #target) == target
-		end
 
-		require("mini.ai").setup({
-			custom_textobjects = {
+			local function get_indentation(line)
+				return vim.api.nvim_buf_get_lines(0, line - 1, line, false)[1]:match("^%s+")
+			end
 
-				e = function() -- Whole buffer
-					local from = { line = 1, col = 1 }
-					local last_line_length = #vim.fn.getline("$")
-					local to = {
-						line = vim.fn.line("$"),
-						col = last_line_length == 0 and 1 or last_line_length,
-					}
-					return {
-						from = from,
-						to = to,
-					}
-				end,
+			local function check_indentation(line, target)
+				if target == nil then
+					return true
+				end
+				local l = vim.api.nvim_buf_get_lines(0, line - 1, line, false)[1]
+				if #l == 0 then
+					return true
+				end
+				return l:sub(1, #target) == target
+			end
 
-				i = function(type) -- Indentation
-					local _, line, _, _ = unpack(vim.fn.getpos("."))
-					local target_indentation = get_indentation(line)
-					local start = line - 1
-					local stop = line + 1
-					local _, end_line, _, _ = unpack(vim.fn.getpos("$"))
-					while true do
-						if start == 0 then
-							start = 1
-							break
-						end
-						if not check_indentation(start, target_indentation) then
-							start = start + 1
-							break
-						end
-						start = start - 1
-					end
-					while true do
-						if stop == end_line + 1 then
-							stop = end_line
-							break
-						end
-						if not check_indentation(stop, target_indentation) then
-							stop = stop - 1
-							break
-						end
-						stop = stop + 1
-					end
-					if type == "a" then
-						start = start == 1 and 1 or start - 1
-						stop = stop == end_line and end_line or stop + 1
-					end
-					return {
-						to = {
-							line = start,
-							col = 1,
-						},
-						from = {
-							line = stop,
-							col = #vim.api.nvim_buf_get_lines(0, stop - 1, stop, false)[1] + 1,
-						},
-					}
-				end,
+			return {
+				custom_textobjects = {
 
-				z = function(type) -- Folds
-					vim.api.nvim_feedkeys("[z" .. (type == "i" and "j0" or ""), "x", true)
-					local from = make_point()
-					vim.api.nvim_feedkeys("]z" .. (type == "i" and "k$" or "$"), "x", true)
-					local to = make_point()
-
-					return {
-						from = from,
-						to = to,
-					}
-				end,
-
-				-- Sentence objects, based on vim-textobj-sentence and how vim-textobj-function works:
-				--   `is` grabs the words of the sentence (without trailing punctuation and quotes)
-				--   `as` grabs ending punctuation but not white space
-				--   `iS` works like `as`
-				--   `aS` grabs ending punctuation and white space
-				s = from_textobject_user({
-					select_function_i = function() -- Rewrite selection results to remove trailing punctuation and quotes
-						local results = vim.fn["textobj#sentence#select_i"]()
-						local start = (results[2][2] == results[3][2] and results[2][3] or 0)
-						local line = vim.api
-							.nvim_buf_get_lines(0, results[3][2] - 1, results[3][2], false)[1]
-							:sub(start, results[3][3])
-						results[3][3] = start + line:find("['\".?!]+$") - 2
-						return results
+					e = function() -- Whole buffer
+						local from = { line = 1, col = 1 }
+						local last_line_length = #vim.fn.getline("$")
+						local to = {
+							line = vim.fn.line("$"),
+							col = last_line_length == 0 and 1 or last_line_length,
+						}
+						return {
+							from = from,
+							to = to,
+						}
 					end,
-					select_function_a = vim.fn["textobj#sentence#select_i"],
-				}),
-				S = from_textobject_user({
-					select_function_a = vim.fn["textobj#sentence#select_a"],
-					select_function_i = vim.fn["textobj#sentence#select_i"],
-				}),
 
-				[","] = { -- Grammatically correct comma matching
-					{
-						"[%.?!][ ]*()()[^,%.?!]+(),[ ]*()", -- Start of sentence
-						"(),[ ]*()[^,%.?!]+()()[%.?!][ ]*", -- End of sentence
-						",[ ]*()[^,%.?!]+(),[ ]*", -- Dependent clause
-						"^()[A-Z][^,%.?!]+(),[ ]*", -- Start of line
+					i = function(type) -- Indentation
+						local _, line, _, _ = unpack(vim.fn.getpos("."))
+						local target_indentation = get_indentation(line)
+						local start = line - 1
+						local stop = line + 1
+						local _, end_line, _, _ = unpack(vim.fn.getpos("$"))
+						while true do
+							if start == 0 then
+								start = 1
+								break
+							end
+							if not check_indentation(start, target_indentation) then
+								start = start + 1
+								break
+							end
+							start = start - 1
+						end
+						while true do
+							if stop == end_line + 1 then
+								stop = end_line
+								break
+							end
+							if not check_indentation(stop, target_indentation) then
+								stop = stop - 1
+								break
+							end
+							stop = stop + 1
+						end
+						if type == "a" then
+							start = start == 1 and 1 or start - 1
+							stop = stop == end_line and end_line or stop + 1
+						end
+						return {
+							to = {
+								line = start,
+								col = 1,
+							},
+							from = {
+								line = stop,
+								col = #vim.api.nvim_buf_get_lines(0, stop - 1, stop, false)[1] + 1,
+							},
+						}
+					end,
+
+					z = function(type) -- Folds
+						vim.api.nvim_feedkeys("[z" .. (type == "i" and "j0" or ""), "x", true)
+						local from = make_point()
+						vim.api.nvim_feedkeys("]z" .. (type == "i" and "k$" or "$"), "x", true)
+						local to = make_point()
+
+						return {
+							from = from,
+							to = to,
+						}
+					end,
+
+					-- Sentence objects, based on vim-textobj-sentence and how vim-textobj-function works:
+					--   `is` grabs the words of the sentence (without trailing punctuation and quotes)
+					--   `as` grabs ending punctuation but not white space
+					--   `iS` works like `as`
+					--   `aS` grabs ending punctuation and white space
+					s = from_textobject_user({
+						select_function_i = function() -- Rewrite selection results to remove trailing punctuation and quotes
+							local results = vim.fn["textobj#sentence#select_i"]()
+							local start = (results[2][2] == results[3][2] and results[2][3] or 0)
+							local line = vim.api
+								.nvim_buf_get_lines(0, results[3][2] - 1, results[3][2], false)[1]
+								:sub(start, results[3][3])
+							results[3][3] = start + line:find("['\".?!]+$") - 2
+							return results
+						end,
+						select_function_a = vim.fn["textobj#sentence#select_i"],
+					}),
+					S = from_textobject_user({
+						select_function_a = vim.fn["textobj#sentence#select_a"],
+						select_function_i = vim.fn["textobj#sentence#select_i"],
+					}),
+
+					[","] = { -- Grammatically correct comma matching
+						{
+							"[%.?!][ ]*()()[^,%.?!]+(),[ ]*()", -- Start of sentence
+							"(),[ ]*()[^,%.?!]+()()[%.?!][ ]*", -- End of sentence
+							",[ ]*()[^,%.?!]+(),[ ]*", -- Dependent clause
+							"^()[A-Z][^,%.?!]+(),[ ]*", -- Start of line
+						},
 					},
 				},
-			},
-			mappings = {
-				around_last = "aN",
-				inside_last = "iN",
-			},
-			n_lines = 50,
-			search_method = "cover", -- Only use next and last mappings to search
-		})
-		-- Per-file textobjects:
-		local spec_pair = require("mini.ai").gen_spec.pair
-		local custom_textobjects = {
-			lua = {
-				["s"] = spec_pair("[[", "]]"),
-			},
-			markdown = {
-				["*"] = spec_pair("*", "*", { type = "greedy" }), -- Grab all asterisks when selecting
-				["_"] = spec_pair("_", "_", { type = "greedy" }), -- Grab all underscores when selecting
-				["l"] = { "%b[]%b()", "^%[().-()%]%([^)]+%)$" }, -- Link targeting name
-				["L"] = { "%b[]%b()", "^%[.-%]%(()[^)]+()%)$" }, -- Link targeting href
-			},
-		}
-		vim.api.nvim_create_autocmd("FileType", {
-			group = "dotfiles-settings",
-			pattern = vim.fn.join(vim.tbl_keys(custom_textobjects), ","),
-			callback = function()
-				local ft = vim.opt.filetype:get()
-				vim.b.miniai_config = {
-					custom_textobjects = custom_textobjects[ft],
-				}
-			end,
-		})
+				mappings = {
+					around_last = "aN",
+					inside_last = "iN",
+				},
+				n_lines = 50,
+				search_method = "cover", -- Only use next and last mappings to search
+			}
+		end,
+		config = function(_, opts)
+			require("mini.ai").setup(opts)
+			-- Per-file textobjects:
+			local spec_pair = require("mini.ai").gen_spec.pair
+			local custom_textobjects = {
+				lua = {
+					["s"] = spec_pair("[[", "]]"),
+				},
+				markdown = {
+					["*"] = spec_pair("*", "*", { type = "greedy" }), -- Grab all asterisks when selecting
+					["_"] = spec_pair("_", "_", { type = "greedy" }), -- Grab all underscores when selecting
+					["l"] = { "%b[]%b()", "^%[().-()%]%([^)]+%)$" }, -- Link targeting name
+					["L"] = { "%b[]%b()", "^%[.-%]%(()[^)]+()%)$" }, -- Link targeting href
+				},
+			}
+			vim.api.nvim_create_autocmd("FileType", {
+				group = "dotfiles-settings",
+				pattern = vim.fn.join(vim.tbl_keys(custom_textobjects), ","),
+				callback = function()
+					local ft = vim.opt.filetype:get()
+					vim.b.miniai_config = {
+						custom_textobjects = custom_textobjects[ft],
+					}
+				end,
+			})
+		end,
+	},
 
-		-- ga,gA for alignment:
-		require("mini.align").setup({})
+	-- ga,gA for alignment:
+	{
+		"echasnovski/mini.align",
+		keys = {
+			{ "ga", mode = { "n", "x" } },
+			{ "gA", mode = { "n", "x" } },
+		},
+	},
 
-		require("mini.basics").setup({
+	{
+		"echasnovski/mini.basics",
+		lazy = false,
+		opts = {
 			options = {
 				basic = false,
 			},
@@ -241,24 +255,58 @@ return {
 				move_with_alt = true,
 				windows = true,
 			},
-		})
-		vim.keymap.del("t", "<C-w>")
+		},
+		config = function(_, opts)
+			require("mini.basics").setup(opts)
+			vim.keymap.del("t", "<C-w>")
+		end,
+	},
 
-		-- :Bd[!] for layout-safe bufdelete
-		require("mini.bufremove").setup({})
-		vim.api.nvim_create_user_command("Bd", function(args)
-			require("mini.bufremove").delete(0, not args.bang)
-		end, {
-			bang = true,
-		})
+	-- :Bd[!] for layout-safe bufdelete
+	{
+		"echasnovski/mini.bufremove",
+		event = "VeryLazy",
+		config = function()
+			vim.api.nvim_create_user_command("Bd", function(args)
+				require("mini.bufremove").delete(0, not args.bang)
+			end, {
+				bang = true,
+			})
+		end,
+	},
 
-		-- gc for commenting/uncommenting:
-		require("mini.comment").setup({})
+	-- gc for commenting/uncommenting:
+	{
+		"echasnovski/mini.comment",
+		keys = {
+			{ "gc", mode = { "o", "n", "x" } },
+			{ "gcc", mode = "n" },
+		},
+	},
 
-		require("mini.move").setup({})
+	{
+		"echasnovski/mini.move",
+		keys = {
+			{ "<M-h>", mode = { "n", "v" } },
+			{ "<M-j>", mode = { "n", "v" } },
+			{ "<M-k>", mode = { "n", "v" } },
+			{ "<M-l>", mode = { "n", "v" } },
+		},
+	},
 
-		-- Replace vim-surround:
-		require("mini.surround").setup({
+	-- Replace vim-surround:
+	{
+		"echasnovski/mini.surround",
+		keys = {
+			{ "ys", mode = "n" },
+			{ "yss", mode = "n" },
+			{ "ds", mode = "n" },
+			{ "sf", mode = "n" },
+			{ "sF", mode = "n" },
+			{ "hs", mode = "n" },
+			{ "cs", mode = "n" },
+		},
+		opts = {
 			custom_surroundings = {
 				["q"] = {
 					input = { "“().-()”" },
@@ -278,51 +326,54 @@ return {
 			},
 			n_lines = 50,
 			search_method = "cover_or_next",
-		})
-		-- Remap adding surrounding to Visual mode selection
-		vim.keymap.del("x", "ys")
-		vim.keymap.set("x", "S", [[:<C-u>lua MiniSurround.add('visual')<CR>]], { noremap = true })
-		-- Make special mapping for "add surrounding for line"
-		vim.keymap.set("n", "yss", "ys_", { noremap = false })
+		},
+		config = function(_, opts)
+			require("mini.surround").setup(opts)
+			-- Remap adding surrounding to Visual mode selection
+			vim.keymap.del("x", "ys")
+			vim.keymap.set("x", "S", [[:<C-u>lua MiniSurround.add('visual')<CR>]], { noremap = true })
+			-- Make special mapping for "add surrounding for line"
+			vim.keymap.set("n", "yss", "ys_", { noremap = false })
 
-		--Per-file surroundings:
-		local custom_surroundings = {
-			lua = {
-				s = {
-					input = { "%[%[().-()%]%]" },
-					output = { left = "[[", right = "]]" },
+			--Per-file surroundings:
+			local custom_surroundings = {
+				lua = {
+					s = {
+						input = { "%[%[().-()%]%]" },
+						output = { left = "[[", right = "]]" },
+					},
 				},
-			},
-			markdown = {
-				["B"] = { -- Surround for bold
-					input = { "%*%*().-()%*%*" },
-					output = { left = "**", right = "**" },
+				markdown = {
+					["B"] = { -- Surround for bold
+						input = { "%*%*().-()%*%*" },
+						output = { left = "**", right = "**" },
+					},
+					["I"] = { -- Surround for italics
+						input = { "%*().-()%*" },
+						output = { left = "*", right = "*" },
+					},
+					["L"] = {
+						input = { "%[().-()%]%([^)]+%)" },
+						output = function()
+							local href = require("mini.surround").user_input("Href")
+							return {
+								left = "[",
+								right = "](" .. href .. ")",
+							}
+						end,
+					},
 				},
-				["I"] = { -- Surround for italics
-					input = { "%*().-()%*" },
-					output = { left = "*", right = "*" },
-				},
-				["L"] = {
-					input = { "%[().-()%]%([^)]+%)" },
-					output = function()
-						local href = require("mini.surround").user_input("Href")
-						return {
-							left = "[",
-							right = "](" .. href .. ")",
-						}
-					end,
-				},
-			},
-		}
-		vim.api.nvim_create_autocmd("FileType", {
-			group = "dotfiles-settings",
-			pattern = vim.fn.join(vim.tbl_keys(custom_surroundings), ","),
-			callback = function()
-				local ft = vim.opt.filetype:get()
-				vim.b.minisurround_config = {
-					custom_surroundings = custom_surroundings[ft],
-				}
-			end,
-		})
-	end,
+			}
+			vim.api.nvim_create_autocmd("FileType", {
+				group = "dotfiles-settings",
+				pattern = vim.fn.join(vim.tbl_keys(custom_surroundings), ","),
+				callback = function()
+					local ft = vim.opt.filetype:get()
+					vim.b.minisurround_config = {
+						custom_surroundings = custom_surroundings[ft],
+					}
+				end,
+			})
+		end,
+	},
 }
