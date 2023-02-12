072586f2c629ba25ac02ab569bd0d376905d938b
oncomouse <oncomouse@gmail.com>
2023-02-12 09:57:02 -0600

    Switch to mini.indentscope

--- conf/vim/lua/dotfiles/plugins/core.lua
+++ conf/vim/lua/dotfiles/plugins/core.lua
@@ -142,7 +142,8 @@ return {
 		opts = {
 			show_end_of_line = true,
 			space_char_blankline = " ",
-			show_current_context = true,
+			show_current_context = false,
+			filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
 		},
 	}, -- Mark and highlight indentations
 	{
