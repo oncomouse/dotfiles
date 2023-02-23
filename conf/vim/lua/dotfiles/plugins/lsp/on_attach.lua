7d22a130c6a44af4e589ac549066a8779e8f87fc
oncomouse <oncomouse@gmail.com>
2023-02-23 13:24:45 -0600

    fix: remove and refactor LSP map options

--- conf/vim/lua/dotfiles/plugins/lsp/on_attach.lua
+++ conf/vim/lua/dotfiles/plugins/lsp/on_attach.lua
@@ -28,57 +28,39 @@ local function on_attach(client, buf_num)
 	end
 	-- LSP-specific maps:
 	vim.keymap.set("n", "<leader>s", vim.lsp.buf.document_symbol, {
-		silent = true,
-		noremap = true,
-		buffer = true,
+		buffer = buf_num,
 		desc = "lua vim.lsp.buf.document_symbol()",
 	})
 	vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, {
-		silent = true,
-		noremap = true,
-		buffer = true,
+		buffer = buf_num,
 		desc = "lua vim.lsp.buf.rename()",
 	})
 	vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {
-		silent = true,
-		noremap = true,
-		buffer = true,
+		buffer = buf_num,
 		desc = "lua vim.lsp.buf.definition()",
 	})
 	vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, {
-		silent = true,
-		noremap = true,
-		buffer = true,
+		buffer = buf_num,
 		desc = "lua vim.lsp.buf.declaration()",
 	})
 	vim.keymap.set("n", "<leader>gy", vim.lsp.buf.type_definition, {
-		silent = true,
-		noremap = true,
-		buffer = true,
+		buffer = buf_num,
 		desc = "lua vim.lsp.buf.type_definition()",
 	})
 	vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, {
-		silent = true,
-		noremap = true,
-		buffer = true,
+		buffer = buf_num,
 		desc = "lua vim.lsp.buf.implementation()",
 	})
 	vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {
-		silent = true,
-		noremap = true,
-		buffer = true,
+		buffer = buf_num,
 		desc = "lua vim.lsp.buf.references()",
 	})
 	vim.keymap.set("n", "<leader>gl", vim.lsp.codelens.run, {
-		silent = true,
-		noremap = true,
-		buffer = true,
+		buffer = buf_num,
 		desc = "lua vim.lsp.codelens.run()",
 	})
 	vim.keymap.set("n", "<leader>ga", vim.lsp.buf.code_action, {
-		silent = true,
-		noremap = true,
-		buffer = true,
+		buffer = buf_num,
 		desc = "lua vim.lsp.buf.code_action()",
 	})
 	vim.keymap.set("v", "<leader>ga", function()
@@ -95,19 +77,17 @@ local function on_attach(client, buf_num)
 			},
 		})
 	end, {
-		silent = true,
-		noremap = true,
-		buffer = true,
+		buffer = buf_num,
 		desc = "'<,'>lua vim.lsp.buf.code_action()",
 	})
 	vim.keymap.set("n", "<M-k>", vim.lsp.buf.signature_help, {
-		silent = true,
-		noremap = true,
-		buffer = true,
+		buffer = buf_num,
 		desc = "lua vim.lsp.buf.signature_help()",
 	})
-	local diagnostic_provider = vim.tbl_contains(require("dotfiles.plugins.lsp.servers")[client.name].provides or {}, "diagnostics")
-	local formatting_provider = vim.tbl_contains(require("dotfiles.plugins.lsp.servers")[client.name].provides or {}, "formatting")
+	local diagnostic_provider =
+		vim.tbl_contains(require("dotfiles.plugins.lsp.servers")[client.name].provides or {}, "diagnostics")
+	local formatting_provider =
+		vim.tbl_contains(require("dotfiles.plugins.lsp.servers")[client.name].provides or {}, "formatting")
 	if diagnostic_provider then
 		vim.api.nvim_create_autocmd("DiagnosticChanged,BufEnter", {
 			buffer = buf_num,
@@ -117,15 +97,11 @@ local function on_attach(client, buf_num)
 			end,
 		})
 		vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {
-			silent = true,
-			noremap = true,
-			buffer = true,
+			buffer = buf_num,
 			desc = "lua vim.diagnostic.goto_next()",
 		})
 		vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {
-			silent = true,
-			noremap = true,
-			buffer = true,
+			buffer = buf_num,
 			desc = "lua vim.diagnostic.goto_prev()",
 		})
 	end
@@ -135,7 +111,10 @@ local function on_attach(client, buf_num)
 			vim.lsp.buf.format({
 				async = true,
 				filter = function(c)
-					return vim.tbl_contains(require("dotfiles.plugins.lsp.servers")[c.name].provides or {}, "formatting")
+					return vim.tbl_contains(
+						require("dotfiles.plugins.lsp.servers")[c.name].provides or {},
+						"formatting"
+					)
 				end,
 			})
 		end, {
