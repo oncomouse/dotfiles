eeb654573dbabb5558fe151f250a0439be5f9bfb
oncomouse <oncomouse@gmail.com>
2023-03-23 10:42:41 -0500

    feat: add gx.nvim; disable gx support in vim-markdown

---
+++ conf/vim/lua/dotfiles/plugins/gx-nvim.lua
@@ -0,0 +1,12 @@
+return {
+	"chrishrb/gx.nvim",
+	keys = {
+		{ "gx", mode = { "n", "v" }},
+	},
+	opts = {
+		handlers = {
+			plugin = false,
+			github = false,
+		},
+	},
+} -- Better URL opening, binds to gx
--- conf/vim/lua/dotfiles/plugins/vim-markdown.lua
+++ conf/vim/lua/dotfiles/plugins/vim-markdown.lua
@@ -9,5 +9,20 @@ return {
 		vim.g.vim_markdown_math = 1 -- Do process MathJaX and LaTeX math
 		vim.g.vim_markdown_auto_insert_bullets = 0 -- autoList handles bullet insertion now
 		vim.g.vim_markdown_new_list_item_indent = 0 -- autoList handles spacing for lists
+		vim.g.vim_markdown_no_default_key_mappings = 1 -- Disable keymaps
+	end,
+	config = function()
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "markdown",
+			callback = function()
+				vim.keymap.set({ "n", "v" }, "]]", "<Plug>Markdown_MoveToNextHeader", { buffer = true })
+				vim.keymap.set({ "n", "v" }, "[[", "<Plug>Markdown_MoveToPreviousHeader", { buffer = true })
+				vim.keymap.set({ "n", "v" }, "][", "<Plug>Markdown_MoveToNextSiblingHeader", { buffer = true })
+				vim.keymap.set({ "n", "v" }, "[]", "<Plug>Markdown_MoveToPreviousSiblingHeader", { buffer = true })
+				vim.keymap.set({ "n", "v" }, "]u", "<Plug>Markdown_MoveToParentHeader", { buffer = true })
+				vim.keymap.set({ "n", "v" }, "]h", "<Plug>Markdown_MoveToCurHeader", { buffer = true })
+				vim.keymap.set({ "n", "v" }, "ge", "<Plug>Markdown_EditUrlUnderCursor", { buffer = true })
+			end,
+		})
 	end,
 } -- Markdown Syntax
