3a66d260c4c1f4c1c9c9bb602248bf82d90771a4
oncomouse <oncomouse@gmail.com>
2023-04-05 09:22:16 -0500

    feat: support ie/ae as separate methods

--- conf/vim/plugin/ae.lua
+++ conf/vim/plugin/ae.lua
@@ -1,4 +1,6 @@
-EntireTextobject = function(visual)
+EntireTextobject = {}
+
+EntireTextobject.outer = function(visual)
 	vim.cmd("normal! m'")
 	vim.cmd("keepjumps normal! gg0")
 	if not visual then
@@ -8,7 +10,20 @@ EntireTextobject = function(visual)
 	vim.cmd("keepjumps normal! G$")
 end
 
-vim.keymap.set("o", "ae", "<cmd>call v:lua.EntireTextobject(v:false)<cr>")
-vim.keymap.set("o", "ie", "<cmd>call v:lua.EntireTextobject(v:false)<cr>")
-vim.keymap.set("x", "ae", "<cmd>call v:lua.EntireTextobject(v:true)<cr>")
-vim.keymap.set("x", "ie", "<cmd>call v:lua.EntireTextobject(v:true)<cr>")
+EntireTextobject.inner = function(visual)
+	vim.cmd("normal! m'")
+	vim.cmd("keepjumps normal! gg0")
+	vim.fn.search("^.", "cW")
+	if not visual then
+		vim.cmd("normal! V")
+	end
+	vim.cmd("normal! o")
+	vim.cmd("keepjumps normal! G$")
+	vim.fn.search("^.", "bcW")
+	vim.cmd("normal! $")
+end
+
+vim.keymap.set("o", "ae", "<cmd>call v:lua.EntireTextobject.outer(v:false)<cr>")
+vim.keymap.set("o", "ie", "<cmd>call v:lua.EntireTextobject.inner(v:false)<cr>")
+vim.keymap.set("x", "ae", "<cmd>call v:lua.EntireTextobject.outer(v:true)<cr>")
+vim.keymap.set("x", "ie", "<cmd>call v:lua.EntireTextobject.inner(v:true)<cr>")
