7cbf30b8d16bdb3956e6ba7fca735ec200b85047
oncomouse <oncomouse@gmail.com>
2023-04-05 15:55:08 -0500

    fix: may need this later

--- conf/vim/lua/dotfiles/plugins/mini-nvim.lua
+++ conf/vim/lua/dotfiles/plugins/mini-nvim.lua
@@ -112,7 +112,20 @@ return {
 			return {
 				custom_textobjects = {
 
-					["q"] = {
+					-- e = function() -- Whole buffer
+					-- 	local from = { line = 1, col = 1 }
+					-- 	local last_line_length = #vim.fn.getline("$")
+					-- 	local to = {
+					-- 		line = vim.fn.line("$"),
+					-- 		col = last_line_length == 0 and 1 or last_line_length,
+					-- 	}
+					-- 	return {
+					-- 		from = from,
+					-- 		to = to,
+					-- 	}
+					-- end,
+
+					q = {
 						"“().-()”",
 						"‘().-()’",
 					},
