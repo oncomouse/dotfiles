633f7c5727bc21eb474f417f19000245feea12d3
oncomouse <oncomouse@gmail.com>
2023-04-05 09:19:07 -0500

    feat: working ae/ie textobject

--- conf/vim/lua/dotfiles/plugins/mini-nvim.lua
+++ conf/vim/lua/dotfiles/plugins/mini-nvim.lua
@@ -112,19 +112,6 @@ return {
 			return {
 				custom_textobjects = {
 
-					-- e = function() -- Whole buffer
-					-- 	local from = { line = 1, col = 1 }
-					-- 	local last_line_length = #vim.fn.getline("$")
-					-- 	local to = {
-					-- 		line = vim.fn.line("$"),
-					-- 		col = last_line_length == 0 and 1 or last_line_length,
-					-- 	}
-					-- 	return {
-					-- 		from = from,
-					-- 		to = to,
-					-- 	}
-					-- end,
-
 					["q"] = {
 						"“().-()”",
 						"‘().-()’",
@@ -309,8 +296,8 @@ return {
 					animation = require("mini.indentscope").gen_animation.none(),
 				},
 				mappings = {
-					object_scope = "ii",
-					object_scope_with_border = "ai",
+					object_scope = "",
+					object_scope_with_border = "",
 				},
 			}
 		end,
--- conf/vim/plugin/ae.lua
+++ conf/vim/plugin/ae.lua
@@ -1,4 +1,14 @@
-vim.keymap.set("o", "ae", "ggG$")
-vim.keymap.set("o", "ie", "ggG$")
-vim.keymap.set("v", "ae", "ggoG$")
-vim.keymap.set("v", "ie", "ggoG$")
+EntireTextobject = function(visual)
+	vim.cmd("normal! m'")
+	vim.cmd("keepjumps normal! gg0")
+	if not visual then
+		vim.cmd("normal! V")
+	end
+	vim.cmd("normal! o")
+	vim.cmd("keepjumps normal! G$")
+end
+
+vim.keymap.set("o", "ae", "<cmd>call v:lua.EntireTextobject(v:false)<cr>")
+vim.keymap.set("o", "ie", "<cmd>call v:lua.EntireTextobject(v:false)<cr>")
+vim.keymap.set("x", "ae", "<cmd>call v:lua.EntireTextobject(v:true)<cr>")
+vim.keymap.set("x", "ie", "<cmd>call v:lua.EntireTextobject(v:true)<cr>")
