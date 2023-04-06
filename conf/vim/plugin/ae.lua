baaf37311bb7dad4ea90b8e1df9cd72327552109
oncomouse <oncomouse@gmail.com>
2023-04-06 09:49:01 -0500

    fix: simplify down to one function

--- conf/vim/plugin/ae.lua
+++ conf/vim/plugin/ae.lua
@@ -1,29 +1,23 @@
 EntireTextobject = {}
 
-EntireTextobject.outer = function(visual)
+EntireTextobject = function(visual, inner)
 	vim.cmd("normal! m'")
 	vim.cmd("keepjumps normal! gg0")
+	if inner then
+		vim.fn.search("^.", "cW")
+	end
 	if not visual then
 		vim.cmd("normal! V")
 	end
 	vim.cmd("normal! o")
 	vim.cmd("keepjumps normal! G$")
-end
-
-EntireTextobject.inner = function(visual)
-	vim.cmd("normal! m'")
-	vim.cmd("keepjumps normal! gg0")
-	vim.fn.search("^.", "cW")
-	if not visual then
-		vim.cmd("normal! V")
+	if inner then
+		vim.fn.search("^.", "bcW")
+		vim.cmd("normal! $")
 	end
-	vim.cmd("normal! o")
-	vim.cmd("keepjumps normal! G$")
-	vim.fn.search("^.", "bcW")
-	vim.cmd("normal! $")
 end
 
-vim.keymap.set("o", "ae", "<cmd>lua EntireTextobject.outer(false)<cr>")
-vim.keymap.set("o", "ie", "<cmd>lua EntireTextobject.inner(false)<cr>")
-vim.keymap.set("x", "ae", "<cmd>lua EntireTextobject.outer(true)<cr>")
-vim.keymap.set("x", "ie", "<cmd>lua EntireTextobject.inner(true)<cr>")
+vim.keymap.set("o", "ae", "<cmd>lua EntireTextobject(false)<cr>")
+vim.keymap.set("o", "ie", "<cmd>lua EntireTextobject(false, true)<cr>")
+vim.keymap.set("x", "ae", "<cmd>lua EntireTextobject(true)<cr>")
+vim.keymap.set("x", "ie", "<cmd>lua EntireTextobject(true, true)<cr>")
