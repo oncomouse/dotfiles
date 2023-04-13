383bd776dde83fe4641a1847c319d53a68c24ceb
oncomouse <oncomouse@gmail.com>
2023-04-13 13:06:07 -0500

    fix: load markdown maps here

--- conf/vim/ftplugin/text.lua
+++ conf/vim/ftplugin/text.lua
@@ -3,3 +3,7 @@ vim.opt_local.linebreak = true
 vim.opt_local.list = false
 vim.opt_local.spell = true
 vim.opt_local.commentstring = "# %s"
+
+-- Load markdown maps:
+vim.b.markdown_nvim_unordered_default = "-"
+require("markdown").setup()
