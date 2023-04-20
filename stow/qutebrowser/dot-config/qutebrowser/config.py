1a7377251b7353a016da8604c10a113a8bc46679
oncomouse <oncomouse@gmail.com>
2023-04-20 15:14:49 -0500

    fix: editor

--- stow/qutebrowser/dot-config/qutebrowser/config.py
+++ stow/qutebrowser/dot-config/qutebrowser/config.py
@@ -4,3 +4,4 @@ config.bind(";M", "spawn --detach mpv {url}")
 config.bind("<Ctrl+Shift+u>", "spawn --userscript qute-bitwarden")
 config.bind("J", "tab-prev")
 config.bind("K", "tab-next")
+c.editor.command = ["st", "-e", "nvim", "-f", "{}"]
