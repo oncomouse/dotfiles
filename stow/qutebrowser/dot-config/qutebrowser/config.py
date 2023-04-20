b7b8082f80f366c4cde510a3b0d8aafcb2635a20
oncomouse <oncomouse@gmail.com>
2023-04-20 14:43:16 -0500

    fix: more qutebrowser settings

--- stow/qutebrowser/dot-config/qutebrowser/config.py
+++ stow/qutebrowser/dot-config/qutebrowser/config.py
@@ -1,2 +1,4 @@
 config.load_autoconfig()
 config.bind("<Ctrl+Shift+u>", "spawn --userscript qute-bitwarden")
+config.set("auto_save.session", True)
+config.set("content.headers.accept_language", "en-US,en;q=0.5")
