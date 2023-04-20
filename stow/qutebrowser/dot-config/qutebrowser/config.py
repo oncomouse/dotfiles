8d7fff99d4eefe5b98b1303145a3b586a0204971
oncomouse <oncomouse@gmail.com>
2023-04-20 15:16:59 -0500

    feat: pinboard bookmarks

--- stow/qutebrowser/dot-config/qutebrowser/config.py
+++ stow/qutebrowser/dot-config/qutebrowser/config.py
@@ -4,4 +4,9 @@ config.bind(";M", "spawn --detach mpv {url}")
 config.bind("<Ctrl+Shift+u>", "spawn --userscript qute-bitwarden")
 config.bind("J", "tab-prev")
 config.bind("K", "tab-next")
+config.bind(
+    "<Ctrl-p>",
+    "jseval document.location='https://pinboard.in/add?next=same&url='+encodeURIComponent(location.href)+'&title='+encodeURIComponent(document.title)",
+    mode="normal",
+)
 c.editor.command = ["st", "-e", "nvim", "-f", "{}"]
