c739db42841730178e809909027dd65a3f3eee1f
oncomouse <oncomouse@gmail.com>
2023-05-12 09:38:48 -0500

    fix: Signs work with new Nerd Font

--- conf/vim/init.lua
+++ conf/vim/init.lua
@@ -398,7 +398,7 @@ vim.keymap.set("n", "<C-W>S", "<cmd>vsplit<cr>")
 vim.keymap.set("n", "<leader>b", "<cmd>b#<cr>", { desc = "Jump to last buffer" })
 -- }}}
 -- Signs {{{
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
 	local hl = "DiagnosticSign" .. type
 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
