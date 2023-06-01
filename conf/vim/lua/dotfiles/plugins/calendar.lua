783ece001b07c4605c1ec45d57f4985a4a9e9abf
oncomouse <oncomouse@gmail.com>
2023-06-01 09:53:38 -0500

    fix: Wrap in pcall() in case credentials are unavailable

--- conf/vim/lua/dotfiles/plugins/calendar.lua
+++ conf/vim/lua/dotfiles/plugins/calendar.lua
@@ -1,9 +1,9 @@
 return {
 	"itchyny/calendar.vim",
 	init = function()
-		require("dotfiles.credentials")
+		pcall(require, "dotfiles.credentials")
 		vim.g.calendar_google_calendar = 1
 		vim.g.calendar_google_task = 1
 	end,
-	cmd = "Calendar"
+	cmd = "Calendar",
 }
