7abf6548ee6068e7e02b07f4e857f145c9133434
oncomouse <oncomouse@gmail.com>
2023-06-02 10:59:54 -0500

    fix: I don't use google tasks

--- conf/vim/lua/dotfiles/plugins/calendar.lua
+++ conf/vim/lua/dotfiles/plugins/calendar.lua
@@ -3,7 +3,7 @@ return {
 	init = function()
 		pcall(require, "dotfiles.credentials")
 		vim.g.calendar_google_calendar = 1
-		vim.g.calendar_google_task = 1
+		vim.g.calendar_google_task = 0
 	end,
 	cmd = "Calendar",
 }
