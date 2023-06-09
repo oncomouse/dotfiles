8e3049ba3eac3370e3f1878d7cf2ae035b4bec1a
oncomouse <oncomouse@gmail.com>
2023-06-09 09:46:52 -0500

    feat: treesitter-playground

--- conf/vim/lua/dotfiles/plugins/nvim-treesitter.lua
+++ conf/vim/lua/dotfiles/plugins/nvim-treesitter.lua
@@ -300,6 +300,24 @@ return {
 			},
 			autotag = { enable = true },
 			matchup = { enable = true },
+			playground = {
+				enable = true,
+				disable = {},
+				updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+				persist_queries = false, -- Whether the query persists across vim sessions
+				keybindings = {
+					toggle_query_editor = "o",
+					toggle_hl_groups = "i",
+					toggle_injected_languages = "t",
+					toggle_anonymous_nodes = "a",
+					toggle_language_display = "I",
+					focus_language = "f",
+					unfocus_language = "F",
+					update = "R",
+					goto_node = "<cr>",
+					show_help = "?",
+				},
+			},
 		},
 		config = function(_, opts)
 			nvim_treesitter.setup_parser_config()
@@ -338,5 +356,9 @@ return {
 				end,
 			},
 		},
+	},
+	{
+		"nvim-treesitter/playground",
+		cmd = "TSPlaygroundToggle",
 	},
 }
