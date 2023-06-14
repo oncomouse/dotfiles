c9fc1c8484ea032b0f60edc8f87750ef3cfe1927
oncomouse <oncomouse@gmail.com>
2023-06-14 14:22:54 -0500

    fix: correct spacing/indenting for org files

--- conf/vim/ftplugin/org.lua
+++ conf/vim/ftplugin/org.lua
@@ -1,2 +1,7 @@
 -- Why isn't this in nvim-orgmode?
 vim.keymap.set("i", "<M-CR>", "<c-o><leader><cr>", { buffer = true, remap = true })
+
+vim.opt_local.tabstop = 2
+vim.opt_local.shiftwidth = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.expandtab = true
