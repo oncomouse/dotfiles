6a199bd9a8659f14d046b81fa2234631f2c733eb
oncomouse <oncomouse@gmail.com>
2023-06-16 14:27:21 -0500

    feat: virtual text wordcount in Markdown documents

--- conf/vim/lua/dotfiles/plugins/core.lua
+++ conf/vim/lua/dotfiles/plugins/core.lua
@@ -133,4 +133,22 @@ return {
 		dev = false,
 		ft = "markdown",
 	},
+
+	{
+		"dimfeld/section-wordcount.nvim",
+		lazy = true,
+		opts = {
+			highlight = "String",
+			virt_text_pos = "eol",
+		},
+		init = function()
+			local ag = vim.api.nvim_create_augroup("dotfiles-wordcount-autocmd", { clear = true })
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "markdown",
+				group = ag,
+				callback = function() require("section-wordcount").wordcounter() end,
+			})
+		end
+	},
 }
