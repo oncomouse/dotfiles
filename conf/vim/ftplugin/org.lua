b8d62477c466973fa7db8bcc9e7a531717d73c30
oncomouse <oncomouse@gmail.com>
2023-06-16 14:42:08 -0500

    fix: better <</>> for org

--- conf/vim/ftplugin/org.lua
+++ conf/vim/ftplugin/org.lua
@@ -1,7 +1,33 @@
--- Why isn't this in nvim-orgmode?
-vim.keymap.set("i", "<M-CR>", "<c-o><leader><cr>", { buffer = true, remap = true })
-
+-- Set indent:
 vim.opt_local.tabstop = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.softtabstop = 2
 vim.opt_local.expandtab = true
+
+-- Do a better >>/<< for my editing style
+local translation = {
+	["<<"] = "promote",
+	[">>"] = "demote",
+}
+local function do_demote_promote(symbol)
+	local line = vim.api.nvim_get_current_line()
+	if line:match("^%*+") then
+		require("orgmode").action("org_mappings.do_" .. translation[symbol])
+	else
+		vim.cmd("exe 'normal! " .. vim.v.count1 .. symbol .. "'")
+	end
+end
+vim.keymap.set("n", "<<", function()
+	do_demote_promote("<<")
+end, {
+	buffer = true,
+})
+vim.keymap.set("n", ">>", function()
+	do_demote_promote(">>")
+end, {
+	buffer = true,
+})
+
+-- Map org-mode's <M-CR> behavior into nvim-orgmode
+vim.keymap.set("i", "<M-CR>", "<c-o><leader><cr>", { buffer = true, remap = true })
+
