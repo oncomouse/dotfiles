fbcd6e37d122af603f0e31d3c8a4f186c741e56f
oncomouse <oncomouse@gmail.com>
2023-06-22 16:30:50 -0500

    feat: sync Google Calendar with org-mode

---
+++ scripts/ical2org.js
@@ -0,0 +1,48 @@
+const fs = require('fs')
+const os = require('os')
+const exec = require('child_process').execSync
+const URL = require('./url')
+
+const OUTFILE = resolveTilde('~/SeaDrive/My Libraries/Todo/inbox.org')
+
+function resolveTilde (filePath) {
+  if (!filePath || typeof filePath !== 'string') {
+    return ''
+  }
+  // '~/folder/path' or '~' not '~alias/folder/path'
+  if (filePath.startsWith('~/') || filePath === '~') {
+    return filePath.replace('~', os.homedir())
+  }
+  return filePath
+}
+
+async function isExecutable (name) {
+  try {
+    exec(name)
+    return true
+  } catch (err) {
+    return false
+  }
+}
+
+const hasIcal2OrgPy = isExecutable('ical2orgpy 2> /dev/null')
+if (!hasIcal2OrgPy) {
+  exec('pipx install ical2orgpy')
+}
+const events = exec(`curl -s "${URL}" | ical2orgpy - -`)
+const cacheFile = `${process.env.HOME}/.cache/ical2org`
+exec(`touch "${cacheFile}"`)
+exec(`touch "${OUTFILE}"`)
+const cache = fs
+  .readFileSync(cacheFile)
+  .toString()
+  .trim()
+  .split(/\n/)
+events.toString().split(/\n\n\*/g).forEach(async function (event) {
+  event = `*${event}`
+  const md5hash = exec(`echo "${event}" | md5sum`).toString().trim().split(/\s+/)[0]
+  if (cache.indexOf(md5hash) < 0) {
+    fs.appendFileSync(cacheFile, `${md5hash}\n`)
+    fs.appendFileSync(OUTFILE, `${event}\n\n`)
+  }
+})
