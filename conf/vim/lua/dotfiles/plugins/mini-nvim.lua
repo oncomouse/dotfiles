2181b15f493fb8f0639be392981cab992640fab3
oncomouse <oncomouse@gmail.com>
2023-06-23 12:59:08 -0500

    feat: support mini.files

--- conf/vim/lua/dotfiles/plugins/mini-nvim.lua
+++ conf/vim/lua/dotfiles/plugins/mini-nvim.lua
@@ -400,6 +400,61 @@ return {
 			require("mini.comment").setup(opts)
 		end,
 	},
+	{
+		"echasnovski/mini.files",
+		opts = {
+			windows = {
+				preview = true,
+			},
+			options = {
+				-- Whether to use for editing directories
+				-- Disabled by default in LazyVim because neo-tree is used for that
+				use_as_default_explorer = true,
+			},
+		},
+		keys = {
+			{
+				"<leader>fm",
+				function()
+					require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+				end,
+				desc = "Open mini.files (directory of current file)",
+			},
+			{
+				"<leader>fM",
+				function()
+					require("mini.files").open(vim.loop.cwd(), true)
+				end,
+				desc = "Open mini.files (cwd)",
+			},
+		},
+		config = function(_, opts)
+			require("mini.files").setup(opts)
+
+			local show_dotfiles = true
+			local filter_show = function(fs_entry)
+				return true
+			end
+			local filter_hide = function(fs_entry)
+				return not vim.startswith(fs_entry.name, ".")
+			end
+
+			local toggle_dotfiles = function()
+				show_dotfiles = not show_dotfiles
+				local new_filter = show_dotfiles and filter_show or filter_hide
+				require("mini.files").refresh({ content = { filter = new_filter } })
+			end
+
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "MiniFilesBufferCreate",
+				callback = function(args)
+					local buf_id = args.data.buf_id
+					-- Tweak left-hand side of mapping to your liking
+					vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id })
+				end,
+			})
+		end,
+	},
 	-- Show current indentation context:
 	{
 		"echasnovski/mini.indentscope",
