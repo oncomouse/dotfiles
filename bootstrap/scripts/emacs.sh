d4ec6fbf7433c22cc5c3f919e30d20ad4802566c
oncomouse <oncomouse@gmail.com>
2023-07-10 12:58:22 -0500

    fix: working

--- bootstrap/scripts/emacs.sh
+++ bootstrap/scripts/emacs.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-stow -R emacs
-git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
+/usr/bin/env python "$HOME/dotfiles/scripts/stow.py" -d "$HOME/dotfiles/stow" -t "$HOME" --no-folding --dotfiles --overwrite  -R emacs
+if [ ! -d ~/.config/emacs ]; then
+  git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
+fi
 PATH="$HOME/.config/emacs/bin:$PATH"
 doom sync
 doom env
