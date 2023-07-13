d4a523958d22bee48e910fa6d7ed8063f2e2b125
oncomouse <oncomouse@gmail.com>
2023-07-13 11:36:48 -0500

    fix: simplify file structure

--- conf/vim/after/plugin/luasnip.lua
+++ conf/vim/after/plugin/luasnip.lua
@@ -1,8 +1,59 @@
--- vim.api.nvim_create_autocmd({ "BufNewFile", "BufEnter"}, {
--- 	pattern = "*",
--- 	group = vim.api.nvim_create_augroup("luasnip-lsp-augroup", {}),
--- 	callback = function()
--- 		require("luasnip.lsp")
--- 	end
--- })
-require("luasnip.lsp")
+local Lsp = require("lspize")
+
+local function get_documentation(snip, data)
+	local header = (snip.name or "") .. " _ `[" .. data.filetype .. "]`\n"
+	local docstring = { "", "```" .. vim.bo.filetype, snip:get_docstring(), "```" }
+	local documentation = { header .. "---", (snip.dscr or ""), docstring }
+	documentation = require("vim.lsp.util").convert_input_to_markdown_lines(documentation)
+	return table.concat(documentation, "\n")
+end
+
+local handlers = {
+	[Lsp.methods.COMPLETION] = function(params, done)
+		local curline = vim.fn.line(".")
+		local line, col = unpack(vim.fn.searchpos([[\k*]], "bcn"))
+		if line ~= curline then
+			done()
+			return
+		end
+		local word_to_complete = vim.api.nvim_get_current_line():sub(col - 1, params.position.character)
+		local filetypes = require("luasnip.util.util").get_snippet_filetypes()
+		local items = {}
+
+		for i = 1, #filetypes do
+			local ft = filetypes[i]
+			local ft_table = require("luasnip").get_snippets(ft)
+			if ft_table then
+				for j, snip in pairs(ft_table) do
+					local data = {
+						type = "luasnip",
+						filetype = ft,
+						ft_indx = j,
+						snip_id = snip.id,
+						show_condition = snip.show_condition,
+					}
+					if not snip.hidden then
+						items[#items + 1] = {
+							word = snip.trigger,
+							label = snip.trigger,
+							detail = snip.description,
+							kind = vim.lsp.protocol.CompletionItemKind.Snippet,
+							data = data,
+							documentation = {
+								value = get_documentation(snip, data),
+								kind = vim.lsp.protocol.MarkupKind.Markdown,
+							},
+						}
+					end
+				end
+			end
+		end
+		local line_to_cursor = require("luasnip.util.util").get_current_line_to_cursor()
+		items = vim.tbl_filter(function(item)
+			return vim.startswith(item.word, word_to_complete) and item.data.show_condition(line_to_cursor)
+		end, items)
+		pcall(done, nil, items)
+	end,
+}
+
+Lsp:new(handlers)
