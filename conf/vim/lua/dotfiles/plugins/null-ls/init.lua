5269db7752b88c461fafbd1a89632cde598ee878
oncomouse <oncomouse@gmail.com>
2023-07-14 13:24:05 -0500

    fix: null-ls is now only for diagnostics and code actions

--- conf/vim/lua/dotfiles/plugins/null-ls/init.lua
+++ conf/vim/lua/dotfiles/plugins/null-ls/init.lua
@@ -2,55 +2,21 @@ return {
 	{
 		"jose-elias-alvarez/null-ls.nvim",
 		ft = {
-			"bash",
-			"css",
-			"fish",
 			"html",
 			"javascript",
 			"javascript.react",
-			"json",
-			"jsonc",
-			"less",
 			"lua",
-			"markdown",
 			"py",
-			"ruby",
-			"scss",
 			"sh",
-			"svelte",
-			"tex",
 			"typescript",
 			"typescript.react",
 			"vim",
-			"vue",
-			"yaml",
 		},
 		opts = function()
 			local eslint_project = require("dotfiles.null-ls.helpers.eslint_project")
 			local b = require("null-ls").builtins
 
 			local sources = {
-				-- LUA
-				b.diagnostics.selene.with({
-					cwd = function(_params)
-						return vim.fs.dirname(
-							vim.fs.find({ "selene.toml" }, { upward = true, path = vim.api.nvim_buf_get_name(0) })[1]
-						) or vim.fn.expand("~/.config/selene/") -- fallback value
-					end,
-				}),
-
-				-- PYTHON
-				b.diagnostics.flake8,
-
-				-- SHELL
-				b.diagnostics.shellcheck.with({
-					diagnostics_format = "#{m} [#{c}]",
-				}),
-				b.code_actions.shellcheck,
-
-				-- VIML
-				b.diagnostics.vint,
-
 				-- HTML
 				require("dotfiles.null-ls.builtins.diagnostics.htmlhint"),
 
@@ -71,6 +37,27 @@ return {
 						return eslint_project()
 					end,
 				}),
+
+				-- LUA
+				b.diagnostics.selene.with({
+					cwd = function(_params)
+						return vim.fs.dirname(
+							vim.fs.find({ "selene.toml" }, { upward = true, path = vim.api.nvim_buf_get_name(0) })[1]
+						) or vim.fn.expand("~/.config/selene/") -- fallback value
+					end,
+				}),
+
+				-- PYTHON
+				b.diagnostics.flake8,
+
+				-- SHELL
+				b.diagnostics.shellcheck.with({
+					diagnostics_format = "#{m} [#{c}]",
+				}),
+				b.code_actions.shellcheck,
+
+				-- VIML
+				b.diagnostics.vint,
 			}
 
 			return {
