c8fd1bcef0e3a76ad636a53f0c12a26fb01802cd
oncomouse <oncomouse@gmail.com>
2023-08-07 11:10:25 -0500

    fix: listitems now update properly

--- conf/vim/ftplugin/org.lua
+++ conf/vim/ftplugin/org.lua
@@ -38,63 +38,78 @@ vim.keymap.set("i", "<M-CR>", "<cmd>lua require('orgmode').action('org_mappings.
 -- Use old todo manager mapping for checkbox toggles:
 vim.keymap.set("n", "gtd", "<cmd>lua require('orgmode').action('org_mappings.toggle_checkbox')<cr>", { buffer = true })
 
+
 -- Update cookie statistics:
 vim.keymap.set("n", "<leader>o#", function()
+	local ts_utils = require("nvim-treesitter.ts_utils")
 	local tree_utils = require("orgmode.utils.treesitter")
+
+	local current_node = tree_utils.get_node_at_cursor()
+	local listitem = tree_utils.find_parent_type(current_node, "list")
+	if listitem then
+		if listitem:parent() and listitem:parent():type() == "listitem" then
+			listitem = listitem:parent()
+		else
+			listitem = nil
+		end
+	end
 	local headline = tree_utils.closest_headline()
+
+	local target
+	if listitem then
+		local Listitem = require("orgmode.treesitter.listitem")
+		target = Listitem:new(listitem)
+		target:update_checkbox("children")
+		return
+	end
 	if headline then
-		local ts_utils = require("nvim-treesitter.ts_utils")
-		local ts = require("orgmode.treesitter.compat")
 		local Headline = require("orgmode.treesitter.headline")
+		target = Headline:new(headline)
+		local cookie = target:cookie()
+
 		local total = 0
 		local done = 0
-		local target = Headline:new(headline)
-		local cookie = target:cookie()
-		if not cookie then
-			local parent_section = target.headline:parent():parent()
-			if parent_section:child(0):type() == "headline" then
-				local parent_headline = Headline:new(parent_section:child(0))
-				cookie = parent_headline:cookie()
-				if cookie ~= nil then
-					target = parent_headline
-				else
-					return nil
-				end
-			else
-				return nil
-			end
-		end
-
+		local node_type
 		-- Parse the children of the headline's parent section for child headlines and lists:
 		for _, node in pairs(ts_utils.get_named_children(tree_utils.find_parent_type(target.headline, "section"))) do
 			-- The child is a list:
 			if node:type() == "body" and node:child(0):type() == "list" then
-				local total_boxes = target:child_checkboxes(node:child(0))
-				local checked_boxes = vim.tbl_filter(function(box)
-					return box:match("%[%w%]")
-				end, total_boxes)
-				total = total + #total_boxes
-				done = done + #checked_boxes
+				if not node_type then
+					node_type = "list"
+				end
+				if node_type == "list" then
+					local total_boxes = target:child_checkboxes(node:child(0))
+					local checked_boxes = vim.tbl_filter(function(box)
+						return box:match("%[%w%]")
+					end, total_boxes)
+					total = total + #total_boxes
+					done = done + #checked_boxes
+				end
 			end
 			-- The child is a section:
 			if node:type() == "section" and node:child(0):type() == "headline" then
-				local hl = Headline:new(node:child(0))
-				local _, word, is_done = hl:todo()
-				if word ~= nil then
-					total = total + 1
+				if not node_type then
+					node_type = "headline"
 				end
-				if is_done then
-					done = done + 1
+				if node_type == "headline" then
+					local hl = Headline:new(node:child(0))
+					local _, word, is_done = hl:todo()
+					if word ~= nil then
+						total = total + 1
+					end
+					if is_done then
+						done = done + 1
+					end
 				end
 			end
-		end
 
-		local new_cookie_val
-		if ts.get_node_text(cookie, 0):find("%%") then
-			new_cookie_val = ("[%d%%]"):format((total == 0 and 0 or done / total) * 100)
-		else
-			new_cookie_val = ("[%d/%d]"):format(done, total)
+			local new_cookie_val
+			if vim.treesitter.get_node_text(cookie, 0):find("%%") then
+				new_cookie_val = ("[%d%%]"):format((total == 0 and 0 or done / total) * 100)
+			else
+				new_cookie_val = ("[%d/%d]"):format(done, total)
+			end
+			tree_utils.set_node_text(cookie, new_cookie_val)
 		end
-		tree_utils.set_node_text(cookie, new_cookie_val)
 	end
 end, { buffer = true })
