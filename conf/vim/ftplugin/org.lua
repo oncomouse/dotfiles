045392c0e36ad33bfb8d1cab4ad66b50e0bf1d5e
oncomouse <oncomouse@gmail.com>
2023-08-09 09:25:40 -0500

    feat: update `Headline:update_cookie()` with new logic

--- conf/vim/ftplugin/org.lua
+++ conf/vim/ftplugin/org.lua
@@ -40,11 +40,15 @@ vim.keymap.set("n", "gtd", "<cmd>lua require('orgmode').action('org_mappings.tog
 
 local Listitem = require("orgmode.treesitter.listitem")
 local tree_utils = require("orgmode.utils.treesitter")
+local Headline = require("orgmode.treesitter.headline")
+local ts_utils = require("nvim-treesitter.ts_utils")
+local ts = require("orgmode.treesitter.compat")
+
 function Listitem:update_cookie(total_child_checkboxes, checked_child_checkboxes)
 	local cookie = self:cookie()
 	if cookie then
 		local new_cookie_val
-		local old_cookie_val = vim.treesitter.get_node_text(cookie, 0)
+		local old_cookie_val = ts.get_node_text(cookie, 0)
 		if old_cookie_val:find("%%") then
 			new_cookie_val = ("[%d%%]"):format((#checked_child_checkboxes / #total_child_checkboxes) * 100)
 		else
@@ -56,45 +60,31 @@ function Listitem:update_cookie(total_child_checkboxes, checked_child_checkboxes
 	end
 end
 
--- Update cookie statistics:
-vim.keymap.set("n", "<leader>o#", function()
-	local ts_utils = require("nvim-treesitter.ts_utils")
-
-	local current_node = tree_utils.get_node_at_cursor()
-	local is_ok, listitem = pcall(tree_utils.find_parent_type, current_node, "list")
-	if is_ok and type(listitem) == "userdata" and listitem:parent() and listitem:parent():type() == "listitem" then
-		listitem = listitem:parent()
-	else
-		listitem = nil
-	end
-	local headline = tree_utils.closest_headline()
-
-	local target
-	if listitem then
-		target = Listitem:new(listitem)
-		target:update_checkbox("children")
+function Headline:update_cookie(list_node)
+	local done = 0
+	local total = 0
+	local cookie = self:cookie()
+	if not cookie then
 		return
 	end
-	if headline then
-		local Headline = require("orgmode.treesitter.headline")
-		target = Headline:new(headline)
-		local cookie = target:cookie()
-		if not cookie then
-			return
-		end
-
-		local total = 0
-		local done = 0
+	if type(list_node) == "userdata" then
+		local total_boxes = self:child_checkboxes(list_node)
+		local checked_boxes = vim.tbl_filter(function(box)
+			return box:match("%[%w%]")
+		end, total_boxes)
+		total = total + #total_boxes
+		done = done + #checked_boxes
+	else
 		local node_type
 		-- Parse the children of the headline's parent section for child headlines and lists:
-		for _, node in pairs(ts_utils.get_named_children(tree_utils.find_parent_type(target.headline, "section"))) do
+		for _, node in pairs(ts_utils.get_named_children(tree_utils.find_parent_type(self.headline, "section"))) do
 			-- The child is a list:
 			if node:type() == "body" and node:child(0):type() == "list" then
 				if not node_type then
 					node_type = "list"
 				end
 				if node_type == "list" then
-					local total_boxes = target:child_checkboxes(node:child(0))
+					local total_boxes = self:child_checkboxes(node:child(0))
 					local checked_boxes = vim.tbl_filter(function(box)
 						return box:match("%[%w%]")
 					end, total_boxes)
@@ -118,14 +108,39 @@ vim.keymap.set("n", "<leader>o#", function()
 					end
 				end
 			end
-
-			local new_cookie_val
-			if vim.treesitter.get_node_text(cookie, 0):find("%%") then
-				new_cookie_val = ("[%d%%]"):format((total == 0 and 0 or done / total) * 100)
-			else
-				new_cookie_val = ("[%d/%d]"):format(done, total)
-			end
+		end
+		local old_cookie_val = ts.get_node_text(cookie, 0)
+		local new_cookie_val
+		if ts.get_node_text(cookie, 0):find("%%") then
+			new_cookie_val = ("[%d%%]"):format((total == 0 and 0 or done / total) * 100)
+		else
+			new_cookie_val = ("[%d/%d]"):format(done, total)
+		end
+		if old_cookie_val ~= new_cookie_val then
 			tree_utils.set_node_text(cookie, new_cookie_val)
 		end
+	end
+end
+
+-- Update cookie statistics:
+vim.keymap.set("n", "<leader>o#", function()
+	local current_node = tree_utils.get_node_at_cursor()
+	local is_ok, listitem = pcall(tree_utils.find_parent_type, current_node, "list")
+	if is_ok and type(listitem) == "userdata" and listitem:parent() and listitem:parent():type() == "listitem" then
+		listitem = listitem:parent()
+	else
+		listitem = nil
+	end
+	local headline = tree_utils.closest_headline()
+
+	local target
+	if listitem then
+		target = Listitem:new(listitem)
+		target:update_checkbox("children")
+		return
+	end
+	if headline then
+		target = Headline:new(headline)
+		target:update_cookie()
 	end
 end, { buffer = true })
