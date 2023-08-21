5e0d123c1c5e15c7ad8e6f5d1dd6870695f2dc88
oncomouse <oncomouse@gmail.com>
2023-08-21 09:59:13 -0500

    fix: didn't work, but trying a simpler dial.nvim config

--- conf/vim/lua/dotfiles/plugins/dial-nvim.lua
+++ conf/vim/lua/dotfiles/plugins/dial-nvim.lua
@@ -1,12 +1,10 @@
 return {
 	"monaqa/dial.nvim",
 	keys = {
-		{ "<C-a>", "<Plug>(dial-increment)", mode = "n" },
-		{ "<C-x>", "<Plug>(dial-decrement)", mode = "n" },
-		{ "<C-a>", "<Plug>(dial-increment)", mode = "v" },
-		{ "<C-x>", "<Plug>(dial-decrement)", mode = "v" },
-		{ "g<C-a>", "g<Plug>(dial-increment)", mode = "v" },
-		{ "g<C-x>", "g<Plug>(dial-decrement)", mode = "v" },
+		{ "<C-a>", "<Plug>(dial-increment)", mode = { "n", "v" } },
+		{ "<C-x>", "<Plug>(dial-decrement)", mode = { "n", "v" } },
+		{ "g<C-a>", "g<Plug>(dial-increment)", mode = { "n", "v" } },
+		{ "g<C-x>", "g<Plug>(dial-decrement)", mode = { "n", "v" } },
 	},
 	config = function()
 		local augend = require("dial.augend")
