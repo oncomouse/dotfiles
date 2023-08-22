f240881db3185d591270380061f3142a50a1d3e0
oncomouse <oncomouse@gmail.com>
2023-08-22 11:10:10 -0500

    feat: Proper date range formatting

--- conf/vim/lua/dotfiles/plugins/orgmode.lua
+++ conf/vim/lua/dotfiles/plugins/orgmode.lua
@@ -25,6 +25,8 @@ return {
 		},
 	},
 	config = function(_, opts)
+		-- Just export the stupid class:
+		local Date = getmetatable(require("orgmode.objects.date").now())
 		local Listitem = require("orgmode.treesitter.listitem")
 		local tree_utils = require("orgmode.utils.treesitter")
 		local Headline = require("orgmode.treesitter.headline")
@@ -33,6 +35,45 @@ return {
 		local OrgMappings = require("orgmode.org.mappings")
 
 		-- OVERRIDES
+		-- Support for proper date ranges
+		local time_format = "%H:%M"
+		local date_format = "%Y-%m-%d"
+		function Date:to_string()
+			local date = ""
+			local format = date_format
+			if self.dayname then
+				format = format .. " %a"
+			end
+
+			date = os.date(format, self.timestamp)
+			if not self.date_only then
+				date = date .. " " .. self:format_time()
+			end
+
+			if #self.adjustments > 0 then
+				date = date .. " " .. table.concat(self.adjustments, " ")
+			end
+
+			return date
+		end
+		function Date:format_time()
+			if self.date_only then
+				return ""
+			end
+			local t = self:format(time_format)
+			if self.timestamp_end then
+				t = t .. "-" .. os.date(time_format, self.timestamp_end)
+			elseif
+				self.related_date_range
+				and self.related_date_range.is_date_range_end
+				and self:is_same(self.related_date_range, "day")
+			then
+				t = t .. "-" .. os.date(time_format, self.related_date_range.timestamp)
+			end
+			return t
+		end
+		-- End support for proper date ranges
+
 		-- Support for org-update-statistics-cookie
 		function Listitem:update_cookie(total_child_checkboxes, checked_child_checkboxes)
 			local cookie = self:cookie()
