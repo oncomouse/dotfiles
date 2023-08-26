ec84d626d0a35faee5d5c3daeb2a77627fdb1306
oncomouse <oncomouse@gmail.com>
2023-08-26 17:11:45 -0500

    fix: properly support snippets

--- conf/vim/lua/dotfiles/lsp/start_server.lua
+++ conf/vim/lua/dotfiles/lsp/start_server.lua
@@ -4,7 +4,28 @@ local function start_server(server)
 		local config = vim.g.dotfiles_lsp[server] or {}
 		config.autostart = false
 		__running[server] = true
-		require("lspconfig")[server].setup(config)
+		local function get_server_capabilities(server)
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			if server.snippets then
+				capabilities.textDocument.completion.completionItem.snippetSupport = true
+				capabilities.textDocument.completion.completionItem.resolveSupport = {
+					properties = {
+						"documentation",
+						"detail",
+						"additionalTextEdits",
+					},
+				}
+			end
+			return capabilities
+		end
+
+		local opts = {
+			capabilities = get_server_capabilities(vim.g.dotfiles_lsp[server]),
+		}
+		if #vim.tbl_keys(config) > 0 then
+			opts = vim.tbl_extend("keep", opts, config)
+		end
+		require("lspconfig")[server].setup(opts)
 		require("lspconfig")[server].launch()
 	end
 end
