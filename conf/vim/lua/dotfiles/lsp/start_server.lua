1d4ab5aba9028ef8a5e78757d9d7334b9a1503dd
oncomouse <oncomouse@gmail.com>
2023-08-27 12:31:18 -0500

    fix: cleaned up and simplified starting

--- conf/vim/lua/dotfiles/lsp/start_server.lua
+++ conf/vim/lua/dotfiles/lsp/start_server.lua
@@ -1,32 +1,33 @@
+-- Track which servers have already started:
 local __running = {}
+
+local function get_server_capabilities(server_config)
+	local capabilities = vim.lsp.protocol.make_client_capabilities()
+	if server_config.snippets then
+		capabilities.textDocument.completion.completionItem.snippetSupport = true
+		capabilities.textDocument.completion.completionItem.resolveSupport = {
+			properties = {
+				"documentation",
+				"detail",
+				"additionalTextEdits",
+			},
+		}
+	end
+	-- See: https://github.com/neovim/neovim/issues/23291
+	capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = false
+	return capabilities
+end
+
 local function start_server(server)
-	if vim.g.dotfiles_lsp[server] and not __running[server] then
+	if not __running[server] then
 		local config = vim.g.dotfiles_lsp[server] or {}
 		config.autostart = false
 		__running[server] = true
-		local function get_server_capabilities(server_config)
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			if server_config.snippets then
-				capabilities.textDocument.completion.completionItem.snippetSupport = true
-				capabilities.textDocument.completion.completionItem.resolveSupport = {
-					properties = {
-						"documentation",
-						"detail",
-						"additionalTextEdits",
-					},
-				}
-			end
-			-- See: https://github.com/neovim/neovim/issues/23291
-			capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = false
-			return capabilities
-		end
 
-		local opts = {
+		local opts = vim.tbl_extend("keep", {
 			capabilities = get_server_capabilities(vim.g.dotfiles_lsp[server]),
-		}
-		if #vim.tbl_keys(config) > 0 then
-			opts = vim.tbl_extend("keep", opts, config)
-		end
+		}, config)
+
 		require("lspconfig")[server].setup(opts)
 		require("lspconfig")[server].launch()
 	end
