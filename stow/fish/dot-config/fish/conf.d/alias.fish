c9af54520f9e84614bdf47db87a3f08e6448c833
oncomouse <oncomouse@gmail.com>
2023-09-08 09:13:11 -0500

    fix: exa or eza for ls alias

--- stow/fish/dot-config/fish/conf.d/alias.fish
+++ stow/fish/dot-config/fish/conf.d/alias.fish
@@ -4,7 +4,7 @@ function ag; /usr/bin/env ag --path-to-ignore ~/.ignore --hidden $argv; end
 function cat;bat --paging=never --theme=wal $argv;end
 function icat;kitty +kitten icat $argv; end
 function top;htop $argv;end
-function ls;exa -g --group-directories-first $argv;end
+function ls;eval (if command -sq exa; echo "exa"; else; echo "eza"; end) -g --group-directories-first $argv;end
 function wal;eval (if command -sq python3; echo "python3"; else; echo "python"; end) $HOME/dotfiles/scripts/wal/custom_wal.py $argv;end
 function stow;eval (if command -sq python3; echo "python3"; else; echo "python"; end) $HOME/dotfiles/scripts/stow.py -d "$HOME/dotfiles/stow" -t "$HOME" --no-folding --dotfiles $argv;end
 # Vim is Neovim in server mode:
