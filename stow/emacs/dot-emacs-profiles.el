41e966438886251c0a9bb7ec9535ca290fd491c2
oncomouse <oncomouse@gmail.com>
2023-09-18 12:03:48 -0500

    fix: purcell emacs.d

--- stow/emacs/dot-emacs-profiles.el
+++ stow/emacs/dot-emacs-profiles.el
@@ -1,2 +1,4 @@
 (("basic" . ((user-emacs-directory . "~/.emacs.configs/basic")))
+ ("purcell" . ((user-emacs-directory . "~/.emacs.configs/purcell")))
  ("default" . ((user-emacs-directory . "~/.emacs.configs/doom"))))
+
