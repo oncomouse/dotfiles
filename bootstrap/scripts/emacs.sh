ea5b54308e9250e24ea833472842994c41b7f842
oncomouse <oncomouse@gmail.com>
2023-09-19 11:02:49 -0500

    fix: fixes for purcell

--- bootstrap/scripts/emacs.sh
+++ bootstrap/scripts/emacs.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 
-/usr/bin/env python "$HOME/dotfiles/scripts/stow.py" -d "$HOME/dotfiles/stow" -t "$HOME" --no-folding --dotfiles --overwrite  -R emacs
 # Install Chemacs2:
 git clone https://github.com/plexus/chemacs2.git ~/.emacs.d
 
-# Install Doom for Chemacs2:
+# Configure Chemacs2:
 mkdir -p ~/.emacs.configs
+
+# Install Doom for Chemacs2:
 if [ ! -d ~/.emacs.configs/doom ]; then
   git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.configs/doom
 fi
+
+# Install Purcell for Chemacs2:
+if [ ! -d ~/.emacs.configs/purcell ]; then
+  git clone https://github.com/purcell/emacs.d ~/.emacs.configs/purcell
+fi
+
+# Stow our configuration
+  /usr/bin/env python "$HOME/dotfiles/scripts/stow.py" -d "$HOME/dotfiles/stow" -t "$HOME" --no-folding --dotfiles --overwrite  -R emacs
+
+# Install doom
 ~/.emacs.configs/doom/bin/doom sync
 ~/.emacs.configs/doom/bin/doom env
--- stow/emacs/dot-emacs.configs/purcell/lisp/init-local.el
+++ stow/emacs/dot-emacs.configs/purcell/lisp/init-local.el
@@ -2,12 +2,4 @@
   (setq mac-command-modifier 'none)
   (setq mac-option-modifier 'meta))
 
-; Load Catppuccin
-; (package! catppuccin
-;   :recipe (:host github :repo "catppuccin/emacs"))
-(download-site-lisp-module 'catppuccin-theme "https://raw.githubusercontent.com/catppuccin/emacs/main/catppuccin-theme.el")
-(setq catppuccin-flavor 'mocha)
-(load-theme 'catppuccin :no-confirm)
-
-
 (provide 'init-local)
