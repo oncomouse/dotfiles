ae76e44ec214f81c3eead982c269ef4b231fbebb
oncomouse <oncomouse@gmail.com>
2023-09-19 09:51:24 -0500

    feat: add ivy, counsel, swiper, and projectile

--- stow/emacs/dot-emacs.configs/basic/init.el
+++ stow/emacs/dot-emacs.configs/basic/init.el
@@ -1,4 +1,5 @@
 (defconst *is-a-mac* (eq system-type 'darwin))
+
 ; Install straight.el
 (defvar bootstrap-version)
 (let ((bootstrap-file
@@ -31,8 +32,48 @@
 (setq use-dialog-box nil)
 (setq inhibit-startup-screen t)
 
+(straight-use-package 'corfu)
+(global-corfu-mode)
+(straight-use-package 'prescient)
+(straight-use-package 'corfu-prescient)
+(corfu-prescient-mode)
+
 (when *is-a-mac*
   (when (straight-use-package 'ns-auto-titlebar)
     (ns-auto-titlebar-mode)))
+
+(straight-use-package 'ivy)
+(straight-use-package 'counsel)
+(straight-use-package 'swiper)
+(ivy-mode)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+;; enable this if you want `swiper' to use it
+;; (setq search-default-mode #'char-fold-to-regexp)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "<f6>") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> o") 'counsel-describe-symbol)
+(global-set-key (kbd "<f1> l") 'counsel-find-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+(global-set-key (kbd "C-c g") 'counsel-git)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-x l") 'counsel-locate)
+(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
+
+
+(straight-use-package 'projectile)
+(projectile-mode +1)
+;; Recommended keymap prefix on macOS
+(when *is-a-mac* (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map))
+;; Recommended keymap prefix on Windows/Linux
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
 (provide 'init)
