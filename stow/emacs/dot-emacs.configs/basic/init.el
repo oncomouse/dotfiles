b8cd876a2c9ea70bca34f84646dbdbe70d1ba4d7
oncomouse <oncomouse@gmail.com>
2023-09-19 09:59:52 -0500

    fix: ripgrep all the things

--- stow/emacs/dot-emacs.configs/basic/init.el
+++ stow/emacs/dot-emacs.configs/basic/init.el
@@ -80,7 +80,7 @@
 (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
 (global-set-key (kbd "C-c g") 'counsel-git)
 (global-set-key (kbd "C-c j") 'counsel-git-grep)
-(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-c k") 'counsel-rg)
 (global-set-key (kbd "C-x l") 'counsel-locate)
 (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
 (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
