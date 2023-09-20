1b9753cac01fe47691dd93b81c55696adc9f2f56
oncomouse <oncomouse@gmail.com>
2023-09-20 14:16:50 -0500

    fix: add paredit

--- stow/emacs/dot-emacs.configs/basic/init.el
+++ stow/emacs/dot-emacs.configs/basic/init.el
@@ -105,4 +105,13 @@
 (global-tree-sitter-mode)
 (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
 
+(straight-use-package 'paredit)
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+
 (provide 'init)
