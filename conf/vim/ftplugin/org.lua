9f0d230115f2ff82387c395c35fd59889b7376d0
oncomouse <oncomouse@gmail.com>
2023-09-26 09:12:56 -0500

    feat: autogenerate EC minutes

--- conf/vim/ftplugin/org.lua
+++ conf/vim/ftplugin/org.lua
@@ -33,10 +33,43 @@ end
 
 -- Map org-mode's <M-CR> behavior into nvim-orgmode
 vim.keymap.set("i", "<M-CR>", "<cmd>lua require('orgmode').action('org_mappings.handle_return')<cr>", { buffer = true })
-vim.keymap.set("i", "<C-c>.", "<cmd>lua require('orgmode').action('org_mappings.insertmode_date')<cr>", { buffer = true })
-vim.keymap.set("i", "<C-c>!", "<cmd>lua require('orgmode').action('org_mappings.insertmode_date','true')<cr>", { buffer = true })
+vim.keymap.set(
+	"i",
+	"<C-c>.",
+	"<cmd>lua require('orgmode').action('org_mappings.insertmode_date')<cr>",
+	{ buffer = true }
+)
+vim.keymap.set(
+	"i",
+	"<C-c>!",
+	"<cmd>lua require('orgmode').action('org_mappings.insertmode_date','true')<cr>",
+	{ buffer = true }
+)
 
 -- Load markdown.nvim for org:
 vim.b.markdown_nvim_unordered_list_chars = "[+-]"
 vim.b.markdown_nvim_unordered_default = "+"
 require("markdown").setup()
+
+vim.api.nvim_buf_create_user_command(0, "ECMinutes", function()
+	local bn = vim.api.nvim_buf_get_name(0)
+	if type(bn) == "string" and string.match(bn, "meetings.*executive") then
+		vim.cmd(
+			string.format(
+				"!pandoc -i %s -o %s/ec-minutes/ec-minutes-%s.docx",
+				vim.fn.fnameescape(vim.fn.expand("%")),
+				vim.fn.fnameescape(vim.fn.expand("%:h")),
+				os.date("%Y-%m-%d")
+			)
+		)
+		vim.print(
+			string.format(
+				"Created: %s/ec-minutes/ec-minutes-%s.docx",
+				vim.fn.fnameescape(vim.fn.expand("%:h")),
+				os.date("%Y-%m-%d")
+			)
+		)
+	end
+end, {
+	force = true,
+})
