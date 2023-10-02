605d7fb1aab2e36882c12723ca8f93ac664a1f1d
oncomouse <oncomouse@gmail.com>
2023-10-02 10:15:22 -0500

    fix: compound arguments

--- stow/fish/dot-config/fish/conf.d/alias.fish
+++ stow/fish/dot-config/fish/conf.d/alias.fish
@@ -8,7 +8,7 @@ function cat
 end
 function ls
   if test (command -sq exa) -o (command -sq eza)
-    eval (if command -sq exa; echo "exa"; else; echo "eza"; end) -g --group-directories-first $argv
+    eval (if command -sq exa; echo "exa"; else; echo "eza"; end) -g --group-directories-first (string escape -- $argv)
   else
     ls $argv
   end
