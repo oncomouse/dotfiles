97d44e8688d9c791f6c26c58ee83d157440a81cc
oncomouse <oncomouse@gmail.com>
2023-10-03 14:42:08 -0500

    fix: one more with multiple spaces

--- conf/vim/lua/dotfiles/plugins/lexima.lua
+++ conf/vim/lua/dotfiles/plugins/lexima.lua
@@ -302,7 +302,7 @@ return {
 		-- Links:
 		add_rule({
 			char = "[",
-			at = [[^\s*[0-9]\.\s\+\[ \] \%#]],
+			at = [[^\s*[0-9]\.\s\+\[ \]\s\+\%#]],
 			input = "<BS><Left><BS>[",
 			input_after = "]",
 			filetype = "org",
