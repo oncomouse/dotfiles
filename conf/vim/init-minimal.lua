12c279e5c6dce940cadf6bf590915432bf6af104
oncomouse <oncomouse@gmail.com>
2023-10-17 10:10:45 -0500

    feat: mini.clue support for minimal

--- conf/vim/init-minimal.lua
+++ conf/vim/init-minimal.lua
@@ -536,6 +536,50 @@ end, {
 	bang = true,
 })
 
+-- Use mini.clue for assisting with keybindings:
+require("mini.clue").setup({
+	triggers = {
+		-- Leader triggers
+		{ mode = "n", keys = "<Leader>" },
+		{ mode = "x", keys = "<Leader>" },
+
+		-- Built-in completion
+		{ mode = "i", keys = "<C-x>" },
+
+		-- `g` key
+		{ mode = "n", keys = "g" },
+		{ mode = "x", keys = "g" },
+
+		-- Marks
+		{ mode = "n", keys = "'" },
+		{ mode = "n", keys = "`" },
+		{ mode = "x", keys = "'" },
+		{ mode = "x", keys = "`" },
+
+		-- Registers
+		{ mode = "n", keys = '"' },
+		{ mode = "x", keys = '"' },
+		{ mode = "i", keys = "<C-r>" },
+		{ mode = "c", keys = "<C-r>" },
+
+		-- Window commands
+		{ mode = "n", keys = "<C-w>" },
+
+		-- `z` key
+		{ mode = "n", keys = "z" },
+		{ mode = "x", keys = "z" },
+	},
+	clues = {
+		-- Enhance this by adding descriptions for <Leader> mapping groups
+		require("mini.clue").gen_clues.builtin_completion(),
+		require("mini.clue").gen_clues.g(),
+		require("mini.clue").gen_clues.marks(),
+		require("mini.clue").gen_clues.registers(),
+		require("mini.clue").gen_clues.windows(),
+		require("mini.clue").gen_clues.z(),
+	},
+})
+
 -- gc for commenting/uncommenting:
 require("mini.comment").setup({})
 
@@ -664,12 +708,7 @@ vim.keymap.set("n", "<C-P>", function()
 	MiniPick.builtin.files({}, { source = { choose = open_files, choose_marked = open_multiple_files } })
 end, { desc = "Files" })
 vim.keymap.set("n", "<leader>a", "<cmd>Pick buffers sort_lastused=true<cr>", { desc = "Buffers" })
-vim.keymap.set(
-	{ "n", "v" },
-	"<leader>*",
-	"<cmd>Pick grep pattern='<cword>'<cr>",
-	{ desc = "Search current word" }
-)
+vim.keymap.set({ "n", "v" }, "<leader>*", "<cmd>Pick grep pattern='<cword>'<cr>", { desc = "Search current word" })
 
 require("mini.statusline").setup({
 	content = {
