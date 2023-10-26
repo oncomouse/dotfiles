97dad3992928a7cf114f12ab070b155d1432f7e3
oncomouse <oncomouse@gmail.com>
2023-10-26 09:14:34 -0500

    Working global line numbers

--- stow/emacs/dot-emacs.configs/basic/init.el
+++ stow/emacs/dot-emacs.configs/basic/init.el
@@ -9,6 +9,8 @@
 ;; Line numbers + relative line numbers
 (display-line-numbers-mode)
 (setq display-line-numbers 'relative)
+(setq display-line-numbers-type 'relative)
+(setq global-linum-mode t)
 
 ; https://stackoverflow.com/questions/29934968/how-to-remove-the-temporary-files-starting-and-ending-with-created-by-emacs
 (setq backup-directory-alist
@@ -337,3 +339,15 @@
 (use-package magit)
 
 (provide 'init)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(global-display-line-numbers-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
