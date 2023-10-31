b8d5841d45a899b513774378a5d3da08bfde2123
oncomouse <oncomouse@gmail.com>
2023-10-31 14:32:48 -0500

    feat: kind-icons + no-littering

--- conf/emacs/init.el
+++ conf/emacs/init.el
@@ -17,12 +17,6 @@
     eshell-mode-hook))
     (add-hook mode (lambda() (display-line-numbers-mode 0))))
 
-; https://stackoverflow.com/questions/29934968/how-to-remove-the-temporary-files-starting-and-ending-with-created-by-emacs
-(setq backup-directory-alist
-    `((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms
-    `((".*" ,temporary-file-directory t)))
-
 (setq completions-detailed t)
 
 ;; Tabs
@@ -45,12 +39,15 @@
   :demand t
   :config
   (general-evil-setup))
-(elpaca-wait)
 
 (use-package diminish
   :config
   (diminish flyspell-mode)
   (diminish eldoc-mode))
+
+(use-package no-littering
+  :config
+  (no-littering-theme-backups))
 (elpaca-wait)
 
 ;; Recent Files
@@ -242,6 +239,9 @@
   :custom
   (corfu-cycle t)
   (corfu-separator ?\s)
+  (corfu-quit-at-boundary 'separator)
+  (corfu-quit-no-match 'separator)
+  (corfu-preview-current 'insert)
   :init
   (global-corfu-mode)
   :config
@@ -252,8 +252,11 @@
    "C-x C-o" 'completion-at-point)
   (general-define-key
    :keymaps 'corfu-map
+   "SPC" #'corfu-insert-separator
    "C-y" 'corfu-insert
-   "C-c" 'corfu-quit))
+   "C-c" 'corfu-quit)
+  (advice-add 'corfu--setup :after 'evil-normalize-keymaps)
+  (advice-add 'corfu--teardown :after 'evil-normalize-keymaps))
 
 (use-package cape
   :config
@@ -266,6 +269,30 @@
    "C-f" 'cape-file
    "C-l" 'cape-line
    "C-k" 'cape-dict))
+(use-package kind-icon
+  :after corfu
+  :custom
+  (kind-icon-use-icons t)
+  (kind-icon-default-face 'corfu-default) ; Have background color be the same as `corfu' face background
+  (kind-icon-blend-background nil)  ; Use midpoint color between foreground and background colors ("blended")?
+  (kind-icon-blend-frac 0.08)
+
+  ;; NOTE 2022-02-05: `kind-icon' depends `svg-lib' which creates a cache
+  ;; directory that defaults to the `user-emacs-directory'. Here, I change that
+  ;; directory to a location appropriate to `no-littering' conventions, a
+  ;; package which moves directories of other packages to sane locations.
+  (svg-lib-icons-dir (no-littering-expand-var-file-name "svg-lib/cache/")) ; Change cache dir
+  :config
+  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter) ; Enable `kind-icon'
+
+  ;; Add hook to reset cache so the icon colors match my theme
+  ;; NOTE 2022-02-05: This is a hook which resets the cache whenever I switch
+  ;; the theme using my custom defined command for switching themes. If I don't
+  ;; do this, then the backgound color will remain the same, meaning it will not
+  ;; match the background color corresponding to the current theme. Important
+  ;; since I have a light theme and dark theme I switch between. This has no
+  ;; function unless you use something similar
+  (add-hook 'kb/themes-hooks #'(lambda () (interactive) (kind-icon-reset-cache))))
 
 (use-package markdown-mode)
 (use-package lua-mode
