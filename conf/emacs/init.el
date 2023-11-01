37eb9d328d03ed0922b3e1bd263fa8b647ca0352
oncomouse <oncomouse@gmail.com>
2023-11-01 15:45:34 -0500

    feat: use nerd-icons for corfu

--- conf/emacs/init.el
+++ conf/emacs/init.el
@@ -257,30 +257,12 @@
    "C-f" 'cape-file
    "C-l" 'cape-line
    "C-k" 'cape-dict))
-(use-package kind-icon
-  :after corfu
-  :custom
-  (kind-icon-use-icons t)
-  (kind-icon-default-face 'corfu-default) ; Have background color be the same as `corfu' face background
-  (kind-icon-blend-background nil)  ; Use midpoint color between foreground and background colors ("blended")?
-  (kind-icon-blend-frac 0.08)
 
-  ;; NOTE 2022-02-05: `kind-icon' depends `svg-lib' which creates a cache
-  ;; directory that defaults to the `user-emacs-directory'. Here, I change that
-  ;; directory to a location appropriate to `no-littering' conventions, a
-  ;; package which moves directories of other packages to sane locations.
-  (svg-lib-icons-dir (no-littering-expand-var-file-name "svg-lib/cache/")) ; Change cache dir
+(use-package nerd-icons)
+(use-package nerd-icons-corfu
+  :after (nerd-icons corfu)
   :config
-  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter) ; Enable `kind-icon'
-
-  ;; Add hook to reset cache so the icon colors match my theme
-  ;; NOTE 2022-02-05: This is a hook which resets the cache whenever I switch
-  ;; the theme using my custom defined command for switching themes. If I don't
-  ;; do this, then the backgound color will remain the same, meaning it will not
-  ;; match the background color corresponding to the current theme. Important
-  ;; since I have a light theme and dark theme I switch between. This has no
-  ;; function unless you use something similar
-  (add-hook 'kb/themes-hooks #'(lambda () (interactive) (kind-icon-reset-cache))))
+  (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
 
 (use-package markdown-mode)
 
@@ -295,6 +277,7 @@
   :after citar embark
   :no-require
   :config (citar-embark-mode))
+
 
 (use-package lua-mode
   :init
