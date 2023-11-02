b7547ec16d385b9b158d7d4dd32015a0292fdfce
oncomouse <oncomouse@gmail.com>
2023-11-02 11:58:38 -0500

    fix: Don't use mf for mf.lua

--- conf/vim/plugin/mf.lua
+++ conf/vim/plugin/mf.lua
@@ -7,15 +7,3 @@ end, {
 	complete = "dir",
 	nargs = 1,
 })
-
--- Auto create dir when saving a file, in case some intermediate directory does not exist
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  group = vim.api.nvim_create_augroup("auto_create_dir", { clear = true }),
-  callback = function(event)
-    if event.match:match("^%w%w+://") then
-      return
-    end
-    local file = vim.loop.fs_realpath(event.match) or event.match
-    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
-  end,
-})
