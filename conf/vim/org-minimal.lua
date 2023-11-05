6491e05b270226f328150b66a2b87a0173959db7
oncomouse <oncomouse@gmail.com>
2023-11-05 08:42:38 -0600

    fix: more in-line orgmode minimal init

--- conf/vim/org-minimal.lua
+++ conf/vim/org-minimal.lua
@@ -1,46 +1,46 @@
 -- Set Leader:
 vim.g.mapleader = " "
-local lazypath = os.getenv("HOME") .. "/.local/share/lazy-org/lazy.nvim"
+local nvim_root = "/tmp/nvim_orgmode"
+local lazy_root = nvim_root .. "/lazy"
+local lazypath = lazy_root .. "/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
-  root = vim.fn.stdpath("data") .. "/lazy-org",
-  dev = {
-    path = vim.fn.expand("~/Projects"),
-  },
-  spec = {
-    {
-      "nvim-orgmode/orgmode",
-      dev = true,
-      opts = {
-        org_default_notes_file = "~/org/inbox.org",
-      },
-      config = function(_, opts)
-        require("orgmode").setup_ts_grammar()
-        require("nvim-treesitter.configs").setup({
-          highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = { "org" },
-          },
-          ensure_installed = { "org" },
-        })
-        require("orgmode").setup(opts)
-      end,
-      dependencies = {
-        {
-          "nvim-treesitter/nvim-treesitter",
-        },
-      },
-    },
-  },
+	root = lazy_root,
+	lockfile = nvim_root .. "/lazy.json",
+	dev = true,
+	opts = {
+		org_default_notes_file = "~/org/inbox.org",
+	},
+	spec = {
+		{
+			"nvim-orgmode/orgmode",
+			config = function(_, opts)
+				require("orgmode").setup_ts_grammar()
+				require("nvim-treesitter.configs").setup({
+					highlight = {
+						enable = true,
+						additional_vim_regex_highlighting = { "org" },
+					},
+					ensure_installed = { "org" },
+				})
+				require("orgmode").setup(opts)
+			end,
+			dependencies = {
+				{
+					"nvim-treesitter/nvim-treesitter",
+				},
+			},
+		},
+	},
 })
 -- # vim:ts=2:sw=2:sts=2:expandtab
