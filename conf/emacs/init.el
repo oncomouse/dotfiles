2d2b288e277623af87daf181de4207dce85ace5b
oncomouse <oncomouse@gmail.com>
2023-11-07 10:09:13 -0600

    fix: update org todo state on a couple of conditions

--- conf/emacs/init.el
+++ conf/emacs/init.el
@@ -384,6 +384,40 @@
                                        #'org-appear-manual-stop
                                        nil
                                        t))))
+(with-eval-after-load 'org
+  (defun dotfiles/org-summary-todo (n-done n-not-done)
+    "Switch entry to DONE when all subentries are done, to TODO otherwise."
+    (let (org-log-done org-log-states)   ; turn off logging
+      (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
+
+  (add-hook 'org-after-todo-statistics-hook 'dotfiles/org-summary-todo)
+
+  (defun dotfiles/org-checkbox-todo ()
+    "Switch header TODO state to DONE when all checkboxes are ticked, to TODO otherwise"
+    (let ((todo-state (org-get-todo-state)) beg end)
+      (unless (not todo-state)
+	(save-excursion
+	  (org-back-to-heading t)
+	  (setq beg (point))
+	  (end-of-line)
+	  (setq end (point))
+	  (goto-char beg)
+	  (if (re-search-forward "\\[\\([0-9]*%\\)\\]\\|\\[\\([0-9]*\\)/\\([0-9]*\\)\\]"
+				 end t)
+              (if (match-end 1)
+		  (if (equal (match-string 1) "100%")
+		      (unless (string-equal todo-state "DONE")
+			(org-todo 'done))
+		    (unless (string-equal todo-state "TODO")
+		      (org-todo 'todo)))
+		(if (and (> (match-end 2) (match-beginning 2))
+			 (equal (match-string 2) (match-string 3)))
+		    (unless (string-equal todo-state "DONE")
+		      (org-todo 'done))
+		  (unless (string-equal todo-state "TODO")
+		    (org-todo 'todo)))))))))
+
+  (add-hook 'org-checkbox-statistics-hook 'dotfiles/org-checkbox-todo))
 
 ;; Writeroom mode for distraction-free writing
 (use-package writeroom-mode
