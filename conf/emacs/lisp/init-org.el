441b2dfaff98b3cfac3b242fc01a509ae9bb408f
oncomouse <oncomouse@gmail.com>
2023-11-09 14:53:14 -0600

    fix: these functions should be in init-org

--- conf/emacs/lisp/init-org.el
+++ conf/emacs/lisp/init-org.el
@@ -1,6 +1,36 @@
 ;;; init-org.el -- Configuration for org-mode
 ;;; Commentary:
 ;;; Code:
+(defun dotfiles/org-summary-todo (n-done n-not-done)
+  "Switch entry to DONE when all subentries are done, to TODO otherwise."
+  (let (org-log-done org-log-states)   ; turn off logging
+    (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
+
+(defun dotfiles/org-checkbox-todo ()
+  "Switch header TODO state to DONE when all checkboxes are ticked, to TODO otherwise"
+  (let ((todo-state (org-get-todo-state)) beg end)
+    (unless (not todo-state)
+      (save-excursion
+	(org-back-to-heading t)
+	(setq beg (point))
+	(end-of-line)
+	(setq end (point))
+	(goto-char beg)
+	(if (re-search-forward "\\[\\([0-9]*%\\)\\]\\|\\[\\([0-9]*\\)/\\([0-9]*\\)\\]"
+			       end t)
+            (if (match-end 1)
+		(if (equal (match-string 1) "100%")
+		    (unless (string-equal todo-state "DONE")
+		      (org-todo 'done))
+		  (unless (string-equal todo-state "TODO")
+		    (org-todo 'todo)))
+	      (if (and (> (match-end 2) (match-beginning 2))
+		       (equal (match-string 2) (match-string 3)))
+		  (unless (string-equal todo-state "DONE")
+		    (org-todo 'done))
+		(unless (string-equal todo-state "TODO")
+		  (org-todo 'todo)))))))))
+
 (use-package org
   :elpaca nil
   :hook
