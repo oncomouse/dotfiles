b730213916facde14b209998277c0860cc4e9dd8
oncomouse <oncomouse@gmail.com>
2023-11-30 15:31:30 -0600

    fix: this doesn't (yet) work as well, but it pairs asterisks for org-mode

--- conf/doom/config.el
+++ conf/doom/config.el
@@ -274,6 +274,19 @@ Don't close any open windows."
         "C-d" (lambda () (interactive) (vertico-next vertico-count))
         "C-u" (lambda () (interactive) (vertico-previous vertico-count))))
 
+;; (defun dotfiles/sp-not-in-cookie-p (_id _action _context)
+;;   (message (format "%s" (sp--looking-back "[[]" 1)))
+;;   (not (sp--looking-back "[[]" 1)))
+
+(after! smartparens
+  (sp-with-modes 'org-mode
+    (sp-local-pair "*" "*"
+                   :unless '(sp-point-at-bol-p))
+    (sp-local-pair "/" "/"
+                   :post-handlers '(("[d1]" "SPC"))
+                   :unless '(sp-point-after-word-p)
+                   :actions '(insert autoskip wrap navigate))))
+
 ;; Add nerd-icons to completion
 (use-package! nerd-icons-completion
   :after (marginalia nerd-icons)
@@ -295,55 +308,55 @@ Don't close any open windows."
 ;; for anything) going.
 ;;
 ;; TODO: figure out how data strcutures in lisp work and use datastrcutures to do all this:
-(defun general-electric/match-before-point (pattern)
-  (string-match pattern (buffer-substring (line-beginning-position) (point))))
-(defun general-electric/match-after-point (pattern)
-  (string-match pattern (buffer-substring (point) (line-end-position))))
-(defun general-electric/match-around-point (before after)
-  (and (general-electric/match-after-point after) (general-electric/match-before-point before)))
-(defun dotfiles/general-electric (&rest r)
-  "Function to advice self-insert-command on matters of the heart"
-  (catch 'rule-match
-    (dotfiles/general-electric--impl r)))
-(defun dotfiles/general-electric--impl (&rest r)
-  (save-excursion
-    (let ((kv (this-command-keys-vector)))
-      (when (length> kv 0)
-        (let ((n (nth 0 r))
-              (c (aref kv (1- (length kv)))))
-          (cond
-           ;; Handle *
-           ((equal c ?*)
-            (when
-                ;; Org mode only
-                (memq major-mode '(org-mode))
-              (cond
-               ;; Do nothing if we're at the start of a line:
-               ((general-electric/match-around-point "^\**$" "^$") (throw 'rule-match t))
-               ;; Jump to the end if we're already in a pair:
-               ((general-electric/match-around-point "\*[^* ]*$" "\*")
-                (delete-char 1)
-                (right-char)
-                (throw 'rule-match t))
-               ))
-            ;; Autocomplete pair
-            (when (memq major-mode '(markdown-mode org-mode)))
-              (cond
+;; (defun general-electric/match-before-point (pattern)
+;;   (string-match pattern (buffer-substring (line-beginning-position) (point))))
+;; (defun general-electric/match-after-point (pattern)
+;;   (string-match pattern (buffer-substring (point) (line-end-position))))
+;; (defun general-electric/match-around-point (before after)
+;;   (and (general-electric/match-after-point after) (general-electric/match-before-point before)))
+;; (defun dotfiles/general-electric (&rest r)
+;;   "Function to advice self-insert-command on matters of the heart"
+;;   (catch 'rule-match
+;;     (dotfiles/general-electric--impl r)))
+;; (defun dotfiles/general-electric--impl (&rest r)
+;;   (save-excursion
+;;     (let ((kv (this-command-keys-vector)))
+;;       (when (length> kv 0)
+;;         (let ((n (nth 0 r))
+;;               (c (aref kv (1- (length kv)))))
+;;           (cond
+;;            ;; Handle *
+;;            ((equal c ?*)
+;;             (when
+;;                 ;; Org mode only
+;;                 (memq major-mode '(org-mode))
+;;               (cond
+;;                ;; Do nothing if we're at the start of a line:
+;;                ((general-electric/match-around-point "^\**$" "^$") (throw 'rule-match t))
+;;                ;; Jump to the end if we're already in a pair:
+;;                ((general-electric/match-around-point "\*[^* ]*$" "\*")
+;;                 (delete-char 1)
+;;                 (right-char)
+;;                 (throw 'rule-match t))
+;;                ))
+;;             ;; Autocomplete pair
+;;             (when (memq major-mode '(markdown-mode org-mode)))
+;;             (cond
 
-               (t (insert "*") (left-char)(throw 'rule-match t)))
-              )
-           ((equal c ?\[)
-            (when
-                (memq major-mode '(org-mode))
-              (cond
-               ((general-electric/match-before-point "^\s*[+]\s+[[]$")(insert " ")(right-char)(throw 'rule-match t))
-               )
-              )
-            )
+;;              (t (insert "*") (left-char)(throw 'rule-match t)))
+;;             )
+;;            ((equal c ?\[)
+;;             (when
+;;                 (memq major-mode '(org-mode))
+;;               (cond
+;;                ((general-electric/match-before-point "^\s*[+]\s+[[]$")(insert " ")(right-char)(throw 'rule-match t))
+;;                )
+;;               )
+;;             )
 
-           )
-          )))))
-(advice-add 'self-insert-command :after 'dotfiles/general-electric)
+;;            )
+;;           )))))
+;; (advice-add 'self-insert-command :after 'dotfiles/general-electric)
 
 ;; Configure treesit-auto
 (use-package! treesit-auto
