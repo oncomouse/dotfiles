12cf2ebc8f4b181cbadeb973add02e3e0fecf7d6
oncomouse <oncomouse@gmail.com>
2024-03-11 16:58:21 -0500

    fix: unused packages

--- stow/doom/dot-config/doom/init.el
+++ stow/doom/dot-config/doom/init.el
@@ -92,11 +92,11 @@
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
        ;;docker
-       editorconfig      ; let someone else argue about tabs vs spaces
+       ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
-       lookup              ; navigate your code and its documentation
+       ;; lookup              ; navigate your code and its documentation
        lsp               ; M-x vscode
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
