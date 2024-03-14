a8c09abdb6333c8ee6f30baca65957f7a61ba2e2
oncomouse <oncomouse@gmail.com>
2024-03-14 13:53:55 -0500

    fix: format

--- conf/emacs/init.el
+++ conf/emacs/init.el
@@ -635,65 +635,65 @@ If not in a clock, move to next headline."
 
   (global-set-key (kbd "C-h v") #'helpful-variable)
   (global-set-key (kbd "C-h k") #'helpful-key)
-  (global-set-key (kbd "C-h x") #'helpful-command))
+  (global-set-key (kbd "C-h x") #'helpful-command)
+  
+  ;; Source: https://github.com/Wilfred/helpful/issues/250
+  (defvar *helpful-buffer-ring-size 5
+    "How many buffers are stored for use with `*helpful-next'.")
 
-;; Source: https://github.com/Wilfred/helpful/issues/250
-(defvar *helpful-buffer-ring-size 5
-  "How many buffers are stored for use with `*helpful-next'.")
+  (defvar *helpful--buffer-ring (make-ring *helpful-buffer-ring-size)
+    "Ring that stores the current Helpful buffer history.")
 
-(defvar *helpful--buffer-ring (make-ring *helpful-buffer-ring-size)
-  "Ring that stores the current Helpful buffer history.")
+  (defun *helpful--buffer-index (&optional buffer)
+    "If BUFFER is a Helpful buffer, return it’s index in the buffer ring."
+    (let ((buf (or buffer (current-buffer))))
+      (and (eq (buffer-local-value 'major-mode buf) 'helpful-mode)
+           (seq-position (ring-elements *helpful--buffer-ring) buf #'eq))))
 
-(defun *helpful--buffer-index (&optional buffer)
-  "If BUFFER is a Helpful buffer, return it’s index in the buffer ring."
-  (let ((buf (or buffer (current-buffer))))
-    (and (eq (buffer-local-value 'major-mode buf) 'helpful-mode)
-         (seq-position (ring-elements *helpful--buffer-ring) buf #'eq))))
+  (advice-add 'helpful--buffer
+              :filter-return (lambda (help-buf)
+                               (let ((buf-ring *helpful--buffer-ring))
+                                 (let ((newer-buffers (or (*helpful--buffer-index) 0)))
+                                   (dotimes (_ newer-buffers) (ring-remove buf-ring 0)))
+                                 (when (/= (ring-size buf-ring) *helpful-buffer-ring-size)
+                                   (ring-resize buf-ring *helpful-buffer-ring-size))
+                                 (ring-insert buf-ring help-buf))))
 
-(advice-add 'helpful--buffer
-            :filter-return (lambda (help-buf)
-                             (let ((buf-ring *helpful--buffer-ring))
-                               (let ((newer-buffers (or (*helpful--buffer-index) 0)))
-                                 (dotimes (_ newer-buffers) (ring-remove buf-ring 0)))
-                               (when (/= (ring-size buf-ring) *helpful-buffer-ring-size)
-                                 (ring-resize buf-ring *helpful-buffer-ring-size))
-                               (ring-insert buf-ring help-buf))))
+  (defun *helpful--next (&optional buffer)
+    "Return the next live Helpful buffer relative to BUFFER."
+    (let ((buf-ring *helpful--buffer-ring)
+          (index (or (*helpful--buffer-index buffer) -1)))
+      (cl-block nil
+        (while (> index 0)
+          (cl-decf index)
+          (let ((buf (ring-ref buf-ring index)))
+            (if (buffer-live-p buf) (cl-return buf)))
+          (ring-remove buf-ring index)))))
 
-(defun *helpful--next (&optional buffer)
-  "Return the next live Helpful buffer relative to BUFFER."
-  (let ((buf-ring *helpful--buffer-ring)
-        (index (or (*helpful--buffer-index buffer) -1)))
-    (cl-block nil
-      (while (> index 0)
-        (cl-decf index)
-        (let ((buf (ring-ref buf-ring index)))
-          (if (buffer-live-p buf) (cl-return buf)))
-        (ring-remove buf-ring index)))))
+  (defun *helpful--previous (&optional buffer)
+    "Return the previous live Helpful buffer relative to BUFFER."
+    (let ((buf-ring *helpful--buffer-ring)
+          (index (1+ (or (*helpful--buffer-index buffer) -1))))
+      (cl-block nil
+        (while (< index (ring-length buf-ring))
+          (let ((buf (ring-ref buf-ring index)))
+            (if (buffer-live-p buf) (cl-return buf)))
+          (ring-remove buf-ring index)))))
 
-(defun *helpful--previous (&optional buffer)
-  "Return the previous live Helpful buffer relative to BUFFER."
-  (let ((buf-ring *helpful--buffer-ring)
-        (index (1+ (or (*helpful--buffer-index buffer) -1))))
-    (cl-block nil
-      (while (< index (ring-length buf-ring))
-        (let ((buf (ring-ref buf-ring index)))
-          (if (buffer-live-p buf) (cl-return buf)))
-        (ring-remove buf-ring index)))))
+  (defun *helpful-next ()
+    "Go to the next Helpful buffer."
+    (interactive)
+    (when-let (buf (*helpful--next))
+      (funcall helpful-switch-buffer-function buf)))
 
-(defun *helpful-next ()
-  "Go to the next Helpful buffer."
-  (interactive)
-  (when-let (buf (*helpful--next))
-    (funcall helpful-switch-buffer-function buf)))
-
-(defun *helpful-previous ()
-  "Go to the previous Helpful buffer."
-  (interactive)
-  (when-let (buf (*helpful--previous))
-    (funcall helpful-switch-buffer-function buf)))
-
-(define-key helpful-mode-map (kbd "C-x C-b") '*helpful-previous)
-(define-key helpful-mode-map (kbd "C-x C-f") '*helpful-next)
+  (defun *helpful-previous ()
+    "Go to the previous Helpful buffer."
+    (interactive)
+    (when-let (buf (*helpful--previous))
+      (funcall helpful-switch-buffer-function buf)))
+  
+  (define-key helpful-mode-map (kbd "C-x C-b") '*helpful-previous)
+  (define-key helpful-mode-map (kbd "C-x C-f") '*helpful-next))
 
 ;; Pop-up support for documentation windows
 (add-to-list 'display-buffer-alist
