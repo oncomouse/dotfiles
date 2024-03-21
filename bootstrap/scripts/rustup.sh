c399bd81683218133de2f04d227a26e7c55019b5
oncomouse <oncomouse@gmail.com>
2024-03-21 10:21:25 -0500

    fix: rust

--- bootstrap/scripts/rustup.sh
+++ bootstrap/scripts/rustup.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
-os=$(bash ~/dotfiles/bootstrap/scripts/os.sh)
-if [ "$os" = "arch" ]; then
-  rustup install stable
-  rustup default stable
-else
-  if rustup -v COMMAND > /dev/null 2>&1; then
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-  fi
-  if [ ! -d ~/.cargo ]; then
-    rustup-init
-    source "$HOME/.cargo/env"
-  fi
-fi
+# os=$(bash ~/dotfiles/bootstrap/scripts/os.sh)
+# if [ "$os" = "arch" ]; then
+#   rustup install stable
+#   rustup default stable
+# else
+#   if rustup -v COMMAND > /dev/null 2>&1; then
+#     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+#   fi
+#   if [ ! -d ~/.cargo ]; then
+#     rustup-init
+#     source "$HOME/.cargo/env"
+#   fi
+# fi
 # if [ "$os" != "arch" ]; then
 # 	cargo install stylua
 # fi
