1210717e72d5541b3a69eb307cf463291884b0db
oncomouse <oncomouse@gmail.com>
2024-03-23 11:41:47 -0500

    feat: add which-key indicators for embark

--- conf/emacs/init.el
+++ conf/emacs/init.el
@@ -340,10 +340,46 @@
 (global-set-key (kbd "C-M-<up>") 'move-dup-duplicate-up)
 (global-set-key (kbd "C-M-<down>") 'move-dup-duplicate-down)
 
-(define-key global-map (kbd "C-;") 'embark-act)
+;; Use which-key for embark suggestions:
+(with-eval-after-load 'embark
+  (define-key global-map (kbd "C-;") 'embark-act)
+  (defun embark-which-key-indicator ()
+    "An embark indicator that displays keymaps using which-key.
+The which-key help message will show the type and value of the
+current target followed by an ellipsis if there are further
+targets."
+    (lambda (&optional keymap targets prefix)
+      (if (null keymap)
+          (which-key--hide-popup-ignore-command)
+        (which-key--show-keymap
+         (if (eq (plist-get (car targets) :type) 'embark-become)
+             "Become"
+           (format "Act on %s '%s'%s"
+                   (plist-get (car targets) :type)
+                   (embark--truncate-target (plist-get (car targets) :target))
+                   (if (cdr targets) "…" "")))
+         (if prefix
+             (pcase (lookup-key keymap prefix 'accept-default)
+               ((and (pred keymapp) km) km)
+               (_ (key-binding prefix 'accept-default)))
+           keymap)
+         nil nil t (lambda (binding)
+                     (not (string-suffix-p "-argument" (cdr binding))))))))
 
-(define-key ap/leader-open-map (kbd "a") 'org-agenda)
-(define-key ap/leader-open-map (kbd "c") 'org-capture)
+  (setq embark-indicators
+        '(embark-which-key-indicator
+          embark-highlight-indicator
+          embark-isearch-highlight-indicator))
+
+  (defun embark-hide-which-key-indicator (fn &rest args)
+    "Hide the which-key indicator immediately when using the completing-read prompter."
+    (which-key--hide-popup-ignore-command)
+    (let ((embark-indicators
+           (remq #'embark-which-key-indicator embark-indicators)))
+      (apply fn args)))
+
+  (advice-add #'embark-completing-read-prompter
+              :around #'embark-hide-which-key-indicator))
 
 (when (require-package 'crux)
   (define-key global-map (kbd "C-k") 'crux-smart-kill-line)
@@ -557,6 +593,10 @@ If mark is active, merge lines in the current region."
 (add-hook 'org-mode-hook (setq-local sentence-end-double-space nil))
 
 (with-eval-after-load 'org
+  
+  (define-key ap/leader-open-map (kbd "a") 'org-agenda)
+  (define-key ap/leader-open-map (kbd "c") 'org-capture)
+
   (setq org-capture-templates
         `(("t" "todo" entry (file+headline "" "Inbox") ; "" => `org-default-notes-file'
            "* TODO %?\n%U\n%i\n")
