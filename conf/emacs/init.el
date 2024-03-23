ce2cb8ac6156c0462647314c0667b38d6f7f74bf
oncomouse <oncomouse@gmail.com>
2024-03-23 11:42:07 -0500

    feat: move vterm to its own configuration file

--- conf/emacs/init.el
+++ conf/emacs/init.el
@@ -482,37 +482,7 @@ If mark is active, merge lines in the current region."
   (define-key surround-keymap (kbd "*") (lambda (&rest _) (interactive) (surround-mark "*")))
   (define-key surround-keymap (kbd "/") (lambda (&rest _) (interactive) (surround-mark "/"))))
 
-(when (require-package 'vterm)
-  (add-hook 'vterm-mode-hook (lambda () ""
-                               (define-key vterm-mode-map "M-g" nil)
-                               (define-key vterm-mode-map "C-c C-c" (lambda (_) (interactive "p") (vterm-send "C-c")))))
-  (setq vterm-kill-buffer-on-exit t)
-
-  ;; 5000 lines of scrollback, instead of 1000
-  (setq vterm-max-scrollback 5000))
-
-(when (require-package 'vterm-toggle)
-  (define-key ap/leader-open-map (kbd "t") 'vterm-toggle)
-  (define-key ap/leader-open-map (kbd "T") 'vterm-toggle-cd)
-  (add-hook 'vterm-mode-hook (lambda () ""
-                               (define-key vterm-mode-map (kdb "s-n") 'vterm-toggle-forward)
-                               (define-key vterm-mode-map (kdb "s-p") 'vterm-toggle-backward)))
-  (setq vterm-toggle-fullscreen-p nil)
-
-  ;; Display vterm at the bottom:
-  (add-to-list 'display-buffer-alist
-               '((lambda (buffer-or-name _)
-                   (let ((buffer (get-buffer buffer-or-name)))
-                     (with-current-buffer buffer
-                       (or (equal major-mode 'vterm-mode)
-                           (string-prefix-p "*vterm*" (buffer-name buffer))))))
-                 (display-buffer-reuse-window display-buffer-at-bottom)
-                 ;;(display-buffer-reuse-window display-buffer-in-direction)
-                 ;;display-buffer-in-direction/direction/dedicated is added in emacs27
-                 ;;(direction . bottom)
-                 ;;(dedicated . t) ;dedicated is supported in emacs27
-                 (reusable-frames . visible)
-                 (window-height . 0.35))))
+(require 'init-vterm)
 
 ;; Patch for fish and ripgrep
 (setq projectile-globally-ignored-directories
