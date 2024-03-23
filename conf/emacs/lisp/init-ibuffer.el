b8418f2c150700f88239e0258c85c3c118b41342
oncomouse <oncomouse@gmail.com>
2024-03-23 10:26:40 -0500

    fix: remove rest of ibuffer-vc stuff

--- conf/emacs/lisp/init-ibuffer.el
+++ conf/emacs/lisp/init-ibuffer.el
@@ -34,28 +34,6 @@
     (:name "Size" :inline t)
     (file-size-human-readable (buffer-size))))
 
-
-;; Modify the default ibuffer-formats (toggle with `)
-(setq ibuffer-formats
-      '((mark modified read-only vc-status-mini " "
-              (name 22 22 :left :elide)
-              " "
-              (size-h 9 -1 :right)
-              " "
-              (mode 12 12 :left :elide)
-              " "
-              vc-relative-file)
-        (mark modified read-only vc-status-mini " "
-              (name 22 22 :left :elide)
-              " "
-              (size-h 9 -1 :right)
-              " "
-              (mode 14 14 :left :elide)
-              " "
-              (vc-status 12 12 :left)
-              " "
-              vc-relative-file)))
-
 (setq ibuffer-filter-group-name-face 'font-lock-doc-face)
 
 (global-set-key (kbd "C-x C-b") 'ibuffer)
