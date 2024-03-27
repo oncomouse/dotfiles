35eaf554e1cad6e9d47cb8ae006666ddf8883753
oncomouse <oncomouse@gmail.com>
2024-03-27 12:28:14 -0500

    feat: better SSH_AUTH_SOCK setting

--- stow/fish/dot-config/fish/conf.d/ssh.fish
+++ stow/fish/dot-config/fish/conf.d/ssh.fish
@@ -1,2 +1,5 @@
 # set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
-set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/keyring/ssh"
+# set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/keyring/ssh"
+if test -e "$XDG_RUNTIME_DIR/gcr/ssh"
+  set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/gcr/ssh"
+end
