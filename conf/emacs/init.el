e0ab2eed2d07a55226cac07d813ef427c7a05253
oncomouse <oncomouse@gmail.com>
2024-03-28 07:40:52 -0500

    fix: continue re-organization

--- conf/emacs/init.el
+++ conf/emacs/init.el
@@ -220,6 +220,10 @@
 (require 'init-format)
 (require 'init-electric)
 (require 'init-crux)
+(require 'init-ace-window)
+(require 'init-undo)
+(require 'init-helpful)
+(require 'init-workspaces)
 ;; Local
 
 ;; Surpress nativecomp warnings:
@@ -276,41 +280,6 @@
   :repeat t
   "s" #'isearch-repeat-forward
   "r" #'isearch-repeat-backward)
-
-;; Resize window using hydras
-(defhydra hydra-window-resizer (:columns 2)
-  "Window Sizing."
-  ("-" shrink-window-horizontally "horizontal shrink")
-  ("=" enlarge-window-horizontally "horizontal enlarge")
-  ("_" shrink-window "vertical shrink")
-  ("+" enlarge-window "vertical enlarge"))
-
-(defun aw-window-resize (window)
-  "Resize WINDOW using `hydra-window-resizer/body'."
-  (aw-switch-to-window window)
-  (hydra-window-resizer/body))
-
-(use-package ace-window
-  :straight t
-  :bind ("C-x o" . ace-window)
-  :custom
-  (aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
-  (aw-dispatch-alist
-   '((?x aw-delete-window "Delete Window")
-     (?m aw-swap-window "Swap Windows")
-     (?M aw-move-window "Move Window")
-     (?c aw-copy-window "Copy Window")
-     (?B aw-switch-buffer-in-window "Select Buffer")
-     (?n aw-flip-window)
-     (?u aw-switch-buffer-other-window "Switch Buffer Other Window")
-     (?c aw-split-window-fair "Split Fair Window")
-     (?v aw-split-window-vert "Split Vert Window")
-     (?b aw-split-window-horz "Split Horz Window")
-     (?o delete-other-windows "Delete Other Windows")
-     (?r aw-window-resize "Resize Window")
-     (?? aw-show-dispatch-help))))
-
-
 
 ;; Restore default move-dup bindings:
 (global-set-key (kbd "M-<up>") 'move-dup-move-lines-up)
@@ -750,10 +719,6 @@ If not in a clock, move to next headline."
   (citar-embark-mode))
 
 
-(require 'init-undo)
-(require 'init-helpful)
-(require 'init-workspaces)
-
 ;; emacs-lisp local map:
 (defvar ap/emacs-lisp-map (make-sparse-keymap))
 (define-key emacs-lisp-mode-map (kbd "C-c l") ap/emacs-lisp-map)
@@ -766,7 +731,7 @@ If not in a clock, move to next headline."
 (define-key ap/emacs-lisp-map (kbd "g f") #'find-function)
 (define-key ap/emacs-lisp-map (kbd "g v") #'find-variable)
 (define-key ap/emacs-lisp-map (kbd "g l") #'find-library)
-
+
 (defun ap/project-buffers (&optional sources)
   "Display buffers using consult-buffer, narrowed to only project files.
 
@@ -777,7 +742,7 @@ Pass SOURCES to consult-buffer, if provided."
 
 (define-key global-map (kbd "C-x b") 'ap/project-buffers)
 (define-key global-map (kbd "C-x B") 'consult-buffer)
-
+
 ;; Configure cape
 (defun ap/writing-cape ()
   "Custom completion-at-point-function for use in writing environments."
