750f7679123e5a5eb432c6127a293ef175d29aa5
oncomouse <oncomouse@gmail.com>
2024-04-09 09:50:05 -0500

    feat: use spell-fu

--- conf/emacs/lisp/init-spelling.el
+++ conf/emacs/lisp/init-spelling.el
@@ -2,44 +2,13 @@
 ;;; Commentary:
 ;;; Code:
 
-;; Adapted from: http://blog.binchen.org/posts/what-s-the-best-spell-check-set-up-in-emacs/
-(defun ap/configure-flyspell (&optional code-spell)
-  "Configure `flyspell'. Set code when CODE-SPELL is non-nil."
-  (interactive)
-  (cond
-   ((and (not code-spell) (executable-find "hunspell"))
-    (setq-local ispell-program-name "hunspell")
-    (setq-local ispell-local-dictionary "en_US")
-    (setq-local ispell-local-dictionary-alist
-                ;; Please note the list `("-d" "en_US")` contains ACTUAL parameters passed to hunspell
-                ;; You could use `("-d" "en_US,en_US-med")` to check with multiple dictionaries
-                '(("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil ("-d" "en_US") nil utf-8)))
-    ;; new variable `ispell-hunspell-dictionary-alist' is defined in Emacs
-    ;; If it's nil, Emacs tries to automatically set up the dictionaries.
-    (when (boundp 'ispell-hunspell-dictionary-alist)
-      (setq-local ispell-hunspell-dictionary-alist ispell-local-dictionary-alist)))
-   ((executable-find "aspell")
-    (setq-local ispell-program-name "aspell")
-    ;; Please note ispell-extra-args contains ACTUAL parameters passed to aspell
-    (setq-local ispell-extra-args '("--sug-mode=ultra" "--lang=en_US"))
-    (when code-spell
-      (setq-local ispell-extra-args '("--camel-case" "--run-together" "--run-together-limit=16"))))
-   (t (setq-local ispell-program-name nil))))
-
-(use-package flyspell
+(use-package spell-fu
   :straight t
-  :hook (
-         ((markdown-mode org-mode text-mode) . flyspell-mode)
-         (prog-mode . flyspell-prog-mode)
-         ((markdown-mode org-mode text-mode) . ap/configure-flyspell)
-         (prog-mode . (lambda () (ap/configure-flyspell t))))
-  :bind (:map flyspell-mode-map
-              ("C-;" . nil)
-              ("C-M-i" . nil)))
-
-(use-package flyspell-lazy
-  :straight t
-  :hook ((flyspell-mode) . (lambda () (flyspell-lazy-mode 1))))
+  :init
+  (setq spell-fu-word-delimit-camel-case t)
+  (setq spell-fu-global-ignore-buffer (lambda (buf) (buffer-local-value 'buffer-read-only buf)))
+  :config
+  (spell-fu-global-mode))
 
 (provide 'init-spelling)
 ;; init-spelling.el ends here
