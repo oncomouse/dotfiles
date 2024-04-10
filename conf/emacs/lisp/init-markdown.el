0cb5e7f2db24d1e87e3c0184484b06d16c8152d8
oncomouse <oncomouse@gmail.com>
2024-04-10 16:01:10 -0500

    todo: make sure this works with slides

--- conf/emacs/lisp/init-markdown.el
+++ conf/emacs/lisp/init-markdown.el
@@ -9,6 +9,7 @@
   (add-auto-mode 'markdown-mode "\\.md\\.html\\'")
   (with-eval-after-load 'whitespace-cleanup-mode
     (add-to-list 'whitespace-cleanup-mode-ignore-modes 'markdown-mode))
+  ;; TODO make sure this works in slide decks.
   (mmm-add-classes
    '((pandoc-yaml
       :submode yaml-mode
