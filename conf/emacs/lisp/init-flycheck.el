8f6c6a952855bab30948cc79710dfa5e38c1fdf8
oncomouse <oncomouse@gmail.com>
2024-04-11 16:11:50 -0500

    fix: load consult-flycheck with flycheck

--- conf/emacs/lisp/init-flycheck.el
+++ conf/emacs/lisp/init-flycheck.el
@@ -19,5 +19,9 @@
 (use-package flycheck-eglot
   :hook (eglot-managed-mode . flycheck-eglot-mode))
 
+(use-package consult-flycheck
+  :straight t
+  :after consult)
+
 (provide 'init-flycheck)
 ;;; init-flycheck.el ends here
