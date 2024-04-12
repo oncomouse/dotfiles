54df73e79c1c88436afd90845d00bce5a66e063a
oncomouse <oncomouse@gmail.com>
2024-04-12 09:26:32 -0500

    fix: use recent for C-x C-r regardless of consult support

--- conf/emacs/lisp/init-minibuffer.el
+++ conf/emacs/lisp/init-minibuffer.el
@@ -87,7 +87,7 @@ targets."
         ([remap switch-to-buffer-other-window] . consult-buffer-other-window)
         ([remap switch-to-buffer-other-frame] . consult-buffer-other-frame)
         ([remap goto-line] . consult-goto-line)
-        ("C-x C-r" . consult-recent-file))
+        ([remap recent] . consult-recent-file))
   :config
   (defmacro sanityinc/no-consult-preview (&rest cmds)
     `(with-eval-after-load 'consult
---
+++ conf/emacs/lisp/init-recentf.el
@@ -0,0 +1,13 @@
+;;; init-recentf.el --- Settings for tracking recent files -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(add-hook 'after-init-hook 'recentf-mode)
+(setq-default
+ recentf-max-saved-items 1000
+ recentf-exclude `("/tmp/" "/ssh:" ,(concat package-user-dir "/.*-autoloads\\.el\\'")))
+
+(define-key global-map (kbd "C-x C-r") 'recentf)
+
+(provide 'init-recentf)
+;;; init-recentf.el ends here
