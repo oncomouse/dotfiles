ed9f2087d07fce35c9fd32bc4a4b8c8d76e763a4
oncomouse <oncomouse@gmail.com>
2024-04-16 11:26:49 -0500

    fix: unused function

--- conf/emacs/lisp/init-projectile.el
+++ conf/emacs/lisp/init-projectile.el
@@ -41,15 +41,6 @@
                                                 ((executable-find "grep") 'projectile-grep)
                                                 (t 'projectile-find-references)))
 
-  ;; Switch to a project buffer if one is open, otherwise run find files:
-  (defun ap/projectile-smart-switch ()
-    "Open latest edited buffer when switched the  exist project, find files when switched to a new project."
-    (if
-        (null (projectile-project-buffer-files))
-        (projectile-find-file)
-      (call-interactively #'projectile-switch-to-buffer)))
-  (setq projectile-switch-project-action #'ap/projectile-smart-switch)
-
   ;; Configure marginalia for projectile
   (with-eval-after-load 'marginalia
     (setq marginalia-command-categories
