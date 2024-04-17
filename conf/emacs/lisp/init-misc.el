9684e301cb5525b15a2314df55c544c45a934ad3
oncomouse <oncomouse@gmail.com>
2024-04-17 13:43:22 -0500

    wip: switch everything to use-package

---
+++ conf/emacs/lisp/init-misc.el
@@ -0,0 +1,57 @@
+;;; init-misc.el --- Miscellaneous config -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+
+;; Misc config - yet to be placed in separate files
+
+(add-auto-mode 'tcl-mode "^Portfile\\'")
+
+(if (boundp 'use-short-answers)
+    (setq use-short-answers t)
+  (fset 'yes-or-no-p 'y-or-n-p))
+
+(add-hook 'prog-mode-hook 'goto-address-prog-mode)
+(add-hook 'conf-mode-hook 'goto-address-prog-mode)
+(setq goto-address-mail-face 'link)
+
+(add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p)
+(add-hook 'after-save-hook 'sanityinc/set-mode-for-new-scripts)
+
+(defun sanityinc/set-mode-for-new-scripts ()
+  "Invoke `normal-mode' if this file is a script and in `fundamental-mode'."
+  (and
+   (eq major-mode 'fundamental-mode)
+   (>= (buffer-size) 2)
+   (save-restriction
+     (widen)
+     (string= "#!" (buffer-substring (point-min) (+ 2 (point-min)))))
+   (normal-mode)))
+
+(use-package info-colors
+  :straight t
+  :config
+  (with-eval-after-load 'info
+    (add-hook 'Info-selection-hook 'info-colors-fontify-node)))
+
+;; Handle the prompt pattern for the 1password command-line interface
+(with-eval-after-load 'comint
+  (setq comint-password-prompt-regexp
+        (concat
+         comint-password-prompt-regexp
+         "\\|^Please enter your password for user .*?:\\s *\\'")))
+
+
+(use-package regex-tool
+  :straight t
+  :custom (regex-tool-backend 'emacs))
+
+(with-eval-after-load 're-builder
+  ;; Support a slightly more idiomatic quit binding in re-builder
+  (define-key reb-mode-map (kbd "C-c C-k") 'reb-quit))
+
+(add-auto-mode 'conf-mode "^Procfile\\'")
+
+
+(provide 'init-misc)
+;;; init-misc.el ends here
