21a4c0399851591d5ee822d65a4b0e10052441d5
oncomouse <oncomouse@gmail.com>
2024-04-17 13:47:53 -0500

    wip: switch everything to use-package

---
+++ conf/emacs/lisp/init-isearch.el
@@ -0,0 +1,61 @@
+;;; init-isearch.el --- isearch settings -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+;; Show number of matches while searching
+
+(use-package anzu
+  :hook (after-init . global-anzu-mode)
+  :custom (anzu-mode-lighter "")
+  :bind (([remap query-replace-regexp] . anzu-query-replace-regexp)
+         ([remap query-replace] . anzu-query-replace)))
+
+(with-eval-after-load 'isearch
+  ;; DEL during isearch should edit the search string, not jump back to the previous result
+  (define-key isearch-mode-map [remap isearch-delete-char] 'isearch-del-char)
+
+  (defun sanityinc/isearch-occur ()
+    "Invoke `consult-line' from isearch."
+    (interactive)
+    (let ((query (if isearch-regexp
+                     isearch-string
+                   (regexp-quote isearch-string))))
+      (isearch-update-ring isearch-string isearch-regexp)
+      (let (search-nonincremental-instead)
+        (ignore-errors (isearch-done t t)))
+      (consult-line query)))
+
+  (define-key isearch-mode-map (kbd "C-o") 'sanityinc/isearch-occur)
+  (define-key isearch-mode-map (kbd "C-c C-o") 'sanityinc/isearch-occur))
+
+
+;; Search back/forth for the symbol at point
+;; See http://www.emacswiki.org/emacs/SearchAtPoint
+(defun isearch-yank-symbol ()
+  "*Put symbol at current point into search string."
+  (interactive)
+  (let ((sym (thing-at-point 'symbol)))
+    (if sym
+        (progn
+          (setq isearch-regexp t
+                isearch-string (concat "\\_<" (regexp-quote sym) "\\_>")
+                isearch-message (mapconcat 'isearch-text-char-description isearch-string "")
+                isearch-yank-flag t))
+      (ding)))
+  (isearch-search-and-update))
+
+(define-key isearch-mode-map (kbd "C-M-w") 'isearch-yank-symbol)
+
+
+(defun sanityinc/isearch-exit-other-end ()
+  "Exit isearch, but at the other end of the search string.
+This is useful when followed by an immediate kill."
+  (interactive)
+  (isearch-exit)
+  (goto-char isearch-other-end))
+
+(define-key isearch-mode-map [(control return)] 'sanityinc/isearch-exit-other-end)
+
+
+(provide 'init-isearch)
+;;; init-isearch.el ends here
