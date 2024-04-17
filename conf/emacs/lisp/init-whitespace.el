e211fe4a74ff6c863df5418367db294496647e4f
oncomouse <oncomouse@gmail.com>
2024-04-17 09:26:42 -0500

    wip: switch everything to use-package

---
+++ conf/emacs/lisp/init-whitespace.el
@@ -0,0 +1,26 @@
+;;; init-whitespace.el --- Special handling for whitespace -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(setq-default show-trailing-whitespace nil)
+
+
+;;; Whitespace
+
+(defun sanityinc/show-trailing-whitespace ()
+  "Enable display of trailing whitespace in this buffer."
+  (setq-local show-trailing-whitespace t))
+
+(dolist (hook '(prog-mode-hook text-mode-hook conf-mode-hook))
+  (add-hook hook 'sanityinc/show-trailing-whitespace))
+
+(use-package whitespace-cleanup-mode
+  :straight t
+  :hook (after-init . global-whitespace-cleanup-mode)
+  :diminish whitespace-cleanup-mode)
+
+(global-set-key [remap just-one-space] 'cycle-spacing)
+
+
+(provide 'init-whitespace)
+;;; init-whitespace.el ends here
